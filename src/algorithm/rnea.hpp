e6f3b92b1e285b2ccf219a7feaa36d48929adafe
Mansard <nmansard@laas.fr>
2014-08-24 22:52:59 +0200

    Moved the RNEA into src/algorithm.

---
+++ src/algorithm/rnea.hpp
@@ -0,0 +1,113 @@
+#ifndef __se3_rne_hpp__
+#define __se3_rne_hpp__
+
+#include "pinocchio/multibody/visitor.hpp"
+#include "pinocchio/multibody/model.hpp"
+  
+namespace se3
+{
+  inline const Eigen::VectorXd&
+  rnea(const Model & model, Data& data,
+       const Eigen::VectorXd & q,
+       const Eigen::VectorXd & v,
+       const Eigen::VectorXd & a);
+
+} // namespace se3 
+
+/* --- Details -------------------------------------------------------------------- */
+namespace se3 
+{
+  struct RneaForwardStep : public fusion::JointVisitor<RneaForwardStep>
+  {
+    typedef boost::fusion::vector< const se3::Model&,
+			    se3::Data&,
+			    const int&,
+			    const Eigen::VectorXd &,
+			    const Eigen::VectorXd &,
+			    const Eigen::VectorXd &
+			    > ArgsType;
+
+    JOINT_VISITOR_INIT(RneaForwardStep);
+
+    template<typename JointModel>
+    static int algo(const se3::JointModelBase<JointModel> & jmodel,
+		    se3::JointDataBase<typename JointModel::JointData> & jdata,
+		    const se3::Model& model,
+		    se3::Data& data,
+		    const int &i,
+		    const Eigen::VectorXd & q,
+		    const Eigen::VectorXd & v,
+		    const Eigen::VectorXd & a)
+    {
+      using namespace Eigen;
+      using namespace se3;
+      
+      jmodel.calc(jdata.derived(),q,v);
+      
+      const Model::Index & parent = model.parents[i];
+      data.liMi[i] = model.jointPlacements[i]*jdata.M();
+      
+      if(parent>0) data.oMi[i] = data.oMi[parent]*data.liMi[i];
+      else         data.oMi[i] = data.liMi[i];
+      
+      data.v[i] = jdata.v();
+      if(parent>0) data.v[i] += data.liMi[i].actInv(data.v[parent]);
+      
+      jmodel.jointMotion(a);
+      data.a[i] =  jdata.S()*jmodel.jointMotion(a) + jdata.c() + (data.v[i] ^ jdata.v()) ; 
+      if(parent>0) data.a[i] += data.liMi[i].actInv(data.a[parent]);
+      
+      data.f[i] = model.inertias[i]*data.a[i] + model.inertias[i].vxiv(data.v[i]); // -f_ext
+      return 0;
+    }
+
+  };
+
+  struct RneaBackwardStep : public fusion::JointVisitor<RneaBackwardStep>
+  {
+    typedef boost::fusion::vector<const Model&,
+				  Data&,
+				  const int &>  ArgsType;
+    
+    JOINT_VISITOR_INIT(RneaBackwardStep);
+
+    template<typename JointModel>
+    static void algo(const JointModelBase<JointModel> & jmodel,
+		     JointDataBase<typename JointModel::JointData> & jdata,
+		     const Model& model,
+		     Data& data,
+		     int i)
+    {
+      const Model::Index & parent  = model.parents[i];      
+      jmodel.jointForce(data.tau)  = jdata.S().transpose()*data.f[i];
+      if(parent>0) data.f[parent] += data.liMi[i].act(data.f[i]);
+    }
+  };
+
+  inline const Eigen::VectorXd&
+  rnea(const Model & model, Data& data,
+       const Eigen::VectorXd & q,
+       const Eigen::VectorXd & v,
+       const Eigen::VectorXd & a)
+  {
+    data.v[0] = Motion::Zero();
+    data.a[0] = -model.gravity;
+
+    for( int i=1;i<model.nbody;++i )
+      {
+	RneaForwardStep::run(model.joints[i],data.joints[i],
+			     RneaForwardStep::ArgsType(model,data,i,q,v,a));
+      }
+    
+    for( int i=model.nbody-1;i>0;--i )
+      {
+	RneaBackwardStep::run(model.joints[i],data.joints[i],
+			      RneaBackwardStep::ArgsType(model,data,i));
+      }
+
+    return data.tau;
+  }
+} // namespace se3
+
+#endif // ifndef __se3_rne_hpp__
+
--- unittest/rnea.cpp
+++ unittest/rnea.cpp
@@ -3,85 +3,12 @@
 #include "pinocchio/multibody/joint.hpp"
 #include "pinocchio/multibody/visitor.hpp"
 #include "pinocchio/multibody/model.hpp"
+#include "pinocchio/algorithm/rnea.hpp"
 
 #include <iostream>
 
 #include "pinocchio/tools/timer.hpp"
 
-  
-
-namespace se3
-{
-
-  struct RneaForwardStep : public fusion::JointVisitor<RneaForwardStep>
-  {
-    typedef boost::fusion::vector< const se3::Model&,
-			    se3::Data&,
-			    const int&,
-			    const Eigen::VectorXd &,
-			    const Eigen::VectorXd &,
-			    const Eigen::VectorXd &
-			    > ArgsType;
-
-    JOINT_VISITOR_INIT(RneaForwardStep);
-
-    template<typename JointModel>
-    static int algo(const se3::JointModelBase<JointModel> & jmodel,
-		    se3::JointDataBase<typename JointModel::JointData> & jdata,
-		    const se3::Model& model,
-		    se3::Data& data,
-		    const int &i,
-		    const Eigen::VectorXd & q,
-		    const Eigen::VectorXd & v,
-		    const Eigen::VectorXd & a)
-    {
-      using namespace Eigen;
-      using namespace se3;
-      
-      jmodel.calc(jdata.derived(),q,v);
-      
-      const Model::Index & parent = model.parents[i];
-      data.liMi[i] = model.jointPlacements[i]*jdata.M();
-      
-      if(parent>0) data.oMi[i] = data.oMi[parent]*data.liMi[i];
-      else         data.oMi[i] = data.liMi[i];
-      
-      data.v[i] = jdata.v();
-      if(parent>0) data.v[i] += data.liMi[i].actInv(data.v[parent]);
-      
-      jmodel.jointMotion(a);
-      data.a[i] =  jdata.S()*jmodel.jointMotion(a) + jdata.c() + (data.v[i] ^ jdata.v()) ; 
-      if(parent>0) data.a[i] += data.liMi[i].actInv(data.a[parent]);
-      
-      data.f[i] = model.inertias[i]*data.a[i] + model.inertias[i].vxiv(data.v[i]); // -f_ext
-      return 0;
-    }
-
-  };
-
-
-  struct RneaBackwardStep : public fusion::JointVisitor<RneaBackwardStep>
-  {
-    typedef boost::fusion::vector<const Model&,
-				  Data&,
-				  const int &>  ArgsType;
-    
-    JOINT_VISITOR_INIT(RneaBackwardStep);
-
-    template<typename JointModel>
-    static void algo(const JointModelBase<JointModel> & jmodel,
-		     JointDataBase<typename JointModel::JointData> & jdata,
-		     const Model& model,
-		     Data& data,
-		     int i)
-    {
-      const Model::Index & parent  = model.parents[i];      
-      jmodel.jointForce(data.tau)  = jdata.S().transpose()*data.f[i];
-      if(parent>0) data.f[parent] += data.liMi[i].act(data.f[i]);
-    }
-  };
-
-} // namespace se3
 
 //#define __SSE3__
 #include <fenv.h>
@@ -141,28 +68,17 @@ int main()
   model.addBody(model.getBodyId("larm6"),JointModelRX(),SE3::Random(),Inertia::Random(),"lgrip");
 
   se3::Data data(model);
+  data.v[0] = Motion::Zero();
+  data.a[0] = -model.gravity;
 
   VectorXd q = VectorXd::Random(model.nq);
   VectorXd v = VectorXd::Random(model.nv);
   VectorXd a = VectorXd::Random(model.nv);
-
-  data.v[0] = Motion::Zero();
-  data.a[0] = -model.gravity;
  
   StackTicToc timer(StackTicToc::US); timer.tic();
   SMOOTH(1000)
     {
-      for( int i=1;i<model.nbody;++i )
-	{
-	  RneaForwardStep::run(model.joints[i],data.joints[i],
-			       RneaForwardStep::ArgsType(model,data,i,q,v,a));
-	}
-
-      for( int i=model.nbody-1;i>0;--i )
-	{
-	  RneaBackwardStep::run(model.joints[i],data.joints[i],
-				RneaBackwardStep::ArgsType(model,data,i));
-	}
+      rnea(model,data,q,v,a);
     }
   timer.toc(std::cout,1000);
 
