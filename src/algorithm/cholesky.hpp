06b04b438e80d1edee496d69e980b5aa1808dd24
Mansard <nmansard@laas.fr>
2014-09-08 15:40:07 +0200

    Working with FF: 3.9us.

--- src/algorithm/cholesky.hpp
+++ src/algorithm/cholesky.hpp
@@ -44,46 +44,102 @@ namespace se3
   		     const Model& model,
   		     Data& data)
     {
-
-      typedef typename Model::Index Index;
-      Eigen::MatrixXd & M = data.M;
-      Eigen::MatrixXd & U = data.U;
-      Eigen::VectorXd & D = data.D;
-      const int j = jmodel.id();
-      const int _j = jmodel.idx_v();
-
-      const int nvSubtree = data.nvSubtree[j];
-      D[_j] = M(_j,_j);
-      for( int m=1;m<nvSubtree;++m )
-	D[_j] -= U(_j,_j+m) * D[_j+m] * U(_j,_j+m);
-
-      Eigen::VectorXd::SegmentReturnType DUt = data.tmp.head(nvSubtree-1);
-      if(nvSubtree>1)
-	  DUt = U.row(_j).segment(_j+1,nvSubtree-1).transpose()
-	    .cwiseProduct(D.segment(_j+1,nvSubtree-1));
-
-      for( Index i=model.parents[j];i>0;i=model.parents[i])
-       	{
-       	  const int _i = idx_v(model.joints[i]);
-	  U(_i,_j) = M(_i,_j);
-	  if( nvSubtree>1 ) 
-	    U(_i,_j) -= U.row(_i).segment(_j+1,nvSubtree-1).dot(DUt);
-	  U(_i,_j) /= D[_j];
-       	}
-      // algoSized<JointModel::NV>(model,data,j,_j);
+      algoSized<JointModel::NV>(model,data,jmodel.id(),jmodel.idx_v());
     }
   };
   
   
+  template<int N,typename M_t>
+  void udut( Eigen::MatrixBase<M_t> & M )
+  {
+    typedef Eigen::Matrix<double,N,N> MatrixNd;
+    typedef Eigen::Matrix<double,1,N> VectorNd;
+    typedef typename M_t::DiagonalReturnType D_t;
+    typedef typename M_t::template TriangularViewReturnType<Eigen::StrictlyUpper>::Type U_t;
+
+    VectorNd tmp;
+    D_t D = M.diagonal();
+    U_t U = M.template triangularView<Eigen::StrictlyUpper>();
+
+    for(int j=N-1;j>=0;--j )
+      {
+	typename VectorNd::SegmentReturnType DUt = tmp.tail(N-j-1);
+	if( j<N-1 ) DUt = M.row(j).tail(N-j-1).transpose().cwiseProduct( D.tail(N-j-1) );
+
+	D[j] -= M.row(j).tail(N-j-1).dot(DUt);
+
+	for(int i=j-1;i>=0;--i)
+	  { U(i,j) -= M.row(i).tail(N-j-1).dot(DUt); U(i,j) /= D[j]; }
+      }
+  }
+  
+
+  template<int NVJ>
+  void CholeskyOuterLoopStep::algoSized( const Model& model,
+  					 Data& data,
+  					 const int & j,
+  					 const int & _j)
+  {
+    typedef typename Model::Index Index;
+    Eigen::MatrixXd & M = data.M;
+    Eigen::MatrixXd & U = data.U;
+    Eigen::VectorXd & D = data.D;
+    const int NVT = data.nvSubtree[j] - NVJ;
+
+    Eigen::Block<Eigen::MatrixXd> DUt = U.block(NVJ,0,NVT,NVJ);
+     DUt  = D.segment(_j+NVJ,NVT).asDiagonal() * U.block( _j,_j+NVJ,NVJ,NVT).transpose();
+
+     Eigen::Block<Eigen::MatrixXd,NVJ,NVJ> Djj = U.template block<NVJ,NVJ>(_j,_j);
+     Djj.template triangularView<Eigen::Upper>() 
+       = M.template block<NVJ,NVJ>(_j,_j) - U.block( _j,_j+NVJ,NVJ,NVT) * DUt;
+     udut<NVJ>(Djj);
+     D.template segment<NVJ>(_j) = Djj.diagonal();
+
+    for( Index i=model.parents[j];i>0;i=model.parents[i])
+      {
+     	const int _i = idx_v(model.joints[i]);
+	const int nvi = nv(model.joints[i]);
+
+	for( int k=0;k<nvi;++k )
+	  {
+	    U.template block<1,NVJ>(_i+k,_j) 
+	      = M.template block<1,NVJ>(_i+k,_j) - U.block(_i+k,_j+1,1,NVT) * DUt;
+	  }
+	// TODO divide by Djj
+	assert(false && "TODO divide by Djj");
+      }
+  }
+ 
   template<>
   void CholeskyOuterLoopStep::algoSized<1>( const Model& model,
 					    Data& data,
 					    const int & j,
 					    const int & _j)
   {
-      Eigen::MatrixXd & M = data.M;
-      Eigen::MatrixXd & U = data.U;
-      Eigen::VectorXd & D = data.D;
+    typedef typename Model::Index Index;
+    Eigen::MatrixXd & M = data.M;
+    Eigen::MatrixXd & U = data.U;
+    Eigen::VectorXd & D = data.D;
+    
+    const int NVT = data.nvSubtree[j]-1;
+    Eigen::VectorXd::SegmentReturnType DUt = data.tmp.head(NVT);
+    if(NVT)
+      DUt = U.row(_j).segment(_j+1,NVT).transpose()
+	.cwiseProduct(D.segment(_j+1,NVT));
+
+    D[_j] = M(_j,_j) - U.row(_j).segment(_j+1,NVT) * DUt;
+    
+    for( Index i=model.parents[j];i>0;i=model.parents[i])
+      {
+	const int _i = idx_v(model.joints[i]);
+	const int nvi = nv(model.joints[i]);
+
+	for( int k=0;k<nvi;++k )
+	  {
+	    U(_i+k,_j) = M(_i+k,_j) - U.row(_i+k).segment(_j+1,NVT).dot(DUt);
+	    U(_i+k,_j) /= D[_j];
+	  }
+      }
   }
  
 
