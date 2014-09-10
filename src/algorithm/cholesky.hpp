0413bf17fe14794b3adf966f58dff857ef068665
Mansard <nmansard@laas.fr>
2014-09-10 14:43:46 +0200

    Indent.

--- src/algorithm/cholesky.hpp
+++ src/algorithm/cholesky.hpp
@@ -23,161 +23,161 @@ namespace se3
   namespace cholesky
   {
 
-  inline const Eigen::MatrixXd&
-  decompose(const Model &           model, 
-	    Data&                   data )
-  {
-    /*  
-     *    D = zeros(n,1);
-     *    U = eye(n);
-     *    for j=n:-1:1
-     *      subtree = j+1:tree(j);
-     *      D(j) = M(j,j) - U(j,subtree)*diag(D(subtree))*U(j,subtree)';
-     *      i=parent(j);
-     *      while i>0
-     *          U(i,j) = (M(i,j) - U(i,subtree)*diag(D(subtree))*U(j,subtree)') / D(j);
-     *          i=parent(i);
-     *      end
-     *    end
-     */
+    inline const Eigen::MatrixXd&
+    decompose(const Model &           model, 
+	      Data&                   data )
+    {
+      /*  
+       *    D = zeros(n,1);
+       *    U = eye(n);
+       *    for j=n:-1:1
+       *      subtree = j+1:tree(j);
+       *      D(j) = M(j,j) - U(j,subtree)*diag(D(subtree))*U(j,subtree)';
+       *      i=parent(j);
+       *      while i>0
+       *          U(i,j) = (M(i,j) - U(i,subtree)*diag(D(subtree))*U(j,subtree)') / D(j);
+       *          i=parent(i);
+       *      end
+       *    end
+       */
 
-    Eigen::MatrixXd & M = data.M;
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd & D = data.D;
+      Eigen::MatrixXd & M = data.M;
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd & D = data.D;
 
-    for(int j=model.nv-1;j>=0;--j )
-      {
-	const int NVT = data.nvSubtree_fromRow[j]-1;
-	Eigen::VectorXd::SegmentReturnType DUt = data.tmp.head(NVT);
-	if(NVT)
-	  DUt = U.row(j).segment(j+1,NVT).transpose()
-	    .cwiseProduct(D.segment(j+1,NVT));
+      for(int j=model.nv-1;j>=0;--j )
+	{
+	  const int NVT = data.nvSubtree_fromRow[j]-1;
+	  Eigen::VectorXd::SegmentReturnType DUt = data.tmp.head(NVT);
+	  if(NVT)
+	    DUt = U.row(j).segment(j+1,NVT).transpose()
+	      .cwiseProduct(D.segment(j+1,NVT));
 	
-	D[j] = M(j,j) - U.row(j).segment(j+1,NVT) * DUt;
+	  D[j] = M(j,j) - U.row(j).segment(j+1,NVT) * DUt;
 	
-	for( int _i=data.parents_fromRow[j];_i>=0;_i=data.parents_fromRow[_i] )
-	  U(_i,j) = (M(_i,j) - U.row(_i).segment(j+1,NVT).dot(DUt)) / D[j]; 
-      }
+	  for( int _i=data.parents_fromRow[j];_i>=0;_i=data.parents_fromRow[_i] )
+	    U(_i,j) = (M(_i,j) - U.row(_i).segment(j+1,NVT).dot(DUt)) / D[j]; 
+	}
 
-    return data.U;
-  }
+      return data.U;
+    }
 
-  /* Compute U*D*v */
-  template<typename Mat>
-  Eigen::VectorXd
-  UDv( const Model &                  model, 
-       Data&                          data ,
-       const Eigen::MatrixBase<Mat> &   v)
-  {
-    /* for i = n:1
-     *    res[i] = L(i,i) x[i]
-     *    while j>0
-     *       y[i] += L(i,j) x[j]
-     */
-    assert(v.size() == model.nv);
-    EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
+    /* Compute U*D*v */
+    template<typename Mat>
+    Eigen::VectorXd
+    UDv( const Model &                  model, 
+	 Data&                          data ,
+	 const Eigen::MatrixBase<Mat> &   v)
+    {
+      /* for i = n:1
+       *    res[i] = L(i,i) x[i]
+       *    while j>0
+       *       y[i] += L(i,j) x[j]
+       */
+      assert(v.size() == model.nv);
+      EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
 
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd & D = data.D;
-    Eigen::VectorXd res = D.asDiagonal()*v;
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd & D = data.D;
+      Eigen::VectorXd res = D.asDiagonal()*v;
 
-    for( int j=0;j<model.nv;++j )
-      res[j] += U.row(j).segment(j+1,data.nvSubtree_fromRow[j]-1)
-	* res.segment(j+1,data.nvSubtree_fromRow[j]-1);
+      for( int j=0;j<model.nv;++j )
+	res[j] += U.row(j).segment(j+1,data.nvSubtree_fromRow[j]-1)
+	  * res.segment(j+1,data.nvSubtree_fromRow[j]-1);
 
-    return res;
-  }
+      return res;
+    }
   
-  /* Compute U*v */
-  template<typename Mat>
-  Eigen::VectorXd
-  Uv( const Model &                  model, 
-      Data&                          data ,
-      const Eigen::MatrixBase<Mat> &   v)
-  {
-    assert(v.size() == model.nv);
-    EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
+    /* Compute U*v */
+    template<typename Mat>
+    Eigen::VectorXd
+    Uv( const Model &                  model, 
+	Data&                          data ,
+	const Eigen::MatrixBase<Mat> &   v)
+    {
+      assert(v.size() == model.nv);
+      EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
 
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd & D = data.D;
-    Eigen::VectorXd res = v;
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd & D = data.D;
+      Eigen::VectorXd res = v;
 
-    for( int j=0;j<model.nv;++j )
-      res[j] += U.row(j).segment(j+1,data.nvSubtree_fromRow[j]-1)
-	* res.segment(j+1,data.nvSubtree_fromRow[j]-1);
+      for( int j=0;j<model.nv;++j )
+	res[j] += U.row(j).segment(j+1,data.nvSubtree_fromRow[j]-1)
+	  * res.segment(j+1,data.nvSubtree_fromRow[j]-1);
 
-    return res;
-  }
+      return res;
+    }
   
-  /* Compute D*U'*v */
-  template<typename Mat>
-  Eigen::VectorXd
-  DUtv( const Model &                  model, 
-	    Data&                          data ,
-	    const Eigen::MatrixBase<Mat> &   v)
-  {
-    assert(v.size() == model.nv);
-    EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
+    /* Compute D*U'*v */
+    template<typename Mat>
+    Eigen::VectorXd
+    DUtv( const Model &                  model, 
+	  Data&                          data ,
+	  const Eigen::MatrixBase<Mat> &   v)
+    {
+      assert(v.size() == model.nv);
+      EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
 
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd & D = data.D;
-    Eigen::VectorXd res = v;
-    const std::vector<int> & parents = data.parents_fromRow;
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd & D = data.D;
+      Eigen::VectorXd res = v;
+      const std::vector<int> & parents = data.parents_fromRow;
 
-    for( int i=model.nv-1;i>=0;--i )
-      {
+      for( int i=model.nv-1;i>=0;--i )
+	{
+	  for( Model::Index j = parents[i];j>=0;j=parents[j] )
+	    res[i] += U(j,i)*v[j];
+	  res[i] *= D[i];
+	}
+
+      return res;
+    }
+
+    /* Compute U'*v */
+    template<typename Mat>
+    Eigen::VectorXd
+    Utv( const Model &                  model, 
+	 Data&                          data ,
+	 const Eigen::MatrixBase<Mat> &   v)
+    {
+      assert(v.size() == model.nv);
+      EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
+
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd & D = data.D;
+      Eigen::VectorXd res = v;
+      const std::vector<int> & parents = data.parents_fromRow;
+
+      for( int i=model.nv-1;i>=0;--i )
 	for( Model::Index j = parents[i];j>=0;j=parents[j] )
 	  res[i] += U(j,i)*v[j];
-	res[i] *= D[i];
-      }
 
-    return res;
-  }
-
-  /* Compute U'*v */
-  template<typename Mat>
-  Eigen::VectorXd
-  Utv( const Model &                  model, 
-       Data&                          data ,
-       const Eigen::MatrixBase<Mat> &   v)
-  {
-    assert(v.size() == model.nv);
-    EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
-
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd & D = data.D;
-    Eigen::VectorXd res = v;
-    const std::vector<int> & parents = data.parents_fromRow;
-
-    for( int i=model.nv-1;i>=0;--i )
-      for( Model::Index j = parents[i];j>=0;j=parents[j] )
-	res[i] += U(j,i)*v[j];
-
-    return res;
-  }
+      return res;
+    }
   
-  /* Compute U^{-1}*v 
-   * Nota: there is no efficient way to compute D^{-1}U^{-1}v
-   * in a single loop, so algorithm is not proposed.*/
-  template<typename Mat>
-  Eigen::VectorXd
-  Uiv( const Model &                  model, 
-       Data&                          data ,
-       const Eigen::MatrixBase<Mat> &   v)
-  {
-    /* We search y s.t. v = U y. 
-     * For any k, v_k = y_k + U_{k,k+1:} y_{k+1:} */
-    assert(v.size() == model.nv);
-    EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
+    /* Compute U^{-1}*v 
+     * Nota: there is no efficient way to compute D^{-1}U^{-1}v
+     * in a single loop, so algorithm is not proposed.*/
+    template<typename Mat>
+    Eigen::VectorXd
+    Uiv( const Model &                  model, 
+	 Data&                          data ,
+	 const Eigen::MatrixBase<Mat> &   v)
+    {
+      /* We search y s.t. v = U y. 
+       * For any k, v_k = y_k + U_{k,k+1:} y_{k+1:} */
+      assert(v.size() == model.nv);
+      EIGEN_STATIC_ASSERT_VECTOR_ONLY(Mat);
 
-    Eigen::MatrixXd & U = data.U;
-    Eigen::VectorXd res = v;
-    const std::vector<int> & nvt = data.nvSubtree_fromRow;
+      Eigen::MatrixXd & U = data.U;
+      Eigen::VectorXd res = v;
+      const std::vector<int> & nvt = data.nvSubtree_fromRow;
 
-    for( int k=model.nv-1;k>=0;--k )
-      res[k] -= U.row(k).segment(k+1,nvt[k]-1).dot(res.segment(k+1,nvt[k]-1));
-    return res;
-  }
+      for( int k=model.nv-1;k>=0;--k )
+	res[k] -= U.row(k).segment(k+1,nvt[k]-1).dot(res.segment(k+1,nvt[k]-1));
+      return res;
+    }
 
     template<typename Mat>
     Eigen::VectorXd
@@ -200,13 +200,13 @@ namespace se3
 	  res[k] -= U(m,k)*res[m];
 
       return res;
-  }
+    }
 
     template<typename Mat>
     Eigen::VectorXd
     UtiDiv( const Model &                  model, 
-	  Data&                          data ,
-	  const Eigen::MatrixBase<Mat> &   v)
+	    Data&                          data ,
+	    const Eigen::MatrixBase<Mat> &   v)
     {
       /* We search y s.t. v = U' y. 
        * For any k, v_k = y_k + sum_{m \in parent{k}} U(m,k) v(k). */
@@ -224,9 +224,8 @@ namespace se3
 	  res[k] -= U(m,k)*res[m];
 
       return res;
-  }
+    }
 
- 
 
   } //   namespace cholesky
 } // namespace se3 
