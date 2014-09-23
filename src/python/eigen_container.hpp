93f88f5f9dad26f52cd833ca10aebb81687ddd44
Mansard <nmansard@laas.fr>
2014-09-23 15:54:33 +0200

    First complete wrapping of the model.

---
+++ src/python/eigen_container.hpp
@@ -0,0 +1,56 @@
+#ifndef __se3_python_eigen_container_hpp__
+#define __se3_python_eigen_container_hpp__
+
+#include <eigenpy/exception.hpp>
+#include <eigenpy/eigenpy.hpp>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/python/return_internal_reference.hpp>
+
+namespace se3
+{
+  namespace python
+  {
+    namespace bp = boost::python;
+
+    /* Expose a std::vector containing aligned (Eigen) objects, whose unaligned
+     * correspondance is defined in eigenpy::UnalignedEquivalent.
+     * Simply call the "expose" method from inside the python module. 
+     */
+    template< typename EigenObject >
+    struct PyWraperForAlignedStdVector
+      : public boost::python::def_visitor< PyWraperForAlignedStdVector<EigenObject> >
+    {
+      typedef typename eigenpy::UnalignedEquivalent<EigenObject>::type EigenObject_fx;
+      typedef std::vector<EigenObject> stdVectorAligned;
+      
+      template<class PyClass>
+      void visit(PyClass& cl) const 
+      {
+	cl
+	  .def("__getitem__", &PyWraperForAlignedStdVector::getItem)
+	  .def("__setitem__", &PyWraperForAlignedStdVector::setItem)
+	  .def("__len__",     &PyWraperForAlignedStdVector::length)
+	  ;
+      }
+
+      static EigenObject getItem( const stdVectorAligned & Ys,int i)
+      { return Ys[i]; }
+
+      static void setItem( stdVectorAligned & Ys,
+			   int i,const EigenObject_fx & Y)
+      { Ys[i] = Y;  }
+      static int length( const stdVectorAligned & Ys )
+      { return Ys.size(); }
+
+      static void expose(const std::string & className)
+      {
+	bp::class_<stdVectorAligned>(className.c_str())
+	  .def(PyWraperForAlignedStdVector());
+      }
+    };
+
+  }} // namespace se3::python
+
+#endif // ifndef __se3_python_eigen_container_hpp__
+
