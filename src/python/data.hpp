8ee2adbd4208253b351ca38faddbd327fcd8fd27
Mansard <nmansard@laas.fr>
2014-09-25 08:48:30 +0200

    Use Handler for python::Data.

--- src/python/data.hpp
+++ src/python/data.hpp
@@ -11,43 +11,9 @@ namespace se3
 {
   namespace python
   {
-
-    /* See the strategy applied for handling the Model object. The same applies
-     * here. */
-    struct DataHandler
-    {
-      typedef boost::shared_ptr<Data> SmartPtr_t;
-      typedef Data * Ptr_t;
-
-      SmartPtr_t smptr;
-      Ptr_t rawptr;
-      bool smart;
-
-      DataHandler(Data * data,bool transmitOwnership=false)
-	: smptr( transmitOwnership ? data : NULL )
-	, rawptr( data )
-	, smart( transmitOwnership ) {}
-      DataHandler( SmartPtr_t data )
-	: smptr(data), rawptr(NULL), smart(true) {}
-      ~DataHandler()
-      {    
-	std::cout << "Destroy data handler " << std::endl;
-	if( (!smart) && (rawptr!=NULL) ) delete rawptr;
-      }
-
-      Data *       ptr()              { return smart ?  smptr.get() :  rawptr; }
-      const Data * ptr()        const { return smart ?  smptr.get() :  rawptr; }
-      Data *       operator->()       { return ptr(); }
-      const Data * operator->() const { return ptr(); }
-
-      Data &       get()              { return smart ? *smptr       : *rawptr; }
-      const Data & get()        const { return smart ? *smptr       : *rawptr; }
-      Data &       operator*()        { return get(); }
-      const Data&  operator*()  const { return get(); }
-    };
-
-
     namespace bp = boost::python;
+
+    typedef Handler<Data> DataHandler;
 
     struct DataPythonVisitor
       : public boost::python::def_visitor< DataPythonVisitor >
@@ -78,9 +44,6 @@ namespace se3
 				 bp::no_init)
 	  .def(DataPythonVisitor());
     
-	/* Not sure if it is a good idea to enable automatic
-	 * conversion. Prevent it for now */
-	//bp::to_python_converter< Data,DataPythonVisitor >();
 	bp::to_python_converter< DataHandler::SmartPtr_t,DataPythonVisitor >();
       }
 
