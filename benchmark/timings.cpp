0fb61c0ad766ed7ce15aa8e89c4e8b0cc384a2b5
Mansard <nmansard@laas.fr>
2014-11-14 15:58:24 +0100

    Benchmark: repaired timings and introduce random reinit to comply with OS remarks. Scores: RNEA = 		4.5206 us CRBA = 		5.29557 us Cholesky = 	2.33944us Jacobian = 	3.6358 us COM+Jcom = 	3.95835 us

--- benchmark/timings.cpp
+++ benchmark/timings.cpp
@@ -15,6 +15,8 @@
 
 #include "pinocchio/tools/timer.hpp"
 
+#include <Eigen/StdVector>
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::VectorXd);
 
 int main(int argc, const char ** argv)
 {
@@ -25,12 +27,12 @@ int main(int argc, const char ** argv)
   const int NBT = 1000*100;
   se3::Model model;
 
-  std::string filename = "../models/simple_humanoid.urdf";
+  std::string filename = PINOCCHIO_SOURCE_DIR"/models/simple_humanoid.urdf";
   if(argc>1) filename = argv[1];
   if( filename == "HS") 
-    se3::urdf::buildModels::humanoidSimple(model,true);
+    se3::buildModels::humanoidSimple(model,true);
   else if( filename == "H2" )
-    se3::urdf::buildModels::humanoid2d(model);
+    se3::buildModels::humanoid2d(model);
   else
     model = se3::urdf::buildModel(filename,true);
   std::cout << "nq = " << model.nq << std::endl;
@@ -39,39 +41,55 @@ int main(int argc, const char ** argv)
   VectorXd q = VectorXd::Random(model.nq);
   VectorXd qdot = VectorXd::Random(model.nv);
   VectorXd qddot = VectorXd::Random(model.nv);
+
+  std::vector<VectorXd> qs     (NBT);
+  std::vector<VectorXd> qdots  (NBT);
+  std::vector<VectorXd> qddots (NBT);
+  for(int i=0;i<NBT;++i) 
+    {
+      qs[i]     = Eigen::VectorXd::Random(model.nq);
+      qs[i].segment<4>(3) /= qs[i].segment<4>(3).norm();
+      qdots[i]  = Eigen::VectorXd::Random(model.nv);
+      qddots[i] = Eigen::VectorXd::Random(model.nv);
+    }
+
  
   timer.tic();
   SMOOTH(NBT)
     {
-      rnea(model,data,q,qdot,qddot);
+      rnea(model,data,qs[_smooth],qdots[_smooth],qddots[_smooth]);
     }
   std::cout << "RNEA = \t\t"; timer.toc(std::cout,NBT);
  
   timer.tic();
   SMOOTH(NBT)
     {
-      crba(model,data,q);
+      crba(model,data,qs[_smooth]);
     }
   std::cout << "CRBA = \t\t"; timer.toc(std::cout,NBT);
- 
-  timer.tic();
+  
+  double total = 0;
   SMOOTH(NBT)
     {
+      crba(model,data,qs[_smooth]);
+      timer.tic();
       cholesky::decompose(model,data);
+      total += timer.toc(timer.DEFAULT_UNIT);
     }
-  std::cout << "Cholesky = \t"; timer.toc(std::cout,NBT);
+  std::cout << "Cholesky = \t" << (total/NBT) 
+	    << timer.unitName(timer.DEFAULT_UNIT) <<std::endl; 
  
   timer.tic();
   SMOOTH(NBT)
     {
-      computeJacobians(model,data,q);
+      computeJacobians(model,data,qs[_smooth]);
     }
   std::cout << "Jacobian = \t"; timer.toc(std::cout,NBT);
 
   timer.tic();
   SMOOTH(NBT)
     {
-      jacobianCenterOfMass(model,data,q,false);
+      jacobianCenterOfMass(model,data,qs[_smooth],false);
     }
   std::cout << "COM+Jcom = \t"; timer.toc(std::cout,NBT);
 
