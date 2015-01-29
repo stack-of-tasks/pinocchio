9512a4cf34a8f1f6502765a03a635d3c34ffb130
Justin Carpentier <justin.carpentier@laas.fr>
2015-01-29 18:29:45 +0100

    [Major] Add geometry update

--- src/algorithm/kinematics.hpp
+++ src/algorithm/kinematics.hpp
@@ -6,15 +6,67 @@
   
 namespace se3
 {
+  inline void geometry(const Model & model,
+                       Data & data,
+                       const Eigen::VectorXd & q);
+
   inline void kinematics(const Model & model,
-			 Data& data,
+			 Data & data,
 			 const Eigen::VectorXd & q,
 			 const Eigen::VectorXd & v);
+
 } // namespace se3 
 
 /* --- Details -------------------------------------------------------------------- */
 namespace se3 
 {
+  struct GeometryStep : public fusion::JointVisitor<GeometryStep>
+  {
+    typedef boost::fusion::vector<const se3::Model &,
+                                  se3::Data &,
+                                  const int &,
+                                  const Eigen::VectorXd &
+                                  > ArgsType;
+
+    JOINT_VISITOR_INIT (GeometryStep);
+
+    template<typename JointModel>
+    static void algo(const se3::JointModelBase<JointModel> & jmodel,
+                     se3::JointDataBase<typename JointModel::JointData> & jdata,
+                     const se3::Model & model,
+                     se3::Data & data,
+                     const int & i,
+                     const Eigen::VectorXd & q)
+    {
+      using namespace se3;
+
+      jmodel.calc (jdata.derived (), q);
+
+      const Model::Index & parent = model.parents[i];
+      data.liMi[i] = model.jointPlacements[i] * jdata.M ();
+
+      if (parent>0)
+        data.oMi[i] = data.oMi[parent] * data.liMi[i];
+      else
+        data.oMi[i] = data.liMi[i];
+    }
+    
+  };
+
+  inline void
+  geometry(const Model & model,
+           Data & data,
+           const Eigen::VectorXd & q)
+  {
+    for (size_t i=1; i < (size_t) model.nbody; ++i)
+    {
+      GeometryStep::run(model.joints[i],
+                        data.joints[i],
+                        GeometryStep::ArgsType (model,data,i,q)
+                        );
+    }
+  }
+
   struct KinematicsStep : public fusion::JointVisitor<KinematicsStep>
   {
     typedef boost::fusion::vector< const se3::Model&,
@@ -41,13 +93,16 @@ namespace se3
       jmodel.calc(jdata.derived(),q,v);
       
       const Model::Index & parent = model.parents[i];
+      data.v[i] = jdata.v();
       data.liMi[i] = model.jointPlacements[i]*jdata.M();
       
-      if(parent>0) data.oMi[i] = data.oMi[parent]*data.liMi[i];
-      else         data.oMi[i] = data.liMi[i];
-      
-      data.v[i] = jdata.v();
-      if(parent>0) data.v[i] += data.liMi[i].actInv(data.v[parent]);
+      if(parent>0)
+      {
+        data.oMi[i] = data.oMi[parent]*data.liMi[i];
+        data.v[i] += data.liMi[i].actInv(data.v[parent]);
+      }
+      else
+        data.oMi[i] = data.liMi[i];
     }
 
   };
