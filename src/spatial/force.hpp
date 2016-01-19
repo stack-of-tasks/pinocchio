0ca9211d0f1ec4cd70c789ede5ba42c226948e24
jcarpent <jcarpent@laas.fr>
2016-01-19 19:04:18 +0100

    [C++] Use Vector6 instead of 2 Vector3 in Motion and Force

--- src/spatial/force.hpp
+++ src/spatial/force.hpp
@@ -33,51 +33,47 @@ namespace se3
 
     typedef Derived  Derived_t;
     SPATIAL_TYPEDEF_TEMPLATE(Derived_t);
+    
+//    typedef typename Vector6::template ConstFixedSegmentReturnType<3>::Type ConstLinear_t;
+//    typedef typename Vector6::template ConstFixedSegmentReturnType<3>::Type ConstAngular_t;
+    typedef typename Eigen::VectorBlock<const Vector6,3> ConstLinear_t;
+    typedef ConstLinear_t ConstAngular_t;
 
   public:
     Derived_t & derived() { return *static_cast<Derived_t*>(this); }
     const Derived_t& derived() const { return *static_cast<const Derived_t*>(this); }
 
-    const Angular_t & angular() const  { return derived().angular_impl(); }
-    const Linear_t & linear() const  { return derived().linear_impl(); }
-    Angular_t & angular()  { return derived().angular_impl(); }
-    Linear_t & linear()   { return derived().linear_impl(); }
-    void angular(const Angular_t & R) { derived().angular_impl(R); }
-    void linear(const Linear_t & R) { derived().linear_impl(R); }
+    ConstAngular_t angular() const  { return derived().angular_impl(); }
+    ConstLinear_t linear() const  { return derived().linear_impl(); }
+    Angular_t angular()  { return derived().angular_impl(); }
+    Linear_t linear()   { return derived().linear_impl(); }
+    void angular(const Vector3 & n) { derived().angular_impl(n); }
+    void linear(const Vector3 & f) { derived().linear_impl(f); }
 
-
-    Vector6 toVector() const
-    {
-      return derived().toVector_impl();
-    }
+    const Vector6 & toVector() const { return derived().toVector_impl(); }
+    Vector6 & toVector() { return derived().toVector_impl(); }
     operator Vector6 () const { return toVector(); }
+    
+//    void disp(std::ostream & os) const
+//    {
+//      static_cast<const Derived_t*>(this)->disp_impl(os);
+//    }
 
-
-    void disp(std::ostream & os) const
-    {
-      static_cast<const Derived_t*>(this)->disp_impl(os);
-    }
-
+    bool operator== (const Derived_t & other) const {return derived().isEqual(other);}
     Derived_t & operator= (const Derived_t & other) { return derived().__equl__(other); }
     Derived_t& operator+= (const Derived_t & phi) { return derived().__pequ__(phi); }
     Derived_t operator+(const Derived_t & phi) const { return derived().__plus__(phi); }
     Derived_t operator*(double a) const    { return derived().__mult__(a); }
     Derived_t operator-() const { return derived().__minus__(); }
     Derived_t operator-(const Derived_t & phi) const { return derived().__minus__(phi); }
+    
+    Scalar_t dot(const Motion & m) const { return static_cast<Derived_t*>(this)->dot(m); }
 
+    Derived_t se3Action(const SE3 & m) const { return derived().se3Action_impl(m); }
+    Derived_t se3ActionInverse(const SE3 & m) const { return derived().se3ActionInverse_impl(m); }
 
-    /// af = aXb.act(bf)
-    Derived_t se3Action(const SE3 & m) const
-    {
-      return derived().se3Action_impl(m);
-    }
-
-    Derived_t se3ActionInverse(const SE3 & m) const
-    {
-      return derived().se3ActionInverse_impl(m);
-    }
-
-    friend std::ostream & operator << (std::ostream & os,const ForceBase<Derived> & X)
+    void disp(std::ostream & os) const { derived().disp_impl(os); }
+    friend std::ostream & operator << (std::ostream & os, const ForceBase<Derived_t> & X)
     { 
       X.disp(os);
       return os;
@@ -96,8 +92,11 @@ namespace se3
     typedef Eigen::Matrix<T,3,3,U> Matrix3;
     typedef Eigen::Matrix<T,4,4,U> Matrix4;
     typedef Eigen::Matrix<T,6,6,U> Matrix6;
-    typedef Vector3 Angular_t;
-    typedef Vector3 Linear_t;
+    typedef typename Eigen::VectorBlock<Vector6,3> Linear_t;
+    //    typedef typename Vector6::template FixedSeegmentReturnType<3>::Type Linear_t;
+    typedef Linear_t Angular_t;
+//    typedef Vector3 Angular_t;
+//    typedef Vector3 Linear_t;
     typedef Matrix6 ActionMatrix_t;
     typedef Eigen::Quaternion<T,U> Quaternion_t;
     typedef SE3Tpl<T,U> SE3;
@@ -118,61 +117,47 @@ namespace se3
     friend class ForceBase< ForceTpl< _Scalar, _Options > >;
     SPATIAL_TYPEDEF_TEMPLATE(ForceTpl);
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    
+//    typedef typename Vector6::template ConstFixedSegmentReturnType<3>::Type ConstLinear_t;
+//    typedef typename Vector6::template ConstFixedSegmentReturnType<3>::Type ConstAngular_t;
+    typedef typename Eigen::VectorBlock<const Vector6,3> ConstLinear_t;
+    typedef ConstLinear_t ConstAngular_t;
 
-
-    ForceTpl() : m_n(), m_f() {}
+    ForceTpl() : data() {}
 
 
     template<typename f3_t,typename n3_t>
     ForceTpl(const Eigen::MatrixBase<f3_t> & f,const Eigen::MatrixBase<n3_t> & n)
-    : m_n(n),
-    m_f(f)
     {
-
+      data << f, n;
     }
 
     template<typename f6>
     explicit ForceTpl(const Eigen::MatrixBase<f6> & f)
-    : m_n(f.template segment<3>(ANGULAR)),
-    m_f(f.template segment<3>(LINEAR)) 
+    : data(f)
     {
       EIGEN_STATIC_ASSERT_VECTOR_ONLY(f6);
       assert( f.size() == 6 );
     }
 
-
     template<typename S2,int O2>
     explicit ForceTpl(const ForceTpl<S2,O2> & clone)
-    : m_n(clone.angular()),
-    m_f(clone.linear())
-    {
-
-    }
-
+    : data(clone.toVector())
+    {}
 
     static ForceTpl Zero() { return ForceTpl(Linear_t::Zero(), Angular_t::Zero()); }
     static ForceTpl Random() { return ForceTpl(Linear_t::Random(), Angular_t::Random()); }
 
+    ForceTpl & setZero () { data.setZero (); return *this; }
+    ForceTpl & setRandom () { data.setRandom (); return *this; }
 
-    ForceTpl & setZero () { m_n.setZero (); m_f.setZero (); return *this; } 
-    ForceTpl & setRandom () { m_n.setRandom (); m_f.setRandom (); return *this; }
-
-
-    Vector6 toVector_impl() const
-    {
-      Vector6 f;
-      f.template segment<3>(ANGULAR) = m_n;
-      f.template segment<3>(LINEAR)  = m_f;
-      return f;
-    }
-
-
+    const Vector6 & toVector_impl() const { return data; }
+    Vector6 & toVector_impl() { return data; }
 
     void disp_impl(std::ostream & os) const
     {
-      os
-      << "f = " << linear_impl().transpose() << std::endl
-      << "tau = " << angular_impl().transpose() << std::endl;
+      os << "  f = " << linear_impl().transpose() << std::endl
+      << "  tau = " << angular_impl().transpose() << std::endl;
     }
 
     /// af = aXb.act(bf)
@@ -188,87 +173,48 @@ namespace se3
       return ForceTpl(m.rotation().transpose()*linear_impl(),
         m.rotation().transpose()*(angular_impl() - m.translation().cross(linear_impl())) );
     }
+    
+    bool isEqual (const ForceTpl & other) const { return data == other.data; }
 
     // Arithmetic operators
     template<typename S2, int O2>
     ForceTpl & operator= (const ForceTpl<S2,O2> & other)
     {
-      m_n = other.angular ();
-      m_f = other.linear ();
+      data = other.toVector();
       return *this;
     }
 
     template<typename F6>
-    ForceTpl & operator=(const Eigen::MatrixBase<F6> & phi)
+    ForceTpl & operator= (const Eigen::MatrixBase<F6> & phi)
     {
       EIGEN_STATIC_ASSERT_VECTOR_ONLY(F6); assert(phi.size() == 6);
-      m_n = phi.template segment<3>(ANGULAR);
-      m_f = phi.template segment<3>(LINEAR);
+      data = phi;
       return *this;
     }
 
-    // template <typename D>
-    // ForceTpl operator + (ForceBase<D> a){
-    //   return ForceTpl(m_f+a.linear(), m_n + a.angular());
-    // }
-
-     // friend ForceTpl operator*(Scalar a, const ForceTpl & phi)
-    // {
-    //   return ForceTpl(phi.n()*a, phi.f()*a);
-    // }
-
-    ForceTpl & __equl__(const ForceTpl & other)
-    {
-      m_n = other.angular();
-      m_f = other.linear();
-      return *this;
-    }
+    ForceTpl & __equl__(const ForceTpl & other) { data = other.data; return *this; }
+    ForceTpl & __pequ__ (const ForceTpl & phi) { data += phi.data; return *this; }
+    ForceTpl __plus__(const ForceTpl & phi) const { return ForceTpl(data + phi.data); }
+    ForceTpl __mult__(const double a) const { return ForceTpl(a*data); }
+    ForceTpl __minus__() const { return ForceTpl(-data); }
+    ForceTpl __minus__(const ForceTpl & phi) const { return ForceTpl(data - phi.data); }
 
 
-    ForceTpl& __pequ__ (const ForceTpl & phi)
-    {
-      m_f += phi.m_f;
-      m_n += phi.m_n;
-      return *this;
-    }
-
-    ForceTpl __plus__(const ForceTpl & phi) const
-    {
-      return ForceTpl(m_f+phi.m_f,m_n+phi.m_n);
-    }
-
-    ForceTpl __mult__(double a) const
-    {
-      return ForceTpl(m_f*a, m_n*a);
-    }
-
-    ForceTpl __minus__() const
-    {
-      return ForceTpl(-m_f, -m_n);
-    }
-
-    ForceTpl __minus__(const ForceTpl & phi) const
-    {
-      return ForceTpl(m_f-phi.m_f,m_n-phi.m_n);
-    }
-
-
-    const Angular_t & angular_impl() const { return m_n; }
-    Angular_t & angular_impl() { return m_n; }
-    void angular_impl(const Angular_t & R) { m_n = R; }
-    const Linear_t & linear_impl() const { return m_f;}
-    Linear_t & linear_impl() { return m_f;}
-    void linear_impl(const Linear_t & p) { m_f=p; }
+    ConstAngular_t angular_impl() const { return data.template segment<3> (ANGULAR); }
+    Angular_t angular_impl() { return data.template segment<3> (ANGULAR); }
+    void angular_impl(const Vector3 & n) { data.template segment<3> (ANGULAR) = n; }
+    ConstLinear_t linear_impl() const { return data.template segment<3> (LINEAR);}
+    Linear_t linear_impl() { return data.template segment<3> (LINEAR);}
+    void linear_impl(const Vector3 & f) { data.template segment<3> (LINEAR) = f; }
+    
+    Scalar_t dot(const Motion & m) const { return data.dot(m.toVector()); }
 
   protected:
-    Angular_t m_n;
-    Linear_t m_f;
+    Vector6 data;
 
   }; // class ForceTpl
 
-
   typedef ForceTpl<double,0> Force;
-
 
 } // namespace se3
 
