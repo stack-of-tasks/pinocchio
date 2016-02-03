e49094e87ea9a972df8558e0f06a096e92bc155a
Florent Lamiraux <florent@laas.fr>
2016-02-03 17:17:50 +0100

    Start documentation using doxygen.

---
+++ doc/additionalDoc/crtp.hh
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2016 CNRS
+// Author: Florent Lamiraux
+//
+// This file is part of Pinocchio
+// Pinocchio is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// Pinocchio is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Lesser Public License for more details. You should have
+// received a copy of the GNU Lesser General Public License along with
+// Pinocchio If not, see
+// <http://www.gnu.org/licenses/>.
+
+/** \page pinocchio_page_crtp Curiously recurring template pattern
+
+Pinocchio library makes intensive use of the so called CRTP desing
+pattern. This patterns is used for performance reasons in the
+implementation of static polymorphism.
+
+\sa https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+ */
