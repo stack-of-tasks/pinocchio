be68199a69a1b2e3416c2244ca5d496b8feb036b
jcarpent <jcarpent@laas.fr>
2016-07-27 19:24:15 +0200

    [unittest] Add exp/log unit test independent from spatial tests

--- unittest/tspatial.cpp
+++ unittest/tspatial.cpp
@@ -1,6 +1,5 @@
 //
 // Copyright (c) 2015-2016 CNRS
-// Copyright (c) 2015 Wandercraft, 86 rue de Paris 91400 Orsay, France.
 //
 // This file is part of Pinocchio
 // Pinocchio is free software: you can redistribute it
@@ -29,7 +28,6 @@
 #define BOOST_TEST_MODULE tspatialTest
 #include <boost/test/unit_test.hpp>
 #include <boost/utility/binary.hpp>
-
 
 BOOST_AUTO_TEST_SUITE ( tspatialTest)
 
@@ -310,44 +308,6 @@ BOOST_AUTO_TEST_CASE ( test_ActOnSet )
   for( int k=0;k<N;++k )
     BOOST_CHECK(jMi.act(se3::Motion(iV.col(k))).toVector().isApprox(jV.col(k), 1e-12));
 
-}
-
-BOOST_AUTO_TEST_CASE ( test_Explog )
-{
-  typedef se3::SE3::Vector3 Vector3;
-  typedef se3::SE3::Matrix3 Matrix3;
-  typedef Eigen::Matrix4d Matrix4;
-  typedef se3::Motion::Vector6 Vector6;
-
-  const double EPSILON = 1e-12;
-
-  // exp3 and log3.
-  Vector3 v3(Vector3::Random());
-  Matrix3 R(se3::exp3(v3));
-  BOOST_CHECK(R.transpose().isApprox(R.inverse(), EPSILON));
-  BOOST_CHECK_SMALL(R.determinant() - 1.0, EPSILON);
-  Vector3 v3FromLog(se3::log3(R));
-  BOOST_CHECK(v3.isApprox(v3FromLog, EPSILON));
-
-  // exp6 and log6.
-  se3::Motion nu = se3::Motion::Random();
-  se3::SE3 m = se3::exp6(nu);
-  BOOST_CHECK(m.rotation().transpose().isApprox(m.rotation().inverse(),
-                                EPSILON));
-  BOOST_CHECK_SMALL(m.rotation().determinant() - 1.0, EPSILON);
-  se3::Motion nuFromLog(se3::log6(m));
-  BOOST_CHECK(nu.linear().isApprox(nuFromLog.linear(), EPSILON));
-  BOOST_CHECK(nu.angular().isApprox(nuFromLog.angular(), EPSILON));
-
-  Vector6 v6(Vector6::Random());
-  se3::SE3 m2(se3::exp6(v6));
-  BOOST_CHECK(m2.rotation().transpose().isApprox(m2.rotation().inverse(),
-                                EPSILON));
-  BOOST_CHECK_SMALL(m2.rotation().determinant() - 1.0, EPSILON);
-  Matrix4 M = m2.toHomogeneousMatrix();
-  se3::Motion nu2FromLog(se3::log6(M));
-  Vector6 v6FromLog(nu2FromLog.toVector());
-  BOOST_CHECK(v6.isApprox(v6FromLog, EPSILON));
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
