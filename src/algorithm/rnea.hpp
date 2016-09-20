ea646e7fedbb79b16e6476555267e041cdfe4f3b
jcarpent <jcarpent@laas.fr>
2016-09-20 18:42:15 +0200

    [Algo] Add RNEA with external forces

--- src/algorithm/rnea.hpp
+++ src/algorithm/rnea.hpp
@@ -40,6 +40,25 @@ namespace se3
        const Eigen::VectorXd & a);
   
   ///
+  /// \brief The Recursive Newton-Euler algorithm. It computes the inverse dynamics, aka the joint torques according to the current state of the system, the desired joint accelerations and the external forces.
+  ///
+  /// \param[in] model The model structure of the rigid body system.
+  /// \param[in] data The data structure of the rigid body system.
+  /// \param[in] q The joint configuration vector (dim model.nq).
+  /// \param[in] v The joint velocity vector (dim model.nv).
+  /// \param[in] a The joint acceleration vector (dim model.nv).
+  /// \param[in] fext Vector of external forces expressed in the local frame of the joints (dim model.njoints)
+  ///
+  /// \return The desired joint torques stored in data.tau.
+  ///
+  inline const Eigen::VectorXd &
+  rnea(const Model & model, Data & data,
+       const Eigen::VectorXd & q,
+       const Eigen::VectorXd & v,
+       const Eigen::VectorXd & a,
+       const std::vector<Force> & fext);
+  
+  ///
   /// \brief Computes the non-linear effects (Corriolis, centrifual and gravitationnal effects), also called the biais terms \f$ b(q,\dot{q}) \f$ of the Lagrangian dynamics:
   /// <CENTER> \f$ \begin{eqnarray} M \ddot{q} + b(q, \dot{q}) = \tau  \end{eqnarray} \f$ </CENTER> <BR>
   /// \note This function is equivalent to se3::rnea(model, data, q, v, 0).
