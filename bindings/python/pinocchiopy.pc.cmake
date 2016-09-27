909f09ad029559a38cdf63c6be4296c44c77bc20
jcarpent <jcarpent@laas.fr>
2016-09-27 11:49:24 +0200

    [CMake] Add packaging of the Python wrapper

---
+++ bindings/python/pinocchiopy.pc.cmake
@@ -0,0 +1,19 @@
+prefix=${_PKG_CONFIG_PREFIX}
+exec_prefix=${_PKG_CONFIG_EXEC_PREFIX}
+libdir=${_PKG_CONFIG_PYWRAP_LIBDIR}
+bindir=${_PKG_CONFIG_PYWRAP_BINDIR}
+pkglibdir=${_PKG_CONFIG_PKGLIBDIR}
+includedir=${_PKG_CONFIG_INCLUDEDIR}
+datarootdir=${_PKG_CONFIG_DATAROOTDIR}
+pkgdatarootdir=${_PKG_CONFIG_PKGDATAROOTDIR}
+docdir=${_PKG_CONFIG_DOCDIR}
+doxygendocdir=${_PKG_CONFIG_DOXYGENDOCDIR}
+
+Name: pinocchiopy
+Description: Python bindings of Pinocchio
+URL: ${_PKG_CONFIG_URL}
+Version: ${_PKG_CONFIG_VERSION}
+Requires: ${_PKG_CONFIG_PYWRAP_REQUIRES}
+Conflicts: ${_PKG_CONFIG_PYWRAP_CONFLICTS}
+Libs: ${_PKG_CONFIG_PYWRAP_LIBS}
+Cflags: ${_PKG_CONFIG_PYWRAP_CFLAGS}
