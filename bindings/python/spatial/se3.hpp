9e3a6c3cb9bf5bce386c726a617b7c8eae544b0a
jcarpent <jcarpent@laas.fr>
2016-09-30 16:29:12 +0200

    [Python] Clean SE3 bindings

--- bindings/python/spatial/se3.hpp
+++ bindings/python/spatial/se3.hpp
@@ -19,12 +19,11 @@
 #ifndef __se3_python_se3_hpp__
 #define __se3_python_se3_hpp__
 
-#include <eigenpy/exception.hpp>
-#include <eigenpy/eigenpy.hpp>
 #include <eigenpy/memory.hpp>
 
 #include "pinocchio/spatial/se3.hpp"
 #include "pinocchio/bindings/python/utils/copyable.hpp"
+#include "pinocchio/bindings/python/utils/printable.hpp"
 
 EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(se3::SE3)
 
@@ -40,9 +39,6 @@ namespace se3
     {
       typedef typename SE3::Scalar Scalar;
       typedef typename SE3::Matrix3 Matrix3;
-      typedef typename SE3::Matrix6 Matrix6;
-      typedef typename SE3::Matrix4 Matrix4;
-      typedef typename SE3::Vector6 Vector6;
       typedef typename SE3::Vector3 Vector3;
       
     public:
@@ -51,77 +47,83 @@ namespace se3
       void visit(PyClass& cl) const 
       {
         cl
+        .def(bp::init<>("Default constructor."))
         .def(bp::init<Matrix3,Vector3>
              ((bp::arg("Rotation"),bp::arg("translation")),
               "Initialize from rotation and translation."))
         .def(bp::init<int>((bp::arg("trivial arg (should be 1)")),"Init to identity."))
-        .def(bp::init<SE3> ((bp::arg("other")), "Copy constructor."))
+        .def(bp::init<SE3>((bp::arg("other")), "Copy constructor."))
 
-	  .add_property("rotation",&SE3PythonVisitor::getRotation,&SE3PythonVisitor::setRotation)
-	  .add_property("translation",&SE3PythonVisitor::getTranslation,&SE3PythonVisitor::setTranslation)
-	  .add_property("homogeneous",&SE3::toHomogeneousMatrix)
-	  .add_property("action",&SE3::toActionMatrix)
+        .add_property("rotation",
+                      &getRotation,
+                      (void (SE3::*)(const Matrix3 &)) &SE3::rotation,
+                      "The rotation part of the transformation."
+                      )
+        .add_property("translation",
+                      &getTranslation,
+                      (void (SE3::*)(const Vector3 &)) &SE3::translation,
+                      "The translation part of the transformation."
+                      )
         
-    .def("setIdentity",&SE3PythonVisitor::setIdentity)
-    .def("setRandom",&SE3PythonVisitor::setRandom)
+        .add_property("homogeneous",&SE3::toHomogeneousMatrix,"Returns the homegeneous matrix of *this.")
+        .add_property("action",&SE3::toActionMatrix,"Returns the action matrix of *this.")
+        
+        .def("setIdentity",&SE3PythonVisitor::setIdentity,"Set *this to the identity placement.")
+        .def("setRandom",&SE3PythonVisitor::setRandom,"Set *this to a random placement.")
 
-	  .def("inverse", &SE3::inverse)
-	  .def("act_point", &SE3PythonVisitor::act_point)
-	  .def("actInv_point", &SE3PythonVisitor::actInv_point)
-	  .def("act_se3", &SE3PythonVisitor::act_se3)
-	  .def("actInv_se3", &SE3PythonVisitor::actInv_se3)
+        .def("inverse", &SE3::inverse)
+        .def("act", (Vector3 (SE3::*)(const Vector3 &) const) &SE3::act,
+             bp::args("point"),
+             "Returns a point which is the result of the entry point transforms by *this.")
+        .def("actInv", (Vector3 (SE3::*)(const Vector3 &) const) &SE3::actInv,
+             bp::args("point"),
+             "Returns a point which is the result of the entry point by the inverse of *this.")
+        .def("act", (SE3 (SE3::*)(const SE3 & other) const) &SE3::act,
+             bp::args("M"), "Returns the result of *this * M.")
+        .def("actInv", (SE3 (SE3::*)(const SE3 & other) const) &SE3::actInv,
+             bp::args("M"), "Returns the result of the inverse of *this times M.")
         
         .def("isApprox",(bool (SE3::*)(const SE3 & other, const Scalar & prec)) &SE3::isApprox,bp::args("other","prec"),"Returns true if *this is approximately equal to other, within the precision given by prec.")
         .def("isApprox",(bool (SE3::*)(const SE3 & other)) &SE3::isApprox,bp::args("other"),"Returns true if *this is approximately equal to other.")
         
         .def("isIdentity",(bool (SE3::*)(const Scalar & prec)) &SE3::isIdentity,bp::args("prec"),"Returns true if *this is approximately equal to the identity placement, within the precision given by prec.")
         .def("isIdentity",(bool (SE3::*)(void)) &SE3::isIdentity,"Returns true if *this is approximately equal to the identity placement.")
-	  
-	  .def("__str__",&SE3PythonVisitor::toString)
-	  .def("__invert__",&SE3::inverse)
-	  .def(bp::self * bp::self)
-	  .add_property("np",&SE3::toActionMatrix)
-
-	  .def("Identity",&SE3::Identity)
-	  .staticmethod("Identity")
-	  .def("Random",&SE3::Random)
-	  .staticmethod("Random")
-	  ;
-	  }
-
-      static Matrix3 getRotation( const SE3 & self ) { return self.rotation(); }
-      static void setRotation( SE3 & self, const Matrix3 & R ) { self.rotation(R); }
-      static Vector3 getTranslation( const SE3 & self ) { return self.translation(); }
-      static void setTranslation( SE3 & self, const Vector3 & p ) { self.translation(p); }
+        
+        .def("__invert__",&SE3::inverse,"Returns the inverse of *this.")
+        .def(bp::self * bp::self)
+        .add_property("np",&SE3::toHomogeneousMatrix)
+        
+        .def("Identity",&SE3::Identity,"Returns the identity transformation.")
+        .staticmethod("Identity")
+        .def("Random",&SE3::Random,"Returns a random transformation.")
+        .staticmethod("Random")
+        ;
+      }
+      
+      static void expose()
+      {
+        bp::class_<SE3>("SE3",
+                        "SE3 transformation composed defined by its translation and its rotation",
+                        bp::init<>())
+        .def(SE3PythonVisitor<SE3>())
+        .def(CopyableVisitor<SE3>())
+        .def(PrintableVisitor<SE3>())
+        ;
+        
+      }
+    private:
+      
+      static Vector3 getTranslation(const SE3 & self) { return self.translation(); }
+      static Matrix3 getRotation(const SE3 & self) { return self.rotation(); }
       
       static void setIdentity(SE3 & self) { self.setIdentity(); }
       static void setRandom(SE3 & self) { self.setRandom(); }
-
-      static Vector3 act_point( const SE3 & self, const Vector3 & pt ) { return self.act(pt); }
-      static Vector3 actInv_point( const SE3 & self, const Vector3 & pt ) { return self.actInv(pt); }
-      static SE3 act_se3( const SE3 & self, const SE3 & x ) { return self.act(x); }
-      static SE3 actInv_se3( const SE3 & self, const SE3 & x ) { return self.actInv(x); }
-      static std::string toString(const SE3& m) 
-      {	  std::ostringstream s; s << m; return s.str();       }
-
-      static void expose()
-      {
-	bp::class_<SE3>("SE3",
-			   "SE3 transformations, composing rotation and translation.\n\n"
-			   "Supported operations ...",
-			   bp::init<>())
-	  .def(SE3PythonVisitor<SE3>())
-        .def(CopyableVisitor<SE3>())
-	;
-    
-    }
-
-
     };
     
 
 
-  }} // namespace se3::python
+  } // namespace python
+} // namespace se3
 
 #endif // ifndef __se3_python_se3_hpp__
 
