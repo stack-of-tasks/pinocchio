3a1947c4ac2674bfb1860663017592354d01d9fb
jcarpent <jcarpent@laas.fr>
2016-10-01 12:54:19 +0200

    [Python] Clean Force bindings

--- bindings/python/spatial/force.hpp
+++ bindings/python/spatial/force.hpp
@@ -19,12 +19,11 @@
 #ifndef __se3_python_force_hpp__
 #define __se3_python_force_hpp__
 
-#include <eigenpy/exception.hpp>
-#include <eigenpy/eigenpy.hpp>
 #include <eigenpy/memory.hpp>
 
 #include "pinocchio/spatial/force.hpp"
 #include "pinocchio/bindings/python/utils/copyable.hpp"
+#include "pinocchio/bindings/python/utils/printable.hpp"
 
 EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(se3::Force)
 
@@ -38,8 +37,6 @@ namespace se3
     struct ForcePythonVisitor
       : public boost::python::def_visitor< ForcePythonVisitor<Force> >
     {
-      typedef typename Force::Matrix3 Matrix3;
-      typedef typename Force::Matrix6 Matrix6;
       typedef typename Force::Vector6 Vector6;
       typedef typename Force::Vector3 Vector3;
       
@@ -47,36 +44,65 @@ namespace se3
       void visit(PyClass& cl) const 
       {
         cl
+        .def(bp::init<>("Default constructor"))
         .def(bp::init<Vector3,Vector3>
              ((bp::arg("linear"),bp::arg("angular")),
               "Initialize from linear and angular components (dont mix the order)."))
-        .def(bp::init<Vector6>((bp::arg("Vector 6d")),"Init from vector 6 [f,n]"))
+        .def(bp::init<Vector6>((bp::arg("Vector 6d")),"Init from a vector 6[f,n]"))
         .def(bp::init<Force>((bp::arg("other")),"Copy constructor."))
         
-        .add_property("linear",&ForcePythonVisitor::getLinear,&ForcePythonVisitor::setLinear)
-        .add_property("angular",&ForcePythonVisitor::getAngular,&ForcePythonVisitor::setAngular)
-        .add_property("vector",&ForcePythonVisitor::getVector,&ForcePythonVisitor::setVector)
+        .add_property("linear",
+                      &ForcePythonVisitor::getLinear,
+                      &ForcePythonVisitor::setLinear,
+                      "Linear part of a *this, corresponding to the linear velocity in case of a Spatial velocity.")
+        .add_property("angular",
+                      &ForcePythonVisitor::getAngular,
+                      &ForcePythonVisitor::setAngular,
+                      "Angular part of a *this, corresponding to the angular velocity in case of a Spatial velocity.")
+        .add_property("vector",
+                      &ForcePythonVisitor::getVector,
+                      &ForcePythonVisitor::setVector,
+                      "Returns the components of *this as a 6d vector.")
         .add_property("np",&ForcePythonVisitor::getVector)
         
-        .def("se3Action",&Force::se3Action)
-        .def("se3ActionInverse",&Force::se3ActionInverse)
+        .def("se3Action",&Force::se3Action,
+             bp::args("M"),"Returns the result of the dual action of M on *this.")
+        .def("se3ActionInverse",&Force::se3ActionInverse,
+             bp::args("M"),"Returns the result of the dual action of the inverse of M on *this.")
         
-        .def("setZero",&ForcePythonVisitor::setZero)
-        .def("setRandom",&ForcePythonVisitor::setRandom)
+        .def("setZero",&ForcePythonVisitor::setZero,
+             "Set the linear and angular components of *this to zero.")
+        .def("setRandom",&ForcePythonVisitor::setRandom,
+             "Set the linear and angular components of *this to random values.")
         
-        .def("__add__",&ForcePythonVisitor::add)
-        .def("__sub__",&ForcePythonVisitor::subst)
-        .def("__neg__",&ForcePythonVisitor::neg)
-        .def(bp::self_ns::str(bp::self_ns::self))
-        .def(bp::self_ns::repr(bp::self_ns::self))
+        .def(bp::self + bp::self)
+        .def(bp::self += bp::self)
+        .def(bp::self - bp::self)
+        .def(bp::self -= bp::self)
+        .def(-bp::self)
         
-        .def("Random",&Force::Random)
+        .def("Random",&Force::Random,"Returns a random Force.")
         .staticmethod("Random")
-        .def("Zero",&Force::Zero)
+        .def("Zero",&Force::Zero,"Returns a zero Force.")
         .staticmethod("Zero")
         ;
       }
-
+      
+      static void expose()
+      {
+        bp::class_<Force>("Force",
+                          "Force vectors, in se3* == F^6.\n\n"
+                          "Supported operations ...",
+                          bp::init<>())
+        .def(ForcePythonVisitor<Force>())
+        .def(CopyableVisitor<Force>())
+        .def(PrintableVisitor<Force>())
+        ;
+        
+      }
+      
+    private:
+      
       static Vector3 getLinear(const Force & self ) { return self.linear(); }
       static void setLinear(Force & self, const Vector3 & f) { self.linear(f); }
       static Vector3 getAngular(const Force & self) { return self.angular(); }
@@ -87,29 +113,11 @@ namespace se3
       
       static Vector6 getVector(const Force & self) { return self.toVector(); }
       static void setVector(Force & self, const Vector6 & f) { self = f; }
-      
-      static Force add(const Force & f1, const Force & f2) { return f1+f2; }
-      static Force subst(const Force & f1, const Force & f2) { return f1-f2; }
-      static Force neg(const Force & f1) { return -f1; }
-
-      static void expose()
-      {
-	bp::class_<Force>("Force",
-			     "Force vectors, in se3* == F^6.\n\n"
-			     "Supported operations ...",
-			     bp::init<>())
-	  .def(ForcePythonVisitor<Force>())
-        .def(CopyableVisitor<Force>())
-	;
-    
-    }
-
 
     };
     
-
-
-  }} // namespace se3::python
+  } // namespace python
+} // namespace se3
 
 #endif // ifndef __se3_python_se3_hpp__
 
