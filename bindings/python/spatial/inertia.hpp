445bd5e1b937adea96b59fb7ed75dcdd2ea393cb
jcarpent <jcarpent@laas.fr>
2017-12-06 13:24:19 +0100

    [Python] Use correct setter for the inertia parameter of a spatial Inertia

--- bindings/python/spatial/inertia.hpp
+++ bindings/python/spatial/inertia.hpp
@@ -121,7 +121,18 @@ namespace se3
       static void setLever( Inertia & self, const Vector3 & lever ) { self.lever() = lever; }
       
       static Matrix3 getInertia( const Inertia & self ) { return self.inertia().matrix(); }
-      static void setInertia( Inertia & self, const Vector6 & minimal_inertia ) { self.inertia().data() = minimal_inertia; }
+//      static void setInertia(Inertia & self, const Vector6 & minimal_inertia) { self.inertia().data() = minimal_inertia; }
+      static void setInertia(Inertia & self, const Matrix3 & symmetric_inertia)
+      {
+        assert(symmetric_inertia.isApprox(symmetric_inertia.transpose()));
+        self.inertia().data() <<
+        symmetric_inertia(0,0),
+        symmetric_inertia(1,0),
+        symmetric_inertia(1,1),
+        symmetric_inertia(0,2),
+        symmetric_inertia(1,2),
+        symmetric_inertia(2,2);
+      }
 
       static Inertia* makeFromMCI(const double & mass,
                                   const Vector3 & lever,
