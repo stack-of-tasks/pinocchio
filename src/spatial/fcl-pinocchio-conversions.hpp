ee54bf6f4214eb695bfe6067a208ef8c724ecf0b
Joseph Mirabel <jmirabel@laas.fr>
2018-01-12 16:21:47 +0100

    [fcl] Remove unnecessary conversion functions

--- src/spatial/fcl-pinocchio-conversions.hpp
+++ src/spatial/fcl-pinocchio-conversions.hpp
@@ -23,30 +23,14 @@
 
 namespace se3
 {
-
-  // TODO When not supporting the version of hpp-fcl (v0.5) not using plain eigen types
-  // the following functions can be removed because
-  //  - fcl::Matrix3f = Eigen::Matrix3d
-  //  - fcl::Vec3f    = Eigen::Vector3d
-  // Currently, they are mandatory to support both v0.5 with FCL_HAVE_EIGEN and
-  // future version
-  inline fcl::Matrix3f   & toFclMatrix3f (Eigen::Matrix3d & mat) { return mat; }
-  inline Eigen::Matrix3d & toMatrix3d    (fcl::Matrix3f   & mat) { return mat; }
-  inline fcl::Vec3f      & toFclVec3f    (Eigen::Vector3d & vec) { return vec; }
-  inline Eigen::Vector3d & toVector3d    (fcl::Vec3f      & vec) { return vec; }
-  inline fcl::Matrix3f   const & toFclMatrix3f (Eigen::Matrix3d const & mat) { return mat; }
-  inline Eigen::Matrix3d const & toMatrix3d    (fcl::Matrix3f   const & mat) { return mat; }
-  inline fcl::Vec3f      const & toFclVec3f    (Eigen::Vector3d const & vec) { return vec; }
-  inline Eigen::Vector3d const & toVector3d    (fcl::Vec3f      const & vec) { return vec; }
-
   inline fcl::Transform3f toFclTransform3f(const SE3 & m)
   {
-    return fcl::Transform3f(toFclMatrix3f(m.rotation()), toFclVec3f(m.translation()));
+    return fcl::Transform3f(m.rotation(), m.translation());
   }
 
   inline SE3 toPinocchioSE3(const fcl::Transform3f & tf)
   {
-    return SE3(toMatrix3d(tf.getRotation()), toVector3d(tf.getTranslation()));
+    return SE3(tf.getRotation(), tf.getTranslation());
   }
 
 } // namespace se3
