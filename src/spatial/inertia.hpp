21600f58c476363000736ca981dd45e4dc09ebe0
jcarpent <jcarpent@laas.fr>
2018-04-30 16:17:12 +0200

    [Spatial] Add additionnal insight to a serious BUG on Inertia::vxi operation

--- src/spatial/inertia.hpp
+++ src/spatial/inertia.hpp
@@ -407,8 +407,12 @@ namespace se3
 
       //// Block 2,2
       skewSquare(-v.linear(),mc,Iout_.template block<3,3>(ANGULAR,ANGULAR));
-//      typename Symmetric3::AlphaSkewSquare mcxcx(I.mass(),I.lever());
-//      const Symmetric3 I_mcxcx(I.inertia() - mcxcx);
+
+      // TODO: I do not why, but depending on the CPU, these three lines can give
+      // wrong output.
+      //      typename Symmetric3::AlphaSkewSquare mcxcx(I.mass(),I.lever());
+      //      const Symmetric3 I_mcxcx(I.inertia() - mcxcx);
+      //      Iout_.template block<3,3>(ANGULAR,ANGULAR) += I_mcxcx.vxs(v.angular());
       Symmetric3 mcxcx(typename Symmetric3::AlphaSkewSquare(I.mass(),I.lever()));
       Iout_.template block<3,3>(ANGULAR,ANGULAR) += I.inertia().vxs(v.angular());
       Iout_.template block<3,3>(ANGULAR,ANGULAR) -= mcxcx.vxs(v.angular());
