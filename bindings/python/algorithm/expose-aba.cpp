0ac2d9cd7170a4563c99876d167829fe1159f4ba
jcarpent <jcarpent@laas.fr>
2018-05-24 23:35:58 +0200

    [Python] Expose computeMinverse

--- bindings/python/algorithm/expose-aba.cpp
+++ bindings/python/algorithm/expose-aba.cpp
@@ -23,6 +23,15 @@ namespace se3
   namespace python
   {
     
+    const Data::RowMatrixXd &
+    computeMinverse_proxy(const Model & model, Data & data, const Eigen::VectorXd & q)
+    {
+      computeMinverse(model,data,q);
+      data.Minv.triangularView<Eigen::StrictlyLower>() =
+      data.Minv.transpose().triangularView<Eigen::StrictlyLower>();
+      return data.Minv;
+    }
+    
     void exposeABA()
     {
       typedef Eigen::VectorXd DataVector;
@@ -45,6 +54,14 @@ namespace se3
                        "Joint torque tau (size Model::nv)",
                        "Vector of external forces expressed in the local frame of each joint (size Model::njoints)"),
               "Compute ABA with external forces, put the result in Data::ddq and return it.",
+              bp::return_value_policy<bp::return_by_value>());
+      
+      bp::def("computeMinverse",
+              &computeMinverse_proxy,
+              bp::args("Model","Data",
+                       "Joint configuration q (size Model::nq)"),
+              "Computes the inverse of the joint space inertia matrix using a variant of the Articulated Body algorithm.\n"
+              "The result is stored in data.Minv.",
               bp::return_value_policy<bp::return_by_value>());
       
     }
