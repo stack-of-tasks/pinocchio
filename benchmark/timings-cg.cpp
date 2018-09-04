f9393783489d9dac3142ca76e977afe7da6c6b54
Justin Carpentier <jcarpent@laas.fr>
2018-09-04 18:06:03 +0200

    [Bench/CodeGen] Use new classes to bench algo and their code generation

--- benchmark/timings-cg.cpp
+++ benchmark/timings-cg.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2018 CNRS
+// Copyright (c) 2018 CNRS
 //
 // This file is part of Pinocchio
 // Pinocchio is free software: you can redistribute it
@@ -28,323 +28,11 @@
 #include "pinocchio/parsers/sample-models.hpp"
 #include "pinocchio/container/aligned-vector.hpp"
 
+#include "pinocchio/codegen/code-generator-algo.hpp"
+
 #include <iostream>
 
 #include "pinocchio/utils/timer.hpp"
-
-template<typename _Scalar>
-struct CodeGenBase
-{
-  typedef _Scalar Scalar;
-  typedef CppAD::cg::CG<Scalar> CGScalar;
-  typedef CppAD::AD<CGScalar> ADScalar;
-  
-  enum { Options = 0 };
-  
-  typedef se3::ModelTpl<Scalar,Options> Model;
-  typedef se3::DataTpl<Scalar,Options> Data;
-  typedef se3::ModelTpl<CGScalar,Options> CGModel;
-  typedef se3::DataTpl<CGScalar,Options> CGData;
-  typedef se3::ModelTpl<ADScalar,Options> ADModel;
-  typedef se3::DataTpl<ADScalar,Options> ADData;
-  
-  typedef Eigen::Matrix<Scalar,Eigen::Dynamic,Eigen::Dynamic,Options> MatrixXs;
-  typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1,Options> VectorXs;
-  typedef Eigen::Matrix<Scalar,Eigen::Dynamic,Eigen::Dynamic,Options|Eigen::RowMajor> RowMatrixXs;
-  typedef Eigen::Matrix<ADScalar,Eigen::Dynamic,1,Options> ADVectorXs;
-  
-  typedef typename Model::ConfigVectorType CongigVectorType;
-  typedef typename Model::TangentVectorType TangentVectorType;
-  
-  typedef typename ADModel::ConfigVectorType ADCongigVectorType;
-  typedef typename ADModel::TangentVectorType ADTangentVectorType;
-  
-  typedef CppAD::ADFun<CGScalar> ADFun;
-  
-  CodeGenBase(const Model & model,
-              const Eigen::DenseIndex dim_input,
-              const Eigen::DenseIndex dim_output,
-              const std::string & function_name,
-              const std::string & library_name)
-  : ad_model(model.template cast<ADScalar>())
-  , ad_data(ad_model)
-  , function_name(function_name)
-  , library_name(library_name)
-  {
-    ad_X = ADVectorXs(model.nq+model.nv*2);
-    ad_Y = ADVectorXs(model.nv);
-    
-    x = VectorXs(ad_X.size());
-    y = VectorXs(ad_Y.size());
-    
-    jac = MatrixXs(y.size(),x.size());
-  }
-  
-  /// \brief build the mapping Y = f(X)
-  virtual void buildMap() = 0;
-  
-  void generateLib()
-  {
-    buildMap();
-    
-    // generates source code
-    cgen_ptr = std::unique_ptr<CppAD::cg::ModelCSourceGen<Scalar> >(new CppAD::cg::ModelCSourceGen<Scalar>(ad_fun, function_name));
-    cgen_ptr->setCreateJacobian(true);
-    cgen_ptr->setCreateForwardZero(true);
-    libcgen_ptr = std::unique_ptr<CppAD::cg::ModelLibraryCSourceGen<Scalar> >(new CppAD::cg::ModelLibraryCSourceGen<Scalar>(*cgen_ptr));
-    
-    dynamicLibManager_ptr
-    = std::unique_ptr<CppAD::cg::DynamicModelLibraryProcessor<Scalar> >(new CppAD::cg::DynamicModelLibraryProcessor<Scalar>(*libcgen_ptr,library_name));
-    
-    CppAD::cg::GccCompiler<Scalar> compiler;
-    std::vector<std::string> compile_options = compiler.getCompileFlags();
-    compile_options[0] = "-Ofast";
-    compiler.setCompileFlags(compile_options);
-    dynamicLibManager_ptr->createDynamicLibrary(compiler,false);
-  }
-  
-  bool existLib() const
-  {
-    const std::string filename = dynamicLibManager_ptr->getLibraryName() + CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION;
-    std::ifstream file(filename.c_str());
-    return file.good();
-  }
-  
-  void loadLib(const bool generate_if_not_exist = true)
-  {
-    if(not existLib() && generate_if_not_exist)
-      generateLib();
-    
-    const auto it = dynamicLibManager_ptr->getOptions().find("dlOpenMode");
-    if (it == dynamicLibManager_ptr->getOptions().end())
-    {
-      dynamicLib_ptr.reset(new CppAD::cg::LinuxDynamicLib<Scalar>(dynamicLibManager_ptr->getLibraryName() +  CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION));
-    }
-    else
-    {
-      int dlOpenMode = std::stoi(it->second);
-      dynamicLib_ptr.reset(new CppAD::cg::LinuxDynamicLib<Scalar>(dynamicLibManager_ptr->getLibraryName() +  CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION, dlOpenMode));
-    }
-    
-    generatedFun_ptr = dynamicLib_ptr->model(function_name.c_str());
-  }
-  
-  template<typename Vector>
-  void evalFunction(const Eigen::MatrixBase<Vector> & x)
-  {
-    generatedFun_ptr->ForwardZero(x,y);
-  }
-  
-  template<typename Vector>
-  void evalJacobian(const Eigen::MatrixBase<Vector> & x)
-  {
-    generatedFun_ptr->Jacobian(x,jac);
-  }
-  
-  Eigen::DenseIndex inputDimension() const { return (size_t)ad_X.size(); }
-  Eigen::DenseIndex outputDimension() const { return (size_t)ad_Y.size(); }
-  
-protected:
-  
-  ADModel ad_model;
-  ADData ad_data;
-  
-  const std::string function_name;
-  const std::string library_name;
-  
-  ADVectorXs ad_X, ad_Y;
-  ADFun ad_fun;
-  
-  ADCongigVectorType ad_q, ad_q_plus;
-  ADTangentVectorType ad_dq, ad_v, ad_a;
-  
-  VectorXs x,y;
-  RowMatrixXs jac;
-
-  std::unique_ptr<CppAD::cg::ModelCSourceGen<Scalar> > cgen_ptr;
-  std::unique_ptr<CppAD::cg::ModelLibraryCSourceGen<Scalar> > libcgen_ptr;
-  std::unique_ptr<CppAD::cg::DynamicModelLibraryProcessor<Scalar> > dynamicLibManager_ptr;
-  std::unique_ptr<CppAD::cg::DynamicLib<Scalar> > dynamicLib_ptr;
-  std::unique_ptr<CppAD::cg::GenericModel<Scalar> > generatedFun_ptr;
-  
-};
-
-template<typename _Scalar>
-struct CodeGenRNEA
-{
-  typedef _Scalar Scalar;
-  typedef CppAD::cg::CG<Scalar> CGScalar;
-  typedef CppAD::AD<CGScalar> ADScalar;
-  
-  enum { Options = 0 };
-  
-  typedef se3::ModelTpl<Scalar,Options> Model;
-  typedef se3::DataTpl<Scalar,Options> Data;
-  typedef se3::ModelTpl<CGScalar,Options> CGModel;
-  typedef se3::DataTpl<CGScalar,Options> CGData;
-  typedef se3::ModelTpl<ADScalar,Options> ADModel;
-  typedef se3::DataTpl<ADScalar,Options> ADData;
-  
-  typedef Eigen::Matrix<Scalar,Eigen::Dynamic,Eigen::Dynamic,Options> MatrixXs;
-  typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1,Options> VectorXs;
-  typedef Eigen::Matrix<ADScalar,Eigen::Dynamic,1,Options> ADVectorXs;
-  
-  typedef typename Model::ConfigVectorType CongigVectorType;
-  typedef typename Model::TangentVectorType TangentVectorType;
-  
-  typedef typename ADModel::ConfigVectorType ADCongigVectorType;
-  typedef typename ADModel::TangentVectorType ADTangentVectorType;
-  
-  typedef CppAD::ADFun<CGScalar> ADFun;
-  
-  CodeGenRNEA(const Model & model,
-              const std::string & function_name = "rnea",
-              const std::string & library_name = "cg_rnea_eval")
-  : ad_model(model.template cast<ADScalar>())
-  , ad_data(ad_model)
-  , function_name(function_name)
-  {
-    // Init variables
-    ad_q = ADCongigVectorType(model.nq);
-//    ad_q_plus = ADCongigVectorType(model.nq);
-//    ad_dq = ADTangentVectorType(model.nv); ad_dq.setZero();
-    ad_v = ADTangentVectorType(model.nv); ad_v.setZero();
-    ad_a = ADTangentVectorType(model.nv); ad_a.setZero();
-    
-    ad_X = ADVectorXs(model.nq+model.nv*2);
-    ad_Y = ADVectorXs(model.nv);
-    
-    x = VectorXs(ad_X.size());
-    
-    // Create the mapping Y = f(X)
-    compute();
-    
-    // generates source code
-    cgen_ptr = std::unique_ptr<CppAD::cg::ModelCSourceGen<Scalar> >(new CppAD::cg::ModelCSourceGen<Scalar>(ad_fun, function_name));
-    //        cgen_ptr->setCreateJacobian(true);
-    cgen_ptr->setCreateForwardZero(true);
-    //    cgen.setCreateForwardOne(true);
-    //    cgen.setCreateReverseOne(true);
-    //    cgen.setCreateReverseTwo(true);
-    libcgen_ptr = std::unique_ptr<CppAD::cg::ModelLibraryCSourceGen<Scalar> >(new CppAD::cg::ModelLibraryCSourceGen<Scalar>(*cgen_ptr));
-    
-    dynamicLibManager_ptr
-    = std::unique_ptr<CppAD::cg::DynamicModelLibraryProcessor<Scalar> >(new CppAD::cg::DynamicModelLibraryProcessor<Scalar>(*libcgen_ptr,library_name));
-  }
-  
-  void compute()
-  {
-//    ad_X.head(ad_model.nv).setZero();
-//    size_t abort_op_index = 0;
-//    bool   record_compare = false;
-//    CppAD::Independent(ad_X,abort_op_index,record_compare,ad_q);
-    CppAD::Independent(ad_X);
-    
-    Eigen::DenseIndex it = 0;
-    ad_q = ad_X.segment(it,ad_model.nq); it += ad_model.nq;
-    ad_v = ad_X.segment(it,ad_model.nv); it += ad_model.nv;
-    ad_a = ad_X.segment(it,ad_model.nv); it += ad_model.nv;
-    
-    //    ad_q_plus = se3::integrate(ad_model,ad_q,ad_dq);
-    se3::rnea(ad_model,ad_data,ad_q,ad_v,ad_a);
-    
-    ad_Y = ad_data.tau;
-    
-    ad_fun.Dependent(ad_X,ad_Y);
-    //    ad_fun.optimize("no_compare_op");
-  }
-  
-  bool existLib() const
-  {
-    const std::string filename = dynamicLibManager_ptr->getLibraryName() + CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION;
-    std::ifstream file(filename.c_str());
-    return file.good();
-  }
-  
-  void generateLib()
-  {
-    CppAD::cg::GccCompiler<Scalar> compiler;
-    std::vector<std::string> compile_options = compiler.getCompileFlags();
-    compile_options[0] = "-Ofast";
-    compiler.setCompileFlags(compile_options);
-    dynamicLibManager_ptr->createDynamicLibrary(compiler,false);
-  }
-  
-  void loadLib(const bool generate_if_not_exist = true)
-  {
-    if(not existLib() && generate_if_not_exist)
-      generateLib();
-    
-    const auto it = dynamicLibManager_ptr->getOptions().find("dlOpenMode");
-    if (it == dynamicLibManager_ptr->getOptions().end())
-    {
-      dynamicLib_ptr.reset(new CppAD::cg::LinuxDynamicLib<Scalar>(dynamicLibManager_ptr->getLibraryName() +  CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION));
-    }
-    else
-    {
-      int dlOpenMode = std::stoi(it->second);
-      dynamicLib_ptr.reset(new CppAD::cg::LinuxDynamicLib<Scalar>(dynamicLibManager_ptr->getLibraryName() +  CppAD::cg::system::SystemInfo<>::DYNAMIC_LIB_EXTENSION, dlOpenMode));
-    }
-    
-    generatedFun_ptr = dynamicLib_ptr->model(function_name.c_str());
-  }
-  
-  template<typename ConfigVectorType, typename TangentVector1, typename TangentVector2>
-  void eval(const Eigen::MatrixBase<ConfigVectorType> & q,
-            const Eigen::MatrixBase<TangentVector1> & v,
-            const Eigen::MatrixBase<TangentVector2> & a)
-  {
-    // fill x
-    Eigen::DenseIndex it = 0;
-    x.segment(it,ad_model.nq) = q; it += ad_model.nq;
-    x.segment(it,ad_model.nv) = v; it += ad_model.nv;
-    x.segment(it,ad_model.nv) = a; it += ad_model.nv;
-    
-    tau = generatedFun_ptr->ForwardZero(x);
-//    ad_fun.new_dynamic(q.derived());
-//
-//    CPPAD_TESTVECTOR(EvalScalar) x((size_t)(ad_X.size()));
-//    Eigen::DenseIndex it = 0;
-//    Eigen::Map<typename Data::TangentVectorType>(x.data(),ad_model.nv,1).setZero();
-//    it += ad_model.nv;
-//    Eigen::Map<typename Data::TangentVectorType>(x.data()+it,ad_model.nv,1) = v;
-//    it += ad_model.nv;
-//    Eigen::Map<typename Data::TangentVectorType>(x.data()+it,ad_model.nv,1) = a;
-//    it += ad_model.nv;
-//
-//    CPPAD_TESTVECTOR(EvalScalar) dy_dx = ad_fun.Jacobian(x);
-//    it = 0;
-//    drnea_dq = Eigen::Map<typename EIGEN_PLAIN_ROW_MAJOR_TYPE(typename EvalData::MatrixXs)>(dy_dx.data(),ad_model.nv,ad_model.nv);
-//    it += ad_model.nv * ad_model.nv;
-//    drnea_dv = Eigen::Map<typename EIGEN_PLAIN_ROW_MAJOR_TYPE(typename EvalData::MatrixXs)>(dy_dx.data()+it,ad_model.nv,ad_model.nv);
-//    it += ad_model.nv * ad_model.nv;
-//    drnea_da = Eigen::Map<typename EIGEN_PLAIN_ROW_MAJOR_TYPE(typename EvalData::MatrixXs)>(dy_dx.data()+it,ad_model.nv,ad_model.nv);
-//    it += ad_model.nv * ad_model.nv;
-  }
-protected:
-  
-  ADModel ad_model;
-  ADData ad_data;
-  
-  const std::string function_name;
-  const std::string library_name;
-  
-  ADVectorXs ad_X, ad_Y;
-  ADFun ad_fun;
-  
-  ADCongigVectorType ad_q, ad_q_plus;
-  ADTangentVectorType ad_dq, ad_v, ad_a;
-  
-  VectorXs x,y,tau;
-  MatrixXs jac;
-  
-  std::unique_ptr<CppAD::cg::ModelCSourceGen<Scalar> > cgen_ptr;
-  std::unique_ptr<CppAD::cg::ModelLibraryCSourceGen<Scalar> > libcgen_ptr;
-  std::unique_ptr<CppAD::cg::DynamicModelLibraryProcessor<Scalar> > dynamicLibManager_ptr;
-  std::unique_ptr<CppAD::cg::DynamicLib<Scalar> > dynamicLib_ptr;
-  std::unique_ptr<CppAD::cg::GenericModel<Scalar> > generatedFun_ptr;
-
-};
 
 int main(int argc, const char ** argv)
 {
@@ -363,28 +51,67 @@ int main(int argc, const char ** argv)
 
   std::string filename = PINOCCHIO_SOURCE_DIR"/models/simple_humanoid.urdf";
   if(argc>1) filename = argv[1];
+  
+  bool with_ff = true;
+  if(argc>2)
+  {
+    const std::string ff_option = argv[2];
+    if(ff_option == "-no-ff")
+      with_ff = false;
+  }
+  
   if( filename == "HS") 
     se3::buildModels::humanoidSimple(model,true);
   else if( filename == "H2" )
     se3::buildModels::humanoid2d(model);
   else
-    se3::urdf::buildModel(filename,JointModelFreeFlyer(),model);
+    if(with_ff)
+      se3::urdf::buildModel(filename,JointModelFreeFlyer(),model);
+    else
+      se3::urdf::buildModel(filename,model);
+  
   std::cout << "nq = " << model.nq << std::endl;
+  std::cout << "nv = " << model.nv << std::endl;
+  std::cout << "--" << std::endl;
 
   se3::Data data(model);
   
   CodeGenRNEA<double> rnea_code_gen(model);
+  rnea_code_gen.initLib();
   rnea_code_gen.loadLib();
+  
+  CodeGenABA<double> aba_code_gen(model);
+  aba_code_gen.initLib();
+  aba_code_gen.loadLib();
+  
+  CodeGenCRBA<double> crba_code_gen(model);
+  crba_code_gen.initLib();
+  crba_code_gen.loadLib();
+  
+  CodeGenMinv<double> minv_code_gen(model);
+  minv_code_gen.initLib();
+  minv_code_gen.loadLib();
+  
+  CodeGenRNEADerivatives<double> rnea_derivatives_code_gen(model);
+  rnea_derivatives_code_gen.initLib();
+  rnea_derivatives_code_gen.loadLib();
+  
+  CodeGenABADerivatives<double> aba_derivatives_code_gen(model);
+  aba_derivatives_code_gen.initLib();
+  aba_derivatives_code_gen.loadLib();
 
   se3::container::aligned_vector<VectorXd> qs     (NBT);
   se3::container::aligned_vector<VectorXd> qdots  (NBT);
   se3::container::aligned_vector<VectorXd> qddots (NBT);
+  se3::container::aligned_vector<VectorXd> taus (NBT);
+  
   for(size_t i=0;i<NBT;++i)
   {
     qs[i]     = Eigen::VectorXd::Random(model.nq);
     qs[i].segment<4>(3) /= qs[i].segment<4>(3).norm();
     qdots[i]  = Eigen::VectorXd::Random(model.nv);
     qddots[i] = Eigen::VectorXd::Random(model.nv);
+    taus[i] = Eigen::VectorXd::Random(model.nv);
   }
 
   timer.tic();
@@ -393,13 +120,90 @@ int main(int argc, const char ** argv)
     rnea(model,data,qs[_smooth],qdots[_smooth],qddots[_smooth]);
   }
   std::cout << "RNEA = \t\t"; timer.toc(std::cout,NBT);
+
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    rnea_code_gen.evalFunction(qs[_smooth],qdots[_smooth],qddots[_smooth]);
+  }
+  std::cout << "RNEA generated = \t\t"; timer.toc(std::cout,NBT);
+
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    rnea_code_gen.evalJacobian(qs[_smooth],qdots[_smooth],qddots[_smooth]);
+  }
+  std::cout << "RNEA partial derivatives auto diff + code gen = \t\t"; timer.toc(std::cout,NBT);
   
   timer.tic();
   SMOOTH(NBT)
   {
-    rnea_code_gen.eval(qs[_smooth],qdots[_smooth],qddots[_smooth]);
+    rnea_derivatives_code_gen.evalFunction(qs[_smooth],qdots[_smooth],qddots[_smooth]);
   }
-  std::cout << "RNEA generated = \t\t"; timer.toc(std::cout,NBT);
+  std::cout << "RNEA partial derivatives code gen = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    aba(model,data,qs[_smooth],qdots[_smooth],taus[_smooth]);
+  }
+  timer.toc();
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    crba(model,data,qs[_smooth]);
+  }
+  std::cout << "CRBA = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    crba_code_gen.evalFunction(qs[_smooth]);
+  }
+  std::cout << "CRBA generated = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    computeMinverse(model,data,qs[_smooth]);
+  }
+  std::cout << "Minv = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    minv_code_gen.evalFunction(qs[_smooth]);
+  }
+  std::cout << "Minv generated = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    aba(model,data,qs[_smooth],qdots[_smooth],taus[_smooth]);
+  }
+  std::cout << "ABA = \t\t"; timer.toc(std::cout,NBT);
+
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    aba_code_gen.evalFunction(qs[_smooth],qdots[_smooth],taus[_smooth]);
+  }
+  std::cout << "ABA generated = \t\t"; timer.toc(std::cout,NBT);
+
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    aba_code_gen.evalJacobian(qs[_smooth],qdots[_smooth],taus[_smooth]);
+  }
+  std::cout << "ABA partial derivatives auto diff + code gen = \t\t"; timer.toc(std::cout,NBT);
+  
+  timer.tic();
+  SMOOTH(NBT)
+  {
+    aba_derivatives_code_gen.evalFunction(qs[_smooth],qdots[_smooth],qddots[_smooth]);
+  }
+  std::cout << "ABA partial derivatives code gen = \t\t"; timer.toc(std::cout,NBT);
   
   return 0;
 }
