f981cdaf670a19c9c1402e80d8bbd6472e17335a
Gabriele Buondonno <gbuondon@laas.fr>
2018-09-11 14:43:53 +0200

    [jacobian] removed use of deprecated function to avoid spurious warning

--- src/algorithm/jacobian.hpp
+++ src/algorithm/jacobian.hpp
@@ -200,7 +200,7 @@ namespace se3
                 const Model::JointIndex jointId)
   {
     data.J.setZero();
-    jacobian(model,data,q,jointId,data.J);
+    jointJacobian(model,data,q,jointId,data.J);
     
     return data.J;
   }
