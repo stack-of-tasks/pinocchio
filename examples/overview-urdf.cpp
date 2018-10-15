5462018671066d277d113b87d7b1ca1555e30561
Mansard <nmansard@laas.fr>
2018-10-15 13:04:11 +0200

    [doc] Moved previous overview example to IK example, and added a new example for the overview.

--- examples/overview-urdf.cpp
+++ examples/overview-urdf.cpp
@@ -1,7 +1,6 @@
 #include "pinocchio/multibody/model.hpp"
 #include "pinocchio/parsers/urdf.hpp"
-#include "pinocchio/algorithm/jacobian.hpp"
-#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
 
 int main(int argc, char ** argv)
 {
@@ -9,25 +8,11 @@ int main(int argc, char ** argv)
   se3::Model model;
   se3::urdf::buildModel(filename,model); 
   se3::Data data(model);
-
-  const int    JOINT_ID = 6;
-  const double DT       = 1e-1;
   Eigen::VectorXd q(model.nq); q    << 2, -.5, 1.8, 1.8, 2.6, -2;
-  Eigen::Vector3d xdes;        xdes << 0, -0.5, 0.5;
 
-  se3::Data::Matrix6x J(6,model.nv); J.fill(0);
-  unsigned int svdOptions = Eigen::ComputeThinU | Eigen::ComputeThinV;
+  se3::forwardKinematics(model,data,q);
 
-  for (int i=0 ; i<100 ; ++i)
-    {
-      se3::computeJointJacobians(model,data,q);
-      se3::getJointJacobian<se3::LOCAL>(model,data,JOINT_ID,J);
-      const Eigen::Vector3d & x   = data.oMi[JOINT_ID].translation();
-      const Eigen::Matrix3d & R   = data.oMi[JOINT_ID].rotation();
-      const Eigen::Vector3d & err = R.transpose()*(x-xdes);
-      const Eigen::VectorXd v     = -J.topRows<3>().bdcSvd(svdOptions).solve(err);
-      q   = se3::integrate(model,q,v*DT);
-      if(!(i%10)) std::cout << err.transpose() << std::endl;
-    }
- 
+  for (int k=0 ; k<model.njoints ; ++k)
+    std::cout << model.names[k] << "\t: "
+              << data.oMi[k].translation().transpose() << std::endl;
 }
--- examples/overview-urdf.py
+++ examples/overview-urdf.py
@@ -2,19 +2,12 @@ import numpy
 import pinocchio
 
 filename = "ur5.urdf" 
-model = pinocchio.buildModelFromUrdf(filename)
-data  = model.createData()
-
-JOINT_ID = 6
-DT       = 1e-1
+model    = pinocchio.buildModelFromUrdf(filename)
+data     = model.createData()
 q        = numpy.matrix([ 2, -.5, 1.8, 1.8, 2.6, -2 ]).T
-xdes     = numpy.matrix([ 0,-0.5,0.5]).T
 
-for i in range(100):
-    J   = pinocchio.jointJacobian(model,data,q,JOINT_ID,pinocchio.ReferenceFrame.LOCAL,True)[:3,:]
-    x   = data.oMi[JOINT_ID].translation
-    R   = data.oMi[JOINT_ID].rotation
-    err = R.T*(x-xdes)
-    v   = - numpy.linalg.pinv(J)*err
-    q   = pinocchio.integrate(model,q,v*DT)
-    if not i % 10:        print 'error = ', (x-xdes).T
+pinocchio.forwardKinematics(model,data,q)
+
+for k in range(model.njoints):
+    print("{:<24} : {: .2f} {: .2f} {: .2f}"
+          .format( model.names[k], *data.oMi[k].translation.T.flat ))
