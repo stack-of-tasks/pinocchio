e2bd44c120a38382eed27d8835ee10a12df46821
Justin Carpentier <justin.carpentier@inria.fr>
2018-11-10 00:27:21 +0100

    python/explog: rework bindings

--- bindings/python/scripts/explog.py
+++ bindings/python/scripts/explog.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015 CNRS
+# Copyright (c) 2015-2018 CNRS INRIA
 # Copyright (c) 2015 Wandercraft, 86 rue de Paris 91400 Orsay, France.
 #
 
@@ -7,34 +7,36 @@ import math
 
 import numpy as np
 
-from . import libpinocchio_pywrap as se3
-
+from . import libpinocchio_pywrap as pin
 
 def exp(x):
-    if isinstance(x, se3.Motion):
-        return se3.exp6FromMotion(x)
+    if isinstance(x, pin.Motion):
+        return pin.exp6(x)
     if np.isscalar(x):
         return math.exp(x)
     if isinstance(x, np.ndarray):
         if x.shape == (6, 1):
-            return se3.exp6FromVector(x)
+            return pin.exp6(pin.Motion(x))
         if x.shape == (3, 1):
-            return se3.exp3(x)
+            return pin.exp3(x)
         raise ValueError('Error only 3 and 6 vectors are allowed.')
-    raise ValueError('Error exp is only defined for real, vector3, vector6 and se3.Motion objects.')
+    raise ValueError('Error exp is only defined for real, vector3, vector6 and pin.Motion objects.')
 
 
 def log(x):
-    if isinstance(x, se3.SE3):
-        return se3.log6FromSE3(x)
+    if isinstance(x, pin.SE3):
+        return pin.log6(x)
     if np.isscalar(x):
         return math.log(x)
     if isinstance(x, np.ndarray):
         if x.shape == (4, 4):
-            return se3.log6FromMatrix(x)
+            M = pin.SE3()
+            M.rotation = x[:3,:3]
+            M.translation = x[:3,-1]
+            return pin.log6(M)
         if x.shape == (3, 3):
-            return se3.log3(x)
+            return pin.log3(x)
         raise ValueError('Error only 3 and 4 matrices are allowed.')
-    raise ValueError('Error log is only defined for real, matrix3, matrix4 and se3.SE3 objects.')
+    raise ValueError('Error log is only defined for real, matrix3, matrix4 and pin.SE3 objects.')
 
 __all__ = ['exp', 'log']
