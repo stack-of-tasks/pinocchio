670649e904a859539c141b7ebeeedf078d283474
Gabriele Buondonno <gbuondon@laas.fr>
2019-01-24 15:38:11 +0100

    Inverse kinematics worked example

--- examples/i-inverse-kinematics.py
+++ examples/i-inverse-kinematics.py
@@ -1,27 +1,33 @@
-import numpy
+from __future__ import print_function
+
+import numpy as np
 import pinocchio
-from sys import argv
 
 model = pinocchio.buildSampleModelManipulator()
 data  = model.createData()
 
 JOINT_ID = 6
-DT       = 1e-1
-q        = model.neutralConfiguration
-xdes     = numpy.matrix([ 0.5,-0.5,0.5]).T
+xdes     = np.matrix([ 0.5,-0.5,0.5]).T
 
-for i in range(100):
-    pinocchio.computeJointJacobians(model,data,q)
-    J   = pinocchio.jointJacobian(model,data,q,JOINT_ID,pinocchio.ReferenceFrame.LOCAL,True)[:3,:]
+q        = model.neutralConfiguration
+eps      = 1e-4
+IT_MAX   = 1000
+DT       = 1e-1
+
+for i in range(IT_MAX):
+    pinocchio.forwardKinematics(model,data,q)
     x   = data.oMi[JOINT_ID].translation
     R   = data.oMi[JOINT_ID].rotation
     err = R.T*(x-xdes)
-    v   = - numpy.linalg.pinv(J)*err
+    if np.linalg.norm(err) < eps:
+        print("Convergence achieved!")
+        break
+    J   = pinocchio.jointJacobian(model,data,q,JOINT_ID,pinocchio.ReferenceFrame.LOCAL,True)[:3,:]
+    v   = - np.linalg.pinv(J)*err
     q   = pinocchio.integrate(model,q,v*DT)
-    if not i % 10:        print 'error = ', (x-xdes).T
+    if not i % 10:        print('error = %s' % (x-xdes).T)
+else:
+    print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
 
-# Computing error for final q
-pinocchio.forwardKinematics(model,data,q)
-x   = data.oMi[JOINT_ID].translation
-err = x-xdes
-print '\nfinal error: ', err.T
+print('\nresult: %s' % q.flatten().tolist())
+print('\nfinal error: %s' % (x-xdes).T)
