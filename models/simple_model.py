e1f6c42a794befd62f0eaeefcb70561f5c915f69
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-02-12 16:36:56 +0100

    [python] simple_humanoid follows pinocchio v2 convention

--- models/simple_model.py
+++ models/simple_model.py
@@ -15,7 +15,7 @@
 # Pinocchio If not, see
 # <http://www.gnu.org/licenses/>.
 
-import pinocchio as se3
+import pinocchio as pin
 from math import pi
 from pinocchio.utils import np, rotate, XYZQUATToViewerConfiguration, se3ToXYZQUAT
 
@@ -23,7 +23,7 @@ DENSITY = 1
 
 
 def placement(x=0, y=0, z=0, rx=0, ry=0, rz=0):
-    m = se3.SE3.Identity()
+    m = pin.SE3.Identity()
     m.translation = np.matrix([[float(i)] for i in [x, y, z]])
     m.rotation *= rotate('x', rx)
     m.rotation *= rotate('y', ry)
@@ -37,9 +37,9 @@ def color(body_number=1):
 
 class ModelWrapper(object):
     def __init__(self, name=None, display=False):
-        self.visuals = [('universe', se3.SE3.Identity(), se3.SE3.Identity().translation)]
+        self.visuals = [('universe', pin.SE3.Identity(), pin.SE3.Identity().translation)]
         self.name = self.__class__.__name__ if name is None else name
-        self.model = se3.Model()
+        self.model = pin.Model()
         self.display = display
         self.add_joints()
 
@@ -50,45 +50,45 @@ class ModelWrapper(object):
     def add_joint(self, joint_name, joint_model=None, joint_placement=None, lever=None, shape="box",
                   dimensions=1, mass=None, body_color=1, parent=0):
         if joint_model is None:
-            joint_model = se3.JointModelFreeFlyer()
+            joint_model = pin.JointModelFreeFlyer()
         elif isinstance(joint_model, str):
-            joint_model = se3.__dict__['JointModel' + joint_model]()
+            joint_model = pin.__dict__['JointModel' + joint_model]()
         if joint_placement is None:
-            joint_placement = se3.SE3.Identity()
+            joint_placement = pin.SE3.Identity()
         elif isinstance(joint_placement, dict):
             joint_placement = placement(**joint_placement)
         if lever is None:
-            lever = se3.SE3.Identity()
+            lever = pin.SE3.Identity()
         elif isinstance(lever, dict):
             lever = placement(**lever)
         joint_name, body_name = ('world/%s_%s_%s' % (self.name, joint_name, i) for i in ('joint', 'body'))
-        body_inertia = se3.Inertia.Random()
+        body_inertia = pin.Inertia.Random()
         if shape == "box":
             w, h, d = (float(i) for i in dimensions) if isinstance(dimensions, tuple) else [float(dimensions)] * 3
             if mass is None:
                 mass = w * h * d * DENSITY
-            body_inertia = se3.Inertia.FromBox(mass, w, h, d)
+            body_inertia = pin.Inertia.FromBox(mass, w, h, d)
             if self.display:
                 self.display.viewer.gui.addBox(body_name, w, h, d, color(body_color))
         elif shape == "cylinder":
             r, h = dimensions
             if mass is None:
                 mass = pi * r ** 2 * h * DENSITY
-            body_inertia = se3.Inertia.FromCylinder(mass, r, h)
+            body_inertia = pin.Inertia.FromCylinder(mass, r, h)
             if self.display:
                 self.display.viewer.gui.addCylinder(body_name, r, h, color(body_color))
         elif shape == "sphere":
             w, h, d = (float(i) for i in dimensions) if isinstance(dimensions, tuple) else [float(dimensions)] * 3
             if mass is None:
                 mass = 4. / 3. * pi * w * h * d * DENSITY
-            body_inertia = se3.Inertia.FromEllipsoid(mass, w, h, d)
+            body_inertia = pin.Inertia.FromEllipsoid(mass, w, h, d)
             if self.display:
                 self.display.viewer.gui.addSphere(body_name, dimensions, color(body_color))
         body_inertia.lever = lever.translation
         joint_id = self.model.addJoint(parent, joint_model, joint_placement, joint_name)
-        self.model.appendBodyToJoint(joint_id, body_inertia, se3.SE3.Identity())
+        self.model.appendBodyToJoint(joint_id, body_inertia, pin.SE3.Identity())
         self.model.addJointFrame(joint_id, -1)
-        self.model.addBodyFrame(body_name, joint_id, se3.SE3.Identity(),-1)
+        self.model.addBodyFrame(body_name, joint_id, pin.SE3.Identity(),-1)
         self.visuals.append((body_name, joint_placement, lever))
         self.data = self.model.createData()
         if self.display:
