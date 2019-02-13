53a8840871915ef123466e3bfdbd0c17e25a38d0
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-02-13 16:49:07 +0100

    [Examples] Python 3 syntax

--- examples/overview-simple.py
+++ examples/overview-simple.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import pinocchio
 
 model = pinocchio.buildSampleModelManipulator()
@@ -8,4 +10,4 @@ v = pinocchio.utils.zero(model.nv)
 a = pinocchio.utils.zero(model.nv)
 
 tau = pinocchio.rnea(model,data,q,v,a)
-print 'tau = ', tau.T
+print('tau = ', tau.T)
