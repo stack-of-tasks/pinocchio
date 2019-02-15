7acb1cc0c4d9850e293a74d7eed165b92a238ba9
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-02-15 11:48:11 +0100

    [Python 3][Tests] syntax

--- unittest/python/test_case.py
+++ unittest/python/test_case.py
@@ -3,7 +3,7 @@ import unittest
 from pinocchio.utils import isapprox
 
 def tracefunc(frame, event, arg):
-    print "%s, %s: %d" % (event, frame.f_code.co_filename, frame.f_lineno)
+    print("%s, %s: %d" % (event, frame.f_code.co_filename, frame.f_lineno))
     return tracefunc
 
 class TestCase(unittest.TestCase):
