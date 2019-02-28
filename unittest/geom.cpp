ecb857bb5884de7cbdaaba043015809a3359919c
Justin Carpentier <justin.carpentier@inria.fr>
2019-02-28 08:47:19 +0100

    test/geom: improve coverty of the geometry algos

--- unittest/geom.cpp
+++ unittest/geom.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2018 CNRS
+// Copyright (c) 2015-2019 CNRS INRIA
 //
 
 #include <iostream>
@@ -13,6 +13,7 @@
 #include "pinocchio/algorithm/kinematics.hpp"
 #include "pinocchio/algorithm/geometry.hpp"
 #include "pinocchio/parsers/urdf.hpp"
+#include "pinocchio/parsers/srdf.hpp"
 
 #include <vector>
 #include <boost/test/unit_test.hpp>
@@ -66,6 +67,18 @@ BOOST_AUTO_TEST_CASE ( simple_boxes )
                                                                            sample2,SE3::Identity(), "", Eigen::Vector3d::Ones()),
                                                             model);
   BOOST_CHECK(geomModel.geometryObjects[idx_geom2].parentJoint == model.frames[body_id_2].parent);
+  
+  boost::shared_ptr<fcl::Box> universe_body_geometry(new fcl::Box(1, 1, 1));
+  model.addBodyFrame("universe_body", 0, SE3::Identity());
+  Model::FrameIndex body_id_3 = model.getBodyId("universe_body");
+  Model::JointIndex joint_parent_3 = model.frames[body_id_3].parent;
+  SE3 universe_body_placement = SE3::Random();
+  Model::JointIndex idx_geom3 = geomModel.addGeometryObject(GeometryObject("universe_collision_object",
+                                                                           model.getBodyId("universe_body"),joint_parent_3,
+                                                                           universe_body_geometry,universe_body_placement, "", Eigen::Vector3d::Ones()),
+                                                            model);
+  
+  BOOST_CHECK(geomModel.geometryObjects[idx_geom3].parentJoint == model.frames[body_id_3].parent);
 
   geomModel.addAllCollisionPairs();
   pinocchio::Data data(model);
@@ -85,6 +98,7 @@ BOOST_AUTO_TEST_CASE ( simple_boxes )
         0, 0, 1, 0 ;
 
   pinocchio::updateGeometryPlacements(model, data, geomModel, geomData, q);
+  BOOST_CHECK(geomData.oMg[idx_geom3].isApprox(universe_body_placement));
   BOOST_CHECK(computeCollision(geomModel,geomData,0) == true);
 
   q <<  2, 0, 1, 0,
@@ -136,9 +150,117 @@ BOOST_AUTO_TEST_CASE ( loading_model )
   pinocchio::updateGeometryPlacements(model, data, geomModel, geomData, q);
   pinocchio::Index idx = geomModel.findCollisionPair(CollisionPair(1,10));
   BOOST_CHECK(computeCollision(geomModel,geomData,idx) == false);
+  
+  fcl::DistanceResult distance_res = computeDistance(geomModel,geomData,idx);
+  BOOST_CHECK(distance_res.min_distance > 0.);
 }
+  
+BOOST_AUTO_TEST_CASE ( test_collisions )
+{
+  typedef pinocchio::Model Model;
+  typedef pinocchio::GeometryModel GeometryModel;
+  typedef pinocchio::Data Data;
+  typedef pinocchio::GeometryData GeometryData;
+  
+  const std::string filename = PINOCCHIO_SOURCE_DIR"/models/romeo/romeo_description/urdf/romeo_small.urdf";
+  std::vector < std::string > packageDirs;
+  const std::string meshDir  = PINOCCHIO_SOURCE_DIR"/models/romeo/";
+  packageDirs.push_back(meshDir);
+  const std::string srdf_filename = PINOCCHIO_SOURCE_DIR"/models/romeo/romeo_description/srdf/romeo.srdf";
+  
+  Model model;
+  pinocchio::urdf::buildModel(filename, pinocchio::JointModelFreeFlyer(),model);
+  GeometryModel geom_model;
+  pinocchio::urdf::buildGeom(model, filename, pinocchio::COLLISION, geom_model, packageDirs);
+  geom_model.addAllCollisionPairs();
+  pinocchio::srdf::removeCollisionPairs(model,geom_model,srdf_filename,false);
+  
+  Data data(model);
+  GeometryData geom_data(geom_model);
+  fcl::CollisionResult result;
+  
+  pinocchio::srdf::loadReferenceConfigurations(model,srdf_filename,false);
+  Eigen::VectorXd q = model.referenceConfigurations["half_sitting"];
 
+  pinocchio::updateGeometryPlacements(model, data, geom_model, geom_data, q);
 
+  BOOST_CHECK(computeCollisions(geom_model,geom_data) == false);
+  
+  {
+    Data data(model);
+    GeometryData geom_data(geom_model);
+    BOOST_CHECK(computeCollisions(model,data,geom_model,geom_data,q) == false);
+  }
+}
+  
+BOOST_AUTO_TEST_CASE ( test_distances )
+{
+  typedef pinocchio::Model Model;
+  typedef pinocchio::GeometryModel GeometryModel;
+  typedef pinocchio::Data Data;
+  typedef pinocchio::GeometryData GeometryData;
+  
+  const std::string filename = PINOCCHIO_SOURCE_DIR"/models/romeo/romeo_description/urdf/romeo_small.urdf";
+  std::vector < std::string > packageDirs;
+  const std::string meshDir  = PINOCCHIO_SOURCE_DIR"/models/romeo/";
+  packageDirs.push_back(meshDir);
+  const std::string srdf_filename = PINOCCHIO_SOURCE_DIR"/models/romeo/romeo_description/srdf/romeo.srdf";
+  
+  Model model;
+  pinocchio::urdf::buildModel(filename, pinocchio::JointModelFreeFlyer(),model);
+  GeometryModel geom_model;
+  pinocchio::urdf::buildGeom(model, filename, pinocchio::COLLISION, geom_model, packageDirs);
+  geom_model.addAllCollisionPairs();
+  pinocchio::srdf::removeCollisionPairs(model,geom_model,srdf_filename,false);
+  
+  Data data(model);
+  GeometryData geom_data(geom_model);
+  fcl::CollisionResult result;
+  
+  pinocchio::srdf::loadReferenceConfigurations(model,srdf_filename,false);
+  Eigen::VectorXd q = model.referenceConfigurations["half_sitting"];
+  
+  pinocchio::updateGeometryPlacements(model, data, geom_model, geom_data, q);
+  
+  BOOST_CHECK(computeDistances(geom_model,geom_data) < geom_model.collisionPairs.size());
+  
+  {
+    Data data(model);
+    GeometryData geom_data(geom_model);
+    BOOST_CHECK(computeDistances(model,data,geom_model,geom_data,q) < geom_model.collisionPairs.size());
+  }
+}
+  
+BOOST_AUTO_TEST_CASE ( test_append_geom_models )
+{
+  typedef pinocchio::Model Model;
+  typedef pinocchio::GeometryModel GeometryModel;
+
+  const std::string filename = PINOCCHIO_SOURCE_DIR"/models/romeo/romeo_description/urdf/romeo_small.urdf";
+  std::vector < std::string > packageDirs;
+  const std::string meshDir  = PINOCCHIO_SOURCE_DIR"/models/romeo/";
+  packageDirs.push_back(meshDir);
+  
+  Model model;
+  pinocchio::urdf::buildModel(filename, pinocchio::JointModelFreeFlyer(),model);
+  GeometryModel geom_model1;
+  pinocchio::urdf::buildGeom(model, filename, pinocchio::COLLISION, geom_model1, packageDirs);
+  
+  GeometryModel geom_model2;
+  pinocchio::urdf::buildGeom(model, filename, pinocchio::COLLISION, geom_model2, packageDirs);
+  
+  appendGeometryModel(geom_model2,geom_model1);
+  BOOST_CHECK(geom_model2.ngeoms == 2*geom_model1.ngeoms);
+  
+  {
+    GeometryModel geom_model_empty;
+    GeometryModel geom_model;
+    BOOST_CHECK(geom_model_empty.ngeoms == 0);
+    appendGeometryModel(geom_model,geom_model_empty);
+    BOOST_CHECK(geom_model.ngeoms== 0);
+  }
+}
+  
 #if defined(PINOCCHIO_WITH_URDFDOM) && defined(PINOCCHIO_WITH_HPP_FCL)
 BOOST_AUTO_TEST_CASE (radius)
 {
