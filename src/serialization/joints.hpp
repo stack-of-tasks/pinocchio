96d9c9a000e1833d753c4f33fc972a0e4cabd9e0
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-10 22:32:56 +0100

    serialization/joints: fix ambiguous call on old GCC compilers
    typically 14.04

--- src/serialization/joints.hpp
+++ src/serialization/joints.hpp
@@ -19,115 +19,132 @@ namespace boost
 {
   namespace serialization
   {
-    template <class Archive, typename Derived>
-    void serialize(Archive & ar,
-                   pinocchio::JointModelBase<Derived> & joint,
-                   const unsigned int /*version*/)
+    
+    // For some older version of gcc, we have to rely on an additional namespace
+    // to avoid ambiguous call to boost::serialization::serialize
+    namespace fix
     {
-      ar & make_nvp("i_id",joint.i_id);
-      ar & make_nvp("i_q",joint.i_q);
-      ar & make_nvp("i_v",joint.i_v);
+      template <class Archive, typename Derived>
+      void serialize(Archive & ar,
+                     pinocchio::JointModelBase<Derived> & joint,
+                     const unsigned int /*version*/)
+      {
+        ar & make_nvp("i_id",joint.i_id);
+        ar & make_nvp("i_q",joint.i_q);
+        ar & make_nvp("i_v",joint.i_v);
+      }
     }
     
     template <class Archive, typename Scalar, int Options, int axis>
     void serialize(Archive & ar,
                    pinocchio::JointModelRevoluteTpl<Scalar,Options,axis> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelRevoluteTpl<Scalar,Options,axis> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options, int axis>
     void serialize(Archive & ar,
                    pinocchio::JointModelRevoluteUnboundedTpl<Scalar,Options,axis> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelRevoluteUnboundedTpl<Scalar,Options,axis> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options, int axis>
     void serialize(Archive & ar,
                    pinocchio::JointModelPrismaticTpl<Scalar,Options,axis> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelPrismaticTpl<Scalar,Options,axis> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelFreeFlyerTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelFreeFlyerTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelPlanarTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelPlanarTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelSphericalTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelSphericalTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelSphericalZYXTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelSphericalZYXTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelTranslationTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelTranslationTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelRevoluteUnalignedTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelRevoluteUnalignedTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
       ar & make_nvp("axis",joint.axis);
     }
     
     template <class Archive, typename Scalar, int Options>
     void serialize(Archive & ar,
                    pinocchio::JointModelPrismaticUnalignedTpl<Scalar,Options> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelPrismaticUnalignedTpl<Scalar,Options> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
       ar & make_nvp("axis",joint.axis);
     }
     
     template <class Archive, typename Scalar, int Options, template<typename S, int O> class JointCollectionTpl>
     void serialize(Archive & ar,
                    pinocchio::JointModelCompositeTpl<Scalar,Options,JointCollectionTpl> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelCompositeTpl<Scalar,Options,JointCollectionTpl> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
       
       ar & make_nvp("m_nq",joint.m_nq);
       ar & make_nvp("m_nv",joint.m_nv);
@@ -144,10 +161,11 @@ namespace boost
     template <class Archive, typename Scalar, int Options, template<typename S, int O> class JointCollectionTpl>
     void serialize(Archive & ar,
                    pinocchio::JointModelTpl<Scalar,Options,JointCollectionTpl> & joint,
-                   const unsigned int /*version*/)
+                   const unsigned int version)
     {
       typedef pinocchio::JointModelTpl<Scalar,Options,JointCollectionTpl> JointType;
-      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+//      ar & make_nvp("base_class",base_object< pinocchio::JointModelBase<JointType> >(joint));
+      fix::serialize(ar,*static_cast<pinocchio::JointModelBase<JointType> *>(&joint),version);
       
       typedef typename JointCollectionTpl<Scalar,Options>::JointModelVariant JointModelVariant;
       ar & make_nvp("base_variant",base_object<JointModelVariant>(joint));
