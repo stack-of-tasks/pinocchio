b85fe5a756086251aec79e29f347b9e5e3a62490
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-10 18:32:50 +0100

    joints: add a single header to include all the joints together

---
+++ src/multibody/joint/joints.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2019 INRIA
+//
+
+#ifndef __pinocchio_joint_joints_hpp__
+#define __pinocchio_joint_joints_hpp__
+
+#include "pinocchio/multibody/joint/joint-free-flyer.hpp"
+#include "pinocchio/multibody/joint/joint-planar.hpp"
+#include "pinocchio/multibody/joint/joint-prismatic.hpp"
+#include "pinocchio/multibody/joint/joint-revolute-unaligned.hpp"
+#include "pinocchio/multibody/joint/joint-prismatic-unaligned.hpp"
+#include "pinocchio/multibody/joint/joint-revolute.hpp"
+#include "pinocchio/multibody/joint/joint-revolute-unbounded.hpp"
+#include "pinocchio/multibody/joint/joint-spherical-ZYX.hpp"
+#include "pinocchio/multibody/joint/joint-spherical.hpp"
+#include "pinocchio/multibody/joint/joint-translation.hpp"
+
+#endif // ifndef __pinocchio_joint_joints_hpp__
