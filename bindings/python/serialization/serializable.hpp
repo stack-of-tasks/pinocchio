f9cb27b7bfce89dfabefa1787d497f5634127bc1
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-10 22:16:34 +0100

    python/serialization: add Serializable visitor

---
+++ bindings/python/serialization/serializable.hpp
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2017-2019 CNRS INRIA
+//
+
+#ifndef __pinocchio_python_serialization_serializable_hpp__
+#define __pinocchio_python_serialization_serializable_hpp__
+
+#include <boost/python.hpp>
+
+namespace pinocchio
+{
+  namespace python
+  {
+    
+    namespace bp = boost::python;
+    
+    template<typename Derived>
+    struct SerializableVisitor
+    : public bp::def_visitor< SerializableVisitor<Derived> >
+    {
+      
+      template<class PyClass>
+      void visit(PyClass& cl) const
+      {
+        cl
+        .def("saveToText",&Derived::saveToText,
+             bp::arg("filename"),"Saves *this inside a text file.")
+        .def("loadFromText",&Derived::loadFromText,
+             bp::arg("filename"),"Loads *this from a text file.")
+        .def("saveToXML",&Derived::saveToXML,
+             bp::args("filename","tag_name"),"Saves *this inside a XML file.")
+        .def("loadFromXML",&Derived::loadFromXML,
+             bp::args("filename","tag_name"),"Loads *this from a XML file.")
+        .def("saveToBinary",&Derived::saveToBinary,
+             bp::arg("filename"),"Saves *this inside a binary file.")
+        .def("loadFromBinary",&Derived::loadFromBinary,
+             bp::arg("filename"),"Loads *this from a binary file.")
+        ;
+      }
+
+    };
+  }
+}
+
+#endif // ifndef __pinocchio_python_serialization_serializable_hpp__
