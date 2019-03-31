2d2826ea8c09ba024af70e9d14a5f616f7ffef3c
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-31 09:32:17 +0200

    algo/rnea-derivatives: use newly introduced oa_gf vectors instaed of oa vectors

--- src/algorithm/rnea-derivatives.hxx
+++ src/algorithm/rnea-derivatives.hxx
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2017-2018 CNRS, INRIA
+// Copyright (c) 2017-2019 CNRS, INRIA
 //
 
 #ifndef __pinocchio_rnea_derivatives_hxx__
@@ -35,7 +35,7 @@ namespace pinocchio
       
       const JointIndex & i = jmodel.id();
       const JointIndex & parent = model.parents[i];
-      const Motion & oa = data.a_gf[0];
+      const Motion & oa_gf = data.oa_gf[0];
       
       jmodel.calc(jdata.derived(),q.derived());
       
@@ -52,7 +52,7 @@ namespace pinocchio
       ColsBlock J_cols = jmodel.jointCols(data.J);
       ColsBlock dAdq_cols = jmodel.jointCols(data.dAdq);
       J_cols = data.oMi[i].act(jdata.S());
-      motionSet::motionAction(oa,J_cols,dAdq_cols);
+      motionSet::motionAction(oa_gf,J_cols,dAdq_cols);
     }
     
   };
@@ -82,7 +82,7 @@ namespace pinocchio
       const JointIndex & parent = model.parents[i];
       
       typename Data::RowMatrix6 & M6tmpR = data.M6tmpR;
-      const Motion & oa = data.a_gf[0];
+      const Motion & oa_gf = data.oa_gf[0];
 
       typedef typename SizeDepType<JointModel::NV>::template ColsReturn<typename Data::Matrix6x>::Type ColsBlock;
 
@@ -96,7 +96,7 @@ namespace pinocchio
       gravity_partial_dq_.block(jmodel.idx_v(),jmodel.idx_v(),jmodel.nv(),data.nvSubtree[i]).noalias()
       = J_cols.transpose()*data.dFdq.middleCols(jmodel.idx_v(),data.nvSubtree[i]);
       
-      data.of[i] = data.oYcrb[i] * oa;
+      data.of[i] = data.oYcrb[i] * oa_gf;
       motionSet::act<ADDTO>(J_cols,data.of[i],dFdq_cols);
       
       lhsInertiaMult(data.oYcrb[i],J_cols.transpose(),M6tmpR.topRows(jmodel.nv()));
@@ -135,7 +135,7 @@ namespace pinocchio
     typedef ModelTpl<Scalar,Options,JointCollectionTpl> Model;
     typedef typename Model::JointIndex JointIndex;
     
-    data.a_gf[0] = -model.gravity;
+    data.oa_gf[0] = -model.gravity;
     
     typedef ComputeGeneralizedGravityDerivativeForwardStep<Scalar,Options,JointCollectionTpl,ConfigVectorType> Pass1;
     for(JointIndex i=1; i<(JointIndex) model.njoints; ++i)
