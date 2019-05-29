c98a2a40c4abe50500e1c89ede68b5ef361ea293
Justin Carpentier <justin.carpentier@inria.fr>
2019-05-30 00:39:28 +0200

    algo: make depracated finiteDifferenceIncrement algo

--- src/algorithm/finite-differences.hxx
+++ src/algorithm/finite-differences.hxx
@@ -5,52 +5,26 @@
 #ifndef __pinocchio_finite_differences_hxx__
 #define __pinocchio_finite_differences_hxx__
 
-#include "pinocchio/multibody/visitor.hpp"
+#include <limits>
 
 /// @cond DEV
 
 namespace pinocchio
 {
-  namespace details
-  {
-    template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl, typename TangentVectorType>
-    struct FinitDiffEpsVisitor
-    : public fusion::JointVisitorBase< FinitDiffEpsVisitor<Scalar,Options,JointCollectionTpl,TangentVectorType> >
-    {
-      typedef boost::fusion::vector< TangentVectorType & > ArgsType;
-      
-      template<typename JointModel>
-      static void algo(const JointModelBase<JointModel> & jmodel,
-                       const Eigen::MatrixBase<TangentVectorType> & fd_increment)
-      {
-        jmodel.jointVelocitySelector(PINOCCHIO_EIGEN_CONST_CAST(TangentVectorType,fd_increment))
-        .fill(jmodel.finiteDifferenceIncrement());
-      }
-      
-    }; // struct FinitDiffEpsVisitor
-    
-  } // namespace details
   
   template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl>
   inline typename ModelTpl<Scalar,Options,JointCollectionTpl>::TangentVectorType
   finiteDifferenceIncrement(const ModelTpl<Scalar,Options,JointCollectionTpl> & model)
   {
-    using namespace pinocchio::details;
-    
     typedef ModelTpl<Scalar,Options,JointCollectionTpl> Model;
-    typedef typename Model::JointIndex JointIndex;
     typedef typename Model::TangentVectorType ReturnType;
     
-    ReturnType fd_increment(model.nv);
-    typedef FinitDiffEpsVisitor<Scalar,Options,JointCollectionTpl,ReturnType> Algo;
-    typename Algo::ArgsType arg(fd_increment);
-    for(JointIndex k = 1; k < (JointIndex)model.njoints; ++k)
-    {
-      Algo::run(model.joints[k],arg);
-    }
-    
+    static Scalar sqrt_eps = math::sqrt(std::numeric_limits<Scalar>::epsilon());
+    ReturnType fd_increment(ReturnType::Constant(model.nv,sqrt_eps));
+
     return fd_increment;
   }
+  
 } // namespace pinocchio
 
 /// @endcond
