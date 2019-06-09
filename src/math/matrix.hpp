af3c042c00b10b9abdc9b8bbb7ba59060c5b5a0e
Justin Carpentier <justin.carpentier@inria.fr>
2019-06-09 10:48:44 +0200

    math: add common isUnitary funtion to handle AutoDiff types

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -6,6 +6,7 @@
 #define __pinocchio_math_matrix_hpp__
 
 #include <Eigen/Core>
+#include <boost/type_traits.hpp>
 
 namespace pinocchio
 {
@@ -25,6 +26,49 @@ namespace pinocchio
     typedef typename Eigen::ProductReturnType<M1,M2>::Type type;
 #endif
   };
+  
+  namespace internal
+  {
+    template<typename VectorLike, bool value = boost::is_floating_point<typename VectorLike::Scalar>::value>
+    struct isUnitaryAlgo
+    {
+      typedef typename VectorLike::Scalar Scalar;
+      typedef typename VectorLike::RealScalar RealScalar;
+      
+      static bool run(const Eigen::MatrixBase<VectorLike> & vec,
+                      const RealScalar & prec =
+                      Eigen::NumTraits< Scalar >::dummy_precision())
+      {
+        return vec.isUnitary(prec);
+      }
+    };
+    
+    template<typename VectorLike>
+    struct isUnitaryAlgo<VectorLike,false>
+    {
+      typedef typename VectorLike::Scalar Scalar;
+      typedef typename VectorLike::RealScalar RealScalar;
+      
+      static bool run(const Eigen::MatrixBase<VectorLike> & /*vec*/,
+                      const RealScalar & prec =
+                      Eigen::NumTraits< Scalar >::dummy_precision())
+      {
+        PINOCCHIO_UNUSED_VARIABLE(prec);
+        return true;
+      }
+    };
+  }
+  
+  template<typename VectorLike>
+  inline bool isUnitary(const Eigen::MatrixBase<VectorLike> & vec,
+                        const typename VectorLike::RealScalar & prec =
+                        Eigen::NumTraits< typename VectorLike::Scalar >::dummy_precision())
+  {
+    EIGEN_STATIC_ASSERT_VECTOR_ONLY(VectorLike);
+    return internal::isUnitaryAlgo<VectorLike>::run(vec,prec);
+  }
+  
+
 
 }
 #endif //#ifndef __pinocchio_math_matrix_hpp__
