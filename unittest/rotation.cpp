e7a57291cf59d15308c6d27cf84833192ffd501e
Justin Carpentier <justin.carpentier@inria.fr>
2019-06-25 17:41:13 +0200

    math/rotation: add toRotationMatrix from cos and sin values

---
+++ unittest/rotation.cpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2019 INRIA
+//
+
+#include <boost/variant.hpp> // to avoid C99 warnings
+
+#include <pinocchio/math/rotation.hpp>
+#include <pinocchio/math/sincos.hpp>
+#include <Eigen/Geometry>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/utility/binary.hpp>
+
+BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
+
+BOOST_AUTO_TEST_CASE(test_toRotationMatrix)
+{
+  using namespace pinocchio;
+  const int max_tests = 1e5;
+  for(int k = 0; k < max_tests; ++k)
+  {
+    const double theta = std::rand();
+    double cos_value, sin_value;
+    
+    pinocchio::SINCOS(theta,&sin_value,&cos_value);
+    Eigen::Vector3d axis(Eigen::Vector3d::Random().normalized());
+
+    Eigen::Matrix3d rot; toRotationMatrix(axis,cos_value,sin_value,rot);
+    Eigen::Matrix3d rot_ref = Eigen::AngleAxisd(theta,axis).toRotationMatrix();
+    
+    BOOST_CHECK(rot.isApprox(rot_ref));
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
