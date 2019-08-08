c2ff68aee045ecc6fc9c4fd1f5c77680c80676af
Justin Carpentier <justin.carpentier@inria.fr>
2019-08-08 18:30:04 +0200

    spatial: add new prototype for classicAcceleration

--- src/spatial/classic-acceleration.hpp
+++ src/spatial/classic-acceleration.hpp
@@ -5,6 +5,7 @@
 #ifndef __pinocchio_spatial_classic_acceleration_hpp__
 #define __pinocchio_spatial_classic_acceleration_hpp__
 
+#include "pinocchio/spatial/se3.hpp"
 #include "pinocchio/spatial/motion.hpp"
 
 namespace pinocchio
@@ -50,6 +51,68 @@ namespace pinocchio
     typedef typename PINOCCHIO_EIGEN_PLAIN_TYPE(typename Motion2::Vector3) ReturnType;
     ReturnType res;
     classicAcceleration(spatial_velocity,spatial_acceleration,res);
+    return res;
+  }
+  
+  ///
+  /// \brief Computes the classic acceleration of a given frame B
+  ///        knowing the spatial velocity and spatial acceleration of a frame A
+  ///        and the relative placement between these two frames
+  ///
+  /// \tparam Motion1 type of the input spatial velocity.
+  /// \tparam Motion2 type of the input spatial acceleration.
+  /// \tparam SE3Scalar Scalar type of the SE3 object.
+  /// \tparam SE3Options Options of the SE3 object.
+  /// \tparam Vector3Like type of the return type (a type similar to a 3D vector).
+  ///
+  /// \param[in] spatial_velocity input spatial velocity.
+  /// \param[in] spatial_acceleration input spatial acceleration.
+  /// \param[in] placement relative placement betwen the frame A and the frame B.
+  /// \param[out] res computed classic acceleration.
+  ///
+  template<typename Motion1, typename Motion2, typename SE3Scalar, int SE3Options, typename Vector3Like>
+  inline void classicAcceleration(const MotionDense<Motion1> & spatial_velocity,
+                                  const MotionDense<Motion2> & spatial_acceleration,
+                                  const SE3Tpl<SE3Scalar,SE3Options> & placement,
+                                  const Eigen::MatrixBase<Vector3Like> & res)
+  {
+    typedef typename PINOCCHIO_EIGEN_PLAIN_TYPE(typename Motion1::LinearType) Vector3;
+    
+    const Vector3 linear_velocity_frame_B
+    = spatial_velocity.linear()
+    + spatial_velocity.angular().cross(placement.translation());
+    
+    const Vector3 linear_acceleration_frame_B // expressed in the coordinate frame A
+    = spatial_acceleration.linear()
+    + spatial_velocity.angular().cross(linear_velocity_frame_B);
+    
+    PINOCCHIO_EIGEN_CONST_CAST(Vector3Like,res).noalias()
+    = placement.rotation().transpose() * (linear_acceleration_frame_B - placement.translation().cross(spatial_acceleration.angular()));
+  }
+  
+  ///
+  /// \brief Computes the classic acceleration of a given frame B
+  ///        knowing the spatial velocity and spatial acceleration of a frame A
+  ///        and the relative placement between these two frames
+  ///
+  /// \tparam Motion1 type of the input spatial velocity.
+  /// \tparam Motion2 type of the input spatial acceleration.
+  /// \tparam SE3Scalar Scalar type of the SE3 object.
+  /// \tparam SE3Options Options of the SE3 object.
+  ///
+  /// \param[in] spatial_velocity input spatial velocity.
+  /// \param[in] spatial_acceleration input spatial acceleration.
+  /// \param[in] placement relative placement betwen the frame A and the frame B.
+  ///
+  template<typename Motion1, typename Motion2, typename SE3Scalar, int SE3Options>
+  inline typename PINOCCHIO_EIGEN_PLAIN_TYPE(typename Motion2::Vector3)
+  classicAcceleration(const MotionDense<Motion1> & spatial_velocity,
+                      const MotionDense<Motion2> & spatial_acceleration,
+                      const SE3Tpl<SE3Scalar,SE3Options> & placement)
+  {
+    typedef typename PINOCCHIO_EIGEN_PLAIN_TYPE(typename Motion2::Vector3) ReturnType;
+    ReturnType res;
+    classicAcceleration(spatial_velocity,spatial_acceleration,placement,res);
     return res;
   }
 }
--- unittest/classic-acceleration.cpp
+++ unittest/classic-acceleration.cpp
@@ -80,4 +80,68 @@ BOOST_AUTO_TEST_CASE(test_classic_acceleration)
   }
 }
 
+BOOST_AUTO_TEST_CASE(test_classic_acceleration_with_placement)
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+  
+  static const int num_tests = 1e2;
+  
+  Model model;
+  buildModels::humanoidRandom(model);
+  
+  model.upperPositionLimit.head<3>().fill(1);
+  model.upperPositionLimit.segment<4>(3).setOnes();
+  model.lowerPositionLimit.head<7>() = - model.upperPositionLimit.head<7>();
+  
+  VectorXd q(model.nq);
+  q = randomConfiguration(model);
+  VectorXd v(VectorXd::Random(model.nv));
+  VectorXd a(VectorXd::Zero(model.nv));
+  
+  Data data_ref(model), data(model);
+  
+  const std::string RF_joint_name = "rleg6_joint";
+  const Model::JointIndex RF_joint_id = model.getJointId(RF_joint_name);
+  
+  for(int k = 0; k < num_tests; ++k)
+  {
+    q = randomConfiguration(model);
+    v = VectorXd::Random(model.nv);
+    
+    forwardKinematics(model,data,q,v,a);
+    
+    const SE3 RF_world_transf = SE3(data.oMi[RF_joint_id].rotation(),
+                                    SE3::Vector3::Zero());
+    
+    const Motion RF_v_global = RF_world_transf.act(data.v[RF_joint_id]);
+    const Motion RF_a_global = RF_world_transf.act(data.a[RF_joint_id]);
+    const Motion::Vector3 RF_classic_acc_ref = classicAcceleration(RF_v_global, RF_a_global);
+    
+    const SE3 identity_placement = SE3::Identity();
+    Motion::Vector3 RF_classic_acc = classicAcceleration(RF_v_global, RF_a_global, identity_placement);
+    
+    BOOST_CHECK(RF_classic_acc.isApprox(RF_classic_acc_ref));
+    
+    const SE3 random_placement = SE3::Random();
+    
+    const Motion & v_A = data.v[RF_joint_id];
+    const Motion & a_A = data.a[RF_joint_id];
+    
+    const Motion v_B = random_placement.actInv(data.v[RF_joint_id]);
+    const Motion a_B = random_placement.actInv(data.a[RF_joint_id]);
+    
+    Motion::Vector3 classic_acc_B_ref = classicAcceleration(v_B, a_B);
+    Motion::Vector3 classic_acc_B = classicAcceleration(v_A, a_A, random_placement);
+    
+    BOOST_CHECK(classic_acc_B.isApprox(classic_acc_B_ref));
+    
+    // Check other signature
+    Motion::Vector3 classic_acc_B_other_signature;
+    classicAcceleration(v_A, a_A, random_placement, classic_acc_B_other_signature);
+    
+    BOOST_CHECK(classic_acc_B_other_signature.isApprox(classic_acc_B));
+  }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
