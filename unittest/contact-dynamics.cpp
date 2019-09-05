66dd84248a6b7172dcd9163560bce2836eca431f
Justin Carpentier <justin.carpentier@inria.fr>
2019-09-05 17:20:33 +0200

    test/contact: add missing test for LOCAL_WORLD_ALIGNED ContactInfo

--- unittest/contact-dynamics.cpp
+++ unittest/contact-dynamics.cpp
@@ -338,6 +338,454 @@ BOOST_AUTO_TEST_CASE ( test_ID )
 
 }
 
+/// \brief Computes forces in the world frame
+pinocchio::Motion computeFrameAcc(const pinocchio::Model & model,
+                                  pinocchio::Data & data,
+                                  const pinocchio::Model::FrameIndex & frame_id,
+                                  pinocchio::ReferenceFrame reference_frame)
+{
+  using namespace pinocchio;
+  Model::JointIndex joint_id = model.frames[frame_id].parent;
+  Motion res(Motion::Zero());
+  
+  updateFramePlacement(model,data,frame_id);
+  
+  const Data::SE3 & oMf = data.oMf[frame_id];
+  const Data::SE3 & oMi = data.oMi[joint_id];
+  
+  const Data::SE3 iMf = oMi.inverse() * oMf;
+  const Motion ov = oMi.act(data.v[joint_id]);
+  const Motion oa = oMi.act(data.a[joint_id]);
+  
+  switch (reference_frame)
+  {
+    case WORLD:
+      classicAcceleration(ov,oa,res.linear());
+      res.angular() = oa.angular();
+      break;
+    case LOCAL_WORLD_ALIGNED:
+      res.linear() = oMf.rotation() * classicAcceleration(data.v[joint_id],data.a[joint_id],iMf);
+      res.angular() = oMi.rotation() * data.a[joint_id].angular();
+      break;
+    case LOCAL:
+      classicAcceleration(data.v[joint_id],data.a[joint_id],iMf,res.linear());
+      res.angular() = iMf.rotation().transpose() * data.a[joint_id].angular();
+      break;
+    default:
+      break;
+  }
+  
+  return res;
+}
+
+BOOST_AUTO_TEST_CASE(test_sparse_forward_dynamics_in_contact_6D)
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+  
+  pinocchio::Model model;
+  pinocchio::buildModels::humanoidRandom(model,true);
+  pinocchio::Data data(model), data_ref(model);
+  
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+
+  VectorXd v = VectorXd::Random(model.nv);
+  VectorXd tau = VectorXd::Random(model.nv);
+  
+  const std::string RF = "rleg6_joint";
+//  const Model::JointIndex RF_id = model.getJointId(RF);
+  const std::string LF = "lleg6_joint";
+//  const Model::JointIndex LF_id = model.getJointId(LF);
+  
+  // Contact info
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(ContactInfo) contact_infos;
+  ContactInfo ci_RF(CONTACT_6D,model.getFrameId(RF),WORLD);
+  contact_infos.push_back(ci_RF);
+  ContactInfo ci_LF(CONTACT_6D,model.getFrameId(LF),WORLD);
+  contact_infos.push_back(ci_LF);
+  
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+    constraint_dim += contact_infos[k].dim();
+  
+  const double mu0 = 0.;
+  
+  Eigen::MatrixXd J_ref(constraint_dim,model.nv);
+  J_ref.setZero();
+  
+  computeAllTerms(model,data_ref,q,v);
+  data_ref.M.triangularView<Eigen::StrictlyLower>() =
+  data_ref.M.transpose().triangularView<Eigen::StrictlyLower>();
+  
+  updateFramePlacements(model,data_ref);
+  getFrameJacobian(model,data_ref,model.getFrameId(RF),WORLD,J_ref.middleRows<6>(0));
+  getFrameJacobian(model,data_ref,model.getFrameId(LF),WORLD,J_ref.middleRows<6>(6));
+  
+  Eigen::VectorXd rhs_ref(constraint_dim);
+  
+  rhs_ref.segment<6>(0) = computeFrameAcc(model,data_ref,model.getFrameId(RF),ci_RF.reference_frame).toVector();
+  rhs_ref.segment<6>(6) = computeFrameAcc(model,data_ref,model.getFrameId(LF),ci_LF.reference_frame).toVector();
+
+  Eigen::MatrixXd KKT_matrix_ref
+  = Eigen::MatrixXd::Zero(model.nv+constraint_dim,model.nv+constraint_dim);
+  KKT_matrix_ref.bottomRightCorner(model.nv,model.nv) = data_ref.M;
+  KKT_matrix_ref.topRightCorner(constraint_dim,model.nv) = J_ref;
+  KKT_matrix_ref.bottomLeftCorner(model.nv,constraint_dim) = J_ref.transpose();
+  
+  forwardDynamics(model,data_ref,q,v,tau,J_ref,rhs_ref,mu0);
+  forwardKinematics(model,data_ref,q,v,data_ref.ddq);
+  
+  initContactDynamics(model,data,contact_infos);
+  contactDynamics(model,data,q,v,tau,contact_infos,mu0);
+  
+  // Check that the decomposition is correct
+  const Data::ContactCholeskyDecomposition & contact_chol = data.contact_chol;
+  Eigen::MatrixXd KKT_matrix = contact_chol.matrix();
+  
+  BOOST_CHECK(KKT_matrix.bottomRightCorner(model.nv,model.nv).isApprox(KKT_matrix_ref.bottomRightCorner(model.nv,model.nv)));
+  BOOST_CHECK(KKT_matrix.isApprox(KKT_matrix_ref));
+  
+  // Check solutions
+  BOOST_CHECK(data.ddq.isApprox(data_ref.ddq));
+  
+  Eigen::DenseIndex constraint_id = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+  {
+    const ContactInfo & cinfo = contact_infos[k];
+    
+    switch(cinfo.type)
+    {
+      case pinocchio::CONTACT_3D:
+      {
+        BOOST_CHECK(data.contact_forces[k].linear().isApprox(data_ref.lambda_c.segment(constraint_id,cinfo.dim())));
+        break;
+      }
+        
+      case pinocchio::CONTACT_6D:
+      {
+        ForceRef<Data::VectorXs::FixedSegmentReturnType<6>::Type> f_ref(data_ref.lambda_c.segment<6>(constraint_id));
+        BOOST_CHECK(data.contact_forces[k].isApprox(f_ref));
+        break;
+      }
+        
+      default:
+        break;
+    }
+    
+    constraint_id += cinfo.dim();
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_sparse_forward_dynamics_in_contact_6D_LOCAL)
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+  
+  pinocchio::Model model;
+  pinocchio::buildModels::humanoidRandom(model,true);
+  pinocchio::Data data(model), data_ref(model);
+  
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+  
+  VectorXd v = VectorXd::Random(model.nv);
+  VectorXd tau = VectorXd::Random(model.nv);
+  
+  const std::string RF = "rleg6_joint";
+  //  const Model::JointIndex RF_id = model.getJointId(RF);
+  const std::string LF = "lleg6_joint";
+  //  const Model::JointIndex LF_id = model.getJointId(LF);
+  
+  // Contact info
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(ContactInfo) contact_infos;
+  ContactInfo ci_RF(CONTACT_6D,model.getFrameId(RF),LOCAL);
+  contact_infos.push_back(ci_RF);
+  ContactInfo ci_LF(CONTACT_6D,model.getFrameId(LF),WORLD);
+  contact_infos.push_back(ci_LF);
+  
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+    constraint_dim += contact_infos[k].dim();
+  
+  const double mu0 = 0.;
+  
+  Eigen::MatrixXd J_ref(constraint_dim,model.nv);
+  J_ref.setZero();
+  
+  computeAllTerms(model,data_ref,q,v);
+  data_ref.M.triangularView<Eigen::StrictlyLower>() =
+  data_ref.M.transpose().triangularView<Eigen::StrictlyLower>();
+  
+  updateFramePlacements(model,data_ref);
+  getFrameJacobian(model,data_ref,
+                   ci_RF.frame_id,ci_RF.reference_frame,
+                   J_ref.middleRows<6>(0));
+  getFrameJacobian(model,data_ref,
+                   ci_LF.frame_id,ci_LF.reference_frame,
+                   J_ref.middleRows<6>(6));
+  
+  Eigen::VectorXd rhs_ref(constraint_dim);
+  
+  rhs_ref.segment<6>(0) = computeFrameAcc(model,data_ref,ci_RF.frame_id,ci_RF.reference_frame).toVector();
+  rhs_ref.segment<6>(6) = computeFrameAcc(model,data_ref,ci_LF.frame_id,ci_LF.reference_frame).toVector();
+  
+  Eigen::MatrixXd KKT_matrix_ref
+  = Eigen::MatrixXd::Zero(model.nv+constraint_dim,model.nv+constraint_dim);
+  KKT_matrix_ref.bottomRightCorner(model.nv,model.nv) = data_ref.M;
+  KKT_matrix_ref.topRightCorner(constraint_dim,model.nv) = J_ref;
+  KKT_matrix_ref.bottomLeftCorner(model.nv,constraint_dim) = J_ref.transpose();
+  
+  forwardDynamics(model,data_ref,q,v,tau,J_ref,rhs_ref,mu0);
+  forwardKinematics(model,data_ref,q,v,data_ref.ddq);
+  
+  initContactDynamics(model,data,contact_infos);
+  contactDynamics(model,data,q,v,tau,contact_infos,mu0);
+  
+  // Check that the decomposition is correct
+  const Data::ContactCholeskyDecomposition & contact_chol = data.contact_chol;
+  Eigen::MatrixXd KKT_matrix = contact_chol.matrix();
+  
+  BOOST_CHECK(KKT_matrix.bottomRightCorner(model.nv,model.nv).isApprox(KKT_matrix_ref.bottomRightCorner(model.nv,model.nv)));
+  BOOST_CHECK(KKT_matrix.isApprox(KKT_matrix_ref));
+  
+  // Check solutions
+  BOOST_CHECK(data.ddq.isApprox(data_ref.ddq));
+  
+  Eigen::DenseIndex constraint_id = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+  {
+    const ContactInfo & cinfo = contact_infos[k];
+    
+    switch(cinfo.type)
+    {
+      case pinocchio::CONTACT_3D:
+      {
+        BOOST_CHECK(data.contact_forces[k].linear().isApprox(data_ref.lambda_c.segment(constraint_id,cinfo.dim())));
+        break;
+      }
+        
+      case pinocchio::CONTACT_6D:
+      {
+        ForceRef<Data::VectorXs::FixedSegmentReturnType<6>::Type> f_ref(data_ref.lambda_c.segment<6>(constraint_id));
+        BOOST_CHECK(data.contact_forces[k].isApprox(f_ref));
+        break;
+      }
+        
+      default:
+        break;
+    }
+    
+    constraint_id += cinfo.dim();
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_sparse_forward_dynamics_in_contact_6D_3D)
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+  
+  pinocchio::Model model;
+  pinocchio::buildModels::humanoidRandom(model,true);
+  pinocchio::Data data(model), data_ref(model);
+  
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+  
+  VectorXd v = VectorXd::Random(model.nv);
+  VectorXd tau = VectorXd::Random(model.nv);
+  
+  const std::string RF = "rleg6_joint";
+  const std::string LF = "lleg6_joint";
+  
+  // Contact info
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(ContactInfo) contact_infos;
+  ContactInfo ci_RF(CONTACT_6D,model.getFrameId(RF),WORLD);
+  contact_infos.push_back(ci_RF);
+  ContactInfo ci_LF(CONTACT_3D,model.getFrameId(LF),WORLD);
+  contact_infos.push_back(ci_LF);
+  
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+    constraint_dim += contact_infos[k].dim();
+  
+  const double mu0 = 0.;
+  
+  Eigen::MatrixXd J_ref(constraint_dim,model.nv);
+  J_ref.setZero();
+  
+  computeAllTerms(model,data_ref,q,v);
+  data_ref.M.triangularView<Eigen::StrictlyLower>() =
+  data_ref.M.transpose().triangularView<Eigen::StrictlyLower>();
+  
+  updateFramePlacements(model,data_ref);
+  getFrameJacobian(model,data_ref,model.getFrameId(RF),WORLD,J_ref.middleRows<6>(0));
+  Data::Matrix6x J_LF(6,model.nv); J_LF.setZero();
+  getFrameJacobian(model,data_ref,model.getFrameId(LF),WORLD,J_LF);
+  J_ref.middleRows<3>(6) = J_LF.middleRows<3>(Motion::LINEAR);
+  
+  Eigen::VectorXd rhs_ref(constraint_dim);
+  
+  rhs_ref.segment<6>(0) = computeFrameAcc(model,data_ref,model.getFrameId(RF),ci_RF.reference_frame).toVector();
+  rhs_ref.segment<3>(6) = computeFrameAcc(model,data_ref,model.getFrameId(LF),ci_LF.reference_frame).linear();
+  
+  Eigen::MatrixXd KKT_matrix_ref
+  = Eigen::MatrixXd::Zero(model.nv+constraint_dim,model.nv+constraint_dim);
+  KKT_matrix_ref.bottomRightCorner(model.nv,model.nv) = data_ref.M;
+  KKT_matrix_ref.topRightCorner(constraint_dim,model.nv) = J_ref;
+  KKT_matrix_ref.bottomLeftCorner(model.nv,constraint_dim) = J_ref.transpose();
+  
+  forwardDynamics(model,data_ref,q,v,tau,J_ref,rhs_ref,mu0);
+  forwardKinematics(model,data_ref,q,v,data_ref.ddq);
+  
+  initContactDynamics(model,data,contact_infos);
+  contactDynamics(model,data,q,v,tau,contact_infos,mu0);
+  
+  // Check that the decomposition is correct
+  const Data::ContactCholeskyDecomposition & contact_chol = data.contact_chol;
+  Eigen::MatrixXd KKT_matrix = contact_chol.matrix();
+  
+  BOOST_CHECK(KKT_matrix.bottomRightCorner(model.nv,model.nv).isApprox(KKT_matrix_ref.bottomRightCorner(model.nv,model.nv)));
+  BOOST_CHECK(KKT_matrix.isApprox(KKT_matrix_ref));
+  
+  // Check solutions
+  BOOST_CHECK(data.ddq.isApprox(data_ref.ddq));
+  
+  Eigen::DenseIndex constraint_id = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+  {
+    const ContactInfo & cinfo = contact_infos[k];
+    
+    switch(cinfo.type)
+    {
+      case pinocchio::CONTACT_3D:
+      {
+        BOOST_CHECK(data.contact_forces[k].linear().isApprox(data_ref.lambda_c.segment(constraint_id,cinfo.dim())));
+        break;
+      }
+        
+      case pinocchio::CONTACT_6D:
+      {
+        ForceRef<Data::VectorXs::FixedSegmentReturnType<6>::Type> f_ref(data_ref.lambda_c.segment<6>(constraint_id));
+        BOOST_CHECK(data.contact_forces[k].isApprox(f_ref));
+        break;
+      }
+        
+      default:
+        break;
+    }
+    
+    constraint_id += cinfo.dim();
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_sparse_forward_dynamics_in_contact_6D_LOCAL_WORLD_ALIGNED)
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+  
+  pinocchio::Model model;
+  pinocchio::buildModels::humanoidRandom(model,true);
+  pinocchio::Data data(model), data_ref(model);
+  
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+  
+  VectorXd v = VectorXd::Random(model.nv);
+  VectorXd tau = VectorXd::Random(model.nv);
+  
+  const std::string RF = "rleg6_joint";
+  //  const Model::JointIndex RF_id = model.getJointId(RF);
+  const std::string LF = "lleg6_joint";
+  //  const Model::JointIndex LF_id = model.getJointId(LF);
+  
+  // Contact info
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(ContactInfo) contact_infos;
+  ContactInfo ci_RF(CONTACT_6D,model.getFrameId(RF),LOCAL_WORLD_ALIGNED);
+  contact_infos.push_back(ci_RF);
+  ContactInfo ci_LF(CONTACT_6D,model.getFrameId(LF),WORLD);
+  contact_infos.push_back(ci_LF);
+  
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+    constraint_dim += contact_infos[k].dim();
+  
+  const double mu0 = 0.;
+  
+  Eigen::MatrixXd J_ref(constraint_dim,model.nv);
+  J_ref.setZero();
+  
+  computeAllTerms(model,data_ref,q,v);
+  data_ref.M.triangularView<Eigen::StrictlyLower>() =
+  data_ref.M.transpose().triangularView<Eigen::StrictlyLower>();
+  
+  updateFramePlacements(model,data_ref);
+  getFrameJacobian(model,data_ref,
+                   ci_RF.frame_id,ci_RF.reference_frame,
+                   J_ref.middleRows<6>(0));
+  getFrameJacobian(model,data_ref,
+                   ci_LF.frame_id,ci_LF.reference_frame,
+                   J_ref.middleRows<6>(6));
+  
+  Eigen::VectorXd rhs_ref(constraint_dim);
+  
+  rhs_ref.segment<6>(0) = computeFrameAcc(model,data_ref,ci_RF.frame_id,ci_RF.reference_frame).toVector();
+  rhs_ref.segment<6>(6) = computeFrameAcc(model,data_ref,ci_LF.frame_id,ci_LF.reference_frame).toVector();
+  
+  Eigen::MatrixXd KKT_matrix_ref
+  = Eigen::MatrixXd::Zero(model.nv+constraint_dim,model.nv+constraint_dim);
+  KKT_matrix_ref.bottomRightCorner(model.nv,model.nv) = data_ref.M;
+  KKT_matrix_ref.topRightCorner(constraint_dim,model.nv) = J_ref;
+  KKT_matrix_ref.bottomLeftCorner(model.nv,constraint_dim) = J_ref.transpose();
+  
+  forwardDynamics(model,data_ref,q,v,tau,J_ref,rhs_ref,mu0);
+  forwardKinematics(model,data_ref,q,v,data_ref.ddq);
+  
+  initContactDynamics(model,data,contact_infos);
+  contactDynamics(model,data,q,v,tau,contact_infos,mu0);
+  
+  // Check that the decomposition is correct
+  const Data::ContactCholeskyDecomposition & contact_chol = data.contact_chol;
+  Eigen::MatrixXd KKT_matrix = contact_chol.matrix();
+  
+  BOOST_CHECK(KKT_matrix.bottomRightCorner(model.nv,model.nv).isApprox(KKT_matrix_ref.bottomRightCorner(model.nv,model.nv)));
+  BOOST_CHECK(KKT_matrix.isApprox(KKT_matrix_ref));
+  
+  // Check solutions
+  BOOST_CHECK(data.ddq.isApprox(data_ref.ddq));
+  
+  Eigen::DenseIndex constraint_id = 0;
+  for(size_t k = 0; k < contact_infos.size(); ++k)
+  {
+    const ContactInfo & cinfo = contact_infos[k];
+    
+    switch(cinfo.type)
+    {
+      case pinocchio::CONTACT_3D:
+      {
+        BOOST_CHECK(data.contact_forces[k].linear().isApprox(data_ref.lambda_c.segment(constraint_id,cinfo.dim())));
+        break;
+      }
+        
+      case pinocchio::CONTACT_6D:
+      {
+        ForceRef<Data::VectorXs::FixedSegmentReturnType<6>::Type> f_ref(data_ref.lambda_c.segment<6>(constraint_id));
+        BOOST_CHECK(data.contact_forces[k].isApprox(f_ref));
+        break;
+      }
+        
+      default:
+        break;
+    }
+    
+    constraint_id += cinfo.dim();
+  }
+}
+
 BOOST_AUTO_TEST_CASE (timings_fd_llt)
 {
   using namespace Eigen;
