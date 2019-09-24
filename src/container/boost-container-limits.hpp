3726dcc9a4cbdb6b63ad63b056bffb19f81804ee
Justin Carpentier <justin.carpentier@inria.fr>
2019-09-24 14:49:56 +0200

    core/boost: set BOOST_MPL_LIMIT_LIST_SIZE only for C++ versions under 11

--- src/container/boost-container-limits.hpp
+++ src/container/boost-container-limits.hpp
@@ -5,25 +5,26 @@
 #ifndef __pinocchio_container_boost_container_limits_hpp__
 #define __pinocchio_container_boost_container_limits_hpp__
 
-#if defined(BOOST_MPL_LIST_HPP_INCLUDED) || defined(BOOST_MPL_VECTOR_HPP_INCLUDED)
-  # error "You should include pinocchio before boost (e.g. #include <pinocchio/fwd.hpp>)"
-#endif
+#ifndef PINOCCHIO_WITH_CXX11_SUPPORT
+  #if defined(BOOST_MPL_LIST_HPP_INCLUDED) || defined(BOOST_MPL_VECTOR_HPP_INCLUDED)
+    # error "You should include pinocchio before boost (e.g. #include <pinocchio/fwd.hpp>)"
+  #endif
 
-#define PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE_DEFAULT 30
+  #define PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE_DEFAULT 30
 
-#ifndef PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
-  #define PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE_DEFAULT
-#endif
+  #ifndef PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
+    #define PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE_DEFAULT
+  #endif
 
-#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
+  #define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
 
-#ifndef BOOST_MPL_LIMIT_LIST_SIZE
-  #define BOOST_MPL_LIMIT_LIST_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
-#endif
+  #ifndef BOOST_MPL_LIMIT_LIST_SIZE
+    #define BOOST_MPL_LIMIT_LIST_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
+  #endif
 
-#ifndef BOOST_MPL_LIMIT_VECTOR_SIZE
-  #define BOOST_MPL_LIMIT_VECTOR_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
-#endif
+  #ifndef BOOST_MPL_LIMIT_VECTOR_SIZE
+    #define BOOST_MPL_LIMIT_VECTOR_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
+  #endif
+#endif // ifndef PINOCCHIO_WITH_CXX11_SUPPORT
 
 #endif // ifndef __pinocchio_container_boost_container_limits_hpp__
-
