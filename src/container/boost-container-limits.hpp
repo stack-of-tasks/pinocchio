910c1eb3ec37dae90e1ba43a155aac016e696d8c
Justin Carpentier <justin.carpentier@inria.fr>
2019-09-24 15:03:18 +0200

    core/boost: do not throw error if BOOST_MPL_LIMIT_LIST_SIZE is defined

--- src/container/boost-container-limits.hpp
+++ src/container/boost-container-limits.hpp
@@ -8,9 +8,6 @@
 #include "pinocchio/macros.hpp"
 
 #ifndef PINOCCHIO_WITH_CXX11_SUPPORT
-  #if defined(BOOST_MPL_LIST_HPP_INCLUDED) || defined(BOOST_MPL_VECTOR_HPP_INCLUDED)
-    # error "You should include pinocchio before boost (e.g. #include <pinocchio/fwd.hpp>)"
-  #endif
 
   #define PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE_DEFAULT 30
 
@@ -20,9 +17,18 @@
 
   #define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
 
-  #ifndef BOOST_MPL_LIMIT_LIST_SIZE
+  #if !defined(BOOST_MPL_LIMIT_LIST_SIZE)
+    // Check the inclusion order
+    #if defined(BOOST_MPL_LIST_HPP_INCLUDED)
+      # error "You should include pinocchio before boost (e.g. #include <pinocchio/fwd.hpp>)"
+    #endif
+
     #define BOOST_MPL_LIMIT_LIST_SIZE PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
+
+  #elif BOOST_MPL_LIMIT_LIST_SIZE < PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE
+    # error "BOOST_MPL_LIMIT_LIST_SIZE value is lower than the value of PINOCCHIO_BOOST_MPL_LIMIT_CONTAINER_SIZE"
   #endif
+
 #endif // ifndef PINOCCHIO_WITH_CXX11_SUPPORT
 
 #endif // ifndef __pinocchio_container_boost_container_limits_hpp__
