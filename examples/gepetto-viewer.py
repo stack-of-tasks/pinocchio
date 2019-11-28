3752ace80d10a4d6dbe894932e3c09a8402dc4b0
Gabriele Buondonno <gbuondon@laas.fr>
2019-11-28 14:23:15 +0100

    [examples/gepetto-viewer.py] Prevent exceptions

--- examples/gepetto-viewer.py
+++ examples/gepetto-viewer.py
@@ -4,6 +4,7 @@
 import pinocchio as pin
 pin.switchToNumpyMatrix()
 import numpy as np
+import sys
 import os
 
 from pinocchio.visualize import GepettoVisualizer
@@ -25,8 +26,19 @@ model, collision_model, visual_model = pin.buildModelsFromUrdf(urdf_model_path, 
 viz = GepettoVisualizer(model, collision_model, visual_model)
 
 # Initialize the viewer.
-viz.initViewer()
-viz.loadViewerModel("pinocchio")
+try:
+    viz.initViewer()
+except ImportError as err:
+    print("Error while initializing the viewer. It seems you should install gepetto-viewer")
+    print(err)
+    sys.exit(0)
+
+try:
+    viz.loadViewerModel("pinocchio")
+except AttributeError as err:
+    print("Error while loading the viewer model. It seems you should start gepetto-viewer")
+    print(err)
+    sys.exit(0)
 
 # Display a robot configuration.
 q0 = np.matrix([
