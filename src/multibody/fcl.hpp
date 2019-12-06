c03673eef963ee599e4edf5a14c7587a152d5d4c
Justin Carpentier <justin.carpentier@inria.fr>
2019-12-06 22:39:00 +0100

    fcl: add CollisionGeometryPtr

--- src/multibody/fcl.hpp
+++ src/multibody/fcl.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2016 CNRS
+// Copyright (c) 2015-2019 CNRS INRIA
 //
 
 #ifndef __pinocchio_fcl_hpp__
@@ -77,6 +77,8 @@ struct GeometryObject
 {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   
+  typedef boost::shared_ptr<fcl::CollisionGeometry> CollisionGeometryPtr;
+  
   /// \brief Name of the geometry object
   std::string name;
 
@@ -91,7 +93,7 @@ struct GeometryObject
   JointIndex parentJoint;
 
   /// \brief The actual cloud of points representing the collision mesh of the object after scaling.
-  boost::shared_ptr<fcl::CollisionGeometry> fcl;
+  CollisionGeometryPtr fcl;
 
   /// \brief Position of geometry object in parent joint frame
   SE3 placement;
@@ -113,8 +115,8 @@ struct GeometryObject
 
   GeometryObject(const std::string & name, const FrameIndex parentF,
                  const JointIndex parentJ,
-                 const boost::shared_ptr<fcl::CollisionGeometry> & collision,
                  const SE3 & placement, const std::string & meshPath = "",
+                 const CollisionGeometryPtr & collision,
                  const Eigen::Vector3d & meshScale = Eigen::Vector3d::Ones(),
                  const bool overrideMaterial = false,
                  const Eigen::Vector4d & meshColor = Eigen::Vector4d::Zero(),
