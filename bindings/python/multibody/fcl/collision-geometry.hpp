fedb6a3fe762f95e91bdd62d7e9a8c8fd96d518a
Justin Carpentier <justin.carpentier@inria.fr>
2019-12-06 23:21:02 +0100

    python: check exposition of FCL types

--- bindings/python/multibody/fcl/collision-geometry.hpp
+++ bindings/python/multibody/fcl/collision-geometry.hpp
@@ -12,6 +12,8 @@
 #include <boost/python/return_internal_reference.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
+#include <eigenpy/registration.hpp>
+
 namespace pinocchio
 {
   namespace python
@@ -29,7 +31,6 @@ namespace pinocchio
         void visit(PyClass& cl) const
         {
           cl
-          
           .def("getObjectType",&CollisionGeometry::getObjectType,"Get the type of the object.")
           .def("getNodeType",&CollisionGeometry::getNodeType,"Get the node type.")
           
@@ -46,41 +47,7 @@ namespace pinocchio
                                                            bp::no_init)
           .def(CollisionGeometryPythonVisitor())
           ;
-          
-          bp::enum_< ::hpp::fcl::OBJECT_TYPE >("OBJECT_TYPE")
-          .value("OT_UNKNOWN",::hpp::fcl::OT_UNKNOWN)
-          .value("OT_BVH",::hpp::fcl::OT_BVH)
-          .value("OT_GEOM",::hpp::fcl::OT_GEOM)
-          .value("OT_OCTREE",::hpp::fcl::OT_OCTREE)
-          .value("OT_COUNT",::hpp::fcl::OT_COUNT)
-          ;
-          
-          bp::enum_< ::hpp::fcl::NODE_TYPE >("NODE_TYPE")
-          .value("BV_UNKNOWN",::hpp::fcl::BV_UNKNOWN)
-          .value("BV_AABB",::hpp::fcl::BV_AABB)
-          .value("BV_OBB",::hpp::fcl::BV_OBB)
-          .value("BV_RSS",::hpp::fcl::BV_RSS)
-          .value("BV_kIOS",::hpp::fcl::BV_kIOS)
-          .value("BV_OBBRSS",::hpp::fcl::BV_OBBRSS)
-          .value("BV_KDOP16",::hpp::fcl::BV_KDOP16)
-          .value("BV_KDOP18",::hpp::fcl::BV_KDOP18)
-          .value("BV_KDOP24",::hpp::fcl::BV_KDOP24)
-          .value("GEOM_BOX",::hpp::fcl::GEOM_BOX)
-          .value("GEOM_SPHERE",::hpp::fcl::GEOM_SPHERE)
-          .value("GEOM_CAPSULE",::hpp::fcl::GEOM_CAPSULE)
-          .value("GEOM_CONE",::hpp::fcl::GEOM_CONE)
-          .value("GEOM_CYLINDER",::hpp::fcl::GEOM_CYLINDER)
-          .value("GEOM_CONVEX",::hpp::fcl::GEOM_CONVEX)
-          .value("GEOM_PLANE",::hpp::fcl::GEOM_PLANE)
-          .value("GEOM_HALFSPACE",::hpp::fcl::GEOM_HALFSPACE)
-          .value("GEOM_TRIANGLE",::hpp::fcl::GEOM_TRIANGLE)
-          .value("GEOM_OCTREE",::hpp::fcl::GEOM_OCTREE)
-          .value("NODE_COUNT",::hpp::fcl::NODE_COUNT)
-          ;
         }
-        
-      private:
-        
         
       };
       
--- bindings/python/multibody/fcl/mesh-loader.hpp
+++ bindings/python/multibody/fcl/mesh-loader.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2017 CNRS
+// Copyright (c) 2017-2019 CNRS INRIA
 //
 
 #ifndef __pinocchio_python_fcl_mesh_loader_hpp__
@@ -8,9 +8,8 @@
 #include "pinocchio/spatial/fcl-pinocchio-conversions.hpp"
 #include <hpp/fcl/mesh_loader/loader.h>
 
-#include <boost/python/copy_const_reference.hpp>
-#include <boost/python/return_internal_reference.hpp>
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/python.hpp>
+#include <typeinfo>
 
 namespace pinocchio
 {
@@ -21,18 +20,16 @@ namespace pinocchio
       
       namespace bp = boost::python;
       
-      struct MeshLoaderPythonVisitor : public bp::def_visitor<MeshLoaderPythonVisitor>
+      template<typename MeshLoader>
+      struct MeshLoaderPythonVisitor
+      : public bp::def_visitor< MeshLoaderPythonVisitor<MeshLoader> >
       {
-        typedef ::hpp::fcl::MeshLoader MeshLoader;
         typedef boost::shared_ptr<MeshLoader> MeshLoaderPtr_t;
-
-        typedef ::hpp::fcl::CachedMeshLoader CachedMeshLoader;
-        typedef boost::shared_ptr<CachedMeshLoader> CachedMeshLoaderPtr_t;
-
+        
         template <typename T>
-        static boost::shared_ptr<T> create ()
+        static boost::shared_ptr<T> create()
         {
-          return boost::shared_ptr<T> (new T);
+          return boost::shared_ptr<T>(new T);
         }
 
         template<class PyClass>
@@ -40,30 +37,20 @@ namespace pinocchio
         {
           cl
           .def("create",&MeshLoaderPythonVisitor::create<MeshLoader>,"Create a new object.")
-          .staticmethod ("create")
+          .staticmethod("create")
           ;
         }
         
-        static void expose()
+        static void expose(const std::string & doc = "")
         {
+          static const std::string class_name = typeid(MeshLoader).name();
           bp::class_<MeshLoader, MeshLoaderPtr_t>
-            ("MeshLoader",
-             "Class to create CollisionGeometry from mesh files.",
+            (class_name.c_str(),
+             doc.c_str(),
              bp::init<>())
             .def(MeshLoaderPythonVisitor())
           ;
-
-          bp::class_<CachedMeshLoader, CachedMeshLoaderPtr_t, bp::bases<MeshLoader> >
-            ("CachedMeshLoader",
-             "Class to create CollisionGeometry from mesh files with cache mechanism.",
-             bp::init<>())
-          .def("create",&MeshLoaderPythonVisitor::create<CachedMeshLoader>,"Create a new object.")
-          .staticmethod ("create")
-          ;
         }
-        
-      private:
-        
         
       };
       
