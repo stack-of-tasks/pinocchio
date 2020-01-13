7cab18bb8dc9350a2c3866cf5235ec031efc111e
Justin Carpentier <justin.carpentier@inria.fr>
2020-01-13 10:46:38 +0100

    python/conversion: add argument info

--- bindings/python/utils/conversions.cpp
+++ bindings/python/utils/conversions.cpp
@@ -12,7 +12,6 @@ namespace pinocchio
     namespace bp = boost::python;
     typedef SE3::Scalar Scalar;
     typedef Eigen::Matrix<Scalar, Eigen::Dynamic,1> VectorXd;
-    typedef Eigen::Matrix<Scalar, 7, 1> Vector7d;
     typedef Eigen::Map<      SE3::Quaternion> QuatMap;
     typedef Eigen::Map<const SE3::Quaternion> QuatConstMap;
 
@@ -59,15 +58,19 @@ namespace pinocchio
     void exposeConversions()
     {
       const char* doc1 = "Convert the input SE3 object to a numpy array.";
-      bp::def("SE3ToXYZQUAT"     , SE3ToXYZQUAT     , doc1);
+      bp::def("SE3ToXYZQUAT"     , SE3ToXYZQUAT     , "M", doc1);
       const char* doc1_tuple = "Convert the input SE3 object to a 7D tuple of floats [X,Y,Z,x,y,z,w].";
-      bp::def("SE3ToXYZQUATtuple", SE3ToXYZQUATtuple, doc1_tuple);
+      bp::def("SE3ToXYZQUATtuple", SE3ToXYZQUATtuple, "M", doc1_tuple);
 
       const char* doc2 = "Reverse function of SE3ToXYZQUAT: convert [X,Y,Z,x,y,z,w] to an SE3 element.";
-      bp::def("XYZQUATToSE3", static_cast<SE3 (*) (const bp::tuple&)> (XYZQUATToSE3_bp<bp::tuple>), doc2);
-      bp::def("XYZQUATToSE3", static_cast<SE3 (*) (const bp::list &)> (XYZQUATToSE3_bp<bp::list >), doc2);
-      bp::def("XYZQUATToSE3", static_cast<SE3 (*) (const VectorXd &)> (XYZQUATToSE3_ei<VectorXd >), doc2);
-      bp::def("XYZQUATToSE3", static_cast<SE3 (*) (const Vector7d &)> (XYZQUATToSE3_ei<Vector7d >), doc2);
+      bp::def("XYZQUATToSE3",
+              static_cast<SE3 (*) (const bp::tuple&)> (XYZQUATToSE3_bp<bp::tuple>),
+              bp::arg("tuple"),doc2);
+      bp::def("XYZQUATToSE3",
+              static_cast<SE3 (*) (const bp::list &)> (XYZQUATToSE3_bp<bp::list >),
+              bp::arg("list"),doc2);
+      bp::def("XYZQUATToSE3", static_cast<SE3 (*) (const VectorXd &)> (XYZQUATToSE3_ei<VectorXd >),
+              bp::arg("array"),doc2);
     }
     
   } // namespace python
