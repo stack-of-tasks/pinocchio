a8d367a168032da672661e6151ef607782ecfbde
Justin Carpentier <justin.carpentier@inria.fr>
2020-01-25 21:50:06 +0100

    python: add converter from Python list to std::vector or aligned_vector

--- bindings/python/utils/std-vector.hpp
+++ bindings/python/utils/std-vector.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2016 CNRS
+// Copyright (c) 2016-2020 CNRS INRIA
 //
 
 #ifndef __pinocchio_python_utils_std_vector_hpp__
@@ -9,6 +9,7 @@
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include <string>
 #include <vector>
+
 #include "pinocchio/bindings/python/utils/pickle-vector.hpp"
 
 namespace pinocchio
@@ -17,27 +18,105 @@ namespace pinocchio
   {
     
     namespace bp = boost::python;
+  
+    ///
+    /// \brief Register the conversion from a Python list to a std::vector
+    ///
+    /// \tparam vector_type A std container (e.g. std::vector or std::list)
+    ///
+    template<typename vector_type>
+    struct StdContainerFromPythonList
+    {
+      typedef typename vector_type::value_type T;
+      
+      /// \brief Check if obj_ptr can be converted
+      static void* convertible(PyObject* obj_ptr)
+      {
+        // Check if it is a list
+        if(!PyList_Check(obj_ptr)) return 0;
+        
+        // Retrieve the underlying list
+        bp::object bp_obj(bp::handle<>(bp::borrowed(obj_ptr)));
+        bp::list bp_list(bp_obj);
+        bp::ssize_t list_size = bp::len(bp_list);
+        
+        // Check if all the elements contained in the current vector is of type T
+        for(bp::ssize_t k = 0; k < list_size; ++k)
+        {
+          bp::extract<T> elt(bp_list[k]);
+          if(!elt.check()) return 0;
+        }
+        
+        return obj_ptr;
+      }
+      
+      /// \brief Allocate the std::vector and fill it with the element contained in the list
+      static void construct(PyObject* obj_ptr,
+                            boost::python::converter::rvalue_from_python_stage1_data * memory)
+      {
+        // Extract the list
+        bp::object bp_obj(bp::handle<>(bp::borrowed(obj_ptr)));
+        bp::list bp_list(bp_obj);
+        bp::ssize_t list_size = bp::len(bp_list);
+        
+        void * storage = reinterpret_cast<bp::converter::rvalue_from_python_storage<vector_type>*>
+        (reinterpret_cast<void*>(memory))->storage.bytes;
+        
+        // Build the std::vector
+        new (storage) vector_type;
+        vector_type & vector = *reinterpret_cast<vector_type*>(storage);
+        vector.reserve((size_t)list_size);
+        
+        // Populate the vector
+        for(bp::ssize_t k = 0; k < list_size; ++k)
+        {
+          bp::extract<T> elt(bp_list[k]);
+          vector.push_back(elt());
+        }
+        
+        // Validate the construction
+        memory->convertible = storage;
+      }
+      
+      static void register_converter()
+      {
+        bp::converter::registry::push_back(&convertible,
+                                           &construct,
+                                           bp::type_id<vector_type>());
+      }
+    };
     
     ///
     /// \brief Expose an std::vector from a type given as template argument.
     ///
     /// \tparam T Type to expose as std::vector<T>.
+    /// \tparam Allocator Type for the Allocator in std::vector<T,Allocator>.
+    /// \tparam NoProxy When set to false, the elements will be copied when returned to Python.
+    /// \tparam EnableFromPythonListConverter Enables the conversion from a Python list to a std::vector<T,Allocator>
     ///
     /// \sa StdAlignedVectorPythonVisitor
     ///
-    template<class T, bool NoProxy = false>
+    template<class T, class Allocator = std::allocator<T>, bool NoProxy = false, bool EnableFromPythonListConverter = true>
     struct StdVectorPythonVisitor
-    : public bp::vector_indexing_suite< typename std::vector<T>,NoProxy >
+    : public bp::vector_indexing_suite<typename std::vector<T,Allocator>, NoProxy>
+    , public StdContainerFromPythonList< std::vector<T,Allocator> >
     {
+      typedef std::vector<T,Allocator> vector_type;
+      typedef StdContainerFromPythonList<vector_type> FromPythonListConverter;
       
       static void expose(const std::string & class_name,
                          const std::string & doc_string = "")
       {
-        bp::class_< std::vector<T> >(class_name.c_str(),doc_string.c_str())
+        bp::class_<vector_type>(class_name.c_str(),doc_string.c_str())
         .def(StdVectorPythonVisitor())
-        .def_pickle(PickleVector<typename std::vector<T> >());        
+        .def_pickle(PickleVector<vector_type>());
+        
+        // Register conversion
+        if(EnableFromPythonListConverter)
+          FromPythonListConverter::register_converter();
       }
     };
+    
   } // namespace python
 } // namespace pinocchio
 
