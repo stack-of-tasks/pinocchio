e896e68faf7ce8100ddf3c96c960570a971ec68f
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2020-01-30 00:32:35 +0100

    [unit] [python] Fix bindings_fcl_transform

--- unittest/python/bindings_fcl_transform.py
+++ unittest/python/bindings_fcl_transform.py
@@ -6,18 +6,16 @@ import numpy as np
 class TestFCLTransformConversion(unittest.TestCase):
 
     def test_from_SE3(self):
-
         M = pin.SE3.Random()
-        #fcl_transform = pin.hppfcl.Transform3f(M)
-        #
-        #self.assertEqual(M.rotation,fcl_transform.getRotation())
-        #self.assertEqual(M.translation,fcl_transform.getTranslation())
-    
-    def test_to_SE3(self):
+        fcl_transform = pin.hppfcl.Transform3f(M)
         
+        self.assertTrue((M.rotation==fcl_transform.getRotation()).all())
+        self.assertTrue((M.translation==fcl_transform.getTranslation()).all())
+
+    def test_to_SE3(self):
         fcl_transform = pin.hppfcl.Transform3f()
         M = pin.SE3(fcl_transform)
-        self.assertEqual(M.isIdentity())
+        self.assertTrue(M.isIdentity())
 
 if __name__ == '__main__':
     if pin.WITH_HPP_FCL_BINDINGS:
