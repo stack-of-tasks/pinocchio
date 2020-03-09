7570689a61f0bf191a65373b29e06d9c69085b7f
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-09 13:49:09 +0100

    urdf: fix warning message

--- src/parsers/urdf/utils.hpp
+++ src/parsers/urdf/utils.hpp
@@ -1,10 +1,10 @@
 //
-// Copyright (c) 2015-2016 CNRS
+// Copyright (c) 2015-2020 CNRS INRIA
 //
 
 #ifndef __pinocchio_parsers_urdf_utils_hpp__
 #define __pinocchio_parsers_urdf_utils_hpp__
 
-#warning "This file is not deprecated and will be removed in future releases. There is no replacement."
+#warning "This file is now deprecated and will be removed in future releases. There is no replacement."
 
 #endif // __pinocchio_parsers_urdf_utils_hpp__
