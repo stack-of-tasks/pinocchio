47ad5da4d49faf3129337880d3782d2c8581a1c5
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-18 21:40:08 +0100

    python/algo: expose new signatures for ABA and its derivatives

--- bindings/python/algorithm/expose-aba-derivatives.cpp
+++ bindings/python/algorithm/expose-aba-derivatives.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "pinocchio/bindings/python/algorithm/algorithms.hpp"
+#include "pinocchio/bindings/python/utils/namespace.hpp"
 #include "pinocchio/algorithm/aba-derivatives.hpp"
 
 namespace pinocchio
@@ -46,6 +47,27 @@ namespace pinocchio
               "store the result in data.ddq_dq, data.ddq_dv and data.Minv\n"
               "which correspond to the partial derivatives of the acceleration output with respect to the joint configuration,\n"
               "velocity and torque vectors.");
+      
+      {
+        bp::scope current_scope = getOrCreatePythonNamespace("optimized");
+        
+        bp::def("computeABADerivatives",
+                (void (*)(const Model &, Data &))optimized::computeABADerivatives<double,0,JointCollectionDefaultTpl>,
+                bp::args("model","data"),
+                "Computes the ABA derivatives, store the result in data.ddq_dq, data.ddq_dv and data.Minv\n"
+                "which correspond to the partial derivatives of the joint acceleration vector output with respect to the joint configuration,\n"
+                "velocity and torque vectors.\n"
+                "By calling this function, the user assumes that pinocchio.optimized.aba has been called first, allowing to significantly reduce the computation timings by not recalculating intermediate results.");
+        
+        bp::def("computeABADerivatives",
+                (void (*)(const Model &, Data &, const container::aligned_vector<Force> &))optimized::computeABADerivatives<double,0,JointCollectionDefaultTpl>,
+                bp::args("model","data","fext"),
+                "Computes the ABA derivatives, store the result in data.ddq_dq, data.ddq_dv and data.Minv\n"
+                "which correspond to the partial derivatives of the joint acceleration vector output with respect to the joint configuration,\n"
+                "velocity and torque vectors.\n"
+                "By calling this function, the user assumes that pinocchio.optimized.aba has been called first, allowing to significantly reduce the computation timings by not recalculating intermediate results.");
+      }
+      
     }
   } // namespace python
 } // namespace pinocchio
--- bindings/python/algorithm/expose-aba.cpp
+++ bindings/python/algorithm/expose-aba.cpp
@@ -1,8 +1,10 @@
 //
-// Copyright (c) 2015-2016 CNRS
+// Copyright (c) 2015-2020 CNRS INRIA
 //
 
 #include "pinocchio/bindings/python/algorithm/algorithms.hpp"
+#include "pinocchio/bindings/python/utils/namespace.hpp"
+
 #include "pinocchio/algorithm/aba.hpp"
 
 namespace pinocchio
@@ -18,6 +20,18 @@ namespace pinocchio
       data.Minv.transpose().triangularView<Eigen::StrictlyLower>();
       return data.Minv;
     }
+  
+    namespace optimized
+    {
+      const Data::RowMatrixXs &
+      computeMinverse_proxy(const Model & model, Data & data)
+      {
+        pinocchio::optimized::computeMinverse(model,data);
+        data.Minv.triangularView<Eigen::StrictlyLower>() =
+        data.Minv.transpose().triangularView<Eigen::StrictlyLower>();
+        return data.Minv;
+      }
+    }
     
     void exposeABA()
     {
@@ -25,30 +39,79 @@ namespace pinocchio
 
       bp::def("aba",
               &aba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)",
-                       "Joint torque tau (size Model::nv)"),
-              "Compute ABA, store the result in Data::ddq and return it.",
+              bp::args("model","data","q","v","tau"),
+              "Compute ABA, store the result in data.ddq and return it.\n"
+              "Parameters:\n"
+              "\t model: Model of the kinematic tree\n"
+              "\t data: Data related to the kinematic tree\n"
+              "\t q: joint configuration (size model.nq)\n"
+              "\t tau: joint velocity (size model.nv)\n"
+              "\t v: joint torque (size model.nv)",
               bp::return_value_policy<bp::return_by_value>());
 
       bp::def("aba",
               &aba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd,VectorXd,Force>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)",
-                       "Joint torque tau (size Model::nv)",
-                       "Vector of external forces expressed in the local frame of each joint (size Model::njoints)"),
-              "Compute ABA with external forces, store the result in Data::ddq and return it.",
+              bp::args("model","data","q","v","tau","fext"),
+              "Compute ABA with external forces, store the result in data.ddq and return it.\n"
+              "Parameters:\n"
+              "\t model: Model of the kinematic tree\n"
+              "\t data: Data related to the kinematic tree\n"
+              "\t q: joint configuration (size model.nq)\n"
+              "\t v: joint velocity (size model.nv)\n"
+              "\t tau: joint torque (size model.nv)\n"
+              "\t fext: vector of external forces expressed in the local frame of the joint (size model.njoints)",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("computeMinverse",
               &computeMinverse_proxy,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)"),
-              "Computes the inverse of the joint space inertia matrix using a variant of the Articulated Body algorithm.\n"
-              "The result is stored in data.Minv.",
+              bp::args("model","data","q"),
+              "Computes the inverse of the joint space inertia matrix using an extension of the Articulated Body algorithm.\n"
+              "The result is stored in data.Minv.\n"
+              "Parameters:\n"
+              "\t model: Model of the kinematic tree\n"
+              "\t data: Data related to the kinematic tree\n"
+              "\t q: joint configuration (size model.nq)",
               bp::return_value_policy<bp::return_by_value>());
+      
+      {
+        bp::scope current_scope = getOrCreatePythonNamespace("optimized");
+        
+        bp::def("aba",
+                &pinocchio::optimized::aba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd,VectorXd>,
+                bp::args("model","data","q","v","tau"),
+                "Compute ABA, store the result in data.ddq and return it.\n"
+                "Parameters:\n"
+                "\t model: Model of the kinematic tree\n"
+                "\t data: Data related to the kinematic tree\n"
+                "\t q: joint configuration (size model.nq)\n"
+                "\t tau: joint velocity (size model.nv)\n"
+                "\t v: joint torque (size model.nv)",
+                bp::return_value_policy<bp::return_by_value>());
+
+        bp::def("aba",
+                &pinocchio::optimized::aba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd,VectorXd,Force>,
+                bp::args("model","data","q","v","tau","fext"),
+                "Compute ABA with external forces, store the result in data.ddq and return it.\n"
+                "Parameters:\n"
+                "\t model: Model of the kinematic tree\n"
+                "\t data: Data related to the kinematic tree\n"
+                "\t q: joint configuration (size model.nq)\n"
+                "\t v: joint velocity (size model.nv)\n"
+                "\t tau: joint torque (size model.nv)\n"
+                "\t fext: vector of external forces expressed in the local frame of the joint (size model.njoints)",
+                bp::return_value_policy<bp::return_by_value>());
+        
+        bp::def("computeMinverse",
+                &optimized::computeMinverse_proxy,
+                bp::args("model","data"),
+                "Computes the inverse of the joint space inertia matrix using an extension of the Articulated Body algorithm.\n"
+                "The result is stored in data.Minv.\n"
+                "Remarks: pinocchio.optimized.aba should have been called first.\n"
+                "Parameters:\n"
+                "\t model: Model of the kinematic tree\n"
+                "\t data: Data related to the kinematic tree",
+                bp::return_value_policy<bp::return_by_value>());
+      }
       
     }
     
