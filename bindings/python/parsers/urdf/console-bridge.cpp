95a59ca57f32466fb340cb5c3b905f5507c1f1f6
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-19 19:13:13 +0100

    python: expose console_bridge (partially)

---
+++ bindings/python/parsers/urdf/console-bridge.cpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2020 INRIA
+//
+
+#include <boost/python.hpp>
+#include "pinocchio/bindings/python/parsers/urdf.hpp"
+
+#ifdef PINOCCHIO_WITH_URDFDOM
+  #include <console_bridge/console.h>
+#endif
+
+namespace pinocchio
+{
+  namespace python
+  {
+    void exposeConsoleBridge()
+    {
+      namespace bp = boost::python;
+      
+#ifdef PINOCCHIO_WITH_URDFDOM
+      
+      // fix CONSOLE_BRIDGE warning level
+      ::console_bridge::setLogLevel(::console_bridge::CONSOLE_BRIDGE_LOG_ERROR);
+      
+      typedef ::console_bridge::LogLevel LogLevel;
+      bp::enum_<LogLevel>("LogLevel")
+      .value("CONSOLE_BRIDGE_LOG_DEBUG",LogLevel::CONSOLE_BRIDGE_LOG_DEBUG)
+      .value("CONSOLE_BRIDGE_LOG_INFO" ,LogLevel::CONSOLE_BRIDGE_LOG_INFO )
+      .value("CONSOLE_BRIDGE_LOG_WARN" ,LogLevel::CONSOLE_BRIDGE_LOG_WARN )
+      .value("CONSOLE_BRIDGE_LOG_ERROR",LogLevel::CONSOLE_BRIDGE_LOG_ERROR)
+      .value("CONSOLE_BRIDGE_LOG_NONE" ,LogLevel::CONSOLE_BRIDGE_LOG_NONE )
+      ;
+      
+#endif
+    }
+  }
+}
