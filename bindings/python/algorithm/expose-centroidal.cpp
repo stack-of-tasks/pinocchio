d4ec672dc0fc17f2271aae54148384ef5c830b0d
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-19 10:51:59 +0100

    python/centroidal: fix doc

--- bindings/python/algorithm/expose-centroidal.cpp
+++ bindings/python/algorithm/expose-centroidal.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2019 CNRS INRIA
+// Copyright (c) 2015-2020 CNRS INRIA
 //
 
 #include "pinocchio/bindings/python/algorithm/algorithms.hpp"
@@ -16,55 +16,45 @@ namespace pinocchio
 
       bp::def("computeCentroidalMomentum",
               &computeCentroidalMomentum<double,0,JointCollectionDefaultTpl>,
-              bp::args("Model","Data"),
+              bp::args("model","data"),
               "Computes the Centroidal momentum, a.k.a. the total momentum of the system expressed around the center of mass.",
               bp::return_value_policy<bp::return_by_value>());
 
       bp::def("computeCentroidalMomentum",
               &computeCentroidalMomentum<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)"),
+              bp::args("model","data","q","v"),
               "Computes the Centroidal momentum, a.k.a. the total momentum of the system expressed around the center of mass.",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("computeCentroidalMomentumTimeVariation",
               &computeCentroidalMomentumTimeVariation<double,0,JointCollectionDefaultTpl>,
-              bp::args("Model","Data"),
+              bp::args("model","data"),
               "Computes the Centroidal momentum and its time derivatives, a.k.a. the total momentum of the system and its time derivative expressed around the center of mass.",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("computeCentroidalMomentumTimeVariation",
               &computeCentroidalMomentumTimeVariation<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)",
-                       "Joint acceleration a (size Model::nv)"),
+              bp::args("model","data","q","v","a"),
               "Computes the Centroidal momentum and its time derivatives, a.k.a. the total momentum of the system and its time derivative expressed around the center of mass.",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("ccrba",
               &ccrba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)"),
+              bp::args("model","data","q","v"),
               "Computes the centroidal mapping, the centroidal momentum and the Centroidal Composite Rigid Body Inertia, puts the result in Data and returns the centroidal mapping."
               "For the same price, it also computes the total joint jacobians (data.J).",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("computeCentroidalMap",
               &computeCentroidalMap<double,0,JointCollectionDefaultTpl,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)"),
+              bp::args("model","data","q"),
               "Computes the centroidal mapping, puts the result in Data.Ag and returns the centroidal mapping.\n"
               "For the same price, it also computes the total joint jacobians (data.J).",
               bp::return_value_policy<bp::return_by_value>());
       
       bp::def("dccrba",
               dccrba<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)"),
+              bp::args("model","data","q","v"),
               "Computes the time derivative of the centroidal momentum matrix Ag in terms of q and v.\n"
               "For the same price, it also computes the centroidal momentum matrix (data.Ag), the total joint jacobians (data.J) "
               "and the related joint jacobians time derivative (data.dJ)",
@@ -72,9 +62,7 @@ namespace pinocchio
       
       bp::def("computeCentroidalMapTimeVariation",
               computeCentroidalMapTimeVariation<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
-              bp::args("Model","Data",
-                       "Joint configuration q (size Model::nq)",
-                       "Joint velocity v (size Model::nv)"),
+              bp::args("model","data","q","v"),
               "Computes the time derivative of the centroidal momentum matrix Ag, puts the result in Data.Ag and returns the centroidal mapping.\n"
               "For the same price, it also computes the centroidal momentum matrix (data.Ag), the total joint jacobians (data.J) "
               "and the related joint jacobians time derivative (data.dJ)",
