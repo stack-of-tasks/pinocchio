202202d7faa326d2b47b4733f4700fd920d420ad
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-24 14:30:13 +0100

    python/utils: handle std::vector for non const reference

--- bindings/python/utils/std-vector.hpp
+++ bindings/python/utils/std-vector.hpp
@@ -17,9 +17,37 @@ namespace pinocchio
 {
   namespace python
   {
+    // Forward declaration
+    template<typename vector_type, bool NoProxy = false>
+    struct StdContainerFromPythonList;
   
     namespace details
     {
+    
+      /// \brief Check if a PyObject can be converted to an std::vector<T>.
+      template<typename T>
+      bool from_python_list(PyObject * obj_ptr,T *)
+      {
+        namespace bp = ::boost::python;
+        
+        // Check if it is a list
+        if(!PyList_Check(obj_ptr)) return false;
+        
+        // Retrieve the underlying list
+        bp::object bp_obj(bp::handle<>(bp::borrowed(obj_ptr)));
+        bp::list bp_list(bp_obj);
+        bp::ssize_t list_size = bp::len(bp_list);
+        
+        // Check if all the elements contained in the current vector is of type T
+        for(bp::ssize_t k = 0; k < list_size; ++k)
+        {
+          bp::extract<T> elt(bp_list[k]);
+          if(!elt.check()) return false;
+        }
+        
+        return true;
+      }
+    
       template<typename vector_type, bool NoProxy>
       struct build_list
       {
@@ -49,16 +77,85 @@ namespace pinocchio
         }
       };
     } // namespace details
+}} // namespace pinocchio::python
+
+namespace boost { namespace python { namespace converter {
+
+  template<typename Type, class Allocator>
+  struct reference_arg_from_python<std::vector<Type,Allocator> &>
+  : arg_lvalue_from_python_base
+  {
+    typedef std::vector<Type,Allocator> vector_type;
+    typedef vector_type & ref_vector_type;
+    typedef ref_vector_type result_type;
+    
+    reference_arg_from_python(PyObject* py_obj)
+    : arg_lvalue_from_python_base(converter::get_lvalue_from_python(py_obj,
+                                                                    registered<vector_type>::converters))
+    , m_data(NULL)
+    , m_source(py_obj)
+    , vec_ptr(NULL)
+    {
+      if(result() != 0) // we have found a lvalue converter
+        return;
+      
+      // Check if py_obj is a py_list, which can then be converted to an std::vector
+      bool is_convertible = ::pinocchio::python::details::from_python_list(py_obj,(Type*)(0));
+      if(!is_convertible)
+        return;
+      
+      typedef ::pinocchio::python::StdContainerFromPythonList<vector_type> Constructor;
+      Constructor::construct(py_obj,&m_data.stage1);
+      
+      void * & m_result = const_cast<void * &>(result());
+      m_result = m_data.stage1.convertible;
+      vec_ptr = reinterpret_cast<vector_type*>(m_data.storage.bytes);
+    }
+    
+    result_type operator()() const
+    {
+      return ::boost::python::detail::void_ptr_to_reference(result(),
+                                                            (result_type(*)())0);
+    }
+    
+    ~reference_arg_from_python()
+    {
+      if(m_data.stage1.convertible == m_data.storage.bytes)
+      {
+        // Copy back the reference
+        const vector_type & vec = *vec_ptr;
+        list bp_list(handle<>(borrowed(m_source)));
+        for(size_t i = 0; i < vec.size(); ++i)
+        {
+          Type & elt = extract<Type &>(bp_list[i]);
+          elt = vec[i];
+        }
+      }
+    }
+      
+   private:
+    rvalue_from_python_data<ref_vector_type> m_data;
+    PyObject* m_source;
+    vector_type * vec_ptr;
+  };
+
+}}} // boost::python::converter
+
+namespace pinocchio
+{
+  namespace python
+  {
     
     ///
     /// \brief Register the conversion from a Python list to a std::vector
     ///
     /// \tparam vector_type A std container (e.g. std::vector or std::list)
     ///
-    template<typename vector_type, bool NoProxy = false>
+    template<typename vector_type, bool NoProxy>
     struct StdContainerFromPythonList
     {
       typedef typename vector_type::value_type T;
+      typedef typename vector_type::allocator_type Allocator;
       
       /// \brief Check if obj_ptr can be converted
       static void* convertible(PyObject* obj_ptr)
