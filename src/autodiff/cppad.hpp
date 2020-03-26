6f410556b3d081b28a3f7f228e731046ca244c97
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-03-26 11:11:30 +0100

    specialize Jlog3 and Jlog6 for cppad

--- src/autodiff/cppad.hpp
+++ src/autodiff/cppad.hpp
@@ -178,7 +178,7 @@ namespace pinocchio
     }
   };
 
-  //\brief Generic evaluation of log3 function
+  /// \brief Implementation of log3 for overloaded CppAD type.
   template<typename ADMatrix3Like, typename _Scalar>
   struct log3Algo<ADMatrix3Like, CppAD::AD<_Scalar>, false>
   {
@@ -229,8 +229,104 @@ namespace pinocchio
   };
 
 
+  /// \brief Implementation of Jlog3 for overloaded CppAD type.
+  template<typename Scalar, typename Vector3Like, typename Matrix3Like>
+  struct Jlog3Algo<CppAD::AD<Scalar>, Vector3Like, Matrix3Like, false>
+  {
+    static void run(const CppAD::AD<Scalar> & theta,
+                    const Eigen::MatrixBase<Vector3Like> & log,
+                    const Eigen::MatrixBase<Matrix3Like> & Jlog)
+    {
+      typedef CppAD::AD<Scalar> ADScalar;
+      PINOCCHIO_ASSERT_MATRIX_SPECIFIC_SIZE (Vector3Like,  log, 3, 1);
+      PINOCCHIO_ASSERT_MATRIX_SPECIFIC_SIZE (Matrix3Like, Jlog, 3, 3);
+      
+      Matrix3Like & Jout = PINOCCHIO_EIGEN_CONST_CAST(Matrix3Like,Jlog);
+
+      ADScalar ct,st; SINCOS(theta,&st,&ct);
+      const ADScalar st_1mct = st/(ADScalar(1)-ct);
+
+      const ADScalar alpha = CppAD::CondExpLt<Scalar>(theta, TaylorSeriesExpansion<ADScalar>::template precision<3>(),
+                                                      ADScalar(1)/ADScalar(12) + theta*theta / ADScalar(720),
+                                                      ADScalar(1)/(theta*theta) - st_1mct/(ADScalar(2)*theta));
+      
+      Jout.noalias() = alpha * log * log.transpose();
+      
+      Jout.diagonal()[0] = CppAD::CondExpLt<Scalar>(theta, TaylorSeriesExpansion<ADScalar>::template precision<3>(),      
+                                                            Jout.diagonal()[0] + ADScalar(0.5) * (2 - theta*theta / ADScalar(6)),
+                                                            Jout.diagonal()[0] + ADScalar(0.5) * (theta*st_1mct));
+      Jout.diagonal()[1] = CppAD::CondExpLt<Scalar>(theta, TaylorSeriesExpansion<ADScalar>::template precision<3>(),      
+                                                            Jout.diagonal()[1] + ADScalar(0.5) * (2 - theta*theta / ADScalar(6)),
+                                                            Jout.diagonal()[1] + ADScalar(0.5) * (theta*st_1mct));
+      Jout.diagonal()[2] = CppAD::CondExpLt<Scalar>(theta, TaylorSeriesExpansion<ADScalar>::template precision<3>(),      
+                                                            Jout.diagonal()[2] + ADScalar(0.5) * (2 - theta*theta / ADScalar(6)),
+                                                            Jout.diagonal()[2] + ADScalar(0.5) * (theta*st_1mct));
+
+      addSkew(ADScalar(0.5) * log, Jlog);      
+    }
+  };
   
 
+  template<typename Scalar, int Options, typename Matrix6Like>
+  struct Jlog6Algo<CppAD::AD<Scalar>, Options, Matrix6Like, false>
+  {
+    static void run(const SE3Tpl<CppAD::AD<Scalar>, Options> & M,
+             const Eigen::MatrixBase<Matrix6Like> & Jlog)
+    {
+      
+      typedef CppAD::AD<Scalar> ADScalar;
+      PINOCCHIO_ASSERT_MATRIX_SPECIFIC_SIZE (Matrix6Like, Jlog, 6, 6);
+
+      typedef SE3Tpl<ADScalar,Options> SE3;
+      typedef typename SE3::Vector3 Vector3;
+      Matrix6Like & value = PINOCCHIO_EIGEN_CONST_CAST(Matrix6Like,Jlog);
+
+      typename SE3::ConstAngularRef R = M.rotation();
+      typename SE3::ConstLinearRef p = M.translation();
+
+      ADScalar t;
+      Vector3 w(log3(R,t));
+
+      // value is decomposed as following:
+      // value = [ A, B;
+      //           C, D ]
+      typedef Eigen::Block<Matrix6Like,3,3> Block33;
+      Block33 A = value.template topLeftCorner<3,3>();
+      Block33 B = value.template topRightCorner<3,3>();
+      Block33 C = value.template bottomLeftCorner<3,3>();
+      Block33 D = value.template bottomRightCorner<3,3>();
+
+      Jlog3(t, w, A);
+      D = A;
+
+      const ADScalar t2 = t*t;
+      ADScalar beta, beta_dot_over_theta;
+
+      const ADScalar tinv = ADScalar(1)/t,
+        t2inv = tinv*tinv;
+      ADScalar st,ct; SINCOS (t, &st, &ct);
+      const ADScalar inv_2_2ct = ADScalar(1)/(ADScalar(2)*(ADScalar(1)-ct));
+      
+      beta = CppAD::CondExpLt<Scalar>(t, TaylorSeriesExpansion<ADScalar>::template precision<3>(),
+                                      ADScalar(1)/ADScalar(12) + t2/ADScalar(720),
+                                      t2inv - st*tinv*inv_2_2ct);
+      beta_dot_over_theta = CppAD::CondExpLt<Scalar>(t, TaylorSeriesExpansion<ADScalar>::template precision<3>(),
+                                                     ADScalar(1)/ADScalar(360),
+                                                     -ADScalar(2)*t2inv*t2inv + (ADScalar(1) + st*tinv) * t2inv * inv_2_2ct);
+
+      ADScalar wTp = w.dot(p);
+
+      Vector3 v3_tmp((beta_dot_over_theta*wTp)*w - (t2*beta_dot_over_theta+ADScalar(2)*beta)*p);
+      // C can be treated as a temporary variable
+      C.noalias() = v3_tmp * w.transpose();
+      C.noalias() += beta * w * p.transpose();
+      C.diagonal().array() += wTp * beta;
+      addSkew(ADScalar(.5)*p,C);
+
+      B.noalias() = C * A;
+      C.setZero();
+    }
+  };
   
 } // namespace pinocchio
 
