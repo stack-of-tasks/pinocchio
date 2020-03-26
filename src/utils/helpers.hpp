4ffbe6b207166549bb812751e28a3f3d0a8d03b2
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-26 09:24:30 +0100

    utils: remove useless helper

--- src/utils/helpers.hpp
+++ src/utils/helpers.hpp
@@ -20,33 +20,6 @@ namespace pinocchio
     {
       static const bool value = true;
     };
-  
-    /// \brief By casting from T1 to T2, does there exist any gain in precision.
-    template<typename T1, typename T2>
-    struct gain_precision
-    {
-      static const bool value = false;
-    };
-  
-    template<>
-    struct gain_precision<float,double>
-    {
-      static const bool value = true;
-    };
-  
-    template<>
-    struct gain_precision<double,long double>
-    {
-    static const bool value = true;
-    };
-    
-    template<>
-    struct gain_precision<float,long double>
-    {
-      static const bool value = true;
-    };
-  
-
   }
 }
 
