289d5e5593790e3cbd74377a6e2ac28e45b814fd
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2020-03-27 01:53:59 +0100

    [bindings] Remove Wcast-function-type

--- bindings/python/algorithm/expose-energy.cpp
+++ bindings/python/algorithm/expose-energy.cpp
@@ -15,7 +15,7 @@ namespace pinocchio
       using namespace Eigen;
       
       bp::def("computeKineticEnergy",
-              (double (*)(const Model &, Data &, const Eigen::VectorXd &, const Eigen::VectorXd &))&computeKineticEnergy<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
+              &computeKineticEnergy<double,0,JointCollectionDefaultTpl,VectorXd,VectorXd>,
               bp::args("model","data","q","v"),
               "Computes the forward kinematics and the kinematic energy of the model for the "
               "given joint configuration and velocity given as input. The result is accessible through data.kinetic_energy.");
@@ -27,7 +27,7 @@ namespace pinocchio
               "given joint placement and velocity stored in data. The result is accessible through data.kinetic_energy.");
       
       bp::def("computePotentialEnergy",
-              (double (*)(const Model &, Data &, const Eigen::VectorXd &))&computePotentialEnergy<double,0,JointCollectionDefaultTpl,VectorXd>,
+              &computePotentialEnergy<double,0,JointCollectionDefaultTpl,VectorXd>,
               bp::args("model","data","q"),
               "Computes the potential energy of the model for the "
               "given the joint configuration given as input. The result is accessible through data.potential_energy.");
