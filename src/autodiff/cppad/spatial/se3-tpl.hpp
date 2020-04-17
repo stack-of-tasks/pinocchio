7888202e08fec13756339c0cd22c4fc462f85751
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-04-17 13:22:49 +0200

    fix syntax in se3-tpl

--- src/autodiff/cppad/spatial/se3-tpl.hpp
+++ src/autodiff/cppad/spatial/se3-tpl.hpp
@@ -12,7 +12,7 @@ namespace pinocchio
   namespace internal
   {
     template<typename Scalar, int Options, typename NewScalar>
-    struct cast_call_normalize_method<SE3Tpl<::CppAD::AD<Scalar>,Options>,NewScalar,::CppAD::AD<Scalar>>
+    struct cast_call_normalize_method<SE3Tpl<CppAD::AD<Scalar>,Options>,NewScalar,CppAD::AD<Scalar> > 
     {
       template<typename T>
       static void run(T &)
@@ -21,8 +21,8 @@ namespace pinocchio
       }
     };
   
-    template<typename Scalar, int Options, typename NewScalar>i
-    struct cast_call_normalize_method<SE3Tpl<Scalar,Options>,::CppAD::AD<NewScalar>,Scalar>
+    template<typename Scalar, int Options, typename NewScalar>
+    struct cast_call_normalize_method<SE3Tpl<Scalar,Options>,CppAD::AD<NewScalar>,Scalar>
     {
       template<typename T>
       static void run(T &)
