496401832909b71f9afa4d741f0ea6e9237ec7fd
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-04-17 13:13:28 +0200

    fix typo

--- src/autodiff/cppad/spatial/se3-tpl.hpp
+++ src/autodiff/cppad/spatial/se3-tpl.hpp
@@ -12,7 +12,7 @@ namespace pinocchio
   namespace internal
   {
     template<typename Scalar, int Options, typename NewScalar>
-    struct cast_call_normalize_method<SE3Tpl<::cppad::AD<Scalar>,Options>,NewScalar,::cppad::AD<Scalar>>
+    struct cast_call_normalize_method<SE3Tpl<::CppAD::AD<Scalar>,Options>,NewScalar,::CppAD::AD<Scalar>>
     {
       template<typename T>
       static void run(T &)
@@ -22,7 +22,7 @@ namespace pinocchio
     };
   
     template<typename Scalar, int Options, typename NewScalar>i
-    struct cast_call_normalize_method<SE3Tpl<Scalar,Options>,::cppad::AD<NewScalar>,Scalar>
+    struct cast_call_normalize_method<SE3Tpl<Scalar,Options>,::CppAD::AD<NewScalar>,Scalar>
     {
       template<typename T>
       static void run(T &)
