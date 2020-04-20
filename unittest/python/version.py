cc45adb2692b267ae9995b19af25aea851d7f541
Justin Carpentier <justin.carpentier@inria.fr>
2020-04-20 13:56:27 +0200

    python/test: add test of Pinocchio version

---
+++ unittest/python/version.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import unittest
+import pinocchio as pin
+
+from test_case import PinocchioTestCase as TestCase
+
+class TestVersion(TestCase):
+    def test_version(self):
+      print("version:",pin.__version__)
+      print("raw_version:",pin.__raw_version__)
+
+if __name__ == '__main__':
+    unittest.main()
