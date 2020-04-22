4bdd221b93613f6c76f0c4c8a4943c6f0b0c4501
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-04-22 18:02:07 +0200

    add unittest for codegenerated difference

--- unittest/cppadcg-joint-configurations.cpp
+++ unittest/cppadcg-joint-configurations.cpp
@@ -47,30 +47,43 @@ void buildModel(Model & model)
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-  BOOST_AUTO_TEST_CASE(test_integrate_code_generation)
-  {
-    typedef double Scalar;
-    typedef CppAD::cg::CG<Scalar> CGScalar;
-    typedef CppAD::AD<CGScalar> ADScalar;
-    
-    typedef pinocchio::ModelTpl<Scalar> Model;   
-    typedef pinocchio::ModelTpl<ADScalar> ADModel;
-
-    Model model; buildModel(model);
-    Eigen::VectorXd qs = Eigen::VectorXd::Random(model.nq);
-    normalize(model,qs);
+BOOST_AUTO_TEST_CASE(test_joint_configuration_code_generation)
+{
+  typedef double Scalar;
+  typedef CppAD::cg::CG<Scalar> CGScalar;
+  typedef CppAD::AD<CGScalar> ADScalar;
   
-    Eigen::VectorXd vs = Eigen::VectorXd::Random(model.nv);
-    std::vector<Eigen::VectorXd> results(2,Eigen::VectorXd::Zero(model.nq));
-    CodeGenIntegrate<double> cg_integrate(model);
-    cg_integrate.initLib();
-    cg_integrate.loadLib();
-    
-    cg_integrate.evalFunction(qs,vs, results[0]);
-    pinocchio::integrate(model, qs,vs,results[1]);
-    
-    BOOST_CHECK(results[1].isApprox(results[0]));
-  }
+  typedef pinocchio::ModelTpl<Scalar> Model;   
+  typedef pinocchio::ModelTpl<ADScalar> ADModel;
+  
+  Model model; buildModel(model);
+  Eigen::VectorXd qs = Eigen::VectorXd::Random(model.nq);
+  Eigen::VectorXd qs2 = Eigen::VectorXd::Random(model.nq);
+  normalize(model,qs);
+  normalize(model,qs2);
+  
+  Eigen::VectorXd vs = Eigen::VectorXd::Random(model.nv);
+  std::vector<Eigen::VectorXd> results_q(2,Eigen::VectorXd::Zero(model.nq));
+  std::vector<Eigen::VectorXd> results_v(2,Eigen::VectorXd::Zero(model.nv));
+
+  //Integrate
+  CodeGenIntegrate<double> cg_integrate(model);
+  cg_integrate.initLib();
+  cg_integrate.loadLib();
+  
+  cg_integrate.evalFunction(qs,vs, results_q[0]);
+  pinocchio::integrate(model, qs,vs,results_q[1]);
+  
+  //Difference
+  CodeGenDifference<double> cg_difference(model);
+  cg_difference.initLib();
+  cg_difference.loadLib();
+  
+  cg_integrate.evalFunction(qs,qs2, results_v[0]);
+  pinocchio::integrate(model, qs,qs2,results_v[1]);
+  
+  BOOST_CHECK(results_v[1].isApprox(results_v[0]));
+}
 
 
 
