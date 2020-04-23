a19413fe22c67804819fdc36e7f0e00cefba66c5
Justin Carpentier <justin.carpentier@inria.fr>
2020-04-23 15:36:58 +0200

    test/cppadcodegen: improve testing

--- unittest/cppadcg-joint-configurations.cpp
+++ unittest/cppadcg-joint-configurations.cpp
@@ -23,12 +23,12 @@ BOOST_AUTO_TEST_CASE(test_joint_configuration_code_generation)
   typedef pinocchio::ModelTpl<Scalar> Model;   
 
   Model model; buildAllJointsModel(model);
-  Eigen::VectorXd qs = Eigen::VectorXd::Random(model.nq);
-  Eigen::VectorXd qs2 = Eigen::VectorXd::Random(model.nq);
-  normalize(model,qs);
-  normalize(model,qs2);
+  Eigen::VectorXd q1 = Eigen::VectorXd::Random(model.nq);
+  Eigen::VectorXd q2 = Eigen::VectorXd::Random(model.nq);
+  normalize(model,q1);
+  normalize(model,q2);
   
-  Eigen::VectorXd vs = Eigen::VectorXd::Random(model.nv);
+  Eigen::VectorXd v = Eigen::VectorXd::Random(model.nv);
   std::vector<Eigen::VectorXd> results_q(2,Eigen::VectorXd::Zero(model.nq));
   std::vector<Eigen::VectorXd> results_v(2,Eigen::VectorXd::Zero(model.nv));
 
@@ -37,23 +37,23 @@ BOOST_AUTO_TEST_CASE(test_joint_configuration_code_generation)
   cg_integrate.initLib();
   cg_integrate.loadLib();
   
-  cg_integrate.evalFunction(qs,vs, results_q[0]);
-  pinocchio::integrate(model, qs,vs,results_q[1]);
+  cg_integrate.evalFunction(q1,v, results_q[0]);
+  pinocchio::integrate(model, q1,v,results_q[1]);
   BOOST_CHECK(results_q[1].isApprox(results_q[0]));
 
-  cg_integrate.evalFunction(qs,0*vs,results_q[0]);
-  BOOST_CHECK(results_q[0].isApprox(qs));
+  cg_integrate.evalFunction(q1,0*v,results_q[0]);
+  BOOST_CHECK(results_q[0].isApprox(q1));
   
   //Difference
   CodeGenDifference<double> cg_difference(model);
   cg_difference.initLib();
   cg_difference.loadLib();
   
-  cg_difference.evalFunction(qs,qs2, results_v[0]);
-  pinocchio::difference(model,qs,qs2,results_v[1]);
+  cg_difference.evalFunction(q1,q2, results_v[0]);
+  pinocchio::difference(model,q1,q2,results_v[1]);
   BOOST_CHECK(results_v[1].isApprox(results_v[0]));
 
-  cg_difference.evalFunction(qs,qs,results_v[0]);
+  cg_difference.evalFunction(q1,q1,results_v[0]);
   BOOST_CHECK(results_v[0].isZero());
 
   //dDifference
@@ -63,14 +63,22 @@ BOOST_AUTO_TEST_CASE(test_joint_configuration_code_generation)
   
   //ARG0
   std::vector<Eigen::MatrixXd> results_J(2,Eigen::MatrixXd::Zero(model.nv,model.nv));
-  cg_dDifference.evalFunction(qs,qs2, results_J[0], pinocchio::ARG0);
-  pinocchio::dDifference(model,qs,qs2,results_J[1], pinocchio::ARG0);
+  cg_dDifference.evalFunction(q1,q2, results_J[0], pinocchio::ARG0);
+  pinocchio::dDifference(model,q1,q2,results_J[1], pinocchio::ARG0);
   BOOST_CHECK(results_J[1].isApprox(results_J[0]));
 
   //ARG1
-  cg_dDifference.evalFunction(qs,qs2, results_J[0], pinocchio::ARG1);
-  pinocchio::dDifference(model,qs,qs2,results_J[1], pinocchio::ARG1);
+  cg_dDifference.evalFunction(q1,q2, results_J[0], pinocchio::ARG1);
+  pinocchio::dDifference(model,q1,q2,results_J[1], pinocchio::ARG1);
   BOOST_CHECK(results_J[1].isApprox(results_J[0]));
+  
+  //ARG0
+  cg_dDifference.evalFunction(q1,q1,results_J[0],pinocchio::ARG0);
+  BOOST_CHECK((-results_J[0]).isIdentity());
+
+  //ARG1
+  cg_dDifference.evalFunction(q1,q1,results_J[0],pinocchio::ARG1);
+  BOOST_CHECK(results_J[0].isIdentity());
   
 }
 
