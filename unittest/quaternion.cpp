912156a56dd0686d22d8df5268e7be1500b43309
Justin Carpentier <justin.carpentier@inria.fr>
2020-04-24 15:59:38 +0200

    test/math: test isNormalized for Quaternion

--- unittest/quaternion.cpp
+++ unittest/quaternion.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2019 INRIA
+// Copyright (c) 2019-2020 INRIA CNRS
 //
 
 #include <pinocchio/math/quaternion.hpp>
@@ -39,6 +39,25 @@ BOOST_AUTO_TEST_CASE(test_uniformRandom)
     quaternion::uniformRandom(q);
     BOOST_CHECK_MESSAGE((q.coeffs().array().abs() <= 1).all(),
         "Quaternion coeffs out of bounds: " << i << ' ' << q.coeffs().transpose());
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_isNormalized)
+{
+  srand(0);
+
+  using namespace pinocchio;
+  typedef Eigen::Quaternion<double> Quaternion;
+  typedef Quaternion::Coefficients Vector4;
+  
+  for(int i = 0; i < 1e6; ++i)
+  {
+    Quaternion q;
+    q.coeffs() = Vector4::Random() + Vector4::Constant(2);
+    BOOST_CHECK(!quaternion::isNormalized(q));
+    
+    q.normalize();
+    BOOST_CHECK(quaternion::isNormalized(q));
   }
 }
 
