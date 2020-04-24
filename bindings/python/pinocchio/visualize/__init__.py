870fb1edf76154bc76a54c001fe227b32b54a2d8
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-04-24 18:22:46 +0100

    [visualize] Fixed installation of panda3d

--- bindings/python/pinocchio/visualize/__init__.py
+++ bindings/python/pinocchio/visualize/__init__.py
@@ -2,3 +2,4 @@
 from .base_visualizer import BaseVisualizer
 from .gepetto_visualizer import GepettoVisualizer
 from .meshcat_visualizer import MeshcatVisualizer
+from .panda3d_visualizer import Panda3dVisualizer
