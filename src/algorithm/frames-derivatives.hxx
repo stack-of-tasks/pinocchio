7b07e5351f996a62e74331be6fb9b8f644d03e6a
Justin Carpentier <justin.carpentier@inria.fr>
2020-08-05 17:03:21 +0200

    algo: use FrameIndex directly

--- src/algorithm/frames-derivatives.hxx
+++ src/algorithm/frames-derivatives.hxx
@@ -15,7 +15,7 @@ namespace pinocchio
   void
   getFrameVelocityDerivatives(const ModelTpl<Scalar,Options,JointCollectionTpl> & model,
                               DataTpl<Scalar,Options,JointCollectionTpl> & data,
-                              const typename ModelTpl<Scalar,Options,JointCollectionTpl>::FrameIndex frame_id,
+                              const FrameIndex frame_id,
                               const ReferenceFrame rf,
                               const Eigen::MatrixBase<Matrix6xOut1> & v_partial_dq,
                               const Eigen::MatrixBase<Matrix6xOut2> & v_partial_dv)
@@ -91,7 +91,7 @@ namespace pinocchio
   void
   getFrameAccelerationDerivatives(const ModelTpl<Scalar,Options,JointCollectionTpl> & model,
                                   DataTpl<Scalar,Options,JointCollectionTpl> & data,
-                                  const typename ModelTpl<Scalar,Options,JointCollectionTpl>::FrameIndex frame_id,
+                                  const FrameIndex frame_id,
                                   const ReferenceFrame rf,
                                   const Eigen::MatrixBase<Matrix6xOut1> & v_partial_dq,
                                   const Eigen::MatrixBase<Matrix6xOut2> & a_partial_dq,
