74b55840139c29fd49340a6d4817a38031003dc4
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-08-14 00:04:20 +0200

    [unittest/impulse-dynamics-derivatives] add local_world_aligned and world unittests

--- unittest/impulse-dynamics-derivatives.cpp
+++ unittest/impulse-dynamics-derivatives.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2019-2020 INRIA
+// Copyright (c) 2020 LAAS-CNRS, INRIA
 //
 
 #include "pinocchio/algorithm/jacobian.hpp"
@@ -143,14 +143,13 @@ BOOST_AUTO_TEST_CASE(test_sparse_impulse_dynamics_derivatives)
   model.gravity = gravity_bk;
 
   BOOST_CHECK(data.dac_da.isApprox(Jc));
-  BOOST_CHECK(data.dtau_dq.isApprox(data_ref.dtau_dq-Jc.transpose()*data.dlambda_dq));
   BOOST_CHECK((data.dvc_dq-(dv_dq-Jc*data.Minv*data_ref.dtau_dq)).norm()<=1e-12);
 
   BOOST_CHECK((data.dlambda_dv+(1+r_coeff)*data.osim*Jc).norm()<=1e-12);
   
 }
 
-BOOST_AUTO_TEST_CASE ( test_impulse_dynamics_derivatives_fd )
+BOOST_AUTO_TEST_CASE ( test_impulse_dynamics_derivatives_WORLD_fd )
 {
   using namespace Eigen;
   using namespace pinocchio;
@@ -200,43 +199,218 @@ BOOST_AUTO_TEST_CASE ( test_impulse_dynamics_derivatives_fd )
   MatrixXd impulse_partial_dq_fd(constraint_dim,model.nv); impulse_partial_dq_fd.setZero();
   MatrixXd impulse_partial_dv_fd(constraint_dim,model.nv); impulse_partial_dv_fd.setZero();
 
-  const VectorXd ddv0 = impulseDynamics(model,data_fd,q,v,contact_models,
+  const VectorXd dqafter0 = impulseDynamics(model,data_fd,q,v,contact_models,
                                         contact_data,r_coeff,mu0);
   const VectorXd impulse0 = data_fd.impulse_c;
   VectorXd v_eps(VectorXd::Zero(model.nv));
   VectorXd q_plus(model.nq);
-  VectorXd ddv_plus(model.nv);
+  VectorXd dqafter_plus(model.nv);
 
-  VectorXd impulse_plus(constraint_dim);
-  
+  VectorXd impulse_plus(constraint_dim);  
   const double alpha = 1e-8;
   for(int k = 0; k < model.nv; ++k)
   {
     v_eps[k] += alpha;
     q_plus = integrate(model,q,v_eps);
-    ddv_plus = impulseDynamics(model,data_fd,q_plus,v,contact_models,contact_data,r_coeff,mu0);
-    dqafter_partial_dq_fd.col(k) = (ddv_plus - ddv0)/alpha;
+    dqafter_plus = impulseDynamics(model,data_fd,q_plus,v,contact_models,contact_data,r_coeff,mu0);
+    dqafter_partial_dq_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
     impulse_partial_dq_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
 
     v_eps[k] = 0.;
   }
-  
+
   BOOST_CHECK(dqafter_partial_dq_fd.isApprox(data.ddq_dq,sqrt(alpha)));
   BOOST_CHECK(impulse_partial_dq_fd.isApprox(data.dlambda_dq,sqrt(alpha)));
-
-  BOOST_CHECK(dqafter_partial_dq_fd.isApprox(-data.Minv*(data.dtau_dq + data.dac_da.transpose()*(data.dlambda_dq - impulse_partial_dq_fd)),sqrt(alpha)));
   
   VectorXd v_plus(v);
   for(int k = 0; k < model.nv; ++k)
   {
     v_plus[k] += alpha;
-    ddv_plus = impulseDynamics(model,data_fd,q,v_plus,contact_models,contact_data,r_coeff,mu0);
+    dqafter_plus = impulseDynamics(model,data_fd,q,v_plus,contact_models,contact_data,r_coeff,mu0);
+    dqafter_partial_dv_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
+    impulse_partial_dv_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
+    v_plus[k] -= alpha;
+  }
 
-    dqafter_partial_dv_fd.col(k) = (ddv_plus - ddv0)/alpha;
+  BOOST_CHECK(dqafter_partial_dv_fd.isApprox(Eigen::MatrixXd::Identity(model.nv,model.nv)+data.ddq_dv,sqrt(alpha)));
+  BOOST_CHECK(impulse_partial_dv_fd.isApprox(data.dlambda_dv,sqrt(alpha)));
+  
+}
+
+BOOST_AUTO_TEST_CASE ( test_impulse_dynamics_derivatives_LOCAL_fd )
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+
+  Model model;
+  buildModels::humanoidRandom(model,true);
+  Data data(model), data_fd(model);
+
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+
+  VectorXd v = VectorXd::Random(model.nv);
+
+  const std::string RF = "rleg6_joint";
+  //  const Model::JointIndex RF_id = model.getJointId(RF);
+  const std::string LF = "lleg6_joint";
+  //  const Model::JointIndex LF_id = model.getJointId(LF);
+
+  // Contact models and data
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidContactModel) contact_models;
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidContactData) contact_data;
+
+  RigidContactModel ci_LF(CONTACT_6D,model.getFrameId(LF),LOCAL);
+  RigidContactModel ci_RF(CONTACT_3D,model.getFrameId(RF),LOCAL);
+
+  contact_models.push_back(ci_LF); contact_data.push_back(RigidContactData(ci_LF));
+  contact_models.push_back(ci_RF); contact_data.push_back(RigidContactData(ci_RF));
+
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_models.size(); ++k)
+    constraint_dim += contact_models[k].size();
+
+  const double mu0 = 0.;
+  const double r_coeff = 0.5;
+
+  initContactDynamics(model,data,contact_models);
+  impulseDynamics(model,data,q,v,contact_models,contact_data,r_coeff,mu0);
+  computeImpulseDynamicsDerivatives(model, data, contact_models, contact_data, r_coeff, mu0);
+  
+  //Data_fd
+  initContactDynamics(model,data_fd,contact_models);
+
+  MatrixXd dqafter_partial_dq_fd(model.nv,model.nv); dqafter_partial_dq_fd.setZero();
+  MatrixXd dqafter_partial_dv_fd(model.nv,model.nv); dqafter_partial_dv_fd.setZero();
+
+  MatrixXd impulse_partial_dq_fd(constraint_dim,model.nv); impulse_partial_dq_fd.setZero();
+  MatrixXd impulse_partial_dv_fd(constraint_dim,model.nv); impulse_partial_dv_fd.setZero();
+
+  const VectorXd dqafter0 = impulseDynamics(model,data_fd,q,v,contact_models,
+                                        contact_data,r_coeff,mu0);
+  const VectorXd impulse0 = data_fd.impulse_c;
+  VectorXd v_eps(VectorXd::Zero(model.nv));
+  VectorXd q_plus(model.nq);
+  VectorXd dqafter_plus(model.nv);
+
+  VectorXd impulse_plus(constraint_dim);  
+  const double alpha = 1e-8;
+  for(int k = 0; k < model.nv; ++k)
+  {
+    v_eps[k] += alpha;
+    q_plus = integrate(model,q,v_eps);
+    dqafter_plus = impulseDynamics(model,data_fd,q_plus,v,contact_models,contact_data,r_coeff,mu0);
+    dqafter_partial_dq_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
+    impulse_partial_dq_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
+
+    v_eps[k] = 0.;
+  }
+
+  BOOST_CHECK(dqafter_partial_dq_fd.isApprox(data.ddq_dq,sqrt(alpha)));
+  BOOST_CHECK(impulse_partial_dq_fd.isApprox(data.dlambda_dq,sqrt(alpha)));
+  
+  VectorXd v_plus(v);
+  for(int k = 0; k < model.nv; ++k)
+  {
+    v_plus[k] += alpha;
+    dqafter_plus = impulseDynamics(model,data_fd,q,v_plus,contact_models,contact_data,r_coeff,mu0);
+
+    dqafter_partial_dv_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
+    impulse_partial_dv_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
+    v_plus[k] -= alpha;
+  }
+
+  BOOST_CHECK(dqafter_partial_dv_fd.isApprox(Eigen::MatrixXd::Identity(model.nv,model.nv)+data.ddq_dv,sqrt(alpha)));
+  BOOST_CHECK(impulse_partial_dv_fd.isApprox(data.dlambda_dv,sqrt(alpha)));  
+}
+
+BOOST_AUTO_TEST_CASE ( test_impulse_dynamics_derivatives_LOCAL_WORLD_ALIGNED_fd )
+{
+  using namespace Eigen;
+  using namespace pinocchio;
+
+  Model model;
+  buildModels::humanoidRandom(model,true);
+  Data data(model), data_fd(model);
+
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill( 1.);
+  VectorXd q = randomConfiguration(model);
+
+  VectorXd v = VectorXd::Random(model.nv);
+
+  const std::string RF = "rleg6_joint";
+  //  const Model::JointIndex RF_id = model.getJointId(RF);
+  const std::string LF = "lleg6_joint";
+  //  const Model::JointIndex LF_id = model.getJointId(LF);
+
+  // Contact models and data
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidContactModel) contact_models;
+  PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidContactData) contact_data;
+
+  RigidContactModel ci_LF(CONTACT_6D,model.getFrameId(LF),LOCAL_WORLD_ALIGNED);
+  RigidContactModel ci_RF(CONTACT_3D,model.getFrameId(RF),LOCAL_WORLD_ALIGNED);
+
+  contact_models.push_back(ci_LF); contact_data.push_back(RigidContactData(ci_LF));
+  contact_models.push_back(ci_RF); contact_data.push_back(RigidContactData(ci_RF));
+
+  Eigen::DenseIndex constraint_dim = 0;
+  for(size_t k = 0; k < contact_models.size(); ++k)
+    constraint_dim += contact_models[k].size();
+
+  const double mu0 = 0.;
+  const double r_coeff = 0.5;
+
+  initContactDynamics(model,data,contact_models);
+  impulseDynamics(model,data,q,v,contact_models,contact_data,r_coeff,mu0);
+  computeImpulseDynamicsDerivatives(model, data, contact_models, contact_data, r_coeff, mu0);
+  
+  //Data_fd
+  initContactDynamics(model,data_fd,contact_models);
+
+  MatrixXd dqafter_partial_dq_fd(model.nv,model.nv); dqafter_partial_dq_fd.setZero();
+  MatrixXd dqafter_partial_dv_fd(model.nv,model.nv); dqafter_partial_dv_fd.setZero();
+
+  MatrixXd impulse_partial_dq_fd(constraint_dim,model.nv); impulse_partial_dq_fd.setZero();
+  MatrixXd impulse_partial_dv_fd(constraint_dim,model.nv); impulse_partial_dv_fd.setZero();
+
+  const VectorXd dqafter0 = impulseDynamics(model,data_fd,q,v,contact_models,
+                                        contact_data,r_coeff,mu0);
+  const VectorXd impulse0 = data_fd.impulse_c;
+  VectorXd v_eps(VectorXd::Zero(model.nv));
+  VectorXd q_plus(model.nq);
+  VectorXd dqafter_plus(model.nv);
+
+  VectorXd impulse_plus(constraint_dim);  
+  const double alpha = 1e-8;
+  for(int k = 0; k < model.nv; ++k)
+  {
+    v_eps[k] += alpha;
+    q_plus = integrate(model,q,v_eps);
+    dqafter_plus = impulseDynamics(model,data_fd,q_plus,v,contact_models,contact_data,r_coeff,mu0);
+    dqafter_partial_dq_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
+    impulse_partial_dq_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
+    v_eps[k] = 0.;
+  }
+
+  BOOST_CHECK(dqafter_partial_dq_fd.isApprox(data.ddq_dq,sqrt(alpha)));
+  BOOST_CHECK(impulse_partial_dq_fd.isApprox(data.dlambda_dq,sqrt(alpha)));
+  
+  VectorXd v_plus(v);
+  for(int k = 0; k < model.nv; ++k)
+  {
+    v_plus[k] += alpha;
+    dqafter_plus = impulseDynamics(model,data_fd,q,v_plus,contact_models,contact_data,r_coeff,mu0);
+    dqafter_partial_dv_fd.col(k) = (dqafter_plus - dqafter0)/alpha;
+    impulse_partial_dv_fd.col(k) = (data_fd.impulse_c - impulse0)/alpha;
     v_plus[k] -= alpha;
   }
   
   BOOST_CHECK(dqafter_partial_dv_fd.isApprox(Eigen::MatrixXd::Identity(model.nv,model.nv)+data.ddq_dv,sqrt(alpha)));
+  BOOST_CHECK(impulse_partial_dv_fd.isApprox(data.dlambda_dv,sqrt(alpha)));  
 }
+
+
 
 BOOST_AUTO_TEST_SUITE_END ()
