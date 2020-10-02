fa693f2f79d2baa59cd90e520ea85b3209f71556
Justin Carpentier <justin.carpentier@inria.fr>
2020-10-02 12:17:35 +0200

    core: handle friction and damping parameters when adding joint

--- src/multibody/model.hpp
+++ src/multibody/model.hpp
@@ -354,38 +354,6 @@ namespace pinocchio
     ///
     bool operator!=(const ModelTpl & other) const
     { return !(*this == other); }
-    
-    ///
-    /// \brief Add a joint to the kinematic tree with given bounds.
-    ///
-    /// \remark This method does not add a Frame of same name to the vector of frames.
-    ///         Use Model::addJointFrame.
-    /// \remark The inertia supported by the joint is set to Zero.
-    ///
-    /// \tparam JointModelDerived The type of the joint model.
-    ///
-    /// \param[in] parent Index of the parent joint.
-    /// \param[in] joint_model The joint model.
-    /// \param[in] joint_placement Placement of the joint inside its parent joint.
-    /// \param[in] joint_name Name of the joint. If empty, the name is random.
-    /// \param[in] max_effort Maximal joint torque.
-    /// \param[in] max_velocity Maximal joint velocity.
-    /// \param[in] min_config Lower joint configuration.
-    /// \param[in] max_config Upper joint configuration.
-    ///
-    /// \return The index of the new joint.
-    ///
-    /// \sa Model::appendBodyToJoint, Model::addJointFrame
-    ///
-    JointIndex addJoint(const JointIndex parent,
-                        const JointModel & joint_model,
-                        const SE3 & joint_placement,
-                        const std::string & joint_name,
-                        const VectorXs & max_effort,
-                        const VectorXs & max_velocity,
-                        const VectorXs & min_config,
-                        const VectorXs & max_config
-                        );
 
     ///
     /// \brief Add a joint to the kinematic tree with infinite bounds.
@@ -408,6 +376,40 @@ namespace pinocchio
                         const JointModel & joint_model,
                         const SE3 & joint_placement,
                         const std::string & joint_name);
+    
+    ///
+    /// \copydoc ModelTpl::addJoint(const JointIndex,const JointModel &,const SE3 &,const std::string &)
+    ///
+    /// \param[in] max_effort Maximal joint torque.
+    /// \param[in] max_velocity Maximal joint velocity.
+    /// \param[in] min_config Lower joint configuration.
+    /// \param[in] max_config Upper joint configuration.
+    ///
+    JointIndex addJoint(const JointIndex parent,
+                        const JointModel & joint_model,
+                        const SE3 & joint_placement,
+                        const std::string & joint_name,
+                        const VectorXs & max_effort,
+                        const VectorXs & max_velocity,
+                        const VectorXs & min_config,
+                        const VectorXs & max_config);
+    
+    ///
+    /// \copydoc ModelTpl::addJoint(const JointIndex,const JointModel &,const SE3 &,const std::string &,const VectorXs &,const VectorXs &,const VectorXs &,const VectorXs &)
+    ///
+    /// \param[in] friction Joint friction parameters.
+    /// \param[in] damping Joint damping parameters.
+    ///
+    JointIndex addJoint(const JointIndex parent,
+                        const JointModel & joint_model,
+                        const SE3 & joint_placement,
+                        const std::string & joint_name,
+                        const VectorXs & max_effort,
+                        const VectorXs & max_velocity,
+                        const VectorXs & min_config,
+                        const VectorXs & max_config,
+                        const VectorXs & friction,
+                        const VectorXs & damping);
 
     ///
     /// \brief Add a joint to the frame tree.
