642a714536d4893fe46b667bdf06e6e8e6ac3ef8
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2020-10-15 00:17:15 +0200

    [unittest] Test isNormalized

--- unittest/joint-configurations.cpp
+++ unittest/joint-configurations.cpp
@@ -362,6 +362,26 @@ BOOST_AUTO_TEST_CASE ( normalize_test )
   BOOST_CHECK(q.tail(n).isApprox(Eigen::VectorXd::Ones(n)));
 }
 
+BOOST_AUTO_TEST_CASE ( is_normalized_test )
+{
+  Model model; buildAllJointsModel(model);
+
+  Eigen::VectorXd q;
+  q = Eigen::VectorXd::Ones(model.nq);
+  BOOST_CHECK(!pinocchio::isNormalized(model, q));
+
+  pinocchio::normalize(model, q);
+  BOOST_CHECK(pinocchio::isNormalized(model, q));
+
+  q = pinocchio::neutral(model);
+  BOOST_CHECK(pinocchio::isNormalized(model, q));
+
+  model.lowerPositionLimit = -1 * Eigen::VectorXd::Ones(model.nq);
+  model.upperPositionLimit = Eigen::VectorXd::Ones(model.nq);
+  q = pinocchio::randomConfiguration(model);
+  BOOST_CHECK(pinocchio::isNormalized(model, q));
+}
+
 BOOST_AUTO_TEST_CASE ( integrateCoeffWiseJacobian_test )
 {
   Model model; buildModels::humanoidRandom(model);
