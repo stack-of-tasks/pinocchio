eae7000a4aaa4cd398f5d29cb1d203fa8459b139
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2020-10-20 11:53:01 +0200

    [unittest] Improve tests for isNormalized

--- unittest/joint-configurations.cpp
+++ unittest/joint-configurations.cpp
@@ -369,17 +369,22 @@ BOOST_AUTO_TEST_CASE ( is_normalized_test )
   Eigen::VectorXd q;
   q = Eigen::VectorXd::Ones(model.nq);
   BOOST_CHECK(!pinocchio::isNormalized(model, q));
+  BOOST_CHECK(!pinocchio::isNormalized(model, q, 1e-8));
+  BOOST_CHECK(pinocchio::isNormalized(model, q, 1e2));
 
   pinocchio::normalize(model, q);
   BOOST_CHECK(pinocchio::isNormalized(model, q));
+  BOOST_CHECK(pinocchio::isNormalized(model, q, 1e-8));
 
   q = pinocchio::neutral(model);
   BOOST_CHECK(pinocchio::isNormalized(model, q));
+  BOOST_CHECK(pinocchio::isNormalized(model, q, 1e-8));
 
   model.lowerPositionLimit = -1 * Eigen::VectorXd::Ones(model.nq);
   model.upperPositionLimit = Eigen::VectorXd::Ones(model.nq);
   q = pinocchio::randomConfiguration(model);
   BOOST_CHECK(pinocchio::isNormalized(model, q));
+  BOOST_CHECK(pinocchio::isNormalized(model, q, 1e-8));
 }
 
 BOOST_AUTO_TEST_CASE ( integrateCoeffWiseJacobian_test )
