5466a09c43cafc286c73fe6b321caeea7cf7dd4f
Justin Carpentier <justin.carpentier@inria.fr>
2020-12-18 16:16:10 +0100

    python: enhance doc

--- bindings/python/spatial/expose-explog.cpp
+++ bindings/python/spatial/expose-explog.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2019 CNRS INRIA
+// Copyright (c) 2015-2020 CNRS INRIA
 // Copyright (c) 2015 Wandercraft, 86 rue de Paris 91400 Orsay, France.
 //
 
@@ -16,57 +16,57 @@ namespace pinocchio
     {
       
       bp::def("exp3",&exp3_proxy<Eigen::Vector3d>,
-              bp::arg("Angular velocity (vector of size 3)"),
+              bp::arg("w"),
               "Exp: so3 -> SO3. Return the integral of the input"
-              " angular velocity during time 1.");
+              " vector w during time 1. This is also known as the Rodrigues formula.");
       
       bp::def("Jexp3",&Jexp3_proxy<Eigen::Vector3d>,
-              bp::arg("v: Angular velocity (vector of size 3)"),
-              "Jacobian of exp(R) which maps from the tangent of SO(3) at exp(v) to"
+              bp::arg("w"),
+              "Jacobian of exp(v) which maps from the tangent of SO(3) at R = exp(v) to"
               " the tangent of SO(3) at Identity.");
       
       bp::def("log3",&log3_proxy<Eigen::Matrix3d>,
-              bp::arg("Rotation matrix (matrix of size 3x3))"),
-              "Log: SO3 -> so3. Pseudo-inverse of log from SO3"
-              " -> { v in so3, ||v|| < 2pi }.Exp: so3 -> SO3.");
+              bp::arg("R"),
+              "Log: SO3 -> so3 is the pseudo-inverse of Exp: so3 -> SO3. Log maps from SO3"
+              " -> { v in so3, ||v|| < 2pi }.");
       
       bp::def("Jlog3",&Jlog3_proxy<Eigen::Matrix3d>,
-              bp::arg("Rotation matrix R (matrix of size 3x3)"),
+              bp::arg("R"),
               "Jacobian of log(R) which maps from the tangent of SO(3) at R to"
               " the tangent of SO(3) at Identity.");
       
       bp::def("exp6",&exp6_proxy<double,0>,
-              bp::arg("Spatial velocity (Motion)"),
+              bp::arg("motion"),
               "Exp: se3 -> SE3. Return the integral of the input"
               " spatial velocity during time 1.");
               
       bp::def("exp6",&exp6_proxy<Motion::Vector6>,
-              bp::arg("Spatial velocity (vector 6x1)"),
+              bp::arg("v"),
               "Exp: se3 -> SE3. Return the integral of the input"
               " spatial velocity during time 1.");
       
       bp::def("Jexp6",&Jexp6_proxy<double,0>,
-              bp::arg("v: Spatial velocity (Motion)"),
+              bp::arg("motion"),
               "Jacobian of exp(v) which maps from the tangent of SE(3) at exp(v) to"
               " the tangent of SE(3) at Identity.");
               
       bp::def("Jexp6",&Jexp6_proxy<Motion::Vector6>,
-              bp::arg("v: Spatial velocity (vector 6x1)"),
+              bp::arg("v"),
               "Jacobian of exp(v) which maps from the tangent of SE(3) at exp(v) to"
               " the tangent of SE(3) at Identity.");
       
       bp::def("log6",(Motion (*)(const SE3 &))&log6<double,0>,
-              bp::arg("Spatial transform (SE3)"),
+              bp::arg("M"),
               "Log: SE3 -> se3. Pseudo-inverse of exp from SE3"
               " -> { v,w in se3, ||w|| < 2pi }.");
       
       bp::def("log6",&log6_proxy<Eigen::Matrix4d>,
-              bp::arg("Homegenious matrix (matrix 4x4)"),
-              "Log: SE3 -> se3. Pseudo-inverse of exp from SE3"
+              bp::arg("homegeneous_matrix"),
+              "Log: SE3 -> se3. Pseudo-inverse of Exp: so3 -> SO3. Log maps from SE3"
               " -> { v,w in se3, ||w|| < 2pi }.");
       
       bp::def("Jlog6",&Jlog6_proxy<double,0>,
-              bp::arg("Spatial transform M (SE3)"),
+              bp::arg("M"),
               "Jacobian of log(M) which maps from the tangent of SE(3) at M to"
               " the tangent of SE(3) at Identity.");
       
