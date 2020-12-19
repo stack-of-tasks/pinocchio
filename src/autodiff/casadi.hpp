c5bdf9be3eb7cffcf628edcfeebaf92a07aa77a9
Justin Carpentier <justin.carpentier@inria.fr>
2020-12-19 17:41:46 +0100

    casadi: add overload for min

--- src/autodiff/casadi.hpp
+++ src/autodiff/casadi.hpp
@@ -44,6 +44,7 @@ namespace pinocchio
     }
     
   };
+
 }
 
 namespace Eigen
@@ -181,7 +182,7 @@ namespace pinocchio
   } // namespace casadi
 } // namespace pinocchio
 
-// Overloading of max operator
+// Overloading of min/max operator
 namespace pinocchio
 {
   namespace math
@@ -189,7 +190,51 @@ namespace pinocchio
     namespace internal
     {
       template<typename Scalar>
-      struct return_type_max< ::casadi::Matrix<Scalar>,::casadi::Matrix<Scalar>>
+      struct return_type_min< ::casadi::Matrix<Scalar>,::casadi::Matrix<Scalar> >
+      {
+        typedef ::casadi::Matrix<Scalar> type;
+      };
+      
+      template<typename Scalar, typename T>
+      struct return_type_min< ::casadi::Matrix<Scalar>,T>
+      {
+        typedef ::casadi::Matrix<Scalar> type;
+      };
+      
+      template<typename Scalar, typename T>
+      struct return_type_min<T,::casadi::Matrix<Scalar> >
+      {
+        typedef ::casadi::Matrix<Scalar> type;
+      };
+      
+      template<typename Scalar>
+      struct call_min< ::casadi::Matrix<Scalar>,::casadi::Matrix<Scalar> >
+      {
+        static inline ::casadi::Matrix<Scalar> run(const ::casadi::Matrix<Scalar> & a,
+                                                   const ::casadi::Matrix<Scalar> & b)
+        { return fmin(a,b); }
+      };
+      
+      template<typename S1, typename S2>
+      struct call_min< ::casadi::Matrix<S1>,S2>
+      {
+        typedef ::casadi::Matrix<S1> CasadiType;
+        static inline ::casadi::Matrix<S1> run(const ::casadi::Matrix<S1> & a,
+                                               const S2 & b)
+        { return fmin(a,static_cast<CasadiType>(b)); }
+      };
+      
+      template<typename S1, typename S2>
+      struct call_min<S1,::casadi::Matrix<S2>>
+      {
+        typedef ::casadi::Matrix<S2> CasadiType;
+        static inline ::casadi::Matrix<S2> run(const S1 & a,
+                                               const ::casadi::Matrix<S2> & b)
+        { return fmin(static_cast<CasadiType>(a),b); }
+      };
+    
+      template<typename Scalar>
+      struct return_type_max< ::casadi::Matrix<Scalar>,::casadi::Matrix<Scalar> >
       {
         typedef ::casadi::Matrix<Scalar> type;
       };
