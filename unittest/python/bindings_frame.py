e126e5ea16f7ae66f2220a68b67f82896be05f2a
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2020-12-23 00:13:27 +0100

    [bindings] Test frame equality

--- unittest/python/bindings_frame.py
+++ unittest/python/bindings_frame.py
@@ -44,6 +44,15 @@ class TestFrameBindings(PinocchioTestCase):
         f.placement = new_placement
         self.assertTrue(np.allclose(f.placement.homogeneous, new_placement.homogeneous))
 
+    def test_frame_equality(self):
+        M = pin.SE3.Random()
+        frame1 = pin.Frame("name", 1, 2, M, pin.OP_FRAME)
+        frame2 = pin.Frame("name", 1, 2, M, pin.OP_FRAME)
+        frame3 = pin.Frame("othername", 3, 4, pin.SE3.Random(), pin.BODY)
+
+        self.assertTrue(frame1 == frame2)
+        self.assertTrue(frame1 != frame3)
+
     def test_getters(self):
         data = self.model.createData()
         q = pin.randomConfiguration(self.model)
