180bccb55a0f1375dc3f5a745026fa45bff1f49a
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-01-03 18:29:01 +0100

    [binding][sdf] add sdf python bindings

---
+++ bindings/python/parsers/sdf.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2020 CNRS
+//
+
+#ifndef __pinocchio_python_parsers_sdf_hpp__
+#define __pinocchio_python_parsers_sdf_hpp__
+
+namespace pinocchio
+{
+  namespace python
+  {
+    void exposeSDFModel();
+  
+    inline void exposeSDFParser()
+    {
+      exposeSDFModel();
+    }
+  
+  }
+}
+
+#endif // ifndef __pinocchio_python_parsers_urdf_hpp__
---
+++ bindings/python/parsers/sdf/model.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 22020 CNRS
+//
+
+#include "pinocchio/parsers/sdf.hpp"
+#include "pinocchio/bindings/python/parsers/sdf.hpp"
+
+#include <boost/python.hpp>
+#include <boost/python/tuple.hpp>
+
+namespace pinocchio
+{
+  namespace python
+  {
+
+    namespace bp = boost::python;
+
+    bp::tuple buildModelFromSdf(const std::string & filename)
+    {
+      Model model;
+      PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidContactModel) contact_models;
+      ::pinocchio::sdf::buildModel(filename, model, contact_models);
+      return bp::make_tuple(model,contact_models);
+    }
+  
+    void exposeSDFModel()
+    {
+      bp::def("buildModelFromSdf",
+              static_cast <bp::tuple (*) (const std::string &)> (pinocchio::python::buildModelFromSdf),
+              bp::args("sdf_filename"),
+              "Parse the SDF file given in input and return a pinocchio Model."
+              );
+    }
+  }
+}
