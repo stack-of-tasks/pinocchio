c1aaab29da495b61b2a7e6a5f6e2fbe69cc4df36
Justin Carpentier <justin.carpentier@inria.fr>
2021-01-04 10:14:28 +0100

    python: start adding bindings for CppAD

---
+++ bindings/python/context/cppad.hpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2021 INRIA
+//
+
+#ifndef __pinocchio_python_context_cppad_hpp__
+#define __pinocchio_python_context_cppad_hpp__
+
+#include "pinocchio/autodiff/cppad.hpp"
+
+#define PINOCCHIO_PYTHON_SCALAR_TYPE ::CppAD::AD<double>
+#include "pinocchio/bindings/python/context/generic.hpp"
+#undef PINOCCHIO_PYTHON_SCALAR_TYPE
+
+#define PINOCCHIO_PYTHON_SKIP_COMPARISON_OPERATIONS
+#define PINOCCHIO_PYTHON_NO_SERIALIZATION
+
+#include <eigenpy/eigenpy.hpp>
+
+namespace pinocchio { namespace python { namespace internal {
+  
+  template<typename T> struct has_operator_equal;
+  
+  template<typename Scalar>
+  struct has_operator_equal< ::CppAD::AD<Scalar> > : boost::false_type
+  {};
+}}}
+
+#endif // #ifndef __pinocchio_python_context_cppad_hpp__
