08661e1210ae614d571724c71eb5b85c18cf23e3
Justin Carpentier <justin.carpentier@inria.fr>
2021-01-07 17:48:57 +0100

    example: add contact cholesky calc

---
+++ examples/contact-cholesky.py
@@ -0,0 +1,49 @@
+import pinocchio as pin 
+import numpy as np
+from pinocchio import SE3 
+
+import time
+
+from numpy.linalg import norm, inv 
+from os.path import join, dirname, abspath
+from math import sqrt
+#import example_robot_data as erd 
+
+pin.seed(int(time.time()))
+
+def normInf(x):
+    return norm(x,np.inf)
+
+pinocchio_model_dir = join(dirname(dirname(str(abspath(__file__)))), "models")
+urdf_filename = pinocchio_model_dir + '/others/robots/anymal_b_simple_description/robots/anymal.urdf' 
+# anymal = erd.loadANYmal()
+# model = anymal.model
+model = pin.buildModelFromUrdf(urdf_filename)
+data = model.createData()
+
+#q0 = model.referenceConfigurations["standing"]
+q0 = pin.neutral(model)
+
+# Add feet frames
+feet_names = ["LH_FOOT","RH_FOOT","LF_FOOT","RF_FOOT"]
+feet_frame_ids = []
+for foot_name in feet_names:
+
+    frame_id = model.getFrameId(foot_name)    
+    feet_frame_ids.append(frame_id)
+
+contact_models = []
+for fid in feet_frame_ids:
+    frame = model.frames[fid]
+    cmodel = pin.RigidContactModel(pin.ContactType.CONTACT_3D,frame.parent,frame.placement,pin.LOCAL_WORLD_ALIGNED)
+    contact_models.append(cmodel)
+
+contact_data = [cmodel.createData() for cmodel in contact_models]
+
+pin.initContactDynamics(model,data,contact_models)
+pin.crba(model,data,q0)
+
+data.contact_chol.compute(model,data,contact_models,contact_data)
+
+delassus_matrix = data.contact_chol.getInverseOperationalSpaceInertiaMatrix()
+delassus_matrix_inv = data.contact_chol.getOperationalSpaceInertiaMatrix()
