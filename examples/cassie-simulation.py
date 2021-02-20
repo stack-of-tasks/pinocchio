40caaf1fb9d791f2b9a796a0ffae7beb31f3bee7
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-02-20 16:21:52 +0100

    [cassie-simulation] com sinusoidal motion

--- examples/cassie-simulation.py
+++ examples/cassie-simulation.py
@@ -21,6 +21,9 @@ q0[17] = -1.398
 q0[28] = -1.398
 q0[26] = -1.398; q0[27] = -q0[26]
 q0[15] = -1.398; q0[16] = -q0[15]
+
+model.q0 = q0.copy()
+
 data = pinocchio.Data(model)
 
 pinocchio.forwardKinematics(model, data, q0)
@@ -97,61 +100,38 @@ def check_joints():
             go_on = query_yes_no("Continue?")
 
 q = q0.copy()
-mu = 1e-4
+raw_input()
+
 pinocchio.computeAllTerms(model,data,q,np.zeros(model.nv))
 kkt_constraint = pinocchio.ContactCholeskyDecomposition(model,constraint_models)
 constraint_dim = sum([cm.size() for cm in constraint_models])
-y = np.ones((constraint_dim))
-N=1000
+N=100000
 eps = 1e-10
-
-pinocchio.computeJointJacobians(model,data,q)
-kkt_constraint.compute(model, data, constraint_models, constraint_datas, mu)
-constraint_value = np.concatenate([cd.c1Mc2.translation for cd in constraint_datas])
-raw_input()
-
-
-
-for k in range(N):
-    pinocchio.computeJointJacobians(model,data,q)
-    kkt_constraint.compute(model, data, constraint_models, constraint_datas, mu)
-    constraint_value = np.concatenate([cd.c1Mc2.translation for cd in constraint_datas])
-    J = np.vstack([pinocchio.getFrameJacobian(model,data,cm.joint1_id,
-                                              cm.joint1_placement,
-                                              cm.reference_frame)[:3,:] for cm in constraint_models])
-    primal_feas = np.linalg.norm(constraint_value,np.inf)
-    dual_feas = np.linalg.norm(J.T.dot(constraint_value + y),np.inf)
-    if primal_feas < eps and dual_feas < eps:
-        print("Convergence achieved")
-        break
-    print("constraint_value:",np.linalg.norm(constraint_value))
-    
-    rhs = np.concatenate([-constraint_value - y*mu, np.zeros(model.nv)])
-    dz = kkt_constraint.solve(rhs) 
-    dy = dz[:constraint_dim]
-    dq = dz[constraint_dim:]
-    
-    alpha = 1.
-    q = pinocchio.integrate(model,q,-alpha*dq)
-    y -= alpha*(-dy + y)
-    viz.display(q)
-    #sleep(1)
-
+mu = 1e-4
 #q_sol = (q[:] + np.pi) % np.pi - np.pi
 q_sol = q.copy()
 viz.display(q_sol)
 
 #Bring CoM between the two feet.
 
-def solve(model, data, q):
-    pinocchio.computeAllTerms(model,data,q,np.zeros(model.nv))
-    kkt_constraint = pinocchio.ContactCholeskyDecomposition(model,constraint_models)
-    constraint_dim = sum([cm.size() for cm in constraint_models])
+def squashing(model, data, q_in):
+    q = q_in.copy()
     y = np.ones((constraint_dim))
-    N=1000
-    eps = 1e-10
+
+    N_full = 200
+    
+    #Decrease CoMz by 0.2
+    com_drop_amp = 0.3
+    pinocchio.computeAllTerms(model,data,q,np.zeros(model.nv))
+    com_base = data.com[0].copy()
+    
+    com_des = lambda k: com_base - np.array([0., 0.,
+                                             np.abs(com_drop_amp*np.sin(2.*np.pi*k/(N_full)))])
     for k in range(N):
+        pinocchio.computeAllTerms(model,data,q,np.zeros(model.nv))
         pinocchio.computeJointJacobians(model,data,q)
+        com_act = data.com[0].copy()
+        com_err = com_act - com_des(k)
         kkt_constraint.compute(model, data, constraint_models, constraint_datas, mu)
         constraint_value = np.concatenate([cd.c1Mc2.translation for cd in constraint_datas])
         J = np.vstack([pinocchio.getFrameJacobian(model,data,cm.joint1_id,
@@ -163,13 +143,13 @@ def solve(model, data, q):
             print("Convergence achieved")
             break
         print("constraint_value:",np.linalg.norm(constraint_value))
-        
-        rhs = np.concatenate([-constraint_value - y*mu, np.zeros(model.nv)])
+        print ("com_error:", np.linalg.norm(com_err))
+        rhs = np.concatenate([-constraint_value - y*mu, 3*com_err, np.zeros(model.nv-3)])
         dz = kkt_constraint.solve(rhs) 
         dy = dz[:constraint_dim]
         dq = dz[constraint_dim:]
-        
         alpha = 1.
         q = pinocchio.integrate(model,q,-alpha*dq)
         y -= alpha*(-dy + y)
         viz.display(q)
+        sleep(0.05)
