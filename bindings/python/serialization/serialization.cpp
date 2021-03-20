36269e37d25513173a70e02f9032c2dc53f65dca
Justin Carpentier <justin.carpentier@inria.fr>
2021-03-20 15:23:22 +0100

    python/serialization: add buffer_copy

--- bindings/python/serialization/serialization.cpp
+++ bindings/python/serialization/serialization.cpp
@@ -12,6 +12,12 @@ namespace pinocchio
   namespace python
   {
     
+    static void buffer_copy(boost::asio::streambuf & dest,
+                            const boost::asio::streambuf & source)
+    {
+      boost::asio::buffer_copy(dest.prepare(source.size()),source.data());
+    }
+    
     void exposeSerialization()
     {
       namespace bp = boost::python;
@@ -21,6 +27,10 @@ namespace pinocchio
       bp::class_<boost::asio::streambuf,boost::noncopyable>("StreamBuffer",
                                                             "Stream buffer to save/load serialized objects in binary mode.",
                                                             bp::init<>(bp::arg("self"),"Default constructor."));
+      
+      bp::def("buffer_copy",buffer_copy,
+              bp::args("dest","source"),
+              "Copy bytes from a source buffer to a target buffer.");
     }
     
   } // namespace python
