d9c42292ddd7c9c19fbb9872a10aff02da75bdad
Justin Carpentier <justin.carpentier@inria.fr>
2021-04-01 13:52:00 +0200

    python: serialize BVHModel

--- bindings/python/multibody/fcl/expose-fcl.cpp
+++ bindings/python/multibody/fcl/expose-fcl.cpp
@@ -1,9 +1,14 @@
 //
-// Copyright (c) 2017-2020 CNRS INRIA
+// Copyright (c) 2017-2021 CNRS INRIA
 //
 
 #include "pinocchio/bindings/python/fwd.hpp"
 #include "pinocchio/bindings/python/multibody/fcl/transform.hpp"
+#include "pinocchio/bindings/python/serialization/serialization.hpp"
+
+#define HPP_FCL_SKIP_EIGEN_BOOST_SERIALIZATION
+  #include <hpp/fcl/serialization/BVH_model.h>
+#undef HPP_FCL_SKIP_EIGEN_BOOST_SERIALIZATION
 
 namespace pinocchio
 {
@@ -19,6 +24,11 @@ namespace pinocchio
       // Register implicit conversion SE3 <=> ::hpp::fcl::Transform3f
       bp::implicitly_convertible< SE3,Transform3f >();
       bp::implicitly_convertible< Transform3f,SE3 >();
+      
+      // Expose serialization of basic geometries to binary buffers
+      serialize< BVHModel<OBB> >();
+      serialize< BVHModel<RSS> >();
+      serialize< BVHModel<OBBRSS> >();
     }
     
   } // namespace python
