c44157e6e82cb32be40154808e51e243327e1e88
Justin Carpentier <justin.carpentier@inria.fr>
2021-04-18 09:28:01 +0200

    python: expose new StaticBuffer and related functions

--- bindings/python/serialization/serialization.hpp
+++ bindings/python/serialization/serialization.hpp
@@ -25,12 +25,20 @@ namespace pinocchio
       bp::scope current_scope = getOrCreatePythonNamespace("serialization");
       
       bp::def("loadFromBinary",(void (*)(T &, boost::asio::streambuf &))pinocchio::serialization::loadFromBinary<T>,
-              bp::args("object","buffer"),
+              bp::args("object","stream_buffer"),
               "Load an object from a binary buffer.");
       
       bp::def("saveToBinary",(void (*)(const T &, boost::asio::streambuf &))pinocchio::serialization::saveToBinary<T>,
-              bp::args("object","buffer"),
+              bp::args("object","stream_buffer"),
               "Save an object to a binary buffer.");
+      
+      bp::def("loadFromBinary",(void (*)(T &, serialization::StaticBuffer &))pinocchio::serialization::loadFromBinary<T>,
+              bp::args("object","static_buffer"),
+              "Load an object from a static binary buffer.");
+      
+      bp::def("saveToBinary",(void (*)(const T &, serialization::StaticBuffer &))pinocchio::serialization::saveToBinary<T>,
+              bp::args("object","static_buffer"),
+              "Save an object to a static binary buffer.");
     }
     
   } // namespace python
