0c1fd0fc84ffc650d2adebd6edc11ff192cec72d
Justin Carpentier <justin.carpentier@inria.fr>
2021-04-27 16:35:16 +0200

    test: add Python test for joints

---
+++ unittest/python/bindings_joints.py
@@ -0,0 +1,28 @@
+import unittest
+import pinocchio as pin 
+import numpy as np
+
+joint_types = [
+    pin.JointModelRX, pin.JointModelRY, pin.JointModelRZ,
+    pin.JointModelPX, pin.JointModelPY, pin.JointModelPZ,
+    pin.JointModelFreeFlyer, pin.JointModelSpherical, pin.JointModelSphericalZYX,
+    pin.JointModelPlanar, pin.JointModelTranslation
+]
+
+class TestJoints(unittest.TestCase):
+
+    def test_comparison_operators(self):
+        for joint_type in joint_types:
+
+            j = joint_type()
+            joint_model = pin.JointModel(j)
+
+            self.assertTrue(j == joint_type())
+            self.assertTrue(j == joint_model)
+
+            j.setIndexes(0,0,0)
+            self.assertFalse(j == joint_model)
+            self.assertTrue(j != joint_model)
+
+if __name__ == '__main__':
+    unittest.main()
