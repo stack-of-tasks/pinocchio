93a5e64f0c85ddf1f23a6df9f492ea95ea1884d4
Justin Carpentier <justin.carpentier@inria.fr>
2021-04-29 22:47:34 +0200

    core: remove bugs with setIndexes

--- src/multibody/joint/joint-generic.hpp
+++ src/multibody/joint/joint-generic.hpp
@@ -183,9 +183,6 @@ namespace pinocchio
     JointModelTpl(const JointModelVariant & jmodel_variant)
     : JointCollection::JointModelVariant(jmodel_variant)
     {
-      setIndexes(::pinocchio::id(jmodel_variant),
-                 ::pinocchio::nq(jmodel_variant),
-                 ::pinocchio::nv(jmodel_variant));
     }
     
     template<typename JointModelDerived>
@@ -193,8 +190,6 @@ namespace pinocchio
     : JointModelVariant((JointModelVariant)jmodel.derived())
     {
       BOOST_MPL_ASSERT((boost::mpl::contains<typename JointModelVariant::types,JointModelDerived>));
-
-      setIndexes(jmodel.id(),jmodel.nq(),jmodel.nv());
     }
     
     JointModelVariant & toVariant()
