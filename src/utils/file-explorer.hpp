7d4fb91f0e5569bdd068f660c9de9894a0d37722
Wolfgang Merkt <w.merkt@gmail.com>
2021-07-09 14:08:15 +0100

    Make file-explorer support Windows, add work-around for ROS paths

--- src/utils/file-explorer.hpp
+++ src/utils/file-explorer.hpp
@@ -17,13 +17,11 @@ namespace pinocchio
    * @brief      Parse an environment variable if exists and extract paths according to the delimiter.
    *
    * @param[in]  env_var_name The name of the environment variable.
-   * @param[in]  delimiter The delimiter between two consecutive paths.
    *
    * @return The vector of paths extracted from the environment variable value.
    */
   PINOCCHIO_DLLAPI std::vector<std::string>
-  extractPathFromEnvVar(const std::string & env_var_name,
-                        const std::string & delimiter = ":");
+  extractPathFromEnvVar(const std::string & env_var_name);
 
 
   /**
@@ -31,12 +29,10 @@ namespace pinocchio
    *
    * @param[in]  env_var_name The name of the environment variable.
    * @param[out] list_of_paths List of path to fill with the paths extracted from the environment variable value.
-   * @param[in]  delimiter The delimiter between two consecutive paths.
    */
   PINOCCHIO_DLLAPI void
   extractPathFromEnvVar(const std::string & env_var_name,
-                        std::vector<std::string> & list_of_paths,
-                        const std::string & delimiter = ":");
+                        std::vector<std::string> & list_of_paths);
 
 
   /**
