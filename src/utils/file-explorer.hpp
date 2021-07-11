12d25a7ae899f718d30ff90f21ac492625febfd1
Wolfgang Merkt <w.merkt@gmail.com>
2021-07-11 14:10:38 +0100

    [file-explorer] Restore API + apply Windows support

--- src/utils/file-explorer.hpp
+++ src/utils/file-explorer.hpp
@@ -17,11 +17,18 @@ namespace pinocchio
    * @brief      Parse an environment variable if exists and extract paths according to the delimiter.
    *
    * @param[in]  env_var_name The name of the environment variable.
+   * @param[in]  delimiter The delimiter between two consecutive paths.
    *
    * @return The vector of paths extracted from the environment variable value.
    */
   PINOCCHIO_DLLAPI std::vector<std::string>
-  extractPathFromEnvVar(const std::string & env_var_name);
+  extractPathFromEnvVar(const std::string & env_var_name,
+#ifdef _WIN32
+                        const std::string & delimiter = ";"
+#else
+                        const std::string & delimiter = ":"
+#endif
+                        );
 
 
   /**
@@ -29,16 +36,23 @@ namespace pinocchio
    *
    * @param[in]  env_var_name The name of the environment variable.
    * @param[out] list_of_paths List of path to fill with the paths extracted from the environment variable value.
+   * @param[in] delimiter The delimiter between two consecutive paths.
    */
   PINOCCHIO_DLLAPI void
   extractPathFromEnvVar(const std::string & env_var_name,
-                        std::vector<std::string> & list_of_paths);
+                        std::vector<std::string> & list_of_paths,
+#ifdef _WIN32
+                        const std::string & delimiter = ";"
+#else
+                        const std::string & delimiter = ":"
+#endif
+                        );
 
 
   /**
-   * @brief      Parse the environment variable ROS_PACKAGE_PATH and extract paths
+   * @brief      Parse the environment variables ROS_PACKAGE_PATH / AMENT_PREFIX_PATH and extract paths
    *
-   * @return     The vector of paths extracted from the environment variable ROS_PACKAGE_PATH
+   * @return     The vector of paths extracted from the environment variables ROS_PACKAGE_PATH / AMENT_PREFIX_PATH
    */
   PINOCCHIO_DLLAPI std::vector<std::string> rosPaths();
 
