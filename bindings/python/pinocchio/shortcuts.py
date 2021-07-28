e1dded447e278186f54dd294c2d8d85c5d3b3589
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-07-28 14:41:35 +0200

    [bindings][shortcuts][sdf] add shortcut for buildmodelfromsdf

--- bindings/python/pinocchio/shortcuts.py
+++ bindings/python/pinocchio/shortcuts.py
@@ -56,4 +56,43 @@ def createDatas(*models):
     """
     return tuple([None if model is None else model.createData() for model in models])
 
-__all__ = ["buildModelsFromUrdf", "createDatas"]
+
+def buildModelsFromSdf(filename, package_dirs=None, root_joint=None, verbose=False, meshLoader=None, geometry_types=[pin.GeometryType.COLLISION,pin.GeometryType.VISUAL]):
+    """Parse the SDF file given in input and return a Pinocchio Model and a list of Constraint Models, followed by corresponding GeometryModels of types specified by geometry_types, in the same order as listed.
+    Examples of usage:
+        # load model, constraint models, collision model, and visual model, in this order (default)
+        model, constraint_models, collision_model, visual_model = buildModelsFromSdf(filename[, ...], geometry_types=[pin.GeometryType.COLLISION,pin.GeometryType.VISUAL])
+        model, constraint_models, collision_model, visual_model = buildModelsFromSdf(filename[, ...]) # same as above
+        
+        model, constraint_models, collision_model = buildModelsFromSdf(filename[, ...], geometry_types=[pin.GeometryType.COLLISION]) # only load the model, constraint models and the collision model
+        model, constraint_models, collision_model = buildModelsFromSdf(filename[, ...], geometry_types=pin.GeometryType.COLLISION)   # same as above
+        model, constraint_models, visual_model    = buildModelsFromSdf(filename[, ...], geometry_types=pin.GeometryType.VISUAL)      # only load the model and the visual model
+        
+        model, constraint_models = buildModelsFromSdf(filename[, ...], geometry_types=[])  # equivalent to buildModelFromSdf(filename[, root_joint])
+    """
+
+    if root_joint is None:
+        model, constraint_models = pin.buildModelFromSdf(filename)
+    else:
+        model, constraint_models = pin.buildModelFromSdf(filename, root_joint)
+
+    if verbose and not WITH_HPP_FCL and meshLoader is not None:
+        print('Info: MeshLoader is ignored. Pinocchio has not been compiled with HPP-FCL.')
+    if verbose and not WITH_HPP_FCL_BINDINGS and meshLoader is not None:
+        print('Info: MeshLoader is ignored. The HPP-FCL Python bindings have not been installed.')
+    if package_dirs is None:
+        package_dirs = []
+
+    lst = [model, constraint_models]
+
+    if not hasattr(geometry_types, '__iter__'):
+        geometry_types = [geometry_types]
+
+    for geometry_type in geometry_types:
+        if meshLoader is None or (not WITH_HPP_FCL and not WITH_HPP_FCL_BINDINGS):
+            geom_model = pin.buildGeomFromSdf(model, constraint_models, filename, geometry_type, package_dirs)
+        else:
+            geom_model = pin.buildGeomFromSdf(model, constraint_models, filename, geometry_type, package_dirs, meshLoader)
+        lst.append(geom_model)
+
+    return tuple(lst)
