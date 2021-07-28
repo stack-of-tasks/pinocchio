2a680b064f299e3c610222f9f13d15628778c031
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-07-28 12:19:05 +0200

    [sdf][buildGEom] add signature for single pacakge path

--- src/parsers/sdf.hpp
+++ src/parsers/sdf.hpp
@@ -17,13 +17,13 @@ namespace pinocchio
 
 
     /**
-     * @brief      Build The GeometryModel from a URDF file. Search for meshes
+     * @brief      Build The GeometryModel from a SDF file. Search for meshes
      *             in the directories specified by the user first and then in
      *             the environment variable ROS_PACKAGE_PATH
      *
      * @param[in]  model         The model of the robot, built with
-     *                           urdf::buildModel
-     * @param[in]  filename      The URDF complete (absolute) file path
+     *                           sdf::buildModel
+     * @param[in]  filename      The SDF complete (absolute) file path
      * @param[in]  packageDirs   A vector containing the different directories
      *                           where to search for models and meshes, typically 
      *                           obtained from calling pinocchio::rosPaths()
@@ -45,15 +45,50 @@ namespace pinocchio
                               GeometryModel & geomModel,
                               const std::vector<std::string> & packageDirs = std::vector<std::string> (),
                               ::hpp::fcl::MeshLoaderPtr meshLoader = ::hpp::fcl::MeshLoaderPtr());
+
+
+    /**
+     * @brief      Build The GeometryModel from a SDF file. Search for meshes
+     *             in the directories specified by the user first and then in
+     *             the environment variable ROS_PACKAGE_PATH
+     *
+     * @param[in]  model         The model of the robot, built with
+     *                           sdf::buildModel
+     * @param[in]  filename      The SDF complete (absolute) file path
+     * @param[in]  package_path    A string containing the path to the directories of the meshes,
+     *                           typically obtained from calling pinocchio::rosPaths().
+     *
+     * @param[in]   type         The type of objects that must be loaded (must be VISUAL or COLLISION)
+     * @param[in]   meshLoader   object used to load meshes: hpp::fcl::MeshLoader [default] or hpp::fcl::CachedMeshLoader.
+     * @param[out]  geomModel    Reference where to put the parsed information.
+     *
+     * @return      Returns the reference on geom model for convenience.
+     *
+     * \warning     If hpp-fcl has not been found during compilation, COLLISION objects can not be loaded
+     *
+     */
+    template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl>
+    GeometryModel & buildGeom(ModelTpl<Scalar,Options,JointCollectionTpl> & model,
+                              PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel)& contact_models,
+                              const std::string & filename,
+                              const GeometryType type,
+                              GeometryModel & geomModel,
+                              const std::string & packagePath,
+                              ::hpp::fcl::MeshLoaderPtr meshLoader = ::hpp::fcl::MeshLoaderPtr())
+    {
+      const std::vector<std::string> dirs(1,packagePath);
+      return buildGeom(model,contact_models,filename,type,geomModel,dirs,meshLoader);
+    };
+
     
 
 
 
     ///
-    /// \brief Build the model from a URDF file with a particular joint as root of the model tree inside
+    /// \brief Build the model from a SDF file with a particular joint as root of the model tree inside
     /// the model given as reference argument.
     ///
-    /// \param[in] filename The URDF complete file path.
+    /// \param[in] filename The SDF complete file path.
     /// \param[in] rootJoint The joint at the root of the model tree.
     /// \param[in] verbose Print parsing info.
     /// \param[out] model Reference model where to put the parsed information.
@@ -69,9 +104,9 @@ namespace pinocchio
 
 
     ///
-    /// \brief Build the model from a URDF file with a fixed joint as root of the model tree.
+    /// \brief Build the model from a SDF file with a fixed joint as root of the model tree.
     ///
-    /// \param[in] filename The URDF complete file path.
+    /// \param[in] filename The SDF complete file path.
     /// \param[in] verbose Print parsing info.
     /// \param[out] model Reference model where to put the parsed information.
     /// \return Return the reference on argument model for convenience.
