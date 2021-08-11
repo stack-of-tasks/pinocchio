aabb925cc1bded103bfe733d360df559f2d80e54
ManifoldFR <wilson.jallet@polytechnique.org>
2021-08-11 14:20:06 +0200

    [unittest/casadi] tests for exp6

--- unittest/python/casadi/bindings_log.py
+++ unittest/python/casadi/bindings_log.py
@@ -21,39 +21,53 @@ class TestLogExpDerivatives(TestCase):
     self.dv0 = SX.sym("v0", 3)
     self.dv1 = SX.sym("v1", 3)
     self.v_all = casadi.vertcat(self.dv0, self.dv1)
-    self.w = SX.sym("v", 3)
+    self.cv2 = SX.sym("v2", 3)
+    self.cdv2 = SX.sym("dv", 3)  # for forming the difference
 
     self.cR0_i = self.cR0 @ cpin.exp3(self.dv0)
     self.cR1_i = self.cR1 @ cpin.exp3(self.dv1)
 
+    # SE(3) examples
+    self.cM0 = SX.sym("M0", 4, 4)
+    self.cM1 = SX.sym("M1", 4, 4)
+    self.cdw0 = SX.sym("dm0", 6)
+    self.cdw1 = SX.sym("dm1", 6)
+    self.cw2 = SX.sym("w2", 6)
+    self.cdw2 = SX.sym("dw2", 6)
+    self.cM0_i = cpin.SE3(self.cM0) * cpin.exp6(self.cdw0)
+    self.cM1_i = cpin.SE3(self.cM1) * cpin.exp6(self.cdw1)
+
   def test_exp3(self):
     """Test the exp map and its derivative."""
-    dw = SX.sym("dw", 3)  # for forming the difference
-    exp_expr = self.cR0_i @ cpin.exp3(self.w + dw)
+    dw = self.cdv2
+    exp_expr = self.cR0_i @ cpin.exp3(self.cv2 + dw)
     repl_dargs = lambda e: casadi.substitute(e, casadi.vertcat(self.dv0, dw), np.zeros(6))
 
-    exp_eval = casadi.Function("exp", [self.cR0, self.w], [repl_dargs(exp_expr)])
+    exp_eval = casadi.Function("exp", [self.cR0, self.cv2], [repl_dargs(exp_expr)])
 
-    diff_expr = cpin.log3(exp_eval(self.cR0, self.w).T @ exp_expr)
+    diff_expr = cpin.log3(exp_eval(self.cR0, self.cv2).T @ exp_expr)
     Jexp_expr = casadi.jacobian(diff_expr, casadi.vertcat(self.dv0, dw))
-    Jexp_eval = casadi.Function("exp", [self.cR0, self.w], [repl_dargs(Jexp_expr)])
+    Jexp_eval = casadi.Function("exp", [self.cR0, self.cv2], [repl_dargs(Jexp_expr)])
 
     np.random.seed(3)
-    R0 = np.eye(3)
+    w0 = np.zeros(3)
     w1 = np.random.randn(3)
     w2 = np.array([np.pi, 0, 0])
+    w3 = np.array([-np.pi, 0, 0])
+    R0 = pin.exp3(w0)  # eye(3)
     R1 = pin.exp3(w1)
     R2 = pin.exp3(w2)
+    R3 = pin.exp3(w3)
     self.assertApprox(exp_eval(R0, np.zeros(3)).full(), R0)
     self.assertApprox(exp_eval(R0, w1).full(), R1)
     self.assertApprox(exp_eval(R0, -w1).full(), R1.T)
     self.assertApprox(exp_eval(R1, -w1).full(), R0)
     self.assertApprox(exp_eval(R0, w2).full(), R2)
 
-    w0 = np.zeros(3)
     J0 = pin.Jexp3(w0)
     J1 = pin.Jexp3(w1)
     J2 = pin.Jexp3(w2)
+    J3 = pin.Jexp3(w3)
     # print(J0)
     # print(Jexp_eval(R0, np.zeros(3)))
     # print(J1)
@@ -61,12 +75,12 @@ class TestLogExpDerivatives(TestCase):
     # print("R1:", R1)
     # print(Jexp_eval(R1, w1))
     # print(Jexp_eval(R2, w1))
-    self.assertApprox(Jexp_eval(R0, w0).full(), np.hstack([np.eye(3), J0]))
-    self.assertApprox(Jexp_eval(R1, w0).full(), np.hstack([np.eye(3), J0]))
+    self.assertApprox(Jexp_eval(R0, w0).full(), np.hstack([R0.T, J0]))
     self.assertApprox(Jexp_eval(R0, w1).full(), np.hstack([R1.T, J1]))
-    self.assertApprox(Jexp_eval(R1, w2).full(), np.hstack([R2.T, J2]))
     self.assertApprox(Jexp_eval(R0, w2).full(), np.hstack([R2.T, J2]))
-
+    self.assertApprox(Jexp_eval(R0, w3).full(), np.hstack([R3.T, J3]))
+    self.assertApprox(Jexp_eval(R1, w0).full(), np.hstack([R0.T, J0]))
+    self.assertApprox(Jexp_eval(R1, w2).full(), np.hstack([R2.T, J2]))
 
   def test_log3(self):
     log_expr = cpin.log3(self.cR0_i.T @ self.cR1_i)
@@ -93,6 +107,39 @@ class TestLogExpDerivatives(TestCase):
     J_1 = pin.Jlog3(R1)
     J_0 = -R1.T @ J_1
     self.assertApprox(Jlog_eval(R0, R1).full(), np.hstack([J_0, J_1]))
+
+  def test_exp6(self):
+    exp_expr = cpin.exp6(self.cw2 + self.cdw2)
+    repl_dargs = lambda e: casadi.substitute(e, self.cdw2, np.zeros(6))
+
+
+    exp_eval = casadi.Function("exp6", [self.cw2], [repl_dargs(exp_expr.np)])
+
+    diff_expr = cpin.log6(cpin.SE3(exp_eval(self.cw2)).actInv(exp_expr)).np
+    Jexp_expr = casadi.jacobian(diff_expr, self.cdw2)
+
+    Jexp_eval = casadi.Function("exp6", [self.cw2], [repl_dargs(Jexp_expr)])
+
+    w0 = np.zeros(6)
+    w1 = np.array([0., 0., 0., np.pi, 0., 0.])
+    w2 = np.random.randn(6)
+    w3 = np.array([0., 0., 0., np.pi/2, 0., 0.])
+    M0 = pin.exp6(w0)
+    M1 = pin.exp6(w1)
+    M2 = pin.exp6(w2)
+    M3 = pin.exp6(w3)
+    self.assertApprox(exp_eval(w0).full(), M0.np)
+    self.assertApprox(exp_eval(w1).full(), M1.np)
+    self.assertApprox(exp_eval(w2).full(), M2.np)
+    self.assertApprox(exp_eval(w3).full(), M3.np)
+
+    np.set_printoptions(precision=3)
+    J0 = pin.Jexp6(w0)
+    self.assertApprox(Jexp_eval(w0).full(), J0)
+    J1 = pin.Jexp6(w1)
+    self.assertApprox(Jexp_eval(w1).full(), J1)
+    J2 = pin.Jexp6(w2)
+    self.assertApprox(Jexp_eval(w2).full(), J2)
 
 
 if __name__ == '__main__':
