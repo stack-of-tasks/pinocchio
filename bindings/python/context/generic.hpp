265872f713c6ac5855311b72fc63f2184b4ecf64
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-13 10:46:00 +0200

    python/context: add missing typedef

--- bindings/python/context/generic.hpp
+++ bindings/python/context/generic.hpp
@@ -1,3 +1,7 @@
+//
+// Copyright (c) 2021 INRIA
+//
+
 #ifndef __pinocchio_python_context_generic_hpp__
 #define __pinocchio_python_context_generic_hpp__
 
@@ -35,8 +39,63 @@ typedef DataTpl<Scalar,Options> Data;
 
 typedef JointCollectionDefaultTpl<Scalar,Options> JointCollectionDefault;
 
+// Joints
 typedef JointModelTpl<Scalar,Options> JointModel;
 typedef JointDataTpl<Scalar,Options> JointData;
+
+typedef JointDataRevoluteTpl<Scalar,Options,0> JointDataRX;
+typedef JointModelRevoluteTpl<Scalar,Options,0> JointModelRX;
+
+typedef JointDataRevoluteTpl<Scalar,Options,1> JointDataRY;
+typedef JointModelRevoluteTpl<Scalar,Options,1> JointModelRY;
+
+typedef JointDataRevoluteTpl<Scalar,Options,2> JointDataRZ;
+typedef JointModelRevoluteTpl<Scalar,Options,2> JointModelRZ;
+
+typedef JointModelRevoluteUnalignedTpl<Scalar> JointModelRevoluteUnaligned;
+typedef JointDataRevoluteUnalignedTpl<Scalar> JointDataRevoluteUnaligned;
+
+typedef JointDataRevoluteUnboundedTpl<Scalar,Options,0> JointDataRUBX;
+typedef JointModelRevoluteUnboundedTpl<Scalar,Options,0> JointModelRUBX;
+
+typedef JointDataRevoluteUnboundedTpl<Scalar,Options,1> JointDataRUBY;
+typedef JointModelRevoluteUnboundedTpl<Scalar,Options,1> JointModelRUBY;
+
+typedef JointDataRevoluteUnboundedTpl<Scalar,Options,2> JointDataRUBZ;
+typedef JointModelRevoluteUnboundedTpl<Scalar,Options,2> JointModelRUBZ;
+
+typedef JointModelRevoluteUnboundedUnalignedTpl<Scalar> JointModelRevoluteUnboundedUnaligned;
+typedef JointDataRevoluteUnboundedUnalignedTpl<Scalar> JointDataRevoluteUnboundedUnaligned;
+
+typedef JointModelSphericalTpl<Scalar> JointModelSpherical;
+typedef JointDataSphericalTpl<Scalar> JointDataSpherical;
+
+typedef JointModelSphericalZYXTpl<Scalar> JointModelSphericalZYX;
+typedef JointDataSphericalZYXTpl<Scalar> JointDataSphericalZYX;
+
+typedef JointDataPrismaticTpl<Scalar,Options,0> JointDataPX;
+typedef JointModelPrismaticTpl<Scalar,Options,0> JointModelPX;
+
+typedef JointDataPrismaticTpl<Scalar,Options,1> JointDataPY;
+typedef JointModelPrismaticTpl<Scalar,Options,1> JointModelPY;
+
+typedef JointDataPrismaticTpl<Scalar,Options,2> JointDataPZ;
+typedef JointModelPrismaticTpl<Scalar,Options,2> JointModelPZ;
+
+typedef JointModelPrismaticUnalignedTpl<Scalar> JointModelPrismaticUnaligned;
+typedef JointDataPrismaticUnalignedTpl<Scalar> JointDataPrismaticUnaligned;
+
+typedef JointModelFreeFlyerTpl<Scalar> JointModelFreeFlyer;
+typedef JointDataFreeFlyerTpl<Scalar> JointDataFreeFlyer;
+
+typedef JointModelPlanarTpl<Scalar> JointModelPlanar;
+typedef JointDataPlanarTpl<Scalar> JointDataPlanar;
+
+typedef JointModelTranslationTpl<Scalar> JointModelTranslation;
+typedef JointDataTranslationTpl<Scalar> JointDataTranslation;
+
+typedef JointModelCompositeTpl<Scalar> JointModelComposite;
+typedef JointDataCompositeTpl<Scalar> JointDataComposite;
 
 // Algorithm
 typedef ProximalSettingsTpl<Scalar> ProximalSettings;
