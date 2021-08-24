8c7c9be9bfe29c5b29130f00a7b0dff26013a782
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-24 10:56:37 +0200

    python/meshcat: allows the display of {COLLISION,VISUAL}

--- examples/meshcat-viewer.py
+++ examples/meshcat-viewer.py
@@ -42,6 +42,8 @@ viz.loadViewerModel()
 # Display a robot configuration.
 q0 = pin.neutral(model)
 viz.display(q0)
+viz.displayCollisions(True)
+viz.displayVisuals(False)
 
 # Display another robot.
 viz2 = MeshcatVisualizer(model, collision_model, visual_model)
