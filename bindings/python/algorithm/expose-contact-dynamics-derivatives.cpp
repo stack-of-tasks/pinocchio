756f17dfc795e8bf317a2b3a45cbb3f2d6d7c41e
Justin Carpentier <justin.carpentier@inria.fr>
2021-09-01 15:32:47 +0200

    python: fix bindings of computeContactDynamicsDerivatives

--- bindings/python/algorithm/expose-contact-dynamics-derivatives.cpp
+++ bindings/python/algorithm/expose-contact-dynamics-derivatives.cpp
@@ -18,10 +18,13 @@ namespace pinocchio
       bp::tuple computeContactDynamicsDerivatives_proxy(const context::Model & model,
                                                         context::Data & data,
                                                         const RigidContactModelVector & contact_models,
-                                                        RigidContactDataVector & contact_datas)
+                                                        RigidContactDataVector & contact_datas,
+                                                        const context::ProximalSettings & settings = context::ProximalSettings())
       {
         pinocchio::computeContactDynamicsDerivatives(model, data,
-                                                     contact_models, contact_datas);
+                                                     contact_models, contact_datas,
+                                                     const_cast<context::ProximalSettings &>(settings));
+        
         return bp::make_tuple(make_ref(data.ddq_dq),
                               make_ref(data.ddq_dv),
                               make_ref(data.ddq_dtau),
@@ -38,13 +41,12 @@ namespace pinocchio
         typedef Eigen::aligned_allocator<context::RigidContactModel> RigidContactModelAllocator;
         
         bp::def("computeContactDynamicsDerivatives",
-                computeContactDynamicsDerivatives_proxy,bp::args("model","data",
-                                                                     "contact_models",
-                                                                     "contact_datas"),
-                "Computes the derivatives of the forward dynamics with kinematic constraints (given in the list of Contact information).\n"
+                computeContactDynamicsDerivatives_proxy,
+                (bp::arg("model"),bp::arg("data"),bp::arg("contact_models"),bp::arg("contact_datas"),bp::arg("settings") = context::ProximalSettings()),
+                "Computes the derivatives of the forward dynamics with kinematic constraints (given in the list of constraint models).\n"
                 "Assumes that contactDynamics has been called first. See contactDynamics for more details.\n"
-                "This function returns derivatives of joint acceleration (ddq) and contact forces (lambda_c) of the system.\n"
-                "The output is a tuple with ddq_dq, ddq_dv, ddq_da, dlambda_dq, dlambda_dv, dlambda_da");
+                "This function returns the derivatives of joint acceleration (ddq) and contact forces (lambda_c) of the system with respect to q, v and tau.\n"
+                "The output is a tuple with ddq_dq, ddq_dv, ddq_da, dlambda_dq, dlambda_dv, dlambda_da.");
       }
     }
 }
