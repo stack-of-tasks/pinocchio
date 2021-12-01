94fdaee17c650485c6f33b2a65a997740bf214b0
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-12-01 16:47:40 +0100

    [unittest/sdf] compare sdf model with urdf model

--- unittest/sdf.cpp
+++ unittest/sdf.cpp
@@ -22,12 +22,13 @@ BOOST_AUTO_TEST_CASE ( build_model )
   const std::string dir = PINOCCHIO_MODEL_DIR;
   
   pinocchio::Model model;
+  const std::string rootLinkName = "pelvis";
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(pinocchio::RigidConstraintModel) contact_models;
-  pinocchio::sdf::buildModel(filename, model, contact_models);
+  pinocchio::sdf::buildModel(filename, model, contact_models,rootLinkName);
   pinocchio::GeometryModel geomModel;
-  pinocchio::sdf::buildGeom(model, filename, pinocchio::COLLISION, geomModel, dir);
+  pinocchio::sdf::buildGeom(model, filename,rootLinkName, pinocchio::COLLISION, geomModel, dir);
 
-  BOOST_CHECK(model.nq == 22);
+  BOOST_CHECK(model.nq == 38);
 }
   
 BOOST_AUTO_TEST_CASE ( build_model_with_joint )
@@ -35,14 +36,14 @@ BOOST_AUTO_TEST_CASE ( build_model_with_joint )
 
   const std::string filename = PINOCCHIO_MODEL_DIR + std::string("/example-robot-data/robots/cassie_description/robots/cassie_v2.sdf");
   const std::string dir = PINOCCHIO_MODEL_DIR;
-  
+  const std::string rootLinkName = "pelvis";
   pinocchio::Model model;
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(pinocchio::RigidConstraintModel) contact_models;
-  pinocchio::sdf::buildModel(filename, pinocchio::JointModelFreeFlyer(), model, contact_models);
+  pinocchio::sdf::buildModel(filename, pinocchio::JointModelFreeFlyer(), model, contact_models,rootLinkName);
   pinocchio::GeometryModel geomModel;
-  pinocchio::sdf::buildGeom(model, filename, pinocchio::COLLISION, geomModel, dir);
+  pinocchio::sdf::buildGeom(model, filename,rootLinkName, pinocchio::COLLISION, geomModel, dir);
 
-  BOOST_CHECK(model.nq == 29);
+  BOOST_CHECK(model.nq == 45);
 }
 
 BOOST_AUTO_TEST_CASE (compare_model_with_urdf)
@@ -50,21 +51,89 @@ BOOST_AUTO_TEST_CASE (compare_model_with_urdf)
   const std::string filename = PINOCCHIO_MODEL_DIR + std::string("/simple_humanoid.sdf");
   const std::string dir = PINOCCHIO_MODEL_DIR;
 
-  pinocchio::Model model;
+  pinocchio::Model model_sdf;
+  const std::string rootLinkName = "WAIST_LINK0";
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(pinocchio::RigidConstraintModel) contact_models;
-  pinocchio::sdf::buildModel(filename, pinocchio::JointModelFreeFlyer(), model, contact_models);
+  pinocchio::sdf::buildModel(filename, pinocchio::JointModelFreeFlyer(), model_sdf, contact_models, rootLinkName);
   pinocchio::GeometryModel geomModel;
-  pinocchio::sdf::buildGeom(model, filename, pinocchio::COLLISION, geomModel, dir);
+  pinocchio::sdf::buildGeom(model_sdf, filename, rootLinkName, pinocchio::COLLISION, geomModel, dir);
 
   const std::string filename_urdf = PINOCCHIO_MODEL_DIR + std::string("/simple_humanoid.urdf");
   const std::string dir_urdf = PINOCCHIO_MODEL_DIR;
   pinocchio::Model model_urdf;
-  pinocchio::urdf::buildModel(filename_urdf, model_urdf);
+  pinocchio::urdf::buildModel(filename_urdf, pinocchio::JointModelFreeFlyer(), model_urdf);
 
+  typedef typename pinocchio::Model::ConfigVectorMap ConfigVectorMap;
   //Compare models
-  BOOST_CHECK(model_urdf==model);
+  //BOOST_CHECK(model_urdf==model);
+
+  BOOST_CHECK(model_urdf.nq == model_sdf.nq);
+  BOOST_CHECK(model_urdf.nv == model_sdf.nv);
+  BOOST_CHECK(model_urdf.njoints == model_sdf.njoints);
+  BOOST_CHECK(model_urdf.nbodies == model_sdf.nbodies);
+  BOOST_CHECK(model_urdf.nframes == model_sdf.nframes);
+  BOOST_CHECK(model_urdf.parents == model_sdf.parents);
+  BOOST_CHECK(model_urdf.children == model_sdf.children);
+  BOOST_CHECK(model_urdf.names == model_sdf.names);
+  BOOST_CHECK(model_urdf.subtrees == model_sdf.subtrees);
+  BOOST_CHECK(model_urdf.gravity == model_sdf.gravity);
+  BOOST_CHECK(model_urdf.name == model_sdf.name);
+  BOOST_CHECK(model_urdf.idx_qs == model_sdf.idx_qs);
+  BOOST_CHECK(model_urdf.nqs == model_sdf.nqs);
+  BOOST_CHECK(model_urdf.idx_vs == model_sdf.idx_vs);
+  BOOST_CHECK(model_urdf.nvs == model_sdf.nvs);
+  BOOST_CHECK(model_urdf.referenceConfigurations.size() == model_sdf.referenceConfigurations.size());
   
+  typename ConfigVectorMap::const_iterator it = model_sdf.referenceConfigurations.begin();
+  typename ConfigVectorMap::const_iterator it_model_urdf = model_urdf.referenceConfigurations.begin();
+  for(long k = 0; k < (long)model_sdf.referenceConfigurations.size(); ++k)
+  {
+    std::advance(it,k); std::advance(it_model_urdf,k);
+    BOOST_CHECK(it->second.size() == it_model_urdf->second.size());
+    BOOST_CHECK(it->second == it_model_urdf->second);    
+  }
+  BOOST_CHECK(model_urdf.armature.size() == model_sdf.armature.size());
   
+  BOOST_CHECK(model_urdf.armature == model_sdf.armature);
+  BOOST_CHECK(model_urdf.friction.size() == model_sdf.friction.size());
+  BOOST_CHECK(model_urdf.friction == model_sdf.friction);
+
+  BOOST_CHECK(model_urdf.damping.size() == model_sdf.damping.size());
+
+  BOOST_CHECK(model_urdf.damping == model_sdf.damping);
+
+  BOOST_CHECK(model_urdf.rotorInertia.size() == model_sdf.rotorInertia.size());
+
+  BOOST_CHECK(model_urdf.rotorInertia == model_sdf.rotorInertia);
+
+  BOOST_CHECK(model_urdf.rotorGearRatio.size() == model_sdf.rotorGearRatio.size());
+  
+  BOOST_CHECK(model_urdf.rotorGearRatio == model_sdf.rotorGearRatio);
+
+  BOOST_CHECK(model_urdf.effortLimit.size() == model_sdf.effortLimit.size());
+  BOOST_CHECK(model_urdf.effortLimit == model_sdf.effortLimit);
+
+  BOOST_CHECK(model_urdf.velocityLimit.size() == model_sdf.velocityLimit.size());
+
+  BOOST_CHECK(model_urdf.velocityLimit == model_sdf.velocityLimit);
+  BOOST_CHECK(model_urdf.lowerPositionLimit.size() == model_sdf.lowerPositionLimit.size());
+  BOOST_CHECK(model_urdf.lowerPositionLimit == model_sdf.lowerPositionLimit);
+
+  BOOST_CHECK(model_urdf.upperPositionLimit.size() == model_sdf.upperPositionLimit.size());
+  BOOST_CHECK(model_urdf.upperPositionLimit == model_sdf.upperPositionLimit);
+
+  for(size_t k = 1; k < model_sdf.inertias.size(); ++k)
+  {
+    BOOST_CHECK(model_urdf.inertias[k] == model_sdf.inertias[k]);
+  }
+
+  for(size_t k = 1; k < model_urdf.jointPlacements.size(); ++k)
+  {
+    BOOST_CHECK(model_urdf.jointPlacements[k] == model_sdf.jointPlacements[k]);
+  }
+
+  BOOST_CHECK(model_urdf.joints == model_sdf.joints);
+  BOOST_CHECK(model_urdf.frames == model_sdf.frames);
 }
 
 
