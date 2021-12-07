1f733b2989a8f83e175003ab6ced85be7fb309bb
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-12-07 14:54:21 +0100

    multibody/model: change frame.parent->parentJOint, new api

--- src/multibody/model.hxx
+++ src/multibody/model.hxx
@@ -448,7 +448,7 @@ namespace pinocchio
   ModelTpl<Scalar,Options,JointCollectionTpl>::
   addFrame(const Frame & frame, const bool append_inertia)
   {
-    PINOCCHIO_CHECK_INPUT_ARGUMENT(frame.parent < (JointIndex)njoints,
+    PINOCCHIO_CHECK_INPUT_ARGUMENT(frame.parentJoint < (JointIndex)njoints,
                                    "The index of the parent joint is not valid.");
     
 //    TODO: fix it
@@ -463,7 +463,7 @@ namespace pinocchio
     // else: we must add a new frames to the current stack
     frames.push_back(frame);
     if(append_inertia)
-      inertias[frame.parent] += frame.placement.act(frame.inertia);
+      inertias[frame.parentJoint] += frame.placement.act(frame.inertia);
     nframes++;
     return FrameIndex(nframes - 1);
   }
