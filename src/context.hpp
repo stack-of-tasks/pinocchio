645cead440e7afecc8fd4c091bd94162e1258c96
Fabian <fabian.schramm@tum.de>
2022-02-15 13:53:05 +0100

    define context::VectorXs

--- src/context.hpp
+++ src/context.hpp
@@ -5,12 +5,15 @@
 #ifndef __pinocchio_context_hpp__
 #define __pinocchio_context_hpp__
 
+#include <Eigen/Dense>
+
+// no pinocchio structure / classes imported here
 
 namespace pinocchio {
   namespace context {
     typedef PINOCCHIO_SCALAR_TYPE Scalar;
     enum { Options = 0 };
-    
+    typedef Eigen::Matrix<Scalar,Eigen::Dymamic,1,Options> VectorXs;
     
   } //namespace context
 } //namespace pinocchio
