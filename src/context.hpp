3855f701a2c997e0e6de4da498e2c51ec824cf6a
Rohan Budhiraja <proyan@users.noreply.github.com>
2022-02-16 14:33:00 +0100

    src/context: fix typo

--- src/context.hpp
+++ src/context.hpp
@@ -5,13 +5,13 @@
 #ifndef __pinocchio_context_hpp__
 #define __pinocchio_context_hpp__
 
-#include <Eigen/Dense>
+#include <Eigen/Core>
 
 namespace pinocchio {
   namespace context {
     typedef PINOCCHIO_SCALAR_TYPE Scalar;
     enum { Options = 0 };
-    typedef Eigen::Matrix<Scalar,Eigen::Dymamic,1,Options> VectorXs;
+    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1,Options> VectorXs;
     typedef Eigen::Matrix<Scalar, 6, Eigen::Dynamic, Options> Matrix6xs;
     
   } //namespace context
