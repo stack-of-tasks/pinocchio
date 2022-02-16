9a7046904eb47ff9642eadd875c3fb0682bc2cbd
Rohan Budhiraja <proyan@users.noreply.github.com>
2022-02-16 14:32:29 +0100

    [multibody/pool] fwd: double-> context::Scalar

--- src/multibody/pool/fwd.hpp
+++ src/multibody/pool/fwd.hpp
@@ -11,12 +11,12 @@ namespace pinocchio
 {
 
   template<typename Scalar, int Options = 0, template<typename,int> class JointCollectionTpl = JointCollectionDefaultTpl> class ModelPoolTpl;
-  typedef ModelPoolTpl<double> ModelPool;
+  typedef ModelPoolTpl<context::Scalar> ModelPool;
 
 #ifdef PINOCCHIO_WITH_HPP_FCL
 
   template<typename Scalar, int Options = 0, template<typename,int> class JointCollectionTpl = JointCollectionDefaultTpl> class GeometryPoolTpl;
-  typedef GeometryPoolTpl<double> GeometryPool;
+  typedef GeometryPoolTpl<context::Scalar> GeometryPool;
 
   template<typename BroadPhaseManagerDerived, typename Scalar, int Options = 0, template<typename,int> class JointCollectionTpl = JointCollectionDefaultTpl> class BroadPhaseManagerPoolTpl;
   template<typename BroadPhaseManagerDerived, typename Scalar>
