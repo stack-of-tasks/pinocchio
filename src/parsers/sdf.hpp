07c9a72a0c57441f7b1125958a7662bab1c6a17b
Rohan Budhiraja <proyan@users.noreply.github.com>
2022-03-07 12:21:43 +0100

    [sdf] add buildModelFromXML signature

--- src/parsers/sdf.hpp
+++ src/parsers/sdf.hpp
@@ -118,6 +118,29 @@ namespace pinocchio
       return buildGeom(model,filename,type,geomModel,rootLinkName,dirs,meshLoader);
     };
 
+
+    ///
+    /// \brief Build the model from an XML stream with a particular joint as root of the model tree inside
+    /// the model given as reference argument.
+    ///
+    /// \param[in] xmlStream xml stream containing SDF model
+    /// \param[in] rootJoint The joint at the root of the model tree.
+    /// \param[in] parentGuidance Joint names which should be preferred for cases where two joints can qualify as parent. The other joint appears in the constraint_model. If empty, joint appearance order in .sdf is taken as default.
+    /// \param[in] verbose Print parsing info.
+    /// \param[out] model Reference model where to put the parsed information.
+    /// \return Return the reference on argument model for convenience.
+    ///
+    template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl>
+    ModelTpl<Scalar,Options,JointCollectionTpl> &
+    buildModelFromXML(const std::string & xmlStream,
+                      const typename ModelTpl<Scalar,Options,JointCollectionTpl>::JointModel & rootJoint,
+                      ModelTpl<Scalar,Options,JointCollectionTpl> & model,
+                      PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel)& contact_models,
+                      const std::string& rootLinkName = "",
+                      const std::vector<std::string>& parentGuidance = {},
+                      const bool verbose = false);
+
+    
     ///
     /// \brief Build the model from a SDF file with a particular joint as root of the model tree inside
     /// the model given as reference argument.
@@ -139,6 +162,25 @@ namespace pinocchio
                const std::vector<std::string>& parentGuidance = {},
                const bool verbose = false);
 
+
+    ///
+    /// \brief Build the model from an xml stream with a fixed joint as root of the model tree.
+    ///
+    /// \param[in] xmlStream The xml stream containing the SDF Model.
+    /// \param[in] verbose Print parsing info.
+    /// \param[out] model Reference model where to put the parsed information.
+    /// \return Return the reference on argument model for convenience.
+    ///
+    template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl>
+    ModelTpl<Scalar,Options,JointCollectionTpl> &
+    buildModelFromXML(const std::string & xmlStream,
+               ModelTpl<Scalar,Options,JointCollectionTpl> & model,
+               PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel)& contact_models,
+               const std::string& rootLinkName = "",
+               const std::vector<std::string>& parentGuidance = {},
+               const bool verbose = false);
+
+    
 
     ///
     /// \brief Build the model from a SDF file with a fixed joint as root of the model tree.
