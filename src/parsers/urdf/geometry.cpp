640c54888d6f576b4e85afe29db2319c07408f63
Justin Carpentier <justin.carpentier@inria.fr>
2022-04-08 13:34:48 +0200

    collision: Add support for hpp-fcl v2 in Pinocchio v2

--- src/parsers/urdf/geometry.cpp
+++ src/parsers/urdf/geometry.cpp
@@ -165,9 +165,18 @@ namespace pinocchio
           bool convex = tree.isMeshConvex (linkName, geomName);
           if (convex) {
             bvh->buildConvexRepresentation (false);
+#if HPP_FCL_MAJOR_VERSION < 2
             geometry = bvh->convex;
-          } else
+#else
+            geometry = boost::shared_ptr<fcl::CollisionGeometry>(bvh->convex.get(), [bvh](...) mutable { bvh->convex.reset(); });
+#endif  // HPP_FCL_MAJOR_VERSION
+          } else {
+#if HPP_FCL_MAJOR_VERSION < 2
             geometry = bvh;
+#else
+            geometry = boost::shared_ptr<fcl::CollisionGeometry>(bvh.get(), [bvh](...) mutable { bvh.reset(); });
+#endif  // HPP_FCL_MAJOR_VERSION
+          }
 #else
           geometry = meshLoader->load (meshPath, scale);
 #endif
