d2b612ea025d5231d08a604dc1c4b4862db7cbfc
Rohan Budhiraja <proyan@users.noreply.github.com>
2022-05-13 17:45:24 +0200

    [codegen-rnea] remove copyright

--- examples/codegen/codegen-rnea.cpp
+++ examples/codegen/codegen-rnea.cpp
@@ -1,17 +1,3 @@
-/* --------------------------------------------------------------------------
- *  CppADCodeGen: C++ Algorithmic Differentiation with Source Code Generation:
- *    Copyright (C) 2012 Ciengis
- *
- *  CppADCodeGen is distributed under multiple licenses:
- *
- *   - Eclipse Public License Version 1.0 (EPL1), and
- *   - GNU General Public License Version 3 (GPL3).
- *
- *  EPL1 terms and conditions can be found in the file "epl-v10.txt", while
- *  terms and conditions for the GPL3 can be found in the file "gpl3.txt".
- * ----------------------------------------------------------------------------
- * Author: Joao Leal
- */
 #include <iosfwd>
 #include <pinocchio/multibody/model.hpp>
 #include <pinocchio/parsers/sample-models.hpp>
@@ -59,7 +45,6 @@ int main(void) {
      * start the special steps for source code generation for a Jacobian
      */
     CodeHandler<double> handler;
-
     CppAD::vector<CGD> indVars(65);
     handler.makeVariables(indVars);
 
