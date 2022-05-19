471a2040dac3de9da06fb9b2827421213c47f84e
fabinsch <55981657+fabinsch@users.noreply.github.com>
2022-05-19 17:58:45 +0200

    [codengen] add python rnea example

---
+++ examples/codegen/codegen-rnea.py
@@ -0,0 +1,41 @@
+from pycppad import AD, ADCG, CG, Independent, Value, ADCGFun, CodeHandler, LanguageC, LangCDefaultVariableNameGenerator
+import pinocchio.cppadcg as cgpin
+import pinocchio as pin
+import numpy as np
+
+pinmodel = pin.buildSampleModelHumanoidRandom()
+model = cgpin.Model(pinmodel)
+data = model.createData()
+
+nq = model.nq
+nv = model.nv
+
+x = np.array([ADCG(CG(0.))]*(nq+nv+nv))
+x[:nq] = cgpin.neutral(model)
+Independent(x)
+
+y = np.array([ADCG(CG(0.))]*(nv))
+
+cgpin.rnea(model, data, x[:nq], x[nq:nq+nv], x[nq+nv:])
+y[:] = data.tau
+
+fun = ADCGFun(x, y)
+
+# /***************************************************************************
+# *                        Generate the C source code
+# **************************************************************************/
+
+# /**
+# * start the special steps for source code generation for a Jacobian
+# */
+handler = CodeHandler(50)
+
+indVars = np.array([CG(1.)]*(nq+nv+nv))
+handler.makeVariables(indVars)
+
+jac = fun.Jacobian(indVars)
+
+langC = LanguageC("double", 3)
+nameGen = LangCDefaultVariableNameGenerator("y","x","v","array","sarray")
+code = handler.generateCode(langC, jac, nameGen, "source")
+print(code)
