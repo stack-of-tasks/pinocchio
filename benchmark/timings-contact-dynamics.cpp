6fe85bd2521a63dbbd4186a8fab553202220ba63
Justin Carpentier <justin.carpentier@inria.fr>
2022-08-05 17:33:39 +0200

    bench: fix function name in std::cout

--- benchmark/timings-contact-dynamics.cpp
+++ benchmark/timings-contact-dynamics.cpp
@@ -24,7 +24,7 @@ int main(int argc, const char ** argv)
 {
   using namespace Eigen;
   using namespace pinocchio;
-  
+
   PinocchioTicToc timer(PinocchioTicToc::US);
 #ifdef NDEBUG
   const int NBT = 1000*100;
@@ -32,21 +32,21 @@ int main(int argc, const char ** argv)
   const int NBT = 1;
   std::cout << "(the time score in debug mode is not relevant) " << std::endl;
 #endif
-  
+
   // Build model
   Model model;
-  
+
   std::string filename = PINOCCHIO_MODEL_DIR + std::string("/simple_humanoid.urdf");
   if(argc>1) filename = argv[1];
   bool with_ff = true;
-  
+
   if(argc>2)
   {
     const std::string ff_option = argv[2];
     if(ff_option == "-no-ff")
       with_ff = false;
   }
-  
+
   if( filename == "HS")
     buildModels::humanoidRandom(model,true);
   else
@@ -55,7 +55,7 @@ int main(int argc, const char ** argv)
   //      pinocchio::urdf::buildModel(filename,JointModelRX(),model);
     else
       pinocchio::urdf::buildModel(filename,model);
-  
+
   const std::string RA = "RARM_LINK6";
   const JointIndex RA_id = model.frames[model.getFrameId(RA)].parent;
   const std::string LA = "LARM_LINK6";
@@ -64,27 +64,27 @@ int main(int argc, const char ** argv)
   const JointIndex RF_id = model.frames[model.getFrameId(RF)].parent;
   const std::string LF = "LLEG_LINK6";
   const JointIndex LF_id = model.frames[model.getFrameId(LF)].parent;
-  
+
   RigidConstraintModel ci_RF_6D(CONTACT_6D,model,RF_id,LOCAL);
   RigidConstraintModel ci_RF_3D(CONTACT_3D,model,RF_id,LOCAL);
-  
+
   RigidConstraintModel ci_LF_6D(CONTACT_6D,model,LF_id,LOCAL);
   RigidConstraintModel ci_LF_3D(CONTACT_3D,model,LF_id,LOCAL);
-  
+
   RigidConstraintModel ci_RA_3D(CONTACT_3D,model,RA_id,LOCAL);
   RigidConstraintModel ci_LA_3D(CONTACT_3D,model,LA_id,LOCAL);
-  
+
   // Define contact infos structure
   static const PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel) contact_models_empty;
   static PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintData) contact_data_empty;
   cholesky::ContactCholeskyDecomposition contact_chol_empty(model,contact_models_empty);
-  
+
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel) contact_models_6D;
   contact_models_6D.push_back(ci_RF_6D);
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintData) contact_data_6D;
   contact_data_6D.push_back(RigidConstraintData(ci_RF_6D));
   cholesky::ContactCholeskyDecomposition contact_chol_6D(model,contact_models_6D);
-  
+
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel) contact_models_6D6D;
   contact_models_6D6D.push_back(ci_RF_6D);
   contact_models_6D6D.push_back(ci_LF_6D);
@@ -96,21 +96,21 @@ int main(int argc, const char ** argv)
   ProximalSettings prox_settings;
   prox_settings.max_iter = 10;
   prox_settings.mu = 1e8;
-  
+
   std::cout << "nq = " << model.nq << std::endl;
   std::cout << "nv = " << model.nv << std::endl;
   std::cout << "--" << std::endl;
-  
+
   Data data(model);
   VectorXd qmax = Eigen::VectorXd::Ones(model.nq);
-  
+
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(VectorXd) qs(NBT);
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(VectorXd) qdots(NBT);
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(VectorXd) qddots(NBT);
   PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(VectorXd) taus(NBT);
-  
+
   static const PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(RigidConstraintModel) contact_models;
-  
+
   for(size_t i=0;i<NBT;++i)
   {
     qs[i]     = randomConfiguration(model,-qmax,qmax);
@@ -118,21 +118,21 @@ int main(int argc, const char ** argv)
     qddots[i] = Eigen::VectorXd::Random(model.nv);
     taus[i] = Eigen::VectorXd::Random(model.nv);
   }
-  
+
   timer.tic();
   SMOOTH(NBT)
   {
     aba(model,data,qs[_smooth],qdots[_smooth],taus[_smooth]);
   }
   std::cout << "ABA = \t\t"; timer.toc(std::cout,NBT);
-  
+
   timer.tic();
   SMOOTH(NBT)
   {
     contactABA(model,data,qs[_smooth],qdots[_smooth],taus[_smooth],contact_models_empty,contact_data_empty);
   }
   std::cout << "contact ABA = \t\t"; timer.toc(std::cout,NBT);
-  
+
   double total_time = 0;
   SMOOTH(NBT)
   {
@@ -143,7 +143,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "Sparse Cholesky = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   total_time = 0;
   SMOOTH(NBT)
   {
@@ -154,7 +154,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholesky {} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   total_time = 0;
   MatrixXd H_inverse(contact_chol_empty.size(),contact_chol_empty.size());
   SMOOTH(NBT)
@@ -167,7 +167,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholeskyInverse {} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   initConstraintDynamics(model,data,contact_models_empty);
   timer.tic();
   SMOOTH(NBT)
@@ -175,7 +175,7 @@ int main(int argc, const char ** argv)
     constraintDynamics(model,data,qs[_smooth],qdots[_smooth],taus[_smooth],contact_models_empty,contact_data_empty);
   }
   std::cout << "constraintDynamics {} = \t\t"; timer.toc(std::cout,NBT);
-  
+
   total_time = 0;
   SMOOTH(NBT)
   {
@@ -186,7 +186,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholesky {6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   total_time = 0;
   H_inverse.resize(contact_chol_6D.size(),contact_chol_6D.size());
   SMOOTH(NBT)
@@ -199,16 +199,16 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholeskyInverse {6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   MatrixXd J(contact_chol_6D.constraintDim(),model.nv);
   J.setZero();
   MatrixXd MJtJ_inv(model.nv+contact_chol_6D.constraintDim(),
                     model.nv+contact_chol_6D.constraintDim());
   MJtJ_inv.setZero();
-  
+
   VectorXd gamma(contact_chol_6D.constraintDim());
   gamma.setZero();
-  
+
   total_time = 0;
   SMOOTH(NBT)
   {
@@ -216,9 +216,9 @@ int main(int argc, const char ** argv)
     timer.tic();
     getJointJacobian(model,data,ci_RF_6D.joint1_id,ci_RF_6D.reference_frame,J.middleRows<6>(0));
     total_time += timer.toc(timer.DEFAULT_UNIT);
-    
+
     forwardDynamics(model,data,qs[_smooth], qdots[_smooth], taus[_smooth], J, gamma);
-    
+
     timer.tic();
     cholesky::decompose(model,data);
     getKKTContactDynamicMatrixInverse(model,data,J,MJtJ_inv);
@@ -226,7 +226,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "KKTContactDynamicMatrixInverse {6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   initConstraintDynamics(model,data,contact_models_6D);
   timer.tic();
   SMOOTH(NBT)
@@ -240,7 +240,7 @@ int main(int argc, const char ** argv)
     contactABA(model,data,qs[_smooth],qdots[_smooth],taus[_smooth],contact_models_6D,contact_data_6D,prox_settings);
   }
   std::cout << "contact ABA {6D} = \t\t"; timer.toc(std::cout,NBT);
-  
+
   total_time = 0;
   SMOOTH(NBT)
   {
@@ -251,7 +251,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholesky {6D,6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   total_time = 0;
   H_inverse.resize(contact_chol_6D6D.size(),contact_chol_6D6D.size());
   SMOOTH(NBT)
@@ -264,25 +264,25 @@ int main(int argc, const char ** argv)
   }
   std::cout << "contactCholeskyInverse {6D,6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   J.resize(contact_chol_6D6D.constraintDim(),model.nv);
   J.setZero();
   MJtJ_inv.resize(model.nv+contact_chol_6D6D.constraintDim(),
                   model.nv+contact_chol_6D6D.constraintDim());
   MJtJ_inv.setZero();
-  
+
   gamma.resize(contact_chol_6D6D.constraintDim());
   gamma.setZero();
-  
+
   total_time = 0;
   SMOOTH(NBT)
   {
     computeAllTerms(model,data,qs[_smooth],qdots[_smooth]);
     getJointJacobian(model,data,ci_RF_6D.joint1_id,ci_RF_6D.reference_frame,J.middleRows<6>(0));
     getJointJacobian(model,data,ci_LF_6D.joint1_id,ci_LF_6D.reference_frame,J.middleRows<6>(6));
-    
+
     forwardDynamics(model,data,qs[_smooth], qdots[_smooth], taus[_smooth], J, gamma);
-    
+
     timer.tic();
     cholesky::decompose(model,data);
     getKKTContactDynamicMatrixInverse(model,data,J,MJtJ_inv);
@@ -290,7 +290,7 @@ int main(int argc, const char ** argv)
   }
   std::cout << "KKTContactDynamicMatrixInverse {6D,6D} = \t\t" << (total_time/NBT)
   << " " << timer.unitName(timer.DEFAULT_UNIT) <<std::endl;
-  
+
   initConstraintDynamics(model,data,contact_models_6D6D);
   timer.tic();
   SMOOTH(NBT)
@@ -304,7 +304,7 @@ int main(int argc, const char ** argv)
     contactABA(model,data,qs[_smooth],qdots[_smooth],taus[_smooth],contact_models_6D6D,contact_data_6D6D,prox_settings);
   }
   std::cout << "contact ABA {6D,6D} = \t\t"; timer.toc(std::cout,NBT);
-  
+
   J.setZero();
   timer.tic();
   SMOOTH(NBT)
@@ -314,10 +314,10 @@ int main(int argc, const char ** argv)
     getJointJacobian(model,data,ci_LF_6D.joint1_id,ci_LF_6D.reference_frame,J.middleRows<6>(6));
     forwardDynamics(model,data,taus[_smooth],J,gamma);
   }
-  std::cout << "constrainedDynamics {6D,6D} = \t\t"; timer.toc(std::cout,NBT);
-  
+  std::cout << "forwardDynamics {6D,6D} = \t\t"; timer.toc(std::cout,NBT);
+
   std::cout << "--" << std::endl;
-  
+
   return 0;
 }
 
