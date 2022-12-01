8d0a31059fb39986d0937ebfcb8f4120b34a6c10
quentinldc <quentin.le-lidec@inria.fr>
2022-12-01 19:52:55 +0100

    fixing exposition and doc of appendModel

--- src/algorithm/model.hpp
+++ src/algorithm/model.hpp
@@ -55,10 +55,15 @@ namespace pinocchio
   }
 
   /**
-   *  \copydoc pinocchio::appendModel(const ModelTpl<Scalar,Options,JointCollectionTpl>&, const ModelTpl<Scalar,Options,JointCollectionTpl> & modelB, FrameIndex frameInModelA, const SE3Tpl<Scalar, Options>& aMb, ModelTpl<Scalar,Options,JointCollectionTpl>& model);
+   *  \brief Append a child model into a parent model, after a specific frame given by its index.
    *
+   *  \param[in] modelA the parent model.
+   *  \param[in] modelB the child model.
    *  \param[in] geomModelA the parent geometry model.
    *  \param[in] geomModelB the child geometry model.
+   *  \param[in] frameInModelA index of the frame of modelA where to append modelB.
+   *  \param[in] aMb pose of modelB universe joint (index 0) in frameInModelA.
+   *  \param[out] model the resulting model.
    *  \param[out] geomModel the resulting geometry model.
    */
   template<typename Scalar, int Options, template<typename,int> class JointCollectionTpl>
