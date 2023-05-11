Fix signs in Jacobian and velocity calculations

examples/inverse-kinematics.py
@@ -29,8 +29,8 @@ while True:
         success = False
         break
     J = pinocchio.computeJointJacobian(model,data,q,JOINT_ID)  # in joint frame
-    J = pinocchio.Jlog6(iMd.inverse()) @ J
-    v = J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+    J = -pinocchio.Jlog6(iMd.inverse()) @ J
+    v = -J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
     q = pinocchio.integrate(model,q,v*DT)
     if not i % 10:
         print('%d: error = %s' % (i, err.T))