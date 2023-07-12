a3fa6f9c7facb997ea2fec198e4e72b2f2b3e343
Justin Carpentier <justin.carpentier@inria.fr>
2023-07-12 15:39:23 +0200

    python/utils: fix __copy__ and __deepcopy__ methods

--- bindings/python/utils/copyable.hpp
+++ bindings/python/utils/copyable.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2016-2023 CNRS INRIA, Heriot-Watt University
+// Copyright (c) 2016-2023 CNRS INRIA
 //
 
 #ifndef __pinocchio_python_utils_copyable_hpp__
@@ -14,41 +14,6 @@ namespace pinocchio
     
     namespace bp = boost::python;
 
-    template <class T>
-    inline PyObject* managingPyObject(T* p)
-    {
-      return typename bp::manage_new_object::apply<T*>::type()(p);
-    }
-
-    template <class Copyable>
-    bp::object generic__copy__(bp::object copyable)
-    {
-      Copyable* newCopyable(new Copyable(bp::extract<const Copyable&>(copyable)));
-      bp::object result(bp::detail::new_reference(managingPyObject(newCopyable)));
-
-      bp::extract<bp::dict>(result.attr("__dict__"))().update(copyable.attr("__dict__"));
-
-      return result;
-    }
-
-    template <class Copyable>
-    bp::object generic__deepcopy__(bp::object copyable, bp::dict memo)
-    {
-      bp::object copyMod = bp::import("copy");
-      bp::object deepcopy = copyMod.attr("deepcopy");
-
-      Copyable* newCopyable(new Copyable(bp::extract<const Copyable&>(copyable)));
-      bp::object result(bp::detail::new_reference(managingPyObject(newCopyable)));
-
-      int copyableId = (long long)(copyable.ptr());
-      memo[copyableId] = result;
-
-      bp::extract<bp::dict>(result.attr("__dict__"))().update(
-          deepcopy(bp::extract<bp::dict>(copyable.attr("__dict__"))(), memo));
-      return result;
-    }
-
-
     ///
     /// \brief Add the Python method copy to allow a copy of this by calling the copy constructor.
     ///
@@ -60,12 +25,13 @@ namespace pinocchio
       void visit(PyClass & cl) const
       {
         cl.def("copy",&copy,bp::arg("self"),"Returns a copy of *this.");
-        cl.def("__copy__", &generic__copy__<C>);
-        cl.def("__deepcopy__", &generic__deepcopy__<C>);
+        cl.def("__copy__", &copy,bp::arg("self"),"Returns a copy of *this.");
+        cl.def("__deepcopy__", &deepcopy,bp::args("self","memo"),"Returns a deep copy of *this.");
       }
       
     private:
       static C copy(const C & self) { return C(self); }
+      static C deepcopy(const C & self, bp::dict) { return C(self); }
     };
   } // namespace python
 } // namespace pinocchio
