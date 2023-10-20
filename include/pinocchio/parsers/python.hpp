73cf05186169b62bef91d0bdf525c4253fd66eb0
Joris Vaillant <joris.vaillant@navya.tech>
2023-10-20 13:33:14 +0200

    [cmake] Set back include/pinocchio/parsers/python.hpp

---
+++ include/pinocchio/parsers/python.hpp
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2015-2023 CNRS INRIA
+//
+
+#ifndef __pinocchio_parser_python_hpp__
+#define __pinocchio_parser_python_hpp__
+
+#include "pinocchio/macros.hpp"
+
+// clang-format off
+PINOCCHIO_PRAGMA_DEPRECATED_HEADER(pinocchio/parsers/python.hpp, pinocchio/bindings/python/parsers/python.hpp)
+// clang-format on
+
+#include "pinocchio/bindings/python/parsers/python.hpp"
+
+#endif // ifndef __pinocchio_parser_python_hpp__
