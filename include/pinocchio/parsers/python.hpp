7711a3191ae2a9640d1c3b6c107e0da4444ce0d5
Joris Vaillant <joris.vaillant@navya.tech>
2023-10-20 14:39:11 +0200

    [cmake] Remove the deprecated warning

--- include/pinocchio/parsers/python.hpp
+++ include/pinocchio/parsers/python.hpp
@@ -7,10 +7,6 @@
 
 #include "pinocchio/macros.hpp"
 
-// clang-format off
-PINOCCHIO_PRAGMA_DEPRECATED_HEADER(pinocchio/parsers/python.hpp, pinocchio/bindings/python/parsers/python.hpp)
-// clang-format on
-
 #include "pinocchio/bindings/python/parsers/python.hpp"
 
 #endif // ifndef __pinocchio_parser_python_hpp__
