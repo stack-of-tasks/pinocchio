5e0ed4838ecf990f398083241fd015f92d6a44b9
Joris Vaillant <joris.vaillant@gmail.com>
2023-10-25 13:19:43 +0200

    Topic/move headers (#2070)
    * Move all headers
    
    * [WIP] Patch CMakeLists.txt to use the new hierarchy
    
    Header installation is broken
    
    * [WIP] First step to have a working installation
    
    * Fix last installation issues
    
    * Use REPLACE instead of REGEX REPLACE
    
    * Fix doxygen
    
    * [cmake] Set back include/pinocchio/parsers/python.hpp
    
    * [cmake] Remove the deprecated warning
    
    * [cmake] Don't use GLOB/GLOB_RECURSE to find sources
    
    * [cmake] Remove optional header from the main sources list
    
    * Python remove useless include
    
    * Update CMakeLists.txt
    
    Co-authored-by: Guilhem Saurel <guilhem.saurel@laas.fr>
    
    * cmake: Rename private headers into generated headers
    
    ---------
    
    Co-authored-by: Joris Vaillant <joris.vaillant@navya.tech>
    Co-authored-by: Guilhem Saurel <guilhem.saurel@laas.fr>

---
+++ include/pinocchio/parsers/python.hpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2015-2023 CNRS INRIA
+//
+
+#ifndef __pinocchio_parser_python_hpp__
+#define __pinocchio_parser_python_hpp__
+
+#include "pinocchio/bindings/python/parsers/python.hpp"
+
+#endif // ifndef __pinocchio_parser_python_hpp__
