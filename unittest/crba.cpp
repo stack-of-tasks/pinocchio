13d0129c278bfebcdd38b188a08f821798641dd7
Justin Carpentier <justin.carpentier@inria.fr>
2023-12-22 10:21:46 +0100

    test/crba: add malloc test for debug mode

--- unittest/crba.cpp
+++ unittest/crba.cpp
@@ -10,6 +10,10 @@
  *
  */
 
+#ifndef EIGEN_RUNTIME_NO_MALLOC 
+  #define EIGEN_RUNTIME_NO_MALLOC
+#endif
+
 #include "pinocchio/multibody/model.hpp"
 #include "pinocchio/multibody/data.hpp"
 #include "pinocchio/algorithm/crba.hpp"
@@ -157,6 +161,25 @@ BOOST_AUTO_TEST_CASE(test_minimal_crba)
   }
   
 }
+
+#ifndef NDEBUG
+
+BOOST_AUTO_TEST_CASE(test_crba_malloc)
+{
+  using namespace pinocchio;
+  pinocchio::Model model;
+  pinocchio::buildModels::humanoidRandom(model);
+
+  model.addJoint(size_t(model.njoints-1), pinocchio::JointModelRevoluteUnaligned(SE3::Vector3::UnitX()), SE3::Random(), "revolute_unaligned");
+  pinocchio::Data data(model);
+
+  const Eigen::VectorXd q = pinocchio::neutral(model);
+  Eigen::internal::set_is_malloc_allowed(false);
+  crba(model, data, q);
+  Eigen::internal::set_is_malloc_allowed(true);
+}
+
+#endif
 
 BOOST_AUTO_TEST_SUITE_END ()
 
