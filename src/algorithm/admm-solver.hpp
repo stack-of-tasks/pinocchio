5a38bd5445650a2eec3e45bcc00241ee1f8515c1
Justin Carpentier <justin.carpentier@inria.fr>
2024-01-23 18:13:59 +0100

    algo/contact: add ADMM solver

---
+++ src/algorithm/admm-solver.hpp
@@ -0,0 +1,131 @@
+//
+// Copyright (c) 2022-2024 INRIA
+//
+
+#ifndef __pinocchio_algorithm_admm_solver_hpp__
+#define __pinocchio_algorithm_admm_solver_hpp__
+
+#include "pinocchio/algorithm/constraints/fwd.hpp"
+#include "pinocchio/math/fwd.hpp"
+#include "pinocchio/math/comparison-operators.hpp"
+
+#include "pinocchio/algorithm/contact-solver-base.hpp"
+
+namespace pinocchio
+{
+  template<typename _Scalar>
+  struct ADMMContactSolverTpl
+  : ContactSolverBaseTpl<_Scalar>
+  {
+    typedef _Scalar Scalar;
+    typedef ContactSolverBaseTpl<_Scalar> Base;
+    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1> VectorXs;
+
+    using Base::problem_size;
+
+    explicit ADMMContactSolverTpl(const int problem_dim)
+    : Base(problem_dim)
+    , rho_power(Scalar(0.2))
+    , ratio_primal_dual(Scalar(10))
+    , x_(problem_dim)
+    , y_(problem_dim)
+    , y_previous(problem_dim)
+    , z_(problem_dim)
+    , s_(VectorXs::Zero(problem_dim))
+    , primal_feasibility_vector(problem_dim)
+    , dual_feasibility_vector(problem_dim)
+    {}
+
+    /// \brief Set the power associated to the problem conditionning.
+    void setRhoPower(const Scalar rho_power)
+    {
+      this->rho_power = rho_power;
+    }
+    /// \brief Get the power associated to the problem conditionning.
+    Scalar getRhoPower() const { return rho_power; }
+
+    /// \brief Set the primal/dual ratio.
+    void setRatioPrimalDual(const Scalar ratio_primal_dual)
+    {
+      PINOCCHIO_CHECK_INPUT_ARGUMENT(ratio_primal_dual > 0.,"The ratio primal/dual should be positive strictly");
+      this->ratio_primal_dual = ratio_primal_dual;
+    }
+    /// \brief Get the primal/dual ratio.
+    Scalar getRatioPrimalDual() const { return ratio_primal_dual; }
+
+    /// \returns the number of updates of the Cholesky factorization due to rho updates.
+    int getCholeskyUpdateCount() const { return cholesky_update_count; }
+
+    ///
+    /// \brief Solve the constrained conic problem composed of problem data (G,g,cones) and starting from the initial guess.
+    ///
+    /// \param[in] G Symmetric PSD matrix representing the Delassus of the contact problem.
+    /// \param[in] g Free contact acceleration or velicity associted with the contact problem.
+    /// \param[in] cones Vector of conic constraints.
+    /// \param[in,out] x Initial guess and output solution of the problem
+    /// \param[in] mu_prox Proximal smoothing value associated to the algorithm.
+    /// \param[in] R Proximal regularization value associated to the compliant contacts (corresponds to the lowest non-zero).
+    /// \param[in] tau Over relaxation value
+    ///
+    /// \returns True if the problem has converged.
+    template<typename DelassusExpression, typename VectorLike, typename ConstraintAllocator, typename VectorLikeOut, typename VectorLikeR>
+    bool solve(DelassusExpression & delassus, const Eigen::MatrixBase<VectorLike> & g,
+               const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+               const Eigen::DenseBase<VectorLikeOut> & x,
+               const Scalar mu_prox,
+               const Eigen::MatrixBase<VectorLikeR> & R,
+               const Scalar tau = Scalar(0.99));
+
+
+    ///
+    /// \brief Solve the constrained conic problem composed of problem data (G,g,cones) and starting from the initial guess.
+    ///
+    /// \param[in] G Symmetric PSD matrix representing the Delassus of the contact problem.
+    /// \param[in] g Free contact acceleration or velicity associted with the contact problem.
+    /// \param[in] cones Vector of conic constraints.
+    /// \param[in,out] x Initial guess and output solution of the problem
+    /// \param[in] mu_prox Proximal smoothing value associated to the algorithm.
+    /// \param[in] tau Over relaxation value
+    ///
+    /// \returns True if the problem has converged.
+    template<typename DelassusExpression, typename VectorLike, typename ConstraintAllocator, typename VectorLikeOut, typename VectorLikeR>
+    bool solve(DelassusExpression & delassus, const Eigen::MatrixBase<VectorLike> & g,
+               const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+               const Eigen::DenseBase<VectorLikeOut> & x,
+               const Scalar mu_prox,
+               const Scalar tau = Scalar(0.99))
+    {
+      return solve(delassus,g.derived(),cones,x.const_cast_derived(),mu_prox,VectorXs::Zero(problem_size),tau);
+    }
+
+    /// \returns the primal solution of the problem
+    const VectorXs & getPrimalSolution() const { return y_; }
+    /// \returns the dual solution of the problem
+    const VectorXs & getDualSolution() const { return z_; }
+
+  protected:
+
+    /// \brief Power value associated to rho. This quantity will be automatically updated.
+    Scalar rho_power;
+    /// \brief Ratio primal/dual
+    Scalar ratio_primal_dual;
+
+    /// \brief Primal variables (corresponds to the contact forces)
+    VectorXs x_, y_;
+    /// \brief Previous value of y.
+    VectorXs y_previous;
+    /// \brief Dual varible of the ADMM (corresponds to the contact velocity or acceleration).
+    VectorXs z_;
+    /// \brief De Saxé shift
+    VectorXs s_;
+
+    VectorXs rhs, primal_feasibility_vector, dual_feasibility_vector;
+
+    int cholesky_update_count;
+
+  }; // struct ADMMContactSolverTpl
+}
+
+#include "pinocchio/algorithm/admm-solver.hxx"
+
+#endif // ifndef __pinocchio_algorithm_admm_solver_hpp__
---
+++ src/algorithm/admm-solver.hxx
@@ -0,0 +1,252 @@
+//
+// Copyright (c) 2022-2024 INRIA
+//
+
+#ifndef __pinocchio_algorithm_admm_solver_hxx__
+#define __pinocchio_algorithm_admm_solver_hxx__
+
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include "pinocchio/algorithm/constraints/coulomb-friction-cone.hpp"
+
+namespace pinocchio
+{
+
+namespace internal
+{
+
+/// \brief Project a vector x on the vector of cones.
+template<typename Scalar, typename ConstraintAllocator, typename VectorLikeIn, typename VectorLikeOut>
+void computeConeProjection(const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+                           const Eigen::DenseBase<VectorLikeIn> & x,
+                           const Eigen::DenseBase<VectorLikeOut> & x_proj_)
+{
+  assert(x.size() == x_proj_.size());
+  Eigen::DenseIndex index = 0;
+  VectorLikeOut & x_proj = x_proj_.const_cast_derived();
+  for(const auto & cone: cones)
+  {
+    x_proj.template segment<3>(index) = cone.project(x.template segment<3>(index));
+    assert(cone.isInside(x_proj.template segment<3>(index),1e-12));
+    index += 3;
+  }
+}
+
+/// \brief Project a vector x on the dual of the cones contained in the vector of cones.
+template<typename Scalar, typename ConstraintAllocator, typename VectorLikeIn, typename VectorLikeOut>
+void computeDualConeProjection(const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+                               const Eigen::DenseBase<VectorLikeIn> & x,
+                               const Eigen::DenseBase<VectorLikeOut> & x_proj_)
+{
+  assert(x.size() == x_proj_.size());
+  Eigen::DenseIndex index = 0;
+  VectorLikeOut & x_proj = x_proj_.const_cast_derived();
+  for(const auto & cone: cones)
+  {
+    x_proj.template segment<3>(index) = cone.dual().project(x.template segment<3>(index));
+    assert(cone.dual().isInside(x_proj.template segment<3>(index),1e-12));
+    index += 3;
+  }
+}
+
+template<typename Scalar, typename ConstraintAllocator, typename VectorLikeVelocity, typename VectorLikeForce>
+Scalar computeMaxConeComplementarity(const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+                                     const Eigen::DenseBase<VectorLikeVelocity> & velocities,
+                                     const Eigen::DenseBase<VectorLikeForce> & forces)
+{
+  assert(velocities.size() == forces.size());
+  Eigen::DenseIndex index = 0;
+  Scalar complementarity = 0;
+  for(const auto & cone: cones)
+  {
+    const Scalar cone_complementarity = cone.computeContactComplementarity(velocities.template segment<3>(index),
+                                                                           forces.template segment<3>(index));
+    complementarity = math::max(complementarity, cone_complementarity);
+    index += 3;
+  }
+
+  return complementarity;
+}
+
+template<typename Scalar, typename ConstraintAllocator, typename VectorLikeIn, typename VectorLikeOut>
+void computeComplementarityShift(const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+                                 const Eigen::DenseBase<VectorLikeIn> & velocities,
+                                 const Eigen::DenseBase<VectorLikeOut> & shift_)
+{
+  assert(velocities.size() == shift_.size());
+  Eigen::DenseIndex index = 0;
+  VectorLikeOut & shift = shift_.const_cast_derived();
+  for(const auto & cone: cones)
+  {
+    shift.template segment<3>(index) = cone.computeNormalCorrection(velocities.template segment<3>(index));
+    index += 3;
+  }
+}
+
+}  // namespace internal
+
+  template<typename _Scalar>
+  template<typename DelassusExpression, typename VectorLike, typename ConstraintAllocator, typename VectorLikeOut, typename VectorLikeR>
+  bool ADMMContactSolverTpl<_Scalar>::solve(DelassusExpression & delassus,
+                                            const Eigen::MatrixBase<VectorLike> & g,
+                                            const std::vector<CoulombFrictionConeTpl<Scalar>,ConstraintAllocator> & cones,
+                                            const Eigen::DenseBase<VectorLikeOut> & y_sol,
+                                            const Scalar mu_prox,
+                                            const Eigen::MatrixBase<VectorLikeR> & R,
+                                            const Scalar tau)
+
+  {
+    using namespace internal;
+
+    PINOCCHIO_CHECK_INPUT_ARGUMENT(tau <= Scalar(1) && tau > Scalar(0),"tau should lie in ]0,1].");
+    PINOCCHIO_CHECK_INPUT_ARGUMENT(mu_prox >= 0,"mu_prox should be positive.");
+    PINOCCHIO_CHECK_INPUT_ARGUMENT((R.array() >= 0).all(),"mu_prox should be positive.");
+//    PINOCCHIO_CHECK_INPUT_ARGUMENT(math::max(R.maxCoeff(),mu_prox) >= 0,"mu_prox and mu_R cannot be both equal to zero.");
+
+    const Scalar mu_R = R.maxCoeff();
+
+    const Scalar L = delassus.computeLargestEigenValue(5); // Largest eigen_value estimate.
+    const Scalar m = math::max(mu_R,mu_prox);
+    const Scalar cond = L / m;
+
+    std::cout << "L: " << L << std::endl;
+    std::cout << "m: " << m << std::endl;
+    std::cout << "cond: " << cond << std::endl;
+    Scalar rho = math::sqrt(m * L) * math::pow(cond,rho_power);
+    std::cout << "rho_power: " << rho_power << std::endl;
+    std::cout << "rho: " << rho << std::endl;
+    Scalar prox_value = mu_prox + tau * rho;
+    std::cout << "y_sol: " << y_sol.transpose() << std::endl;
+
+    int it = 0;
+
+    PINOCCHIO_EIGEN_MALLOC_NOT_ALLOWED();
+
+    // Update the cholesky decomposition
+    rhs = R + VectorXs::Constant(this->problem_size,prox_value);
+    delassus.updateDamping(rhs);
+    cholesky_update_count = 1;
+
+    // Initial update of the variables
+    x_ = y_sol;
+    computeConeProjection(cones, x_, y_);
+    delassus.applyOnTheRight(y_,z_); // z = G * y
+    z_.noalias() += -prox_value * y_ + g;
+    computeDualConeProjection(cones, z_, z_);
+    computeComplementarityShift(cones, z_, s_);
+
+    Scalar
+    complementarity,
+    proximal_metric, // proximal metric between two successive iterates.
+    primal_feasibility,
+    dual_feasibility;
+    bool abs_prec_reached = false, rel_prec_reached = false;
+
+    Scalar y_previous_norm_inf = y_.template lpNorm<Eigen::Infinity>();
+
+    std::cout << std::setprecision(12);
+    std::cout << "x0: " << x_.transpose() << std::endl;
+    std::cout << "y0: " << y_.transpose() << std::endl;
+    std::cout << "z0: " << z_.transpose() << std::endl;
+    std::cout << "s0: " << s_.transpose() << std::endl;
+
+    for(; it < Base::max_it; ++it)
+    {
+      std::cout << "it: " << it << std::endl;
+      y_previous = y_;
+      complementarity = Scalar(0);
+
+      // s-update
+      computeComplementarityShift(cones, z_, s_);
+
+      // x-update
+      rhs = -(g + s_ - (rho*tau) * y_ - mu_prox * x_ - z_);
+      delassus.solveInPlace(rhs);
+      x_ = rhs;
+
+      // z-update
+      z_ -= (tau*rho) * (x_ - y_);
+
+      // y-update
+      rhs -= z_/(tau*rho);
+      computeConeProjection(cones, rhs, y_);
+
+      // z-update
+      z_ -= (tau*rho) * (x_ - y_);
+//      computeDualConeProjection(cones, z_, z_);
+
+      std::cout << "\tx: " << x_.transpose() << std::endl;
+      std::cout << "\ty: " << y_.transpose() << std::endl;
+      std::cout << "\tz: " << z_.transpose() << std::endl;
+      std::cout << "\ts: " << s_.transpose() << std::endl;
+
+      // check termination criteria
+      primal_feasibility_vector = x_ - y_;
+      delassus.applyOnTheRight(x_,dual_feasibility_vector);
+      dual_feasibility_vector.noalias() += g + s_ - prox_value * x_ - z_;
+
+      primal_feasibility = primal_feasibility_vector.template lpNorm<Eigen::Infinity>();
+      dual_feasibility = dual_feasibility_vector.template lpNorm<Eigen::Infinity>();
+      complementarity = computeMaxConeComplementarity(cones,z_,y_);
+
+      // Checking stopping residual
+      if(   check_expression_if_real<Scalar,false>(complementarity <= this->absolute_precision)
+         && check_expression_if_real<Scalar,false>(dual_feasibility <= this->absolute_precision))
+        abs_prec_reached = true;
+      else
+        abs_prec_reached = false;
+
+      proximal_metric = (y_ - y_previous).template lpNorm<Eigen::Infinity>();
+      const Scalar y_norm_inf = y_.template lpNorm<Eigen::Infinity>();
+      if(check_expression_if_real<Scalar,false>(proximal_metric <= this->relative_precision * math::max(y_norm_inf,y_previous_norm_inf)))
+        rel_prec_reached = true;
+      else
+        rel_prec_reached = false;
+
+      if(abs_prec_reached || rel_prec_reached)
+        break;
+
+      // Account for potential update of rho
+      const Scalar rho_power_factor = 0.05;
+      bool update_delassus_factorization = false;
+      if(primal_feasibility > ratio_primal_dual * dual_feasibility)
+      {
+        rho *= math::pow(cond,rho_power_factor);
+        rho_power += rho_power_factor;
+        update_delassus_factorization = true;
+      }
+      else if(dual_feasibility > ratio_primal_dual * primal_feasibility)
+      {
+        rho *= math::pow(cond,-rho_power_factor);
+        rho_power -= rho_power_factor;
+        update_delassus_factorization = true;
+      }
+
+      if(update_delassus_factorization)
+      {
+        prox_value = mu_prox + tau * rho;
+        rhs = R + VectorXs::Constant(this->problem_size,prox_value);
+        delassus.updateDamping(rhs);
+        cholesky_update_count++;
+      }
+
+      y_previous_norm_inf = y_norm_inf;
+      std::cout << "------" << std::endl;
+    }
+
+    PINOCCHIO_EIGEN_MALLOC_ALLOWED();
+
+    this->absolute_residual = math::max(complementarity,dual_feasibility);
+    this->relative_residual = proximal_metric;
+    this->it = it;
+    y_sol.const_cast_derived() = y_;
+
+    if(abs_prec_reached || rel_prec_reached)
+      return true;
+
+    return false;
+  }
+}
+
+#endif // ifndef __pinocchio_algorithm_admm_solver_hxx__
