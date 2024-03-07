0581d760d0e587e140cb12c28025f20be9acc747
Justin Carpentier <justin.carpentier@inria.fr>
2024-03-07 18:08:47 +0100

    test: remove compilation warning

--- unittest/serialization.cpp
+++ unittest/serialization.cpp
@@ -372,7 +372,6 @@ struct init<pinocchio::JointModelUniversalTpl<Scalar, Options>>
 
   static JointModel run()
   {
-    typedef typename JointModel::Vector3 Vector3;
     JointModel jmodel(pinocchio::XAxis::vector(), pinocchio::YAxis::vector());
 
     jmodel.setIndexes(0, 0, 0);
