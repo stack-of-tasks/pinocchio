1ad20f00368d17f0bdc25e8a728c7ef2d72cfb4d
Florent Lamiraux <florent@laas.fr>
2024-03-26 13:59:29 +0100

    [doc] Improve documentation of ReferenceFrame.

--- include/pinocchio/multibody/fwd.hpp
+++ include/pinocchio/multibody/fwd.hpp
@@ -39,13 +39,17 @@ namespace pinocchio
   struct GeometryData;
   
   ///
-  /// \brief List of Reference Frames supported by Pinocchio.
+  /// \brief Various conventions to express the velocity of a moving frame
   ///
+  /// The velocity of a moving frame is uniquely defined by the velocity of a
+  /// point \f$P\f$ of the frame and the angular velocity of the frame. Several
+  /// conventions exist in pinocchio depending on the point \f$P\f$ we choose
+  /// and on the basis in which the above velocities are projected.
   enum ReferenceFrame
   {
-    WORLD = 0, ///<  The WORLD frame convention corresponds to the frame concident with the Universe/Inertial frame but moving with the moving part (Joint, Frame, etc.).
-    LOCAL = 1, ///<  The LOCAL frame convention corresponds to the frame directly attached to the moving part (Joint, Frame, etc.) where the coordinates basis matches the local coordinates system associated with the moving part. It also called the BODY representation in the litterature.
-    LOCAL_WORLD_ALIGNED = 2 ///<  The LOCAL_WORLD_ALIGNED frame convention corresponds to the frame centered on the moving part (Joint, Frame, etc.) but with axes aligned with the frame of the Universe. This a MIXED representation betwenn the LOCAL and the WORLD conventions.
+    WORLD = 0, ///<  \f$P\f$ is the point coinciding with the origin of the world frame and the velocities are projected in the basis of the world frame.
+    LOCAL = 1, ///<  \f$P\f$ is the origin of the moving frame and the velocities are projected in the basis of the moving frame.
+    LOCAL_WORLD_ALIGNED = 2 ///< \f$P\f$ is the origin of the moving frame and the velocities are projected in the basis of the world frame.
   };
 
   ///
