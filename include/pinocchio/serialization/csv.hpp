116c7a178ef516a09339ce5ba198ecda8fbe46a5
Justin Carpentier <justin.carpentier@inria.fr>
2024-04-12 16:31:46 +0200

    serialization: add toCSVfile for Eigen objects

---
+++ include/pinocchio/serialization/csv.hpp
@@ -0,0 +1,27 @@
+  //
+  // Copyright (c) 2024 INRIA
+  //
+
+#ifndef __pinocchio_serialization_csv_hpp__
+#define __pinocchio_serialization_csv_hpp__
+
+#include "pinocchio/serialization/fwd.hpp"
+
+#include <Eigen/Core>
+#include <fstream>
+
+namespace pinocchio
+{
+  
+  template <typename Derived>
+  void toCSVfile(const std::string & filename,
+                 const Eigen::MatrixBase<Derived> & matrix)
+  {
+    const Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+    std::ofstream file(filename.c_str());
+    file << matrix.format(CSVFormat);
+  }
+}
+
+#endif // ifndef __pinocchio_serialization_csv_hpp__
+
