a3dfcf12ad86305986a0666c6ffdb6858fdab303
Joris Vaillant <joris.vaillant@inria.fr>
2024-11-12 14:49:26 +0100

    cmake: Try to use pre compiled header to accelerate the build

---
+++ include/pinocchio/bindings/python/pch.hpp
@@ -0,0 +1,8 @@
+#include "pinocchio/container/boost-container-limits.hpp"
+#include <eigenpy/eigenpy.hpp>
+#include <boost/python.hpp>
+#include <boost/variant.hpp>
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <Eigen/Geometry>
+#include <unsupported/Eigen/CXX11/Tensor>
