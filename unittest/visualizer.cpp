59bbde0aee3e62ca9725f33cee2c7ae36b9d3ec5
ManifoldFR <wilson.jallet@polytechnique.org>
2025-02-14 19:07:31 +0100

    [unittesŧ] add test body

--- unittest/visualizer.cpp
+++ unittest/visualizer.cpp
@@ -1,4 +1,5 @@
 #include "pinocchio/visualizers/base-visualizer.hpp"
+#include "pinocchio/multibody/sample-models.hpp"
 
 #include <boost/test/unit_test.hpp>
 
@@ -6,8 +7,9 @@ using namespace pinocchio::visualizers;
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-struct Viz : BaseVisualizer
+struct TestViz : BaseVisualizer
 {
+  using BaseVisualizer::BaseVisualizer;
   void loadViewerModel() override
   {
   }
@@ -15,5 +17,33 @@ struct Viz : BaseVisualizer
   {
   }
 };
+
+BOOST_AUTO_TEST_CASE(viz_ctor_create_datas)
+{
+  pinocchio::Model model;
+  pinocchio::buildModels::manipulator(model);
+  pinocchio::GeometryModel geom_model; // empty
+  TestViz viz{model, geom_model};
+  BOOST_CHECK(!viz.hasExternalData());
+  BOOST_CHECK(!viz.hasCollisionModel());
+  // dtor destroys internal datas
+}
+
+BOOST_AUTO_TEST_CASE(viz_ctor_borrow_datas)
+{
+  pinocchio::Model model;
+  pinocchio::buildModels::manipulator(model);
+  pinocchio::GeometryModel geom_model; // empty
+  pinocchio::Data data{model};
+  pinocchio::GeometryData geom_data{geom_model};
+
+  {
+    TestViz viz{model, geom_model, nullptr, data, geom_data, nullptr};
+    BOOST_CHECK(viz.hasExternalData());
+    BOOST_CHECK(!viz.hasCollisionModel());
+    // viz destroyed, does not destroy datas
+  }
+  // no double free segfault
+}
 
 BOOST_AUTO_TEST_SUITE_END()
