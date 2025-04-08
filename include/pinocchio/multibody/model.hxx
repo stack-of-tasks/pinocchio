eadc90c4bb84d12a0abe9798ef6bb316e78e2fd7
Florent Lamiraux <florent@laas.fr>
2025-04-08 09:16:04 +0000

    [Model] Improve error message.

--- include/pinocchio/multibody/model.hxx
+++ include/pinocchio/multibody/model.hxx
@@ -468,10 +468,13 @@ namespace pinocchio
   {
     typename PINOCCHIO_ALIGNED_STD_VECTOR(Frame)::const_iterator it =
       std::find_if(frames.begin(), frames.end(), details::FilterFrame(name, type));
+    std::ostringstream os;
+    os << "Several frames match the filter - please specify the FrameType (name=\""
+       << name << "\", type=\"" << type << "\")";
     PINOCCHIO_CHECK_INPUT_ARGUMENT(
       ((it == frames.end()
         || (std::find_if(boost::next(it), frames.end(), details::FilterFrame(name, type)) == frames.end()))),
-      "Several frames match the filter - please specify the FrameType");
+      os.str().c_str());
     return FrameIndex(it - frames.begin());
   }
 
