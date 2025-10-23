e5f2b5c587af85fbcc9290aa42619e510e6627f1
Megane Millan <millan.megane@gmail.com>
2025-10-23 10:04:29 +0200

    Unittest - put full knee trajectory

--- unittest/joint-spline.cpp
+++ unittest/joint-spline.cpp
@@ -39,6 +39,73 @@ void addJointAndBody(
   model.appendBodyToJoint(idx, Y);
 }
 
+std::vector<SE3> getTrajectory(std::vector<SE3> & ctrlFrames)
+{
+  Eigen::Matrix3d rotation;
+  Eigen::Vector3d translation;
+  rotation << -0.500004, 0, -0.866023, 0, 1, 0, 0.866023, 0, -0.500004;
+  translation << -0.0032, -0.4226, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << -0.173649, 0, -0.984808, 0, 1, 0, 0.984808, 0, -0.173649;
+  translation << 0.00179, -0.416947, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.173652, 0, -0.984807, 0, 1, 0, 0.984807, 0, 0.173652;
+  translation << 0.00411, -0.411057, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.342021, 0, -0.939692, 0, 1, 0, 0.939692, 0, 0.342021;
+  translation << 0.00438827, -0.4082, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.499998, 0, -0.866027, 0, 1, 0, 0.866027, 0, 0.499998;
+  translation << 0.0041, -0.405495, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.766044, 0, -0.642788, 0, 1, 0, 0.642788, 0, 0.766044;
+  translation << 0.00212, -0.400825, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.866025, 0, -0.5, 0, 1, 0, 0.5, 0, 0.866025;
+  translation << 0.000757726, -0.399, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.939693, 0, -0.34202, 0, 1, 0, 0.34202, 0, 0.939693;
+  translation << -0.001, -0.3976, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.984808, 0, -0.173648, 0, 1, 0, 0.173648, 0, 0.984808;
+  translation << -0.0031, -0.3966, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.987363, 0, 0.158478, 0, 1, 0, -0.158478, 0, 0.987363;
+  translation << -0.00513719, -0.395264, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.980591, 0, 0.196066, 0, 1, 0, -0.196066, 0, 0.980591;
+  translation << -0.005227, -0.395149, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.94362, 0, 0.33103, 0, 1, 0, -0.33103, 0, 0.94362;
+  translation << -0.005435, -0.394792, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.882249, 0, 0.470783, 0, 1, 0, -0.470783, 0, 0.882249;
+  translation << -0.005574, -0.394507, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << 0.0493163, 0, 0.998783, 0, 1, 0, -0.998783, 0, 0.0493163;
+  translation << -0.005435, -0.394812, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  rotation << -0.500004, 0, 0.866023, 0, 1, 0, -0.866023, 0, -0.500004;
+  translation << -0.00525, -0.396, 0;
+  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
+
+  return ctrlFrames;
+}
+
 BOOST_AUTO_TEST_SUITE(JointSpline)
 
 BOOST_AUTO_TEST_CASE(makeKnots)
@@ -300,74 +367,6 @@ BOOST_AUTO_TEST_CASE(vsRevoluteX)
   BOOST_CHECK(jdata.c.isApprox(jdataRx.c, 1e-12));
 }
 
-BOOST_AUTO_TEST_CASE(vsFiniteDiff)
-{
-  using namespace pinocchio;
-  typedef SE3::Vector3 Vector3;
-  typedef SE3::Matrix3 Matrix3;
-
-  typedef typename JointModelSpline::ConfigVector_t CV;
-  typedef typename JointModelSpline::TangentVector_t TV;
-  typedef typename LieGroup<JointModelSpline>::type LieGroupType;
-
-  std::vector<SE3> ctrlFrames;
-  ctrlFrames.push_back(SE3::Identity());
-  ctrlFrames.push_back(SE3::Random());
-  ctrlFrames.push_back(SE3::Random());
-  ctrlFrames.push_back(SE3::Random());
-
-  JointModelSpline jmodel(ctrlFrames);
-  JointDataSpline jdata = jmodel.createData();
-
-  jmodel.setIndexes(0, 0, 0);
-
-  double eps = 1e-8;
-  CV q_ref = LieGroupType().randomConfiguration(CV::Zero(), CV::Ones());
-
-  q_ref[0] = 0.6;
-  CV q(q_ref);
-
-  const Eigen::DenseIndex nv = jdata.S.nv();
-  TV q_dot(nv);
-  TV q_dot_ref(nv);
-  q_dot.setZero();
-
-  q_dot[0] = eps;
-  q_dot_ref[0] = 0.3;
-
-  q = LieGroupType().integrate(q_ref, q_dot);
-
-  {
-    // Check S
-    jmodel.calc(jdata, q_ref);
-    SE3 M_ref(jdata.M);
-    Eigen::Matrix<double, 6, JointModelSpline::NV> S(6, JointModelSpline::NV),
-      S_ref(jdata.S.matrix());
-
-    jmodel.calc(jdata, q);
-    SE3 M_ = jdata.M;
-
-    S.col(0) = log6(M_ref.inverse() * M_).toVector();
-    S.col(0) /= eps;
-
-    BOOST_CHECK(S.isApprox(S_ref, eps * 1e1));
-  }
-  // Check bias
-  // {
-  //   jmodel.calc(jdata, q_ref, q_dot_ref);
-  //   const Motion & c_ref = jdata.c;
-  //   Eigen::Matrix<double, 6, JointModelSpline::NV> S_ref(jdata.S.matrix());
-
-  //   jmodel.calc(jdata, q);
-  //   Eigen::Matrix<double, 6, JointModelSpline::NV> S_(jdata.S.matrix());
-
-  //   Motion dSdq_fd((S_ - S_ref) / eps);
-  //   Motion c_fd = dSdq_fd * q_dot_ref[0];
-
-  //   BOOST_CHECK(c_ref.isApprox(c_fd, eps * 1e1));
-  // }
-}
-
 /// @brief Test out rnea vs aba
 /// Control frames were taken from an opensim model and represent the movement of the knee joint
 BOOST_AUTO_TEST_CASE(abaVSrnea)
@@ -380,32 +379,7 @@ BOOST_AUTO_TEST_CASE(abaVSrnea)
   Inertia inertia(1., Vector3(0., 0., 0.0), Matrix3::Identity());
 
   std::vector<SE3> ctrlFrames;
-  Eigen::Matrix3d rotation;
-  Eigen::Vector3d translation;
-  rotation << -0.500004, 0, -0.866023, 0, 1, 0, 0.866023, 0, -0.500004;
-  translation << -0.0032, -0.4226, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
-  rotation << -0.173649, 0, -0.984808, 0, 1, 0, 0.984808, 0, -0.173649;
-  translation << 0.00179, -0.416947, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
-  rotation << 0.173652, 0, -0.984807, 0, 1, 0, 0.984807, 0, 0.173652;
-  translation << 0.00411, -0.411057, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
-  rotation << 0.342021, 0, -0.939692, 0, 1, 0, 0.939692, 0, 0.342021;
-  translation << 0.00438827, -0.4082, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
-  rotation << 0.499998, 0, -0.866027, 0, 1, 0, 0.866027, 0, 0.499998;
-  translation << 0.0041, -0.405495, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
-  rotation << 0.766044, 0, -0.642788, 0, 1, 0, 0.642788, 0, 0.766044;
-  translation << 0.00212, -0.400825, 0;
-  ctrlFrames.push_back(pinocchio::SE3(rotation, translation));
-
+  getTrajectory(ctrlFrames);
   addJointAndBody(
     modelSpline, JointModelSpline(ctrlFrames), 0, SE3::Identity(), "kneeSpline", inertia);
   Data dataSplineRnea(modelSpline);
