9caf92aa51ba93ef9f6725d52b0a33bfb0514d96
Megane Millan <millan.megane@gmail.com>
2025-10-27 10:45:25 +0100

    Bindings - add modelgraph bindings constant

--- bindings/python/parsers/graph/expose-edges.cpp
+++ bindings/python/parsers/graph/expose-edges.cpp
@@ -53,6 +53,9 @@ namespace pinocchio
 
     const int JointMimic::nq;
     const int JointMimic::nv;
+
+    const int JointSpline::nq;
+    const int JointSpline::nv;
   } // namespace graph
 
   namespace python
