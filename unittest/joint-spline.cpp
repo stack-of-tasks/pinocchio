2ddef28bb2cbf523d75ad81b6793b307d03b535c
Megane Millan <millan.megane@gmail.com>
2025-10-27 12:00:15 +0100

    Unittest - add a finite difference test for S and bias c for JointSpline

--- unittest/joint-spline.cpp
+++ unittest/joint-spline.cpp
@@ -39,6 +39,8 @@ void addJointAndBody(
   model.appendBodyToJoint(idx, Y);
 }
 
+/// @brief Get a predefined  spline joint trajectory.
+/// This was taken from the model opensim gait10dof18mus.osim, and correspond the knee joint.
 void getTrajectory(std::vector<SE3> & ctrlFrames)
 {
   Eigen::Matrix3d rotation;
@@ -106,6 +108,7 @@ void getTrajectory(std::vector<SE3> & ctrlFrames)
 
 BOOST_AUTO_TEST_SUITE(JointSpline)
 
+/// @brief Test on the knot vector generation
 BOOST_AUTO_TEST_CASE(makeKnots)
 {
   int degree = 3;
@@ -130,6 +133,7 @@ BOOST_AUTO_TEST_CASE(makeKnots)
   BOOST_CHECK(jmodel.knots == knots_expected);
 }
 
+/// @brief Test to make sure the relative motions are correct
 BOOST_AUTO_TEST_CASE(relativeMotions)
 {
   int degree = 3;
@@ -151,6 +155,7 @@ BOOST_AUTO_TEST_CASE(relativeMotions)
       pinocchio::log6(ctrlFrames[i].inverse() * ctrlFrames[i + 1])));
 }
 
+/// @brief Test on the basisSpline function and its first derivative
 BOOST_AUTO_TEST_CASE(basisSplineFunctions)
 {
   int degree = 3;
@@ -200,6 +205,7 @@ BOOST_AUTO_TEST_CASE(basisSplineFunctions)
   }
 }
 
+/// @brief Test the spanning function
 BOOST_AUTO_TEST_CASE(findSpan)
 {
   int degree = 2;
@@ -226,52 +232,8 @@ BOOST_AUTO_TEST_CASE(findSpan)
   BOOST_CHECK(indexes.end_idx == ctrlFrames.size());
 }
 
-BOOST_AUTO_TEST_CASE(expectedMotion)
-{
-  using namespace pinocchio;
-  typedef SE3::Vector3 Vector3;
-  typedef SE3::Matrix3 Matrix3;
-
-  Motion expected_v_J(Motion::Zero());
-  Motion expected_c_J(Motion::Zero());
-
-  SE3 expected_configuration(SE3(Eigen::Matrix3d::Identity(), Eigen::Vector3d(0., 0., 0.2)));
-
-  std::vector<SE3> ctrlFrames;
-  ctrlFrames.push_back(SE3::Identity());
-  ctrlFrames.push_back(SE3(Eigen::Matrix3d::Identity(), Eigen::Vector3d(0., 0., 1.)));
-
-  JointModelSpline jmodel(ctrlFrames, 1);
-  JointDataSpline jdata = jmodel.createData();
-
-  jmodel.setIndexes(0, 0, 0);
-
-  Eigen::VectorXd q(Eigen::VectorXd::Zero(1));
-
-  // -------
-  q << 0.2;
-
-  jmodel.calc(jdata, q);
-
-  BOOST_CHECK(expected_configuration.rotation().isApprox(jdata.M.rotation(), 1e-12));
-  BOOST_CHECK(expected_configuration.translation().isApprox(jdata.M.translation(), 1e-12));
-
-  // -------
-  Eigen::VectorXd q_dot(Eigen::VectorXd::Zero(1));
-  q << 0.3;
-  q_dot << 0.4;
-
-  jmodel.calc(jdata, q, q_dot);
-
-  expected_configuration.translation() << 0, 0, 0.3;
-  expected_v_J.linear() << 0., 0., 0.4;
-
-  BOOST_CHECK(expected_configuration.rotation().isApprox(jdata.M.rotation(), 1e-12));
-  BOOST_CHECK(expected_configuration.translation().isApprox(jdata.M.translation(), 1e-12));
-  BOOST_CHECK(expected_v_J.toVector().isApprox(((Motion)jdata.v).toVector(), 1e-12));
-  BOOST_CHECK(expected_c_J.isApprox((Motion)jdata.c, 1e-12));
-}
-
+/// @brief Comparing a simple spline joint with a PZ
+/// Make sure pose and joint subspace are the same
 BOOST_AUTO_TEST_CASE(vsPrismaticZ)
 {
   using namespace pinocchio;
@@ -317,6 +279,8 @@ BOOST_AUTO_TEST_CASE(vsPrismaticZ)
   BOOST_CHECK(jdata.c.isApprox(jdataPz.c, 1e-12));
 }
 
+/// @brief Comparing a simple spline joint with a RX
+/// Make sure pose and joint subspace are the same
 BOOST_AUTO_TEST_CASE(vsRevoluteX)
 {
   using namespace pinocchio;
@@ -366,7 +330,6 @@ BOOST_AUTO_TEST_CASE(vsRevoluteX)
 }
 
 /// @brief Test out rnea vs aba
-/// Control frames were taken from an opensim model and represent the movement of the knee joint
 BOOST_AUTO_TEST_CASE(abaVSrnea)
 {
   typedef SE3::Vector3 Vector3;
@@ -395,6 +358,71 @@ BOOST_AUTO_TEST_CASE(abaVSrnea)
   aAba = pinocchio::aba(modelSpline, dataSplineAba, q, vq, tauRnea);
 
   BOOST_CHECK(aq.isApprox(aAba));
+}
+
+/// @brief Test S and bias c computation via finite differences
+BOOST_AUTO_TEST_CASE(vsFiniteDifference)
+{
+  using namespace pinocchio;
+
+  typedef typename JointModelSpline::ConfigVector_t CV;
+  typedef typename JointModelSpline::TangentVector_t TV;
+  typedef typename LieGroup<JointModelSpline>::type LieGroupType;
+
+  std::vector<SE3> ctrlFrames;
+  getTrajectory(ctrlFrames);
+
+  JointModelSpline jmodel(ctrlFrames);
+  JointDataSpline jdata = jmodel.createData();
+
+  jmodel.setIndexes(0, 0, 0);
+
+  double eps = 1e-8;
+  CV q_ref(1);
+  q_ref[0] = 0.6;
+  CV q(q_ref);
+
+  const Eigen::DenseIndex nv = jdata.S.nv();
+  TV q_dot(nv);
+  TV q_dot_ref(nv);
+  q_dot.setZero();
+
+  q_dot[0] = eps;
+  q_dot_ref[0] = 0.3;
+
+  q = LieGroupType().integrate(q_ref, q_dot);
+  // Check S
+  {
+    jmodel.calc(jdata, q_ref);
+    SE3 M_ref(jdata.M);
+    Eigen::Matrix<double, 6, JointModelSpline::NV> S(6, JointModelSpline::NV),
+      S_ref(jdata.S.matrix());
+
+    jmodel.calc(jdata, q);
+    SE3 M_ = jdata.M;
+
+    S.col(0) = log6(M_ref.inverse() * M_).toVector();
+    S.col(0) /= eps;
+
+    BOOST_CHECK(S.isApprox(S_ref, 1e-6));
+  }
+  // Check bias
+  {
+    jmodel.calc(jdata, q_ref, q_dot_ref);
+    const Motion & c_ref = jdata.c;
+    Eigen::Matrix<double, 6, JointModelSpline::NV> S_ref(jdata.S.matrix());
+
+    jmodel.calc(jdata, q);
+    Eigen::Matrix<double, 6, JointModelSpline::NV> S_(jdata.S.matrix());
+
+    Motion dSdq_fd((S_ - S_ref) / eps);
+    Motion c_fd = dSdq_fd * q_dot_ref[0] * q_dot_ref[0];
+
+    BOOST_CHECK(c_ref.isApprox(c_fd, 1e-6));
+
+    std::cout << c_fd << std::endl;
+    std::cout << c_ref << std::endl;
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
