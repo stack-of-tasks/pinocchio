38f32c8d63ee3f890affc18c21e43ab7673b1598
Megane Millan <millan.megane@gmail.com>
2025-10-30 15:20:53 +0100

    Algo/Splines - remove comments

--- include/pinocchio/algorithm/splines.hpp
+++ include/pinocchio/algorithm/splines.hpp
@@ -13,20 +13,6 @@ namespace pinocchio
     size_t end_idx;
   };
 
-  // template<typename Scalar, int Options>
-  // struct FindSpan
-  // {
-  //   template<typename ConfigVector, typename KnotsVector>
-  //   static SpanIndexes run(
-  //     const Eigen::MatrixBase<ConfigVector> & /*q*/,
-  //     const int /*degree*/,
-  //     const int nbCtrlFrames,
-  //     const Eigen::MatrixBase<KnotsVector> & /*knots*/)
-  //   {
-  //     return {0, static_cast<size_t>(nbCtrlFrames)};
-  //   }
-  // };
-
   template<typename Scalar, int Options>
   struct FindSpan
   {
