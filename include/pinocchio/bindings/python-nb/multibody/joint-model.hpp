9d4f0e678192e138ddcce26f18401c128893f074
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 20:15:35 +0100

    nanobind : bind vector of JointModel

--- include/pinocchio/bindings/python-nb/multibody/joint-model.hpp
+++ include/pinocchio/bindings/python-nb/multibody/joint-model.hpp
@@ -6,6 +6,7 @@
 
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/variant.h>
+#include <nanobind/stl/bind_vector.h>
 
 PINOCCHIO_PYTHON_NAMESPACE_BEGIN
 template<class JointModel>
@@ -14,5 +15,7 @@ void exposeJointModel(nb::module_ m)
   nb::class_<JointModel>(m, "JointModel", "Generic Joint Model")
     .def(nb::init<>(), "Default constructor")
     .def(nb::init<const JointModel &>(), nb::arg("other"), "Copy constructor.");
+
+  nb::bind_vector<std::vector<JointModel>>(m, "JointModelStdVec");
 }
 PINOCCHIO_PYTHON_NAMESPACE_END
