75e58c81803e6b5c2643792110d36cb150cc6bf2
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 20:22:44 +0100

    nanobind : add stump __init__ file

---
+++ bindings/nanobind/pinocchio/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2026 INRIA
+#
+# ruff: noqa: E402, F401, F403, F405
+
+from .pinocchio_pywrap_default import *
+from .pinocchio_pywrap_default import (
+    __bindings_framework_,
+    __raw_version__,
+    __version__,
+)
