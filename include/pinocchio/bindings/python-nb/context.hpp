8617951aa843cac9cae800eace13179a005699ba
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 18:19:20 +0100

    declare some opaque containers

--- include/pinocchio/bindings/python-nb/context.hpp
+++ include/pinocchio/bindings/python-nb/context.hpp
@@ -13,3 +13,29 @@
 // #endif
 
 // #include PINOCCHIO_PYTHON_CONTEXT_FILE
+
+#define PINOCCHIO_PYTHON_NAMESPACE_BEGIN                                                           \
+  namespace pinocchio::python_nb                                                                   \
+  {
+#define PINOCCHIO_PYTHON_NAMESPACE_END } // pinocchio::python_nb
+
+#include "pinocchio/fwd.hpp"
+#include "pinocchio/spatial/fwd.hpp"
+#include "pinocchio/multibody/fwd.hpp"
+#include "pinocchio/multibody/joint/fwd.hpp"
+
+PINOCCHIO_PYTHON_NAMESPACE_BEGIN
+using Scalar = PINOCCHIO_PYTHON_SCALAR_TYPE_DEFAULT;
+static constexpr int Options = 0;
+
+// Spatial
+using SE3 = SE3Tpl<Scalar, Options>;
+using Motion = MotionTpl<Scalar, Options>;
+using Force = ForceTpl<Scalar, Options>;
+using Inertia = Inertia;
+
+// Multibody
+
+// Joints
+using JointModel = JointModelTpl<Scalar, Options>;
+PINOCCHIO_PYTHON_NAMESPACE_END
