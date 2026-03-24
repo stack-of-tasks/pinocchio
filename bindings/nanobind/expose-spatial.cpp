15f037ff8ae395f02fde29501b0f05cb5b4ef28a
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 19:00:22 +0100

    bindings/nanobind : add expose-spatial.cpp

---
+++ bindings/nanobind/expose-spatial.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 INRIA
+
+#include "pinocchio/bindings/python-nb/fwd.hpp"
+#include "pinocchio/bindings/python-nb/spatial/se3.hpp"
+#include "pinocchio/bindings/python-nb/spatial/inertia.hpp"
+
+namespace nb = nanobind;
+
+void exposeSpatial(nb::module_ m)
+{
+  using namespace pinocchio::python_nb;
+
+  exposeSE3<pinocchio::SE3>(m);
+  exposeInertia<pinocchio::Inertia>(m);
+}
