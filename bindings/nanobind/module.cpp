a691b6b58b2a06ee51304d254d7a23ea432926d5
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 14:48:04 +0100

    bindings/nanobind : expose version functions

--- bindings/nanobind/module.cpp
+++ bindings/nanobind/module.cpp
@@ -6,6 +6,7 @@
 #include <nanobind/stl/string.h>
 
 namespace nb = nanobind;
+using namespace nb::literals;
 
 // PINOCCHIO_PYTHON_MODULE_NAME is defined by the build system as the target name
 // (e.g. pinocchio_pywrap_default).
@@ -15,6 +16,20 @@ NB_MODULE(PINOCCHIO_PYTHON_MODULE_NAME, m)
   m.attr("__bindings_framework__") = "Nanobind";
   m.attr("__version__") = pinocchio::printVersion();
   m.attr("__raw_version__") = PINOCCHIO_VERSION;
+
+  m.attr("PINOCCHIO_MAJOR_VERSION") = PINOCCHIO_MAJOR_VERSION;
+  m.attr("PINOCCHIO_MINOR_VERSION") = PINOCCHIO_MINOR_VERSION;
+  m.attr("PINOCCHIO_PATCH_VERSION") = PINOCCHIO_PATCH_VERSION;
+
+  m.def(
+    "printVersion", pinocchio::printVersion, "delimiter"_a = ".",
+    "Returns the current version of Pinocchio as a string.\n"
+    "The user may specify the delimiter between the different semantic numbers.");
+
+  m.def(
+    "checkVersionAtLeast", &pinocchio::checkVersionAtLeast, "major"_a, "minor"_a, "patch"_a,
+    "Checks if the current version of Pinocchio is at least"
+    " the version provided by the input arguments.");
 
 #if defined(PINOCCHIO_PYTHON_INTERFACE_WITH_COLLISION_PYTHON_BINDINGS)
   m.import_("coal");
