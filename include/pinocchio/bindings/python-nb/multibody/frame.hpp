0509471e529a0fb60b790c77783fb5e19e86dd1e
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-24 19:16:30 +0100

    Finish exposing frame (no serialization)

--- include/pinocchio/bindings/python-nb/multibody/frame.hpp
+++ include/pinocchio/bindings/python-nb/multibody/frame.hpp
@@ -5,20 +5,35 @@
 #include "pinocchio/multibody.hpp"
 
 #include "../fwd.hpp"
+#include "../utils/printable.hpp"
 
 #include <nanobind/stl/bind_vector.h>
+#include <nanobind/stl/string.h>
 
 PINOCCHIO_PYTHON_NAMESPACE_BEGIN
 template<class Frame>
 void exposeFrame(nb::module_ m)
 {
+  using namespace nb::literals;
+
   nb::class_<Frame>(
     m, "Frame", "A Plucker coordinate frame related to a parent joint inside a kinematic tree.")
     .def(nb::init<>())
-    .def(nb::init<const Frame &>(), nb::arg("other"))
+    .def(nb::init<const Frame &>(), "other"_a)
+    .def(
+      nb::init<const std::string &, JointIndex, const SE3 &, FrameType>(), "name"_a,
+      "parent_joint"_a, "placement"_a, "inertia"_a = Inertia::Zero())
+    .def(
+      nb::init<const std::string &, JointIndex, FrameIndex, const SE3 &, FrameType>(), "name"_a,
+      "parent_joint"_a, "parent_frame"_a, "placement"_a, "inertia"_a = Inertia::Zero())
     .def_rw("name", &Frame::name)
     .def_rw("parentJoint", &Frame::parentJoint)
-    .def_rw("parentFrame", &Frame::parentFrame);
+    .def_rw("parentFrame", &Frame::parentFrame)
+    //
+    .def_rw("placement", &Frame::placement)
+    .def_rw("type", &Frame::type)
+    .def_rw("inertia", &Frame::inertia, "Inertia information attached to the frame.")
+    .def(PrintableVisitor<Frame>());
 
   nb::bind_vector<std::vector<Frame>>(m, "FrameStdVec");
 };
