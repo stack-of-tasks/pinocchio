2715146ca1a2b1c1ca2e0a762197f0e9a3727ca2
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-25 18:24:45 +0100

    nanobind/spatial : add binding for Symmetric3

--- bindings/nanobind/expose-spatial.cpp
+++ bindings/nanobind/expose-spatial.cpp
@@ -4,6 +4,7 @@
 #include "pinocchio/bindings/python-nb/spatial/force.hpp"
 #include "pinocchio/bindings/python-nb/spatial/se3.hpp"
 #include "pinocchio/bindings/python-nb/spatial/inertia.hpp"
+#include "pinocchio/bindings/python-nb/spatial/symmetric3.hpp"
 
 PINOCCHIO_PYTHON_NAMESPACE_BEGIN
 namespace nb = nanobind;
@@ -14,5 +15,6 @@ void exposeSpatial(nb::module_ m)
   exposeForce<pinocchio::Force>(m);
   exposeSE3<pinocchio::SE3>(m);
   exposeInertia<pinocchio::Inertia>(m);
+  exposeSymmetric3<pinocchio::Symmetric3>(m);
 }
 PINOCCHIO_PYTHON_NAMESPACE_END
