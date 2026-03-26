50b8c015774a400373cbd52925ad88c822ab9181
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-26 09:58:19 +0100

    bindings/nanobind/__init__ : fix typo

--- bindings/nanobind/pinocchio/__init__.py
+++ bindings/nanobind/pinocchio/__init__.py
@@ -5,7 +5,7 @@
 
 from .pinocchio_pywrap_default import *
 from .pinocchio_pywrap_default import (
-    __bindings_framework_,
+    __bindings_framework__,
     __raw_version__,
     __version__,
 )
