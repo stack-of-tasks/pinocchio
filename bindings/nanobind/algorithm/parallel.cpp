5c031dcdda3870a80e164d7c89f83e7e525942e8
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-27 16:52:04 +0100

    nanobind/algorithm : expose abaInParallel()

--- bindings/nanobind/algorithm/parallel.cpp
+++ bindings/nanobind/algorithm/parallel.cpp
@@ -19,20 +19,60 @@ void exposeParallelAlgorithms(nb::module_ m)
   // doesn't work because OpenMPException doesn't implement `what()`
   // nb::exception<OpenMPException>(m, "OpenMPException", PyExc_Exception);
 
+  // abaInParallel() -------------------------------------
   m.def(
-    "rneaInParallel",
+    "abaInParallel",
     [](
-      const size_t num_thread, ModelPool & pool, const MatrixXs & q, const MatrixXs & v,
-      const MatrixXs & a,
-      Eigen::Ref<MatrixXs> tau_out) { rneaInParallel(num_thread, pool, q, v, a, tau_out); },
-    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "as"_a, "taus_out"_a,
-    "Computes in parallel the RNEA and returns the result.\n\n"
+      const size_t num_threads, ModelPool & pool, const MatrixXs & q, const MatrixXs & v,
+      const MatrixXs & tau,
+      Eigen::Ref<MatrixXs> a_out) { abaInParallel(num_threads, pool, q, v, tau, a_out); },
+    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "taus"_a,
+    "as_out"_a
+    "Computes in parallel the ABA and returns the result.\n\n"
     "Parameters:\n"
-    "\tnum_thread: number of threads used for the computation\n"
+    "\tnum_threads: number of threads used for the computation\n"
     "\tpool: pool of model/data\n"
     "\tqs: the joint configuration vectors (size model.nq x batch_size)\n"
     "\tvs: the joint velocity vectors (size model.nv x batch_size)\n"
-    "\tas: the joint acceleration vectors (size model.nv x batch_size)\n");
+    "\ttaus: the joint torque vectors (size model.nv x batch_size)\n"
+    "\ta_out: output joint acceleration vectors (size model.nv x batch_size)\n");
+
+  m.def(
+    "abaInParallel",
+    [](
+      const size_t num_threads, ModelPool & pool, const MatrixXs & q, const MatrixXs & v,
+      const MatrixXs & tau) -> MatrixXs {
+      MatrixXs a_out(v.rows(), v.cols());
+      abaInParallel(num_threads, pool, q, v, tau, a_out);
+      return a_out;
+    },
+    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "taus"_a,
+    "Computes in parallel the ABA and returns the result.\n\n"
+    "Parameters:\n"
+    "\tnum_threads: number of threads used for the computation\n"
+    "\tpool: pool of model/data\n"
+    "\tqs: the joint configuration vectors (size model.nq x batch_size)\n"
+    "\tvs: the joint velocity vectors (size model.nv x batch_size)\n"
+    "\ttaus: the joint torque vectors (size model.nv x batch_size)\n"
+    "Returns:\n"
+    "\ta_out: output joint acceleration vectors (size model.nv x batch_size)\n");
+
+  // rneaaInParallel() ------------------------------------
+  m.def(
+    "rneaInParallel",
+    [](
+      const size_t num_threads, ModelPool & pool, const MatrixXs & q, const MatrixXs & v,
+      const MatrixXs & a,
+      Eigen::Ref<MatrixXs> tau_out) { rneaInParallel(num_threads, pool, q, v, a, tau_out); },
+    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "a"_a, "taus_out"_a,
+    "Computes in parallel the RNEA and returns the result.\n\n"
+    "Parameters:\n"
+    "\tnum_threads: number of threads used for the computation\n"
+    "\tpool: pool of model/data\n"
+    "\tqs: the joint configuration vectors (size model.nq x batch_size)\n"
+    "\tvs: the joint velocity vectors (size model.nv x batch_size)\n"
+    "\ta: the joint acceleration vectors (size model.nv x batch_size)\n"
+    "\ttaus_out: output joint torque vectors (size model.nv x batch_size)\n");
 
   m.def(
     "rneaInParallel",
@@ -43,14 +83,14 @@ void exposeParallelAlgorithms(nb::module_ m)
       rneaInParallel(num_threads, pool, q, v, a, tau);
       return tau;
     },
-    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "as"_a,
+    "num_threads"_a, "pool"_a, "qs"_a, "vs"_a, "a"_a,
     "Computes in parallel the RNEA and stores the result in tau.\n\n"
     "Parameters:\n"
-    "\tnum_thread: number of threads used for the computation\n"
+    "\tnum_threads: number of threads used for the computation\n"
     "\tpool: pool of model/data\n"
     "\tqs: the joint configuration vectors (size model.nq x batch_size)\n"
     "\tvs: the joint velocity vectors (size model.nv x batch_size)\n"
-    "\tas: the joint acceleration vectors (size model.nv x batch_size)\n"
+    "\ta: the joint acceleration vectors (size model.nv x batch_size)\n"
     "Returns:\n"
     "\tthe resulting joint torque vectors (size model.nv x batch_size)\n");
 
