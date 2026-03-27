95a94888096033062ce786f5b45c57b18c145136
ManifoldFR <wilson.jallet@polytechnique.org>
2026-03-27 14:54:40 +0100

    nanobind/math : expose LanczosDecomposition

--- bindings/nanobind/expose-math.cpp
+++ bindings/nanobind/expose-math.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 INRIA
 
 #include "pinocchio/bindings/python-nb/fwd.hpp"
+#include "pinocchio/bindings/python-nb/utils/comparable.hpp"
 
 #include "pinocchio/math.hpp"
 
@@ -37,10 +38,51 @@ static void exposeLinalg(nb::module_ m_)
     "inv", &inv<Eigen::Matrix<long double, Dynamic, Dynamic>>, "Computes the inverse of a matrix.");
 }
 
+static void exposeLanczosDecomposition(nb::module_ m)
+{
+  using LanczosDecomposition = LanczosDecompositionTpl<MatrixXs>;
+  using TridiagonalMatrix = LanczosDecomposition::TridiagonalMatrix;
+  using PlainMatrix = LanczosDecomposition::PlainMatrix;
+
+  nb::class_<LanczosDecomposition>(m, "LanczosDecomposition", "Lanczos decomposition")
+    .def(
+      nb::init<const MatrixXs &, Eigen::Index>(), "matrix"_a, "decomposition_size"_a,
+      "Default constructor from a given matrix and a given decomposition size.")
+    .def(
+      nb::init<Eigen::Index, Eigen::Index>(), "size"_a, "decomposition_size"_a,
+      "Default constructor for the Lanczos decomposition from a given matrix size.")
+    .def(
+      "compute", &LanczosDecomposition::compute<MatrixXs>, "matrix"_a,
+      "Computes the Lanczos decomposition for the given input matrix.")
+    // decompositon data
+    .def(
+      "Ts", (TridiagonalMatrix & (LanczosDecomposition::*)()) & LanczosDecomposition::Ts,
+      "Returns the tridiagonal matrix associated with the Lanczos decomposition.",
+      nb::rv_policy::reference_internal)
+    .def(
+      "Qs", (PlainMatrix & (LanczosDecomposition::*)()) & LanczosDecomposition::Qs,
+      "Returns the orthogonal basis associated with the Lanczos decomposition.",
+      nb::rv_policy::reference_internal)
+    //
+    .def(
+      "computeDecompositionResidual", &LanczosDecomposition::computeDecompositionResidual<MatrixXs>,
+      "matrix"_a,
+      "Computes the residual associated with the decomposition, namely, the quantity \f$ A "
+      "Q_s - Q_s T_s \f$")
+    // size
+    .def("size", &LanczosDecomposition::size, "Returns the size of the underlying matrix.")
+    .def(
+      "decompositionSize", &LanczosDecomposition::decompositionSize,
+      "Returns the size of the decomposition.")
+    // comparison
+    .def(ComparableVisitor<LanczosDecomposition>());
+}
+
 void exposeMathUtil(nb::module_ m)
 {
 
   exposeLinalg(m);
+  exposeLanczosDecomposition(m);
 }
 
 PINOCCHIO_PYTHON_NAMESPACE_END
