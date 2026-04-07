020bfaf965a5fdf78d9346e313e93294c0f95fb6
ManifoldFR <wilson.jallet@polytechnique.org>
2026-04-07 20:35:43 +0200

    nanobind/collision : expose pool of collision (GeometryPool, BroadphaseManager and TreeBroadPhaseManager pools)

--- bindings/nanobind/expose-coal.cpp
+++ bindings/nanobind/expose-coal.cpp
@@ -3,14 +3,24 @@
 #include "pinocchio/spatial.hpp"
 
 PINOCCHIO_PYTHON_NAMESPACE_BEGIN
+// collision/broadphase.cpp
 void exposeBroadphase(nb::module_ m);
+// collision/broadphase-callbacks.cpp
 void exposeBroadphaseCallbacks(nb::module_ m);
+// collision/collision.cpp
 void exposeCollision(nb::module_ m);
+#ifdef PINOCCHIO_PYTHON_INTERFACE_WITH_OPENMP
+// collision/pool.cpp
+void exposePoolCollision(nb::module_ m);
+#endif
 
 void exposeCoal(nb::module_ m)
 {
   exposeBroadphase(m);
   exposeBroadphaseCallbacks(m);
   exposeCollision(m);
+#ifdef PINOCCHIO_PYTHON_INTERFACE_WITH_OPENMP
+  exposePoolCollision(m);
+#endif
 }
 PINOCCHIO_PYTHON_NAMESPACE_END
