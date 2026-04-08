0cb83d5b4c97cceff75361e9cf21689f55e24505
ManifoldFR <wilson.jallet@polytechnique.org>
2026-04-08 16:12:49 +0200

    nanobind/multibody/joints : fix exposing joint {model, data} base API on generic JointModel, JointData

--- include/pinocchio/bindings/python-nb/multibody/joint-generic.hpp
+++ include/pinocchio/bindings/python-nb/multibody/joint-generic.hpp
@@ -2,9 +2,10 @@
 
 #pragma once
 
-#include "../fwd.hpp"
-#include "../utils/printable.hpp"
-#include "../utils/boost-variant.hpp"
+#include "pinocchio/bindings/python-nb/fwd.hpp"
+#include "pinocchio/bindings/python-nb/utils/printable.hpp"
+#include "pinocchio/bindings/python-nb/utils/boost-variant.hpp"
+#include "pinocchio/bindings/python-nb/multibody/joint-crtp-base.hpp"
 
 #include "pinocchio/multibody/joint.hpp"
 
@@ -25,6 +26,7 @@ void exposeJointModel(nb::module_ m)
     .def(nb::init<const JointModel &>(), nb::arg("other"), "Copy constructor.")
     .def(nb::init_implicit<const JointModelVariant &>(), nb::arg("joint_model"))
     .def("extract", [](JointModel & self) -> JointModelVariant & { return self; })
+    .def(JointModelBaseVisitor<JointModel>())
     .def(PrintableVisitor<JointModel>());
 
   nb::bind_vector<std::vector<JointModel>, nb::rv_policy::reference_internal>(
@@ -40,6 +42,7 @@ void exposeJointData(nb::module_ m)
   nb::class_<JointData>(m, "JointData", "Generic Joint Data")
     .def(nb::init_implicit<const JointDataVariant &>(), "joint_data"_a)
     .def("extract", [](JointData & self) -> JointDataVariant & { return self; })
+    .def(JointDataBaseVisitor<JointData>())
     .def(PrintableVisitor<JointData>());
 
   nb::bind_vector<std::vector<JointData>, nb::rv_policy::reference_internal>(m, "StdVec_JointData");
