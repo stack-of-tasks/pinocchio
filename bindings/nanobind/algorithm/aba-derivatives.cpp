24ce831e58d4c4e3a1bd304506ecfa368639e705
ManifoldFR <wilson.jallet@polytechnique.org>
2026-04-09 16:01:11 +0200

    nanobind/algorithm/aba-derivatives : apply no mimic policy

--- bindings/nanobind/algorithm/aba-derivatives.cpp
+++ bindings/nanobind/algorithm/aba-derivatives.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 INRIA
 
 #include "pinocchio/bindings/python-nb/fwd.hpp"
+#include "pinocchio/bindings/python-nb/utils/model-checker.hpp"
 
 #include "pinocchio/algorithm/aba-derivatives.hpp"
 
@@ -17,9 +18,9 @@ void exposeABADerivatives(nb::module_ m)
       -> nb::tuple {
       computeABADerivatives(model, data, q, v, tau);
       make_symmetric(data.Minv);
-      return nb::make_tuple<nb::rv_policy::reference_internal>(data.ddq_dq, data.ddq_dv, data.Minv);
+      return nb::make_tuple(make_ref(data.ddq_dq), make_ref(data.ddq_dv), make_ref(data.Minv));
     },
-    "model"_a, "data"_a, "q"_a, "v"_a, "tau"_a,
+    "model"_a, "data"_a, "q"_a, "v"_a, "tau"_a, nb::call_policy<mimic_not_supported_policy<0>>(),
     "Computes the ABA derivatives, store the result in data.ddq_dq, data.ddq_dv and data.Minv "
     "(aka ddq_dtau)\n"
     "which correspond to the partial derivatives of the joint acceleration vector output with "
@@ -40,9 +41,10 @@ void exposeABADerivatives(nb::module_ m)
       const std::vector<Force> & fext) -> nb::tuple {
       computeABADerivatives(model, data, q, v, tau, fext);
       make_symmetric(data.Minv);
-      return nb::make_tuple<nb::rv_policy::reference_internal>(data.ddq_dq, data.ddq_dv, data.Minv);
+      return nb::make_tuple(make_ref(data.ddq_dq), make_ref(data.ddq_dv), make_ref(data.Minv));
     },
     "model"_a, "data"_a, "q"_a, "v"_a, "tau"_a, "fext"_a,
+    nb::call_policy<mimic_not_supported_policy<0>>(),
     "Computes the ABA derivatives with external contact forces,\n"
     "store the result in data.ddq_dq, data.ddq_dv and data.Minv (aka ddq_dtau)\n"
     "which correspond to the partial derivatives of the acceleration output with respect to "
@@ -63,9 +65,9 @@ void exposeABADerivatives(nb::module_ m)
     [](const Model & model, Data & data) -> nb::tuple {
       computeABADerivatives(model, data);
       make_symmetric(data.Minv);
-      return nb::make_tuple<nb::rv_policy::reference_internal>(data.ddq_dq, data.ddq_dv, data.Minv);
+      return nb::make_tuple(make_ref(data.ddq_dq), make_ref(data.ddq_dv), make_ref(data.Minv));
     },
-    "model"_a, "data"_a,
+    "model"_a, "data"_a, nb::call_policy<mimic_not_supported_policy<0>>(),
     "Computes the ABA derivatives, store the result in data.ddq_dq, data.ddq_dv and data.Minv\n"
     "which correspond to the partial derivatives of the joint acceleration vector output with "
     "respect to the joint configuration,\n"
@@ -80,9 +82,9 @@ void exposeABADerivatives(nb::module_ m)
     [](const Model & model, Data & data, const std::vector<Force> & fext) -> nb::tuple {
       computeABADerivatives(model, data, fext);
       make_symmetric(data.Minv);
-      return nb::make_tuple<nb::rv_policy::reference_internal>(data.ddq_dq, data.ddq_dv, data.Minv);
+      return nb::make_tuple(make_ref(data.ddq_dq), make_ref(data.ddq_dv), make_ref(data.Minv));
     },
-    "model"_a, "data"_a, "fext"_a,
+    "model"_a, "data"_a, "fext"_a, nb::call_policy<mimic_not_supported_policy<0>>(),
     "Computes the ABA derivatives, store the result in data.ddq_dq, data.ddq_dv and data.Minv\n"
     "which correspond to the partial derivatives of the joint acceleration vector output with "
     "respect to the joint configuration,\n"
