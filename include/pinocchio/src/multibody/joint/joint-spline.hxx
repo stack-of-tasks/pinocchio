9be75c576e25cecf3f13390129579e6fb20373c5
Megane Millan <millan.megane@gmail.com>
2026-06-08 14:37:48 +0200

    core - make test casadi compatible

--- include/pinocchio/src/multibody/joint/joint-spline.hxx
+++ include/pinocchio/src/multibody/joint/joint-spline.hxx
@@ -192,7 +192,7 @@ namespace pinocchio
 
       for (Eigen::Index i = 1; i < knotVector.size(); ++i)
       {
-        if (knotVector[i] < knotVector[i - 1])
+        if (!check_expression_if_real<Scalar>(knotVector[i] >= knotVector[i - 1]))
           PINOCCHIO_THROW_PRETTY(
             std::invalid_argument, "JointSpline - Knot vector must be non-decreasing (knots must "
                                    "satisfy knots[i] <= knots[i+1]).");
