f5600c393176947da3c0564b82030fb84ad2b0b9
Joris Vaillant <joris.vaillant@inria.fr>
2026-07-24 11:05:04 +0200

    cmake: Modify Findcppad.cmake to create a target

--- cmake/find-external/CppAD/Findcppad.cmake
+++ cmake/find-external/CppAD/Findcppad.cmake
@@ -1,41 +1,24 @@
-#
-# Copyright 2020 CNRS INRIA
-#
-# Author: Guilhem Saurel, Rohan Budhiraja
-#
+# Copyright 2026 Inria
 
-# Try to find cppad in standard prefixes and in ${cppad_PREFIX} Once done this
-# will define cppad_FOUND - System has cppad cppad_INCLUDE_DIR - The cppad
-# include directories cppad_LIBRARY - The libraries needed to use cppad
-# cppad_DEFINITIONS - Compiler switches required for using cppad cppad_VERSION -
-# Version of cppad found
+find_path(cppad_INCLUDE_DIR NAMES cppad/configure.hpp)
+find_library(cppad_LIBRARY NAMES cppad_lib)
 
-find_path(
-  cppad_INCLUDE_DIR
-  NAMES cppad/configure.hpp
-  PATHS ${cppad_PREFIX}
-  PATH_SUFFIXES include
-)
-find_library(
-  cppad_LIBRARY
-  NAMES cppad_lib
-  PATHS ${cppad_PREFIX}
-  PATH_SUFFIXES lib
-)
+mark_as_advanced(cppad_LIBRARY cppad_INCLUDE_DIR)
 
-if(cppad_INCLUDE_DIR AND EXISTS "${cppad_INCLUDE_DIR}/cppad/configure.hpp")
+if(cppad_INCLUDE_DIR AND NOT TARGET cppad::cppad)
   file(
-    STRINGS "${cppad_INCLUDE_DIR}/cppad/configure.hpp"
-    cppad_version_str
-    REGEX "^# *define[\t ]+CPPAD_PACKAGE_STRING[\t ]+\"cppad-.*\""
+    READ "${cppad_INCLUDE_DIR}/cppad/configure.hpp"
+    cppad_configure_hpp
   )
+  # Version is stored on the following line:
+  # `# define CPPAD_PACKAGE_STRING "cppad-20260000.0"`
   string(
-    REGEX REPLACE
-    "^# *define[\t ]+CPPAD_PACKAGE_STRING[\t ]+\"cppad-([^\"]*)\".*"
-    "\\1"
-    cppad_VERSION
-    "${cppad_version_str}"
+    REGEX MATCH
+    "#[\t ]+define[\t ]+CPPAD_PACKAGE_STRING[\t ]+\"cppad-([0-9\.]*)\""
+    _
+    ${cppad_configure_hpp}
   )
+  set(cppad_VERSION ${CMAKE_MATCH_1})
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -44,4 +27,14 @@ find_package_handle_standard_args(
   REQUIRED_VARS cppad_LIBRARY cppad_INCLUDE_DIR
   VERSION_VAR cppad_VERSION
 )
-mark_as_advanced(cppad_INCLUDE_DIR cppad_LIBRARY)
+
+if(cppad_FOUND AND NOT TARGET cppad::cppad)
+    add_library(cppad::cppad UNKNOWN IMPORTED)
+    set_target_properties(
+        cppad::cppad
+        PROPERTIES
+            IMPORTED_LOCATION ${cppad_LIBRARY}
+            VERSION ${cppad_VERSION}
+            INCLUDE_DIRECTORIES ${cppad_INCLUDE_DIR}
+    )
+endif()
