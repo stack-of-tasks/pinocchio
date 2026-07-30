c247868b03c6c9e65467bf4d98578c79daeb0385
Joris Vaillant <joris.vaillant@inria.fr>
2026-07-30 16:59:13 +0200

    cmake: Refactor gmp finder

--- cmake/find-external/GMP/FindGMP.cmake
+++ cmake/find-external/GMP/FindGMP.cmake
@@ -1,37 +1,38 @@
-# Try to find the GNU Multiple Precision Arithmetic Library (GMP) See
-# http://gmplib.org/
+# Copyright 2026 Inria
 
-if(GMP_INCLUDES AND GMP_LIBRARIES)
-    set(GMP_FIND_QUIETLY TRUE)
-endif(GMP_INCLUDES AND GMP_LIBRARIES)
+find_path(GMP_INCLUDE_DIR NAMES gmp.h)
+find_library(GMP_LIBRARY NAMES gmp)
 
-find_path(GMP_INCLUDES NAMES gmp.h PATHS $ENV{GMPDIR} ${INCLUDE_INSTALL_DIR})
+mark_as_advanced(GMP_LIBRARY GMP_INCLUDE_DIR)
 
-find_library(GMP_LIBRARIES NAMES gmp PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDES GMP_LIBRARIES)
-
-# Set gmp target
-if(GMP_FOUND)
-    add_library(gmp SHARED IMPORTED)
-    set_target_properties(
-        gmp
-        PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}"
-            IMPORTED_CONFIGURATIONS "RELEASE"
-    )
-    if(WIN32)
-        set_target_properties(
-            gmp
-            PROPERTIES IMPORTED_IMPLIB_RELEASE "${GMP_LIBRARIES}"
-        )
-    else()
-        set_target_properties(
-            gmp
-            PROPERTIES IMPORTED_LOCATION_RELEASE "${GMP_LIBRARIES}"
-        )
-    endif()
+if(GMP_INCLUDE_DIR AND NOT TARGET GMP::GMP)
+    # Version is stored on the following line:
+    # `#define __GNU_MP_VERSION            6`
+    # `#define __GNU_MP_VERSION_MINOR      3`
+    # `#define __GNU_MP_VERSION_PATCHLEVEL 0`
+    file(READ ${GMP_INCLUDE_DIR}/gmp.h gmp_h)
+    string(REGEX MATCH "#define[ \t]__GNU_MP_VERSION[ \t]+([0-9]+)" _match_major ${gmp_h})
+    set(GMP_VERSION_MAJOR ${CMAKE_MATCH_1})
+    string(REGEX MATCH "#define[ \t]__GNU_MP_VERSION_MINOR[ \t]+([0-9]+)" _match_major ${gmp_h})
+    set(GMP_VERSION_MINOR ${CMAKE_MATCH_1})
+    string(REGEX MATCH "#define[ \t]__GNU_MP_VERSION_PATCHLEVEL[ \t]+([0-9]+)" _match_major ${gmp_h})
+    set(GMP_VERSION_MICRO ${CMAKE_MATCH_1})
+    set(GMP_VERSION ${GMP_VERSION_MAJOR}.${GMP_VERSION_MINOR}.${GMP_VERSION_MICRO})
 endif()
 
-mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    GMP
+    REQUIRED_VARS GMP_LIBRARY GMP_INCLUDE_DIR
+    VERSION_VAR GMP_VERSION)
+
+if(GMP_FOUND AND NOT TARGET GMP::GMP)
+    add_library(GMP::GMP UNKNOWN IMPORTED)
+    set_target_properties(
+        GMP::GMP
+        PROPERTIES
+            IMPORTED_LOCATION ${GMP_LIBRARY}
+            VERSION ${GMP_VERSION}
+            INCLUDE_DIRECTORIES ${GMP_INCLUDE_DIR}
+    )
+endif()
