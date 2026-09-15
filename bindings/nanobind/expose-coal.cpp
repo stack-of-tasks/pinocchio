e016c8e30a0d792bc0a2595ef0902bc257581a9a
Louis Montaut <louismontaut@gmail.com>
2026-09-15 16:51:47 +0200

    nanobind: add conversion btw coal::Transform3s and pinocchio::SE3

--- bindings/nanobind/expose-coal.cpp
+++ bindings/nanobind/expose-coal.cpp
@@ -18,6 +18,16 @@ void exposePoolCollision(nb::module_ m);
 
 void exposeCoal(nb::module_ m)
 {
+  // Implicit conversion invokes the destination's Python constructor.
+  // Register these after both libraries have exposed their transform types.
+  nb::borrow<nb::class_<coal::Transform3s>>(nb::type<coal::Transform3s>())
+    .def("__init__", [](coal::Transform3s * self, const pinocchio::SE3 & placement) {
+      new (self) coal::Transform3s(placement.rotation(), placement.translation());
+    });
+  nb::borrow<nb::class_<pinocchio::SE3>>(nb::type<pinocchio::SE3>())
+    .def("__init__", [](pinocchio::SE3 * self, const coal::Transform3s & placement) {
+      new (self) pinocchio::SE3(placement.getRotation(), placement.getTranslation());
+    });
   nb::implicitly_convertible<pinocchio::SE3, coal::Transform3s>();
   nb::implicitly_convertible<coal::Transform3s, pinocchio::SE3>();
 
